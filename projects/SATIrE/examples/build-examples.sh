7b196491bfc5877815edd8e60e5f67e3dac4be65
markus <none>
2007-10-04 10:45:12 -0700

    build examples reminder added
    darcs-hash:20071004174512-300a2-2aea77485dde5125bcd0be020d5af3639569f876.gz

--- projects/SATIrE/examples/build-examples.sh
+++ projects/SATIrE/examples/build-examples.sh
@@ -1,41 +1,19 @@
-TESTDIR=executable-examples
-BASEDIR=`pwd`
+ mkdir $(testdir)
+        echo "Testing sl1rd"
+        cp -ur $(top_srcdir)/examples/sl1rd $(testdir)
+        chmod u+rwx -R $(testdir)/sl1rd
+        cd $(testdir) ; $(DESTDIR)/$(bindir)/newanalysis sl1rd
+        $(MAKE) check -C $(testdir)/sl1rd
 
-mkdir -p $TESTDIR
+        echo "Testing sl2rd"
+        cp -ur $(top_srcdir)/examples/sl2rd $(testdir)
+        chmod u+rwx -R $(testdir)/sl2rd
+        cd $(testdir) ; $(DESTDIR)/$(bindir)/newanalysis sl2rd
+        $(MAKE) check -C $(testdir)/sl2rd
 
-echo "Testing sl1rd"
-cp -ur sl1rd $TESTDIR
-chmod u+rwX -R $TESTDIR/sl1rd
-cd $TESTDIR ; newanalysis -u sl1rd
-cd $BASEDIR
-make check -C $TESTDIR/sl1rd
-
-echo "Testing sl2rd"
-cp -ur sl2rd $TESTDIR
-chmod u+rwX -R $TESTDIR/sl2rd
-cd $TESTDIR ; newanalysis -u sl2rd
-cd $BASEDIR
-make check -C $TESTDIR/sl2rd
-
-echo "Testing constprop"
-cp -ur constprop $TESTDIR
-chmod u+rwX -R $TESTDIR/constprop
-cd $TESTDIR ; newanalysis constprop 
-cd $BASEDIR
-make check -C $TESTDIR/constprop
-
-echo "Testing nnh99_shape"
-cp -ur nnh99_shape $TESTDIR
-chmod u+rwX -R $TESTDIR/nnh99_shape
-cd $TESTDIR ; newanalysis -u nnh99_shape 
-cd $BASEDIR
-make check -C $TESTDIR/nnh99_shape
-
-echo "Testing srw98_shape"
-cp -ur srw98_shape $TESTDIR
-chmod u+rwX -R $TESTDIR/srw98_shape
-cd $TESTDIR ; newanalysis -u srw98_shape 
-cd $BASEDIR
-make check -C $TESTDIR/srw98_shape
-
-
+        echo "Testing constprop"
+        cp -ur $(top_srcdir)/examples/constprop $(testdir)
+        chmod u+rwx -R $(testdir)/constprop
+        cd $(testdir) ; $(DESTDIR)/$(bindir)/newanalysis constprop
+        $(MAKE) check -C $(testdir)/constprop
+#       rm -rf $(testdir)
