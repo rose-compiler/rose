ea259cde3437af5040210de32ce76165ee0b694f
adrian <none>
2007-10-24 05:43:34 -0700

    *) FIXED a bug in termlexer
    *) FIXED a bug in termlexer
    *) RENAMED variables in newanalysis
    
    darcs-hash:20071024124334-a1b90-cebf089d82ec90e0c5955c5c13ff6f2f3dc719dd.gz

--- projects/SATIrE/bootstrap.sh
+++ projects/SATIrE/bootstrap.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-# Run this on g0.complang.tuwien.ac.at
+if [ x$1 = x ]
+then
+    echo "Usage: $0 <INSTALL_PREFIX>"
+    echo "  autoreconf, configure, test and install SATIrE to <INSTALL_PREFIX>"
+    echo "  Paths are hardcoded for g0.complang.tuwien.ac.at"
+    exit 1
+fi
+
 
 autoreconf -i && ./configure --prefix=$1 --with-rosedir=/usr/local/mstools/rose/ --with-pagdir=/usr/local/mstools/pag CXXFLAGS="-O0 -ggdb" && make && make distcheck && make install && make installcheck
