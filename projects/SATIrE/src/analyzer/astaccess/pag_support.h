a75edd185c770721158e73d900af61bf38fa7621
gergo <none>
2007-10-31 04:52:08 -0700

    Added extern "C" to two declarations to fix an error reported by a user.
    Added extern "C" to two declarations to fix an error reported by a user.
    Cosmetic changes.
    
    darcs-hash:20071031115208-d8bd4-9f44fa521cc88cf1a8a32050c6a39320cfdef10c.gz

--- projects/SATIrE/src/analyzer/astaccess/pag_support.h
+++ projects/SATIrE/src/analyzer/astaccess/pag_support.h
@@ -1,5 +1,5 @@
 // Copyright 2005,2006,2007 Markus Schordan, Gergo Barany
-// $Id: pag_support.h,v 1.3 2007-07-15 02:02:27 markus Exp $
+// $Id: pag_support.h,v 1.4 2007-10-31 10:52:08 gergo Exp $
 
 #ifndef H_PAG_SUPPORT
 #define H_PAG_SUPPORT
@@ -13,8 +13,16 @@ extern "C"
 #endif
 void syntax_init(void);
 
+// GB (2007-10-31): Made these extern "C" to fix an error reported by
+// Stefan Stattelmann.
+#ifdef __cplusplus
+extern "C" {
+#endif
 extern int syntaxtype;
 extern int e_syntaxtype;
+#ifdef __cplusplus
+}
+#endif
 
 #include "syn_typedefs.h"
 
