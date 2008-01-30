00d3d99b4eff439e7aba801fc66530f6ee7c691d
adrian <none>
2008-01-30 08:11:48 -0800

    latest termite changes
    darcs-hash:20080130161148-a1b90-c6d97b6e64ac6321cdc8de1cf52b5ed386b521a7.gz

--- projects/SATIrE/src/termite/toC.C
+++ projects/SATIrE/src/termite/toC.C
@@ -4,20 +4,23 @@ see LICENSE in the root folder of this project
 */
 #include "TermRep.h"
 #include <iostream>
+#include <stdio.h>
 #include "rose.h"
 #include "PrologToRose.h"
 #include "termparser.tab.h++"
 
 extern int yyparse();
+extern FILE* yyin;
 extern PrologTerm* prote;
 
 int main(int argc, char** argv) {
-  yyparse();
-//  cout << prote->getRepresentation();
+  //  cout << prote->getRepresentation();
   if (argc != 3) {
     cerr << "Usage: " << argv[0] << " termfile.pl sourcefile" << endl;
     return 1;
   }
+  yyin = fopen( argv[1], "r" );
+  yyparse();
 
   PrologToRose unparser(argv[2]);
   unparser.toRose(prote);
