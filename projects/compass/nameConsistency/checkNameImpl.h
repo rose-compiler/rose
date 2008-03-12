6e31f41bcba6510aab82c695d88092f5bc0723e8
panas2 <panas2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-03-12 18:56:57 +0000

    fixed so boost version of ROSE compiles
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@164 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- projects/compass/nameConsistency/checkNameImpl.h
+++ projects/compass/nameConsistency/checkNameImpl.h
@@ -1,4 +1,9 @@
+#ifndef COMPASS_CHECKNAMEIMPL_H
+#define COMPASS_CHECKNAMEIMPL_H
+
+
 #include "rose.h"
+
 
 #if USE_ROSE_BOOST_WAVE_SUPPORT //GMY 12/26/2007 added #if...#endif to pass compass make verify
 #include <boost/wave.hpp>
@@ -156,3 +161,4 @@ class SynthesizedAttribute
           void display() const;
    };
 
+#endif
