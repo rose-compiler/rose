7c472604224afedd515acf3009689bc0902c219d
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-03-14 01:35:40 +0000

    added a tutorial using high level AST construction interface revised web page
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@196 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ tutorial/addVariableDeclaration2.C
@@ -0,0 +1,38 @@
+// SageBuilder contains all high level buildXXX() functions,
+// such as buildVariableDeclaration(), buildLabelStatement() etc.
+// SageInterface contains high level AST manipulation and utility functions,
+// e.g. appendStatement(), lookupFunctionSymbolInParentScopes() etc.
+#include "rose.h"
+using namespace SageBuilder;
+using namespace SageInterface;
+
+class SimpleInstrumentation:public SgSimpleProcessing
+{
+public:
+  void visit (SgNode * astNode);
+};
+
+void
+SimpleInstrumentation::visit (SgNode * astNode)
+{
+  SgBasicBlock *block = isSgBasicBlock (astNode);
+  if (block != NULL)
+    {
+      SgVariableDeclaration *variableDeclaration =
+        	buildVariableDeclaration ("newVariable", buildIntType ());
+      prependStatement (variableDeclaration, block);
+    }
+}
+
+int
+main (int argc, char *argv[])
+{
+  SgProject *project = frontend (argc, argv);
+  ROSE_ASSERT (project != NULL);
+
+  SimpleInstrumentation treeTraversal;
+  treeTraversal.traverseInputFiles (project, preorder);
+
+  AstTests::runAllTests (project);
+  return backend (project);
+}
