f1751403da20721bfcf7fb7619b1bf070b3827b2
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-03-31 22:41:51 +0000

    add tutorials using high level interfaces
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@285 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ tutorial/addFunctionDeclaration2.C
@@ -0,0 +1,63 @@
+// This example shows how to construct a defining function (with a function body)
+// using high level AST construction interfaces.
+//
+#include "rose.h"
+using namespace SageBuilder;
+using namespace SageInterface;
+
+class SimpleInstrumentation : public SgSimpleProcessing
+   {
+     public:
+          void visit ( SgNode* astNode );
+   };
+
+void
+SimpleInstrumentation::visit ( SgNode* astNode )
+   {
+     SgGlobal* globalScope = isSgGlobal(astNode);
+     if (globalScope != NULL)
+        {
+       // ********************************************************************
+       // Create a parameter list with a parameter
+       // ********************************************************************
+          SgName var1_name = "var_name";
+          SgReferenceType *ref_type = buildReferenceType(buildIntType());
+          SgInitializedName *var1_init_name = buildInitializedName(var1_name, ref_type);
+          SgFunctionParameterList* parameterList = buildFunctionParameterList();
+          appendArg(parameterList,var1_init_name);
+
+       // *****************************************************
+       // Create a defining functionDeclaration (with a function body)
+       // *****************************************************
+          SgName func_name                    = "my_function";
+          SgFunctionDeclaration * func        = buildDefiningFunctionDeclaration 
+                        (func_name, buildIntType(), parameterList,globalScope);
+          SgBasicBlock*  func_body    = func->get_definition()->get_body();
+
+       // ********************************************************
+       // Insert a statement in the function body
+       // *******************************************************
+
+          SgVarRefExp *var_ref = buildVarRefExp(var1_name,func_body);
+          SgPlusPlusOp *pp_expression = buildPlusPlusOp(var_ref);
+          SgExprStatement* new_stmt = buildExprStatement(pp_expression);
+			   
+       // insert a statement into the function body
+          prependStatement(new_stmt,func_body);
+          prependStatement(func,globalScope);
+       
+        }
+   }
+
+int
+main ( int argc, char * argv[] )
+   {
+     SgProject* project = frontend(argc,argv);
+     ROSE_ASSERT(project != NULL);
+
+     SimpleInstrumentation treeTraversal;
+     treeTraversal.traverseInputFiles ( project, preorder );
+
+     AstTests::runAllTests(project);
+     return backend(project);
+   }
---
+++ tutorial/addFunctionDeclaration3.C
@@ -0,0 +1,50 @@
+// This example shows how to construct a defining function (with a function body)
+// using high level AST construction interfaces.
+// A scope stack is used to pass scope information implicitly to some builder functions
+#include "rose.h"
+using namespace SageBuilder;
+using namespace SageInterface;
+
+int
+main ( int argc, char * argv[] )
+   {
+     SgProject* project = frontend(argc,argv);
+     ROSE_ASSERT(project != NULL);
+     SgGlobal *globalScope = getFirstGlobalScope (project);
+
+ //push global scope into stack
+     pushScopeStack (isSgScopeStatement (globalScope));
+
+ // Create a parameter list with a parameter
+    SgName var1_name = "var_name";
+    SgReferenceType *ref_type = buildReferenceType(buildIntType());
+    SgInitializedName *var1_init_name = buildInitializedName(var1_name, ref_type);
+    SgFunctionParameterList* parameterList = buildFunctionParameterList();
+    appendArg(parameterList,var1_init_name);
+
+ // Create a defining functionDeclaration (with a function body)
+    SgName func_name                    = "my_function";
+    SgFunctionDeclaration * func        = buildDefiningFunctionDeclaration 
+                  (func_name, buildIntType(), parameterList);
+    SgBasicBlock*  func_body    = func->get_definition()->get_body();
+
+ // push function body scope into stack
+    pushScopeStack(isSgScopeStatement(func_body));
+
+ // build a statement in the function body
+    SgVarRefExp *var_ref = buildVarRefExp(var1_name);
+    SgPlusPlusOp *pp_expression = buildPlusPlusOp(var_ref);
+    SgExprStatement* new_stmt = buildExprStatement(pp_expression);
+                     
+ // insert a statement into the function body
+    appendStatement(new_stmt);
+//  pop function body off the stack
+    popScopeStack();
+    
+//  insert the function declaration into the scope at the top of the scope stack    
+    prependStatement(func);
+    popScopeStack();
+
+    AstTests::runAllTests(project);
+    return backend(project);
+ }
