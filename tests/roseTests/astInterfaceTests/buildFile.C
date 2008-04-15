603c3b81f2341cf82641f7587e13b1963b8e11b8
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-04-15 01:42:15 +0000

    A test case for NULL SgProject parameter
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@371 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- tests/roseTests/astInterfaceTests/buildFile.C
+++ tests/roseTests/astInterfaceTests/buildFile.C
@@ -11,23 +11,35 @@ using namespace SageInterface;
 
 int main (int argc, char *argv[])
 {
- std::string fileName="/home/liao6/buildrose/tests/roseTests/astInterfaceTests/testfile525.cpp";
-#if 1
-  SgProject *project = frontend (argc, argv);
-//  SgGlobal *globalScope = getFirstGlobalScope (project);
+  //absolute filename
+ // std::string fileName="/home/liao6/buildrose/tests/roseTests/astInterfaceTests/testfile525.cpp";
+  std::string fileName="testfile525.cpp";
+  std::string fileName2="testfile626.cpp";
 
-// TODO relative path case
+//case 1. Calling it with project available 
+  SgProject *project = frontend (argc, argv);
+
   SgFile *sgfile = buildFile(fileName,project);
 
-// try to add something into the new file
+  // try to add something into the new file
   SgGlobal* global = sgfile->get_globalScope();
   SgVariableDeclaration *varDecl = buildVariableDeclaration("j", buildIntType());
-   appendStatement (varDecl,isSgScopeStatement(global));
+  appendStatement (varDecl,isSgScopeStatement(global));
 
   AstTests::runAllTests(project);
-  return backend (project);
-#else  
-  SgFile *sgfile = buildFile(fileName);
-#endif   
+  project->unparse();
+// case 2. Calling it without the project parameter  
+  SgFile *sgfile2 = buildFile(fileName2);
+
+  // try to add something into the new file
+  SgGlobal* global2 = sgfile2->get_globalScope();
+  SgVariableDeclaration *varDecl2 = buildVariableDeclaration("x", buildIntType());
+  appendStatement (varDecl2,isSgScopeStatement(global2));
+
+  SgProject * project2= sgfile2->get_project();
+  AstTests::runAllTests(project2);
+  project2->unparse();
+
+  return 0;
 }
 
