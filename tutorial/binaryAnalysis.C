f65d562ae115d790bbbc45b32f4b1fa49e15b1c3
panas2 <panas2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-04-24 01:07:05 +0000

    fixed binary tutorial -- but it seems the figures are different
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@425 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- tutorial/binaryAnalysis.C
+++ tutorial/binaryAnalysis.C
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
   RoseBin_DotGraph* dotGraph = new RoseBin_DotGraph();
   RoseBin_GMLGraph* gmlGraph = new RoseBin_GMLGraph();
   char* cfgFileName = "cfg.dot";
-  RoseBin_ControlFlowAnalysis* cfganalysis = new RoseBin_ControlFlowAnalysis(file->get_global_block(), forward, new RoseFile(""), edges);
+  RoseBin_ControlFlowAnalysis* cfganalysis = new RoseBin_ControlFlowAnalysis(file->get_global_block(), forward, new RoseObj(), edges);
   cfganalysis->run(dotGraph, cfgFileName, mergedEdges);
 
 
@@ -42,7 +42,7 @@ int main(int argc, char** argv)
   cout << " creating call graph ... " << endl;
   char* callFileName = "callgraph.gml";
   forward = true;
-  RoseBin_CallGraphAnalysis* callanalysis = new RoseBin_CallGraphAnalysis(file->get_global_block(), new RoseFile(""));
+  RoseBin_CallGraphAnalysis* callanalysis = new RoseBin_CallGraphAnalysis(file->get_global_block(), new RoseObj());
 
   // Building a GML file for the call graph
      callanalysis->run(gmlGraph, callFileName, !mergedEdges);
@@ -58,7 +58,7 @@ int main(int argc, char** argv)
   forward = true;
   bool printEdges = true;
   bool interprocedural = true;
-  RoseBin_DataFlowAnalysis* dfanalysis = new RoseBin_DataFlowAnalysis(file->get_global_block(), forward, new RoseFile(""));
+  RoseBin_DataFlowAnalysis* dfanalysis = new RoseBin_DataFlowAnalysis(file->get_global_block(), forward, new RoseObj());
   dfanalysis->init(interprocedural, printEdges);
 
   // Building a DOT file for the data-flow graph
