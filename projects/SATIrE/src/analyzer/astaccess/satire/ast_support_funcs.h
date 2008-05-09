58eb3bc765c32d0bb10f45ad5c3a0a38c3771272
gergo <none>
2008-05-09 09:01:09 -0700

    Changed expression/type numbers from signed to unsigned.
    darcs-hash:20080509160109-d8bd4-a107a12f53ce0021884d9552d8f4400baecd6adf.gz

--- projects/SATIrE/src/analyzer/astaccess/satire/ast_support_funcs.h
+++ projects/SATIrE/src/analyzer/astaccess/satire/ast_support_funcs.h
@@ -1,20 +1,20 @@
 // Copyright 2005,2006,2007 Markus Schordan, Gergo Barany
-// $Id: ast_support_funcs.h,v 1.1 2007-09-20 09:25:31 adrian Exp $
+// $Id: ast_support_funcs.h,v 1.2 2008-05-09 14:01:09 gergo Exp $
 
 #ifndef H_AST_SUPPORT_FUNCS
 #define H_AST_SUPPORT_FUNCS
 
 #include <rose.h>
 
-SgType *typenum_to_type(int n);
-int type_to_typenum(SgType *type);
-std::string typenum_to_str(int n);
-SgExpression *exprnum_to_expr(int n);
-int expr_to_exprnum(SgExpression *expr);
-std::string exprnum_to_str(int n);
+SgType *typenum_to_type(unsigned int n);
+unsigned int type_to_typenum(SgType *type);
+std::string typenum_to_str(unsigned int n);
+SgExpression *exprnum_to_expr(unsigned int n);
+unsigned int expr_to_exprnum(SgExpression *expr);
+std::string exprnum_to_str(unsigned int n);
 SgType *expr_type(SgExpression *expr);
-int exprnum_typenum(int n);
+unsigned int exprnum_typenum(unsigned int n);
 bool is_subtype_of(SgClassType *a, SgClassType *b);
-bool is_subtypenum_of(int a, int b);
+bool is_subtypenum_of(unsigned int a, unsigned int b);
 
 #endif
