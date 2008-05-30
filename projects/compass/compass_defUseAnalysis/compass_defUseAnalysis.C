87c47c1e9c4c9503d841251ba07ad295be404172
saebjornsen1 <saebjornsen1@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-05-30 18:16:51 +0000

    Reverted pre-merge versions of C and h files
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@581 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- projects/compass/compass_defUseAnalysis/compass_defUseAnalysis.C
+++ projects/compass/compass_defUseAnalysis/compass_defUseAnalysis.C
@@ -13,19 +13,14 @@ namespace CompassAnalyses
 { 
   namespace Compass_defUseAnalysis
   { 
-    /*namespace Traversal {
-      int loc=0;
-      int loc_actual=0;
-    }*/
-    //GMY 12/26/2007 changed namespace Traversal to explicit scope because of
-    //error in EDG front-end processing.
-    //These are static data member initializations of class Traversal
 
     int Traversal::loc = 0;
     int Traversal::loc_actual = 0;
-    //12/26/2007 END
 
     const std::string checkerName      = "Compass_defUseAnalysis";
+
+    bool Traversal::projectSet = false;
+    DFAnalysis* Traversal::defuse = NULL;
 
     // Descriptions should not include the newline character "\n".
     std::string shortDescription = 
@@ -48,7 +43,7 @@ Traversal(Compass::Parameters inputParameters, Compass::OutputObject* output)
   // Initalize checker specific parameters here, for example: 
   // YourParameter = Compass::parseInteger(inputParameters["Compass_defUseAnalysis.YourParameter"]);
 
-  loc = Compass::parseInteger(inputParameters["Compass_defUseAnalysis.Size"]);
+  //  loc = Compass::parseInteger(inputParameters["Compass_defUseAnalysis.Size"]);
   setShortDescription(shortDescription +  Traversal::getLOC_actual()+" > "+Traversal::getLOC());
   setLongDescription (longDescription +  Traversal::getLOC_actual()+" > "+Traversal::getLOC());
 }
@@ -58,22 +53,16 @@ void
 CompassAnalyses::Compass_defUseAnalysis::Traversal::
 visit(SgNode* sgNode)
 { 
+  if (projectSet==false) {
+    SgProject* project = TransformationSupport::getProject(sgNode);
+    defuse = new DefUseAnalysis(project);
+    projectSet=true;
+  }
   // Implement your traversal here.  
-  if (isSgFunctionDeclaration(sgNode)) {
-    SgFunctionDeclaration* funcDecl = isSgFunctionDeclaration(sgNode);
-    SgFunctionDefinition* funcDef = funcDecl->get_definition();
-    if (funcDef) {
-      Sg_File_Info* start = funcDef->get_body()->get_startOfConstruct();
-      Sg_File_Info* end = funcDef->get_body()->get_endOfConstruct();
-      ROSE_ASSERT(start);
-      ROSE_ASSERT(end);
-      int lineS = start->get_line();
-      int lineE = end->get_line();
-      loc_actual = lineE-lineS;
-      if (loc_actual>loc) {
-        output->addOutput(new CheckerOutput(funcDef));
-      }
-    }
+  if (isSgFunctionDefinition(sgNode)) {
+    SgFunctionDefinition* funcDef = isSgFunctionDefinition(sgNode);
+    int nrNodes = ((DefUseAnalysis*)defuse)->start_traversal_of_one_function(funcDef);
+    output->addOutput(new CheckerOutput(funcDef));
   }
 
 } //End of the visit function.
--- projects/compass/compass_defUseAnalysis/compass_defUseAnalysis.h
+++ projects/compass/compass_defUseAnalysis/compass_defUseAnalysis.h
@@ -1,57 +1,69 @@
-// DefUseAnalysis
+// -*- mode: C++; c-basic-offset: 2; indent-tabs-mode: nil -*-
+// vim: expandtab:shiftwidth=2:tabstop=2
+
+// Loc Per Function Analysis
 // Author: Thomas Panas
-// Date: 21March 2008
+// Date: 23-July-2007
 
 #include "compass.h"
+#include "compass_defUseAnalysis.h"
 
-#ifndef COMPASS_DEF_USE_H
-#define COMPASS_DEF_USE_H
 
 namespace CompassAnalyses
-   { 
-     namespace Compass_defUseAnalysis
-        { 
-        /*! \brief Loc Per Function: Add your description here 
-         */
-	  
-          extern const std::string checkerName;
-          extern std::string shortDescription;
-          extern std::string longDescription;
+{ 
+  namespace Compass_defUseAnalysis
+  { 
 
-       // Specification of Checker Output Implementation
-          class CheckerOutput: public Compass::OutputViolationBase
-             { 
-               public:
-                    CheckerOutput(SgNode* node);
-             };
+    int Traversal::loc = 0;
+    int Traversal::loc_actual = 0;
 
-       // Specification of Checker Traversal Implementation
+    const std::string checkerName      = "Compass_defUseAnalysis";
 
-          class Traversal
-             : public AstSimpleProcessing, public Compass::TraversalBase
-             {
-            // Checker specific parameters should be allocated here.
-	       static int loc;
-	       static int loc_actual;
-               public:
-                    Traversal(Compass::Parameters inputParameters, Compass::OutputObject* output);
-                 // The implementation of the run function has to match the traversal being called.
-                    void run(SgNode* n){ this->traverse(n, preorder); };
-		    static std::string getLOC() { 
-		      std::ostringstream myStream; //creates an ostringstream object
-		      myStream << loc << std::flush;
-		      return (myStream.str()); //returns the string form of the stringstream object
-		    }
-		    static std::string getLOC_actual() { 
-		      std::ostringstream myStream; //creates an ostringstream object
-		      myStream << loc_actual << std::flush;
-		      return (myStream.str()); //returns the string form of the stringstream object
-		    }
-                    void visit(SgNode* n);
-             };
-        }
-   }
+    bool Traversal::projectSet = false;
+    DFAnalysis* Traversal::defuse = NULL;
 
-// Compass_DefUseAnalysis
-#endif 
+    // Descriptions should not include the newline character "\n".
+    std::string shortDescription = 
+        "This function has too many lines of code :: LOC = " ;
+    std::string longDescription  = 
+    "This analysis detects functions with lines of code larger than a predefined value. The current value is ";
+  } //End of namespace Compass_defUseAnalysis.
+} //End of namespace CompassAnalyses.
 
+CompassAnalyses::Compass_defUseAnalysis::
+CheckerOutput::CheckerOutput ( SgNode* node )
+  : OutputViolationBase(node,checkerName,shortDescription+
+                        Traversal::getLOC_actual()+" > "+Traversal::getLOC())
+{}
+
+CompassAnalyses::Compass_defUseAnalysis::Traversal::
+Traversal(Compass::Parameters inputParameters, Compass::OutputObject* output)
+  : Compass::TraversalBase(output, checkerName, shortDescription, longDescription)
+{
+  // Initalize checker specific parameters here, for example: 
+  // YourParameter = Compass::parseInteger(inputParameters["Compass_defUseAnalysis.YourParameter"]);
+
+  //  loc = Compass::parseInteger(inputParameters["Compass_defUseAnalysis.Size"]);
+  setShortDescription(shortDescription +  Traversal::getLOC_actual()+" > "+Traversal::getLOC());
+  setLongDescription (longDescription +  Traversal::getLOC_actual()+" > "+Traversal::getLOC());
+}
+
+
+void
+CompassAnalyses::Compass_defUseAnalysis::Traversal::
+visit(SgNode* sgNode)
+{ 
+  if (projectSet==false) {
+    SgProject* project = TransformationSupport::getProject(sgNode);
+    defuse = new DefUseAnalysis(project);
+    projectSet=true;
+  }
+  // Implement your traversal here.  
+  if (isSgFunctionDefinition(sgNode)) {
+    SgFunctionDefinition* funcDef = isSgFunctionDefinition(sgNode);
+    int nrNodes = ((DefUseAnalysis*)defuse)->start_traversal_of_one_function(funcDef);
+    output->addOutput(new CheckerOutput(funcDef));
+  }
+
+} //End of the visit function.
+   
