65037855ea407ff3f78df132f797e31b646f677f
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-06-18 01:29:35 +0000

    Fixed a few details.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@780 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/frontend/ExecFormats/ExecELF.C
+++ src/frontend/ExecFormats/ExecELF.C
@@ -968,14 +968,27 @@ is_ELF(ExecFile *f)
 
 /* Parses the structure of an ELF file and adds the information to the asmFile */
 void
-parseBinaryFormat(ExecFile *ef, SgAsmFile* asmFile)
-{
-    ElfFileHeader *fhdr = parse(ef);
-    ROSE_ASSERT(fhdr != NULL);
-    SgAsmElfHeader* roseElfHeader = new SgAsmElfHeader(fhdr);
-    ROSE_ASSERT(roseElfHeader != NULL);
-    asmFile->set_header(roseElfHeader);
-}
+parseBinaryFormat(ExecFile *f, SgAsmFile* asmFile)
+   {
+     ROSE_ASSERT(f);
+    
+     ElfFileHeader *fhdr = new ElfFileHeader(f, 0);
+
+     ROSE_ASSERT(fhdr != NULL);
+     SgAsmElfHeader* roseElfHeader = new SgAsmElfHeader(fhdr);
+     ROSE_ASSERT(roseElfHeader != NULL);
+     asmFile->set_header(roseElfHeader);
+
+  /* Read the optional section and segment tables and the sections/segments to which they point. */
+     if (fhdr->e_shnum)
+          fhdr->section_table = new ElfSectionTable(fhdr);
+
+     if (fhdr->e_phnum)
+          fhdr->segment_table = new ElfSegmentTable(fhdr);
+
+  /* Identify parts of the file that we haven't encountered during parsing */
+     f->fill_holes();
+   }
 
 /* Parses the structure of an ELF file and adds the info to the ExecFile */
 ElfFileHeader *
