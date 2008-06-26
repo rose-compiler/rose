d935ef6458c80739878bb290399fb83134887dfe
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-06-26 01:32:45 +0000

    Missed the test and input files
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@834 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ tests/roseTests/astInterfaceTests/buildCpreprocessorDefineDeclaration.C
@@ -0,0 +1,23 @@
+#include "sageBuilder.h"
+#include <iostream>
+#include "rose.h"
+using namespace std;
+using namespace SageInterface;
+using namespace SageBuilder;
+
+int main (int argc, char *argv[])
+{
+  SgProject *project = frontend (argc, argv);
+
+  // go to the function body
+  SgFunctionDeclaration* mainFunc= findMain(project);
+
+  buildCpreprocessorDefineDeclaration(mainFunc, "int i;"); 
+  //buildCpreprocessorDefineDeclaration(mainFunc, "#define UPCR_WANT_MAJOR 3"); 
+  //buildCpreprocessorDefineDeclaration(mainFunc, "Test for source comment."); 
+
+  AstTests::runAllTests(project);
+  return backend (project);
+}
+
+
