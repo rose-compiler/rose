d7c4af73f7dace8a43643a4c1e612081dd25ebae
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-06-28 20:47:42 +0000

    Added input file.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@869 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ projects/bugSeeding/inputCode_bufferOverflow_arrayIndexing.C
@@ -0,0 +1,14 @@
+
+void foobar()
+   {
+  // Static array declaration
+     float array[10];
+
+     array[0] = 0;
+
+     for (int i=0; i < 5; i++)
+        {
+          array[i] = 0;
+        }
+   }
+
