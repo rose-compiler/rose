3cc789a06280bd1aea9e45c3fdf9dd294760e096
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-07-08 16:07:05 +0000

    Added a tutorial example for instrumenting end of functions using the high level interface.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1011 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ tutorial/instrumentEndOfFunction.C
@@ -0,0 +1,36 @@
+/*! \brief  test instrumentation right before the end of a function
+*/
+#include "rose.h"
+#include <iostream>
+using namespace SageInterface;
+using namespace SageBuilder;
+
+int main (int argc, char *argv[])
+{
+  SgProject *project = frontend (argc, argv);
+
+  // Find all function definition we want to instrument
+  std::vector<SgNode* > funcDefList = 
+    NodeQuery::querySubTree (project, V_SgFunctionDefinition);
+
+ std::vector<SgNode*>::iterator iter;
+ for (iter = funcDefList.begin(); iter!= funcDefList.end(); iter++)
+ { 
+   SgFunctionDefinition* cur_def = isSgFunctionDefinition(*iter);
+   ROSE_ASSERT(cur_def);
+  SgBasicBlock* body = cur_def->get_body();
+
+  SgExprStatement* callStmt1 = buildFunctionCallStmt("call1",
+               buildIntType(),buildExprListExp() ,body);  
+
+  // instrument the main function
+  int i= instrumentEndOfFunction(cur_def->get_declaration(), callStmt1); 
+  std::cout<<"Instrumented "<<i<<" places. "<<std::endl;
+
+ }  // end of instrumentation
+
+  AstTests::runAllTests(project); 
+  // translation only
+   project->unparse();
+}
+
