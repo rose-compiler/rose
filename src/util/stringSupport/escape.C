4f150f83b2c4d7b61cfe2b02554d0878ef1eb305
willcock2 <willcock2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-07-09 20:37:31 +0000

    Added unescapeString function
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1021 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/util/stringSupport/escape.C
+++ src/util/stringSupport/escape.C
@@ -2,10 +2,11 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <cassert>
 
 std::string escapeString(const std::string & s) {
   std::string result;
-  for (unsigned int i = 0; i < s.length(); ++i) {
+  for (size_t i = 0; i < s.length(); ++i) {
     switch (s[i]) {
       case '\\': 
         result += "\\\\";
@@ -41,6 +42,50 @@ std::string escapeString(const std::string & s) {
           result += stream.str();
         }
         break;
+    }
+  }
+  return result;
+}
+
+std::string unescapeString(const std::string& s) {
+  std::string result;
+  for (size_t i = 0; i < s.length(); ++i) {
+    // Body of this loop can change i
+    if (s[i] == '\\') {
+      assert (i + 1 < s.length());
+      switch (s[i + 1]) {
+        case 'a': result += '\a'; break;
+        case 'e': result += '\033'; break;
+        case 'f': result += '\f'; break;
+        case 'n': result += '\n'; break;
+        case 'r': result += '\r'; break;
+        case 't': result += '\t'; break;
+        case 'v': result += '\v'; break;
+        case '0':
+        case '1':
+        case '2':
+        case '3': {
+          ++i;
+          if (s[i] == '0' && i + 1 == s.length()) {
+            result += '\0';
+            break;
+          }
+          assert (i + 2 < s.length());
+          unsigned char c = (s[i] - '0') << 6;
+          ++i;
+          assert (s[i] >= '0' && s[i] <= '7');
+          c |= (s[i] - '0') << 3;
+          ++i;
+          assert (s[i] >= '0' && s[i] <= '7');
+          c |= (s[i] - '0');
+          break;
+        }
+        default: ++i; result += s[i]; break;
+      }
+    } else if (s[i] == '"') {
+      return result;
+    } else {
+      result += s[i];
     }
   }
   return result;
