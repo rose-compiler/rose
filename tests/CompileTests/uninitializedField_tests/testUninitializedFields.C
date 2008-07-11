d788c9d3c839009b491c7299d3593cdfbe6815e2
willcock2 <willcock2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-07-11 00:37:58 +0000

    Added test for uninitialized ROSE AST fields
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1042 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ tests/CompileTests/uninitializedField_tests/testUninitializedFields.C
@@ -0,0 +1,31 @@
+// Uninitialized field tester: when run with Valgrind, prints out warnings
+// for ROSE AST fields that are not initialized
+
+#include "rose.h"
+
+#if ROSE_USE_VALGRIND
+
+#include <valgrind/valgrind.h>
+#include <valgrind/memcheck.h>
+
+using namespace std;
+
+struct Vis: public ROSE_VisitTraversal {
+  void visit(SgNode* node) {
+    // When Valgrind is enabled, this checks for uninitialized fields; we
+    // do not need the answer from it
+    node->roseRTI();
+  }
+};
+
+int main(int argc, char *argv[]) {
+  frontend(argc,argv);
+  Vis().traverseMemoryPool();
+  return (VALGRIND_COUNT_ERRORS != 0) ? 1 : 0;
+}
+
+#else // !ROSE_USE_VALGRIND
+
+#error "testUninitializedFields should not be built unless valgrind/valgrind.h is found"
+
+#endif // ROSE_USE_VALGRIND
