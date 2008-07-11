4509d59c15243729dd7473bda3355a9f611be3c9
panas2 <panas2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-07-11 16:28:51 +0000

    removed defuse from compass as a checker
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1044 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- projects/compass/compass_defUseAnalysis/compass_defUseAnalysisMain.C
+++
@@ -1,9 +0,0 @@
-//
-// Do not modify this file
-//
-
-#include "compass_defUseAnalysis.h"
-typedef CompassAnalyses::Compass_defUseAnalysis::Traversal Checker;
-
-#include "compass.C"
-#include "compassTestMain.C"
