21316f9652fa6e41bca9a03860e2a1694d4b8b35
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-07-21 23:40:18 +0000

    Worked on bug with LANL.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1158 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/util/support/VectorCommandOptions.C
+++ src/util/support/VectorCommandOptions.C
@@ -1,6 +1,7 @@
 #include <general.h>
 #include <VectorCommandOptions.h>
 #include <iostream>
+// #include <vector>
 
 using namespace std;
 
@@ -12,13 +13,13 @@ VectorCmdOptions* VectorCmdOptions::GetInstance()
   return inst;
 } 
 
-size_t VectorCmdOptions:: HasOption( const string& opt) const
+size_t
+VectorCmdOptions:: HasOption( const string& opt) const
 {
 // DQ (1/16/2008): Brian White ask that this output be removed!
 // cerr << "Searching for option " << opt << endl;
   assert (cmd);
-  vector<string>::const_iterator i =
-    std::find(cmd->begin(), cmd->end(), opt);
+  vector<string>::const_iterator i = find(cmd->begin(), cmd->end(), opt);
   if (i == cmd->end()) {
 // DQ (1/16/2008): Brian White ask that this output be removed!
  // cerr << "Not found" << endl;
