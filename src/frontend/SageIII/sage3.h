c3bbdff1f38e607b16b5091b3124bc6ef3d9f7a0
willcock2 <willcock2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-07-28 17:31:02 +0000

    (Probably) fixed regression test bug in definition of ROSE_ASSERT with NDEBUG enabled
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1270 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/frontend/SageIII/sage3.h
+++ src/frontend/SageIII/sage3.h
@@ -168,7 +168,7 @@ using namespace __gnu_cxx;
 #ifndef NDEBUG
 #define ROSE_ASSERT assert
 #else // We use assert(false) equivalents so often for "should not get here", but we don't want nontrivial side effects in asserts to be run when assert is disabled
-#define ROSE_ASSERT(x) (__builtin_constant_p(x) ? (x ? (void)0 : (abort())) : (void)0)
+#define ROSE_ASSERT(x) do {(void)(__builtin_constant_p(x) ? (x ? 0 : abort()) : 0);} while (0)
 #endif
 #define ROSE_ABORT  abort
 
