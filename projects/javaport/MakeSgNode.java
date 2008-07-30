1222bffdc3cf11196745ccc5aaeeeafc9ebb4d96
willcock2 <willcock2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-07-30 18:30:34 +0000

    Working on fixing bug with NULL C++ pointers being returned to Java
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1345 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- projects/javaport/MakeSgNode.java
+++ projects/javaport/MakeSgNode.java
@@ -2,6 +2,7 @@ package gov.llnl.casc.rose;
 
 class MakeSgNode {
   public static SgNode makeDerivedJavaNodeClass(long cPtr, boolean cMemoryOwn) {
+    if (cPtr == 0) return null;
     SgNode n = new SgNode(cPtr, false);
     VariantT v = n.variantT();
     if (v == VariantT.V_SgFunctionDeclaration) return new SgFunctionDeclaration(cPtr, cMemoryOwn);
