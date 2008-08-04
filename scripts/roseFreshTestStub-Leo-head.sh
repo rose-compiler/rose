dc46cb1110e4aee4eafd192af19a32acc8db6b0e
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-08-04 19:54:55 +0000

    Use the common script instead of own ones.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1414 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/roseFreshTestStub-Leo-head.sh
+++ scripts/roseFreshTestStub-Leo-head.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Stub file to include settings for roseFreshTest-common
-# the java libraries are platform-dependent! e.g.: i386 vs. amd64
 
 #source /usr/apps/graphviz/2.6/setup.sh
 #export PATH="/usr/apps/java/jdk1.5.0_11/bin:$PATH"
+#export LD_LIBRARY_PATH="/home/dquinlan/ROSE/OpenFortranParser/ofp_install/lib/:$LD_LIBRARY_PATH"
 
 export JAVA_HOME=/usr/apps/java/jdk1.5.0_11
 export BOOST_ROOT="/home/liao6/opt/boost_1_35_0"
-export LD_LIBRARY_PATH="${JAVA_HOME}/lib:${JAVA_HOME}/jre/lib/i386/server:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/apps/java/jdk1.5.0_11/lib:${JAVA_HOME}/jre/lib/i386/server:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:${LD_LIBRARY_PATH}"
 
 source /usr/apps/gcc/4.2.2/setup.sh
@@ -32,14 +32,14 @@ SVN=/nfs/apps/subversion/1.4.5/bin/svn
 SVNOP=checkout
 # check out the revision of the past 11:00pm PDT (06:00am UTC)
 # using data as revision number needs {}
-SVNVERSIONOPTION="{`date -d '06:00Z' -u '+%FT%H:%M:%SZ'`}"
+# default is head
+#SVNVERSIONOPTION="{`date -d '06:00Z' -u '+%FT%H:%M:%SZ'`}"
 #SVNVERSIONOPTION=1303
 
 # How many processes for make
 MAKEFLAGS="-j16"
 
 # my additional stuff
-# Disable upload for now, waiting for binary distribution package to be ready
 #ENABLE_UPLOAD=yes
 KEEP_TEST_DIR=yes
 NORMAL_INSTALL_DIR=yes # no chmod 000 for 'install'
