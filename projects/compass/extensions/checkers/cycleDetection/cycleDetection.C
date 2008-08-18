44fafa3e5981a2d517f588b1594383e5a05de04e
panas2 <panas2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-08-18 17:31:14 +0000

    updated checker
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1630 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- projects/compass/extensions/checkers/cycleDetection/cycleDetection.C
+++ projects/compass/extensions/checkers/cycleDetection/cycleDetection.C
@@ -12,70 +12,67 @@ using namespace std;
 extern const Compass::Checker* const cycleDetectionChecker;
 
 namespace CompassAnalyses
-   { 
-     namespace CycleDetection
-        { 
-        /*! \brief Cycle Detection: Add your description here 
-         */
+{ 
+  namespace CycleDetection
+  { 
+    /*! \brief Cycle Detection: Add your description here 
+     */
 
-       // Specification of Checker Output Implementation
-          class CheckerOutput: public Compass::OutputViolationBase
-             { 
-               public:
-                    CheckerOutput(SgNode* node);
-             };
+    // Specification of Checker Output Implementation
+    class CheckerOutput: public Compass::OutputViolationBase
+    { 
+    public:
+      CheckerOutput(SgNode* node);
+    };
 
-       // Specification of Checker Traversal Implementation
+    // Specification of Checker Traversal Implementation
 
-          class Traversal
-            : public RoseBin_DataFlowAbstract
-             {
-                    Compass::OutputObject* output;
-            // Checker specific parameters should be allocated here.
-               bool debug;
-               std::vector<SgDirectedGraphNode*> successors;
-               std::set<SgDirectedGraphNode*> visited;
-               std::map<SgDirectedGraphNode*,SgDirectedGraphNode*> cycleFound;
+    class Traversal
+      : public RoseBin_DataFlowAbstract
+    {
+      Compass::OutputObject* output;
+      // Checker specific parameters should be allocated here.
+      bool debug;
+      std::vector<SgDirectedGraphNode*> successors;
+      std::set<SgDirectedGraphNode*> visited;
+      std::map<SgDirectedGraphNode*,SgDirectedGraphNode*> cycleFound;
 
-               public:
-                    Traversal(Compass::Parameters inputParameters, Compass::OutputObject* output);
+    public:
+      Traversal(Compass::Parameters inputParameters, Compass::OutputObject* output);
 
-                 // Change the implementation of this function if you are using inherited attributes.
-                    void *initialInheritedAttribute() const { return NULL; }
+      // Change the implementation of this function if you are using inherited attributes.
+      void *initialInheritedAttribute() const { return NULL; }
 
-                 // The implementation of the run function has to match the traversal being called.
-                 // If you use inherited attributes, use the following definition:
-                 // void run(SgNode* n){ this->traverse(n, initialInheritedAttribute()); }
-               bool run(string& name, SgDirectedGraphNode* node, SgDirectedGraphNode* previous);
+      // The implementation of the run function has to match the traversal being called.
+      bool run(string& name, SgDirectedGraphNode* node, SgDirectedGraphNode* previous);
 
-               bool runEdge(SgDirectedGraphNode* node, SgDirectedGraphNode* next) {
-                 return false;
-               }
-               bool checkIfValidCycle(SgDirectedGraphNode* node,
-                                      SgDirectedGraphNode* next);
+      bool runEdge(SgDirectedGraphNode* node, SgDirectedGraphNode* next) {
+        return false;
+      }
+      bool checkIfValidCycle(SgDirectedGraphNode* node, SgDirectedGraphNode* next);
                
-               void init(RoseBin_Graph* vg) {
-                 vizzGraph = vg;
-                }
+      void init(RoseBin_Graph* vg) {
+        vizzGraph = vg;
+      }
 
-             };
-        }
-   }
+    };
+  }
+}
 
 CompassAnalyses::CycleDetection::
 CheckerOutput::CheckerOutput ( SgNode* node )
-   : OutputViolationBase(node,::cycleDetectionChecker->checkerName,::cycleDetectionChecker->shortDescription)
-   {}
+  : OutputViolationBase(node,::cycleDetectionChecker->checkerName,::cycleDetectionChecker->shortDescription)
+{}
 
 CompassAnalyses::CycleDetection::Traversal::
 Traversal(Compass::Parameters inputParameters, Compass::OutputObject* output)
-   : output(output)
-   {
+  : output(output)
+{
   // Initalize checker specific parameters here, for example: 
   // YourParameter = Compass::parseInteger(inputParameters["CycleDetection.YourParameter"]);
-     debug=false;
+  debug=false;
 
-   }
+}
 
 
 // Checker main run function and metadata
@@ -89,25 +86,20 @@ static void run(Compass::Parameters params, Compass::OutputObject* output) {
 
   CompassAnalyses::CycleDetection::Traversal checker(params, output);
   checker.init(Compass::binDataFlowPrerequisite.getGraph());
-  //  CompassAnalyses::CycleDetection::Traversal(params, output).init(graph);
-  //dfanalysis->traverseGraph(rootNodes, &CompassAnalyses::CycleDetection::Traversal(params, output), interprocedural);
 
-               bool interprocedural = false;
-               RoseBin_DataFlowAnalysis* dfanalysis = Compass::binDataFlowPrerequisite.getBinDataFlowInfo();
+  bool interprocedural = false;
+  RoseBin_DataFlowAnalysis* dfanalysis = Compass::binDataFlowPrerequisite.getBinDataFlowInfo();
   vector<SgDirectedGraphNode*> rootNodes;
   dfanalysis->getRootNodes(rootNodes);
   dfanalysis->init();
   
   dfanalysis->traverseGraph(rootNodes, &checker, interprocedural);
-
-
 }
 
 // Remove this function if your checker is not an AST traversal
 //static Compass::GraphProcessing* createTraversal(Compass::Parameters params, Compass::OutputObject* output) {
 //  return new CompassAnalyses::CycleDetection::Traversal(params, output);
 //}
-
 
 static Compass::PrerequisiteList getPrerequisites() {
   Compass::PrerequisiteList defusePre;
@@ -118,21 +110,24 @@ static Compass::PrerequisiteList getPrerequisites() {
 
 
 extern const Compass::Checker* const cycleDetectionChecker =
-  //  new Compass::CheckerUsingGraph(
   new Compass::Checker(
-        "CycleDetection",
-     // Descriptions should not include the newline character "\n".
-        "Short description not written yet!",
-        "Long description not written yet!",
-        Compass::X86Assembly,
-        getPrerequisites(),
-        run);
+                       "CycleDetection",
+                       // Descriptions should not include the newline character "\n".
+                       "Short description not written yet!",
+                       "Long description not written yet!",
+                       Compass::X86Assembly,
+                       getPrerequisites(),
+                       run);
    
 
-
+/**************************************************************************
+ * If we think that a cycle exists. We want to make sure.
+ * Therefore, we traverse the graph from the next node to the previous node
+ * and check if such a path exists.
+ **************************************************************************/
 bool 
 CompassAnalyses::CycleDetection::Traversal::checkIfValidCycle(SgDirectedGraphNode* node_n,
-			      SgDirectedGraphNode* next_n){
+                                                              SgDirectedGraphNode* next_n){
   // traverse the graph from next to node
   bool foundCycle=false;
   std::vector<SgDirectedGraphNode*> successors_f;
@@ -188,9 +183,14 @@ CompassAnalyses::CycleDetection::Traversal::checkIfValidCycle(SgDirectedGraphNod
   return foundCycle;
 }
 
+/**************************************************************************
+ * Main function. This function is run on each node that is being traversed
+ * in the graph. For each node, we determine the successors and check
+ * if those have been previously seen. If yes, a cycle may exist.
+ **************************************************************************/
 bool 
 CompassAnalyses::CycleDetection::Traversal::run(string& name, SgDirectedGraphNode* node,
-			      SgDirectedGraphNode* previous){
+                                                SgDirectedGraphNode* previous){
   // check known function calls and resolve variables
   ROSE_ASSERT(node);
 
