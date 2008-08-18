2523a977aee2a74673dfed1438a2d5311a0da484
panas2 <panas2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-08-18 17:58:02 +0000

    added another graph checker to compass
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1631 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ projects/compass/extensions/checkers/binaryInterruptAnalysis/binaryInterruptAnalysisMain.C
@@ -0,0 +1,10 @@
+//
+// Do not modify this file
+//
+
+#include "compass.h"
+extern const Compass::Checker* const binaryInterruptAnalysisChecker;
+extern const Compass::Checker* const myChecker = binaryInterruptAnalysisChecker;
+
+#include "compass.C"
+#include "compassTestMain.C"
