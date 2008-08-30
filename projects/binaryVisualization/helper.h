80cb3918731fac319bb4f0c4d5c2de5e56f0fca0
panas2 <panas2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-08-30 23:24:16 +0000

    work on visualization
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1835 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ projects/binaryVisualization/helper.h
@@ -0,0 +1,190 @@
+
+
+#ifndef __helper__
+#define __helper__
+
+#include <rose.h>
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include "ltdl.h"
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define KEY_ESC 27
+#define KEY_UP 101
+#define KEY_DOWN 103
+#define KEY_X 120
+#define KEY_Y 121
+#define KEY_Z 122
+
+GLfloat mat_red_diffuse[] = { 0.7, 0.0, 0.1, 1.0 };
+GLfloat mat_green_diffuse[] = { 0.0, 0.7, 0.1, 1.0 };
+GLfloat mat_blue_diffuse[] = { 0.0, 0.1, 0.7, 1.0 };
+GLfloat mat_yellow_diffuse[] = { 0.7, 0.8, 0.1, 1.0 };
+GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat mat_shininess[] = { 100.0 };
+//GLfloat knots[4] = { 0.0, 0.0, 1.0, 1.0};
+//GLfloat knots[8] = { 0.0, 0.0, 0.0, 0.0,  1.0, 1.0, 1.0, 1.0 };
+GLfloat knots[16] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+GLUnurbsObj *nurb;
+
+
+namespace __gnu_cxx {
+  template <> struct hash<std::pair<int,int> > {
+    unsigned long operator()( const std::pair<int,int> &s) const {
+      return (size_t) s.first;
+    }
+  };
+}
+
+bool containsArgument(int argc, char** argv, string pattern) {
+  for (int i = 2; i < argc ; i++) {
+    if (argv[i]== pattern) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int getdir (string dir, vector<string> &files)
+{
+  DIR *dp;
+  struct dirent *dirp;
+  if((dp  = opendir(dir.c_str())) == NULL) {
+    cout << "Error(" << errno << ") opening " << dir << endl;
+    return errno;
+  }
+
+  while ((dirp = readdir(dp)) != NULL) {
+    string name = string(dirp->d_name);
+    //int find = name.find(".lo");
+    //if (find>=0) {
+    //  name = name.substr(0,find);
+    if (name!="." && name!="..")
+      files.push_back(name);
+      //}
+  }
+  closedir(dp);
+  return 0;
+}
+
+
+
+
+
+
+class FunctionInfo {
+public:
+  int x;
+  int y;
+  int height;
+  int weight;
+  std::vector<std::string> names;
+  void addName(std::string name) {names.push_back(name);}
+  FunctionInfo(int x, int y, int height, int weight,
+	       std::string name):x(x),y(y),
+				 height(height), weight(weight)
+  { addName(name);};
+
+};
+
+
+typedef rose_hash::hash_map< pair<int,int>,
+			     FunctionInfo* > FunctionType;
+
+class Traversal : public AstSimpleProcessing {
+
+public:
+  unsigned int maxX;
+  unsigned int maxY;
+  int divHeight;
+  int maxim;
+  FunctionType functions;
+
+  void run(SgNode* node, int m) {
+    maxX=0;
+    maxY=0;
+    divHeight=3;
+    maxim=m;
+    this->traverse(node,preorder);
+  }
+  void visit(SgNode* node) {
+    SgAsmFunctionDeclaration* funcDecl =
+      isSgAsmFunctionDeclaration(node);
+    if (funcDecl) {
+      cerr << " preprocessing function: " << funcDecl->get_name() <<endl;
+      vector<SgAsmStatement*> instructions= 
+	funcDecl->get_statementList();
+      int nrInst=0;
+      unsigned int control=0;
+      unsigned int call=0;
+      vector<SgAsmStatement*>::iterator it= instructions.begin();
+      for (;it!=instructions.end();it++) {
+	SgAsmx86Instruction* inst = isSgAsmx86Instruction(*it);
+	if (inst) {
+	  nrInst++;
+	  if (x86InstructionIsControlTransfer(inst) ||
+	      x86InstructionIsConditionalFlagControlTransfer(inst) || 
+	      x86InstructionIsConditionalControlTransfer(inst) ||
+	      x86InstructionIsConditionalBranch(inst) ||
+	      x86InstructionIsUnconditionalBranch(inst)
+	      ) {
+	    control++;
+	  } else if (x86InstructionIsDataTransfer(inst) ||
+		     x86InstructionIsConditionalFlagDataTransfer(inst) ||
+		     x86InstructionIsConditionalDataTransfer(inst)
+		     ) {
+	    call++;
+	  }
+	}
+      } // for
+      // another algo
+
+      for (unsigned int u=0; u < funcDecl->get_name().size() ; u++) {
+	char c = (funcDecl->get_name().c_str())[u];
+	call += int(c); 
+	control += int(c);
+      }
+      //	cerr << " call = " << call << endl;
+
+
+                  call=(call%64);
+      control=(control%64);
+
+      if (call>maxY) maxY=call;
+      if (control>maxX) maxX=control;
+      //      if (nrInst>0)
+      //	nrInst =nrInst/divHeight;
+
+      FunctionType::iterator fit= 
+	functions.find(make_pair(control,call));
+      if (fit==functions.end()) {
+	// not found
+	FunctionInfo* info = new FunctionInfo(control, call,
+					      nrInst, 0, 
+					      funcDecl->get_name());
+	functions[make_pair(control,call)] = info;
+	cerr << "  .. creating : x="<<control<<" y="<<call
+	     << "  height="<<nrInst<<" weight=0"<<endl;
+      } else {
+	FunctionInfo* info = fit->second;
+	info->height=info->height+nrInst;
+	//	info->height=5;
+	info->weight=info->weight+1;
+	info->addName(funcDecl->get_name());
+	functions[make_pair(control,call)] = info;
+	cerr << "  .. adding : x="<<control<<" y="<<call
+	     << "  height="<<info->height<<" weight="<<info->weight<<endl;
+      }
+    } // if
+  }
+};
+
+#endif
+
