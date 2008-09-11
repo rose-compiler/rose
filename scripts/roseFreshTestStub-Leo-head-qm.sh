fa9b41e8c297f86c2549785f96b39192667d00b3
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-09-11 23:06:57 +0000

    Hooked up with Gary's PlumHall test
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1965 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/roseFreshTestStub-Leo-head-qm.sh
+++ scripts/roseFreshTestStub-Leo-head-qm.sh
@@ -4,6 +4,8 @@
 
 #required by make docs!
 source /usr/apps/graphviz/2.6/setup.sh
+#export PATH="/usr/apps/java/jdk1.5.0_11/bin:$PATH"
+#export LD_LIBRARY_PATH="/home/dquinlan/ROSE/OpenFortranParser/ofp_install/lib/:$LD_LIBRARY_PATH"
 
 export JAVA_HOME=/usr/apps/java/jdk1.5.0_11
 export BOOST_ROOT="/home/liao6/opt/boost_1_35_0"
@@ -15,19 +17,17 @@ export PATH="/usr/apps/automake/1.9.6/bin:$PATH"
 
 # using a time stamp to avoid overwriting previous tests
 TIMESTAMP=$(date +%Y%m%d_%H%M%S) 
-#ROSE_TOP=/home/liao6/daily-test-rose/${TIMESTAMP}
-ROSE_TOP=${ROSE_TEST_REGRESSION_ROOT}/rose/fresh/`date +%F-%H%M`
-
+ROSE_TOP=/home/liao6/daily-test-rose/${TIMESTAMP}
+#ROSE_TOP=/home/liao6/daily-test-rose/20080911_131715
+export ROSE_TOP
+LBL_RRQM_ROOT=liaoch@web-dev.nersc.gov:/www/host/rosecompiler/rrqm
+export LBL_RRQM_ROOT 
 
 # for regular build and tests
-#CONFIGURE_FLAGS=(--with-boost=${BOOST_ROOT} --enable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install "$@")
-# QMtest 
-RPREFIX="${ROSE_TEST_REGRESSION_ROOT}/rose/install"
-CONFIGURE_FLAGS=( --with-edg_source_code=true --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --with-CXXFLAGS=-fPIC -with-CFLAGS=-fPIC --enable-ltdl-convenience --with-boost=${BOOST_ROOT} --prefix=${RPREFIX} "$@")
+CONFIGURE_FLAGS=(--with-boost=${BOOST_ROOT} --enable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install "$@")
 
 ROSE_SVNROOT=file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE
-#MAILADDRS="liaoch@llnl.gov willcock2@llnl.gov panas2@llnl.gov yuan5@llnl.gov quinlan1@llnl.gov"
-MAILADDRS="liaoch@llnl.gov" 
+MAILADDRS="liaoch@llnl.gov yuan5@llnl.gov"
 
 # the right version of subversion is essential!!
 SVN=/nfs/apps/subversion/1.4.5/bin/svn
@@ -41,10 +41,17 @@ SVNOP=checkout
 # How many processes for make
 MAKEFLAGS="-j16"
 
-# my additional stuff
-KEEP_TEST_DIR=yes
-#NORMAL_INSTALL_DIR=yes # no chmod 000 for 'install'
-SKIP_DIST_TEST=yes
+#ENABLE_BUILD_BINARY_EDG=1
 
+# my additional stuff
+#ENABLE_UPLOAD_SVN=yes
+#ENABLE_UPLOAD_WEB=yes
+
+KEEP_TEST_DIR=yes
+NORMAL_INSTALL_DIR=yes # no chmod 000 for 'install'
+
+source /home/liao6/daily-test-rose/NIGHTLY_ROSE_Regression/scripts/rosetestrc
+ENABLE_RRQM=yes
+SKIP_DIST_TEST=yes
 set -e
 set -o pipefail
