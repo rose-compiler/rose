fa8f05da42319c1d99c534097487f11b4cc426ae
yuan5 <yuan5@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-09-12 15:51:53 +0000

    fresh test stubs
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@1973 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/roseFreshTestStub-Gary-head-qm.sh
+++ scripts/roseFreshTestStub-Gary-head-qm.sh
@@ -17,10 +17,12 @@ export PATH="/home/yuan5/lib/swig-1.3.36/install/bin:/usr/apps/java/jdk1.5.0_11/
 
 export BOOST_ROOTX="/home/yuan5/lib/boost_1_35_0/install"
 
+export ROSE_TOP=/home/yuan5/tmp/fresh/`date +%F-%H%M`
+export LBL_RRQM_ROOT=liaoch@web-dev.nersc.gov:/www/host/rosecompiler/rrqm
+source /home/yuan5/tmp/fresh/NIGHTLY_ROSE_Regression/scripts/rosetestrc
+
 ROSE_SVNROOT=file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE
 MAILADDRS="yuangm@gmail.com"
-
-export ROSE_TOP=${ROSE_TEST_REGRESSION_ROOT}/rose/fresh/`date +%F-%H%M`
 
 KEEP_TEST_DIR=yes
 NORMAL_INSTALL_DIR=yes
@@ -28,11 +30,12 @@ NORMAL_INSTALL_DIR=yes
 SVNOP=checkout
 SVN=/nfs/apps/subversion/1.4.5/bin/svn
 
-RPREFIX="${ROSE_TEST_REGRESSION_ROOT}/rose/install"
+CONFIGURE_FLAGS=( --with-edg_source_code=true --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --with-CXXFLAGS=-fPIC -with-CFLAGS=-fPIC --enable-ltdl-convenience --with-boost=${BOOST_ROOTX} --prefix=${ROSE_TOP}/install "$@")
 
-CONFIGURE_FLAGS=( --with-edg_source_code=true --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --with-CXXFLAGS=-fPIC -with-CFLAGS=-fPIC --enable-ltdl-convenience --with-boost=${BOOST_ROOTX} --prefix=${RPREFIX} "$@")
+KEEP_TEST_DIR=yes
+NORMAL_INSTALL_DIR=yes # no chmod 000 for 'install'
+ENABLE_RRQM=yes
+SKIP_DIST_TEST=yes
 
 set -e
 set -o pipefail
-
-SKIP_DIST_TEST=yes
