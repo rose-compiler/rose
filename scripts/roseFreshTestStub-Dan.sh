2ceac42fb0a08c840afd808370d7fb94801223a7
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-09-23 02:32:10 +0000

    Update tree edit distance on AST.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@2060 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/roseFreshTestStub-Dan.sh
+++ scripts/roseFreshTestStub-Dan.sh
@@ -12,7 +12,11 @@ export LD_LIBRARY_PATH="/usr/apps/java/jdk1.5.0_11/lib:/usr/apps/java/jdk1.5.0_1
 export JAVA_HOME=/usr/apps/java/jdk1.5.0_11
 export CLASSPATH="/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-3.0.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-2.7.7.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/stringtemplate-3.0.jar:/home/dquinlan/ROSE/OpenFortranParser/ofp_install/lib/OpenFortranParser.jar"
 export LD_LIBRARY_PATH="/home/dquinlan/ROSE/OpenFortranParser/ofp_install/lib/:${MPILIB}:$LD_LIBRARY_PATH"
-source /usr/apps/gcc/4.2.2/setup.sh
+
+# DQ (9/17/2008): Test with the 4.1.2 to be a little different
+# source /usr/apps/gcc/4.2.2/setup.sh
+source /usr/apps/gcc/4.1.2/setup.sh
+
 export PATH="/usr/apps/automake/1.9.6/bin:$PATH"
 #export PATH="/home/dquinlan/swig-install/bin:$PATH"
 export BOOST_ROOT="/home/dquinlan/local/boost_1_35_0_install"
