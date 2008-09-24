264b21e8944524473d82e8df8441dfbfdc6a7ac5
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-09-24 00:21:24 +0000

    Added comment in fortran test code.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@2081 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- tests/CompileTests/Fortran_tests/test2007_263.f90
+++ tests/CompileTests/Fortran_tests/test2007_263.f90
@@ -3,6 +3,9 @@
 !
 ! Without the "interface" this code is not legal F90 code.  It may still be
 ! legal Fortran 77 code however, so I have left the older code as a comment.
+! The older code passed gfortran 4.2, but fails with gforgran 4.4.  The 
+! modification by Craig fixes the problem so it works with grogram 4.4.
+!
 ! function my_sum(var1,var2) result(x)
 ! integer x
 ! integer var1,var2
