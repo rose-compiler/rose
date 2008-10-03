65d8d26a4507d1e4bd01db72b945eb3e124f342a
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-10-03 00:43:24 +0000

    Modified few details of nightly tests.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@2183 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/roseFreshTestStub-RegressionTests-Dan.sh
+++ scripts/roseFreshTestStub-RegressionTests-Dan.sh
@@ -10,13 +10,9 @@ source /usr/apps/graphviz/2.6/setup.sh
 export PATH="/usr/apps/java/jdk1.5.0_11/bin:${MPIPATH}:$PATH"
 export LD_LIBRARY_PATH="/usr/apps/java/jdk1.5.0_11/lib:/usr/apps/java/jdk1.5.0_11/jre/lib/amd64/server:$LD_LIBRARY_PATH"
 export JAVA_HOME=/usr/apps/java/jdk1.5.0_11
-export CLASSPATH="/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-3.0.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-2.7.7.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/stringtemplate-3.0.jar"
+export CLASSPATH="/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-3.0.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-2.7.7.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/stringtemplate-3.0.jar:/home/dquinlan/ROSE/OpenFortranParser/ofp_install/lib/OpenFortranParser.jar"
 export LD_LIBRARY_PATH="/home/dquinlan/ROSE/OpenFortranParser/ofp_install/lib/:${MPILIB}:$LD_LIBRARY_PATH"
-
-# DQ (9/17/2008): Test with the 4.1.2 to be a little different
 source /usr/apps/gcc/4.2.2/setup.sh
-# source /usr/apps/gcc/4.1.2/setup.sh
-
 export PATH="/usr/apps/automake/1.9.6/bin:$PATH"
 #export PATH="/home/dquinlan/swig-install/bin:$PATH"
 export BOOST_ROOT="/home/dquinlan/local/boost_1_35_0_install"
@@ -25,6 +21,9 @@ export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:${LD_LIBRARY_PATH}"
 # DQ (7/19/2008): Permissions were not reset after my 3rd striped disk was added! So use another directory for now.
 # ROSE_TOP="/home/dquinlan/ROSE/svn-test-rose/$$/"
 ROSE_TOP="/home/dquinlan/ROSE/svn-test-rose-alt/$$/"
+export ROSE_TOP
+
+export LD_LIBRARY_PATH=/usr/apps/xercesc/2.7.0/lib/:${LD_LIBRARY_PATH}
 
 CONFIGURE_FLAGS=(--with-edg_source_code=true --with-boost=${BOOST_ROOT} --enable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install "$@")
 BINARY_CONFIGURE_FLAGS=(--with-boost=${BOOST_ROOT} --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install_binary "$@")
@@ -35,6 +34,18 @@ MAILADDRS="dquinlan@llnl.gov"
 # the right version of subversion is essential!!
 SVN=/nfs/apps/subversion/1.4.5/bin/svn
 SVNOP=checkout
+
+# Variables controling use of ROSE Regression Tests
+KEEP_TEST_DIR=yes
+NORMAL_INSTALL_DIR=yes # no chmod 000 for 'install'
+# ENABLE_RRQM=yes
+ENABLE_RRQM_PLUMHALL=yes
+ENABLE_RRQM_MOZILLA=yes
+# ENABLE_RRQM_PUBLISH=yes
+SKIP_DIST_TEST=yes
+
+export LBL_RRQM_ROOT=dquinlan@web-dev.nersc.gov:/www/host/rosecompiler/rrqm
+source /home/dquinlan/ROSE/svn-test-rose-alt/ROSE_RegressionTests/scripts/rosetestrc
 
 MAKEFLAGS="-j16"
 
