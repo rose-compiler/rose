67c45ba38e5a96232298bcccb420c72e957527f9
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-10-04 02:52:36 +0000

    Removing options and environment variables no longer required.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@2195 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/roseFreshTestStub-Dan.sh
+++ scripts/roseFreshTestStub-Dan.sh
@@ -10,8 +10,9 @@ source /usr/apps/graphviz/2.6/setup.sh
 export PATH="/usr/apps/java/jdk1.5.0_11/bin:${MPIPATH}:$PATH"
 export LD_LIBRARY_PATH="/usr/apps/java/jdk1.5.0_11/lib:/usr/apps/java/jdk1.5.0_11/jre/lib/amd64/server:$LD_LIBRARY_PATH"
 export JAVA_HOME=/usr/apps/java/jdk1.5.0_11
-export CLASSPATH="/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-3.0.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-2.7.7.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/stringtemplate-3.0.jar"
-export LD_LIBRARY_PATH="/home/dquinlan/ROSE/OpenFortranParser/ofp_install/lib/:${MPILIB}:$LD_LIBRARY_PATH"
+#export CLASSPATH="/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-3.0.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/antlr-2.7.7.jar:/home/dquinlan/ANTLR/antlr-3.0/lib/stringtemplate-3.0.jar"
+#export LD_LIBRARY_PATH="/home/dquinlan/ROSE/OpenFortranParser/ofp_install/lib/:${MPILIB}:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="${MPILIB}:$LD_LIBRARY_PATH"
 
 # DQ (9/17/2008): Test with the 4.1.2 to be a little different
 source /usr/apps/gcc/4.2.2/setup.sh
@@ -19,15 +20,17 @@ source /usr/apps/gcc/4.2.2/setup.sh
 
 export PATH="/usr/apps/automake/1.9.6/bin:$PATH"
 #export PATH="/home/dquinlan/swig-install/bin:$PATH"
-export BOOST_ROOT="/home/dquinlan/local/boost_1_35_0_install"
+#export BOOST_ROOT="/home/dquinlan/local/boost_1_35_0_install"
+export BOOST_ROOT="/home/dquinlan/local/boost_1_35_0_installTree-gxx-4.2.2"
 export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:${LD_LIBRARY_PATH}"
 
 # DQ (7/19/2008): Permissions were not reset after my 3rd striped disk was added! So use another directory for now.
 # ROSE_TOP="/home/dquinlan/ROSE/svn-test-rose/$$/"
-ROSE_TOP="/home/dquinlan/ROSE/svn-test-rose-alt/$$/"
+ROSE_TOP="/home/dquinlan/ROSE/svn-test-rose/$$/"
 
-CONFIGURE_FLAGS=(--with-edg_source_code=true --with-boost=${BOOST_ROOT} --enable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install "$@")
-BINARY_CONFIGURE_FLAGS=(--with-boost=${BOOST_ROOT} --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install_binary "$@")
+# CONFIGURE_FLAGS=(--with-edg_source_code=true --with-boost=${BOOST_ROOT} --enable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install "$@")
+CONFIGURE_FLAGS=(--with-boost=${BOOST_ROOT} --enable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install "$@")
+# BINARY_CONFIGURE_FLAGS=(--with-boost=${BOOST_ROOT} --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install_binary "$@")
 
 ROSE_SVNROOT=file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE
 MAILADDRS="dquinlan@llnl.gov"
