3867b9549dd54be82450f1e029475c464f4c44e6
willcock2 <willcock2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-10-06 04:22:56 +0000

    Small fixes to scripts
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@2212 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/roseFreshTestStub-Jeremiah-OSX-scidac.sh
+++ scripts/roseFreshTestStub-Jeremiah-OSX-scidac.sh
@@ -13,6 +13,7 @@ CONFIGURE_FLAGS=( --disable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=
 ROSE_SVNROOT=https://outreach.scidac.gov/svn/rose/trunk
 MAILADDRS="willcock2@llnl.gov liaoch@llnl.gov dquinlan@llnl.gov"
 SKIP_DIST_TEST=yes
+SKIP_BUILD_SOURCE=0
 
 # the right version of subversion is essential!!
 SVN=/usr/bin/svn
