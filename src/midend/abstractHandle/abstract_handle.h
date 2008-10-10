37cea89270be41682dc1d492ad1001f9f5a1f612
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-10-10 23:36:34 +0000

    Added work about abstract handle code and tutorial chapter
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@2412 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ src/midend/abstractHandle/abstract_handle.h
@@ -0,0 +1,206 @@
+/*!
+ * A reference implementation of abstract handles for language constructs
+ * Used for specifying a unique language construct in source files.
+ *
+ * Users are required to implement the interface using their own
+ * IR node types.
+ *
+ * Goal : 
+ *     Only define interfaces and abstract implementation
+ *     independent to any particular compilers or tools
+ */
+#ifndef abstract_handle_INCLUDED
+#define abstract_handle_INCLUDED
+
+#include <string>
+#include <utility>
+#include <limits.h>
+
+namespace AbstractHandle{
+
+  //source position information:
+  struct source_position
+  {
+    size_t line;
+    size_t column;
+  };
+
+  struct source_position_pair
+  {
+    source_position first;
+    source_position second;
+  };
+
+  std::string toString(const source_position &);
+  std::string toString(const source_position_pair &);
+  bool isEqual(const source_position &pos1, const source_position &pos2);
+  bool isEqual(const source_position_pair &pair1, const source_position_pair &pair2);
+   
+  // Parse a string (such as 12.10, 13, 0 etc )into source position data structure
+  void fromString(source_position&, const std::string& input);
+  // Parse a string into source position pair data structure
+  void fromString(source_position_pair&, const std::string& input);
+
+ //----------specifiers------------------------------------
+  // specifier type and values
+  typedef  enum {e_name, e_position, e_numbering, e_int_label, e_str_label} 
+    specifier_type_t;
+
+  typedef union u_specifier_value
+     {
+       char str_v[PATH_MAX+255];
+       size_t int_v; 
+       source_position_pair positions;
+     } specifier_value_t;
+
+  // construct specifier 
+  // could be used to specify a construct by name, position, numbering, labels, etc
+  // (e_name, foo) (e_position, 15)
+  class specifier
+  {
+  public:
+    specifier(){};
+    specifier (specifier_type_t t, specifier_value_t v):type(t),value(v){}
+    specifier_type_t get_type(){return type;}
+    specifier_value_t  get_value(){return value;}
+    void set_type(specifier_type_t stype) { type=stype;}
+    void set_value(specifier_value_t svalue) {value=svalue;}
+    std::string toString();
+  private:  
+     specifier_type_t type; 
+     specifier_value_t value ;
+  };
+
+  // Parse a string into a specifier 
+  void fromString(specifier&, const std::string& input);
+
+ //----------------Abstract AST/IR node---------------
+ //Users  should provide a concrete node implementation
+ //
+ class abstract_node
+ {
+  public: 
+  virtual ~abstract_node(){}
+
+  // Get the construct' s type name, like function
+  virtual std::string getConstructTypeName() const =0;
+
+  //If the node has meaningful line and column numbers associated with a file
+  virtual bool hasSourcePos() const = 0;
+
+  //If the node has legal names defined by language standards
+  virtual bool hasName() const = 0;
+
+  // Get the name of the construct if it is named, like function name
+  virtual std::string getName() const = 0;
+
+  //Get the start source file position of the construct
+  // Get the abstract node for file containing the current node.
+  virtual abstract_node* getFileNode() const =0;
+
+  //Get parent node, used for generate parent handle automatically
+  virtual abstract_node* getParent() const =0;
+
+  //Find a node from a string for a abstract handle's string format, starting from this node
+  // eg. find a file node from a string like SgSourceFile<name,/home/liao6/names.cpp>
+  virtual abstract_node* findNode(std::string handle_str) const =0;  
+
+  //Find a node of a given type, it also matches the specifier.
+  virtual abstract_node* findNode(std::string construct_type_str, specifier mspecifier) const =0;  
+
+  virtual std::string getFileName() const =0;
+
+  virtual source_position_pair getSourcePos() const = 0;
+  virtual source_position getStartPos() const = 0;
+  virtual source_position getEndPos() const = 0;
+
+  virtual std::string toString() const =0;
+
+  //Get integer label
+  virtual size_t getIntLabel() const =0;
+  //Get string label
+  virtual std::string getStringLabel() const =0;
+
+  //Get the ordering of the construct relative to another construct in a higher scope
+  // Numbering start from 1
+  //e.g. a function construct may be the 2nd function relative to a file
+  virtual size_t getNumbering( const abstract_node*  another_node) const =0;
+
+  virtual bool operator == (const abstract_node & x) const = 0;
+ }; // end class abstract_node
+
+  //---------------- An abstract handle --------------------------
+  // to specify a construct using a specifier
+  // Can be used alone or with parent handles when relative specifiers are used
+  class abstract_handle 
+  {
+  public:
+    //Constructors
+    
+    abstract_handle();
+    // Create a handle from a node, using the source position as the specifier by default
+    // Or use name if source position is not available, then use numbering as the final resort.
+    abstract_handle(abstract_node* node);
+
+    // Create a handle from an optional parent handle, 
+    // with explicit type and numbering information available.
+    //It also tries its best to recursively generate parent handles using the given specifier type 
+    //if parent_handle is not provided in the parameter list. 
+    abstract_handle(abstract_node* node, specifier_type_t stype, \
+                   specifier_value_t svalue, abstract_handle* p_handle=NULL);
+
+    //Create a handle using specified type, automatically fill out the corresponding value
+    //Most useful to create a numbering or name specifier within another handle
+    //It also tries its best to recursively generate parent handles using the given specifier type 
+    //if parent_handle is not provided in the parameter list. 
+     abstract_handle(abstract_node* node, specifier_type_t stype, \
+               abstract_handle* p_handle=NULL);
+
+    //construct a handle from a handle string  within the scope of an existing handle
+    //e.g: <SgSourceFile<name,/home/liao6/names.cpp> will create a corresponding file handle 
+    abstract_handle(abstract_handle* phandle, const std::string& handle_string);
+    
+    virtual ~abstract_handle() { if (m_specifier !=NULL) delete m_specifier;}
+
+    // the language construct type name
+    virtual std::string get_construct_type_name();
+
+    // Output the handle to a string, including all parent handles
+    virtual std::string toString();
+
+    // Initialize a handle from a handle string, generate full parent handles when necessary
+    virtual void fromString(abstract_handle* ancester_handle, const std::string &handle_str_multiple);
+
+    // Only output the handle itself to a string, excluding parent handles
+    virtual std::string toStringSelf();
+
+    //Instantiate a handle using a handle element's string (a single handle only),the handle refers to a node under a parent
+    virtual void fromStringSelf(abstract_handle* p_handle, const std::string& handle_str_item);
+
+   virtual abstract_node* getNode() const {return m_node;}
+  // allow multiple specifiers or not?
+   specifier* get_specifier(){return m_specifier;}
+  
+  abstract_handle* get_parent_handle(){return parent_handle; }
+  void set_parent_handle(abstract_handle* parent){ parent_handle= parent;}
+
+  // operator ==
+  //Any other way to implement this??
+  bool operator == (const abstract_handle& x) const
+    { return m_node == x.getNode();}
+
+  private: 
+    // initialize all internal members to NULL
+    void init();
+    abstract_handle * parent_handle;
+    // shared handles cannot have a single next handle
+    // abstract_handle * next_handle; // expose them or not?
+    
+    // associated IR node 
+    abstract_node* m_node; 
+    specifier * m_specifier;
+  };
+
+}// end namespace 
+
+#endif 
---
+++ src/midend/abstractHandle/roseAdapter.h
@@ -0,0 +1,43 @@
+#ifndef rose_adapter_INCLUDED
+#define rose_adapter_INCLUDED
+
+#include <rose.h>
+#include <iostream>
+#include <string>
+
+#include "abstract_handle.h"
+
+class roseNode: public AbstractHandle::abstract_node 
+{
+ public:
+   roseNode(SgNode* snode):mNode(snode){};
+
+   virtual ~roseNode(){};
+
+   SgNode* getNode() const {return mNode; }
+
+   // must provide non-pure virtual function prototypes here
+   virtual std::string getConstructTypeName() const;
+   virtual bool hasSourcePos() const;
+   virtual bool hasName() const;
+   virtual std::string getName() const;
+   virtual AbstractHandle::abstract_node* getFileNode() const;
+   virtual AbstractHandle::abstract_node* getParent() const;
+   virtual AbstractHandle::abstract_node* findNode(std::string) const;
+   virtual AbstractHandle::abstract_node* findNode(std::string construct_type_str, AbstractHandle::specifier mspecifier) const;
+   virtual std::string getFileName() const;
+   virtual AbstractHandle::source_position_pair getSourcePos() const;
+   virtual AbstractHandle::source_position getStartPos() const;
+   virtual AbstractHandle::source_position getEndPos() const;
+
+  virtual size_t getNumbering (const AbstractHandle::abstract_node* another_node) const;
+  virtual size_t getIntLabel() const; 
+  virtual std::string getStringLabel() const;
+  virtual std::string toString() const;
+  virtual bool operator == (const abstract_node & x) const;
+  
+ protected:
+    SgNode* mNode;
+};
+
+#endif
