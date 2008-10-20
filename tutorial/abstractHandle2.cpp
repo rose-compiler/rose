f594dfbeef835fef581c87be6d797e3c93ebb499
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-10-20 17:28:33 +0000

    Made abstract handles less influenced by ROSE names.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@2583 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- tutorial/abstractHandle2.cpp
+++ tutorial/abstractHandle2.cpp
@@ -24,21 +24,21 @@ int main(int argc, char * argv[])
   cout<<"Created a file handle:\n"<<handle1->toString()<<endl<<endl;;
 
   //Create a handle to a namespace given its name and parent handle 
-  string input1("SgNamespaceDeclarationStatement<name,space1>");
+  string input1("NamespaceDeclarationStatement<name,space1>");
   abstract_handle* handle0 = new abstract_handle(handle1,input1);
   cout<<"Created a handle:\n"<<handle0->toString()<<endl<<endl;
   cout<<"It points to:\n"<<handle0->getNode()->toString()<<endl<<endl;
 
   // Create a handle within the file, given a string specifying  
   // its construct type (class declaration) and source position 
-  string input("SgClassDeclaration<position,4.3-9.2>");
+  string input("ClassDeclaration<position,4.3-9.2>");
   abstract_handle* handle2 = new abstract_handle(handle1,input);
 
   cout<<"Created a handle:\n"<<handle2->toString()<<endl<<endl;
   cout<<"It points to:\n"<<handle2->getNode()->toString()<<endl<<endl;;
 
   // find the second function declaration within handle2
-  abstract_handle handle3(handle2,"SgFunctionDeclaration<numbering,2>");
+  abstract_handle handle3(handle2,"FunctionDeclaration<numbering,2>");
 
   cout<<"Created a handle:\n"<<handle3.toString()<<endl<<endl;
   cout<<"It points to:\n"<<handle3.getNode()->toString()<<endl;
