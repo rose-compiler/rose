141986b02531a8c146719a813efa0df9807b8fa9
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-10-30 01:38:19 +0000

    Initial work to automatically * make a release package off the head of the external repository * conduct a set of sanity check on the package * upload the package to the web site
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@2775 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/makeRelease.sh
+++ scripts/makeRelease.sh
@@ -1,18 +1,39 @@
-#!/bin/sh
+#!/bin/sh -x
+#
+# This script tries to upload a distribution package to the Scidac Outreach center web site
+# The assumption is that the machine running this script has a firefox browser with 
+# saved session which has already logged into the website. 
+# So we don't need put user account/password into this script
+# 
+# It will be called by another script: makeAndReleaseROSE.sh
+#
 # by Jeremiah, 10/14/2008
+# Modified by Liao, 10.29.2008
+if [ $# -lt 3 ]
+then
+  echo This script needs three arguments
+  echo Usage:$0 VERSION_NO REVISION_NO FULL_PACKAGE_NAME
+  echo Exampe:$0 0.9.3a 2975 /file/rose-0.9.4a-source-with-EDG-binary-2759.tar.gz
+  exit
+fi
+
+VERSION_NO=$1
+REVISION_NO=$2
+FULL_PACKAGE_NAME=$3
 
 # ls ~/.mozilla/firefox/ to find out your firefox id
 FIREFOXID=58zvv6td.default
-
 SESSION=`sed -n '/^outreach\.scidac\.gov\t.*\tsession_ser\t/p' < $HOME/.mozilla/firefox/$FIREFOXID/cookies.txt | cut -f 7`
-
 # This needs to match the browser you used to log in to Outreach; find it from http://browserspy.dk/useragent.php
 USERAGENT='Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.17) Gecko/20080829 Firefox/2.0.0.17'
-
 # The actual content to be filled into the web form
-RELEASENAME="0.9.3a-multiplatform-2484"
+
+#RELEASENAME="0.9.3a-multiplatform-2484"
+
+RELEASENAME="${VERSION_NO}-multiplatform-${REVISION_NO}"
 RELEASEDATE=`date '+%F %H:%M'`
-FILENAME="/home/liao6/daily-test-rose/20081014_120001/build/rose-0.9.3a-source-with-EDG-binary-2484.tar.gz"
+
+#FILENAME="/home/liao6/daily-test-rose/20081014_120001/build/rose-0.9.3a-source-with-EDG-binary-2484.tar.gz"
 TYPEID=5020 # Source .tar.gz
 PROCESSORID=8000 # any processor type
 # PROCESSORID=9999 # other
@@ -28,7 +49,7 @@ curl \
   -F "package_id=21" \
   -F "release_name=$RELEASENAME" \
   -F "release_date=$RELEASEDATE" \
-  -F "userfile=@$FILENAME" \
+  -F "userfile=@$FULL_PACKAGE_NAME" \
   -F "type_id=$TYPEID" \
   -F "processor_id=$PROCESSORID" \
   -F "release_notes=This is the latest stable ROSE source release with the EDG binaries for multiple platforms, including i686-pc-linux-gnu, x86_64-pc-linux-gnu, and i686-apple-darwin9.4.0." \
---
+++ scripts/roseFreshTestStub-Leo-scidac-release.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Stub file to test the head of the external ROSE repository and 
+# generate a release package
+#
+# Using the external repository for the final release is safer than using
+# the internal one with EDG source files. 
+# Liao 10/29/2008
+
+export JAVA_HOME=/usr/apps/java/jdk1.5.0_11
+export BOOST_ROOT="/home/liao6/opt/boost_1_35_0"
+export LD_LIBRARY_PATH="/usr/apps/java/jdk1.5.0_11/lib:${JAVA_HOME}/jre/lib/i386/server:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:${LD_LIBRARY_PATH}"
+
+source /usr/apps/gcc/4.2.2/setup.sh
+export PATH="/usr/apps/automake/1.9.6/bin:$JAVA_HOME/bin:$PATH"
+
+# using a time stamp to avoid overwriting previous tests
+TIMESTAMP=$(date +%Y%m%d_%H%M%S) 
+#ROSE_TOP=/home/liao6/daily-test-rose/${TIMESTAMP}
+ROSE_TOP=/home/liao6/daily-test-rose/release
+
+# for regular build and tests
+CONFIGURE_FLAGS=( --with-boost=${BOOST_ROOT} --enable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS=-Wall --prefix=${ROSE_TOP}/install "$@")
+
+#ROSE_SVNROOT=file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE
+ROSE_SVNROOT=https://outreach.scidac.gov/svn/rose/trunk
+MAILADDRS="liaoch@llnl.gov willcock2@llnl.gov"
+
+# the right version of subversion is essential!!
+SVN=/nfs/apps/subversion/1.5.1/bin/svn
+# avoid .svn directories
+SVNOP=export
+#SVNOP=checkout
+
+# check out the revision of the past 11:00pm PDT (06:00am UTC)
+# using data as revision number needs {}
+# default is head
+
+#SVNVERSIONOPTION="{`date -d '06:00Z' -u '+%FT%H:%M:%SZ'`}"
+#SVNVERSIONOPTION=1303
+
+# How many processes for make
+MAKEFLAGS="-j16"
+
+# my additional stuff, not suitable for the external repository
+#ENABLE_UPLOAD_SVN=yes
+#ENABLE_UPLOAD_WEB=yes
+
+KEEP_TEST_DIR=yes
+NORMAL_INSTALL_DIR=yes # no chmod 000 for 'install'
+# skip make dist, make distcheck, 
+#SKIP_DIST_TEST=yes
+# end-users are not supposed to run ./build in source tree
+#SKIP_BUILD_SOURCE=yes
+#make source_with_binary_edg_dist etc
+#ENABLE_BUILD_BINARY_EDG=1
+set -e
+set -o pipefail
