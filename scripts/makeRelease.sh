28ea97cecb531844f8e7144a4cce32433366b4e6
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-11-17 21:09:59 +0000

    Updated the auto-release script to the new updated browser
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@3231 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/makeRelease.sh
+++ scripts/makeRelease.sh
@@ -28,7 +28,7 @@ FULL_PACKAGE_NAME=$3
 FIREFOXID=58zvv6td.default
 SESSION=`sed -n '/^outreach\.scidac\.gov\t.*\tsession_ser\t/p' < $HOME/.mozilla/firefox/$FIREFOXID/cookies.txt | cut -f 7`
 # This needs to match the browser you used to log in to Outreach; find it from http://browserspy.dk/useragent.php
-USERAGENT='Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.17) Gecko/20080829 Firefox/2.0.0.17'
+USERAGENT='Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.18) Gecko/20081029 Firefox/2.0.0.18'
 # The actual content to be filled into the web form
 
 #RELEASENAME="0.9.3a-multiplatform-2484"
