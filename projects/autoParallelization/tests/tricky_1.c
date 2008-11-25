145b714168c0dbac7790cd49192e45e9edb2a979
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-11-25 01:03:30 +0000

    Added two tricky cases Fixed the include path to omp.h
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@3328 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ projects/autoParallelization/tests/tricky_1.c
@@ -0,0 +1,14 @@
+/*
+ * The impact of loop bounds
+ */
+void foo(void)   
+{
+  int i,j;
+  int a[10];
+//  for(i=5;i< 10;i++) 
+  for(i=0;i< 10;i++) 
+  {  
+   // a[i]=a[i-10]+i;
+    a[i]=a[i-5]+i;
+  }   
+}
