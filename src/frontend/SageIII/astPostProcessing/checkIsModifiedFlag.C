ea48fc6a2e02820aa785664032a5dde5f9dc45bd
matzke <matzke@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2008-12-03 16:53:54 +0000

    checkIsModified() returns bool rather than void
    This function still recursively clears the p_isModified bit. Now, in
    addition, it returns true if any of the p_isModified bits were true to
    start with.
    
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@3506 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/frontend/SageIII/astPostProcessing/checkIsModifiedFlag.C
+++ src/frontend/SageIII/astPostProcessing/checkIsModifiedFlag.C
@@ -3,30 +3,41 @@
 
 using namespace std;
 
-void checkIsModifiedFlag( SgNode *node)
+/** Clears the p_isModified bit for @p node and all children, recurisvely. Returns true iff node or one of its descendents had
+ *  p_isModified set. */
+bool checkIsModifiedFlag( SgNode *node)
    {
   // DQ (7/7/2005): Introduce tracking of performance of ROSE.
      TimingPerformance timer ("Check the isModifiedFlag in each IR node:");
 
-     CheckIsModifiedFlagSupport(node);
+     return CheckIsModifiedFlagSupport(node);
    }
 
 // QY
-void CheckIsModifiedFlagSupport( SgNode *node)
+bool
+CheckIsModifiedFlagSupport(SgNode *node)
 {
-  if (node->get_isModified()) {
-     //cerr << "Warning: the isModified flag is set in frontend for class " << node->sage_class_name() << endl;
-     //assert(false);
-     node->set_isModified(false);
-  }
-//  cerr << " visiting " << node->sage_class_name() << " : " << node << endl;
-  vector <SgNode*> children = node->get_traversalSuccessorContainer(); 
-  for (vector<SgNode*>::const_iterator p = children.begin(); p != children.end(); ++p) {
-    SgNode *cur = *p;
-     if ( cur != 0) {
-         if (node == cur->get_parent())
-             CheckIsModifiedFlagSupport(cur);
-     }
-  }
+    bool retval=false;
+    if (node->get_isModified()) {
+        node->set_isModified(false);
+        retval = true;
+    }
+
+    vector <SgNode*> children = node->get_traversalSuccessorContainer(); 
+    for (vector<SgNode*>::const_iterator p = children.begin(); p != children.end(); ++p) {
+        SgNode *cur = *p;
+        if (cur && node==cur->get_parent() && CheckIsModifiedFlagSupport(cur))
+            retval = true;
+    }
+
+    /* It seems that the genericFile stored in an SgAsmFile is not considered to be a child. [RPM 2008-12-02] */
+    SgAsmFile *asmFile = isSgAsmFile(node);
+    if (asmFile) {
+        SgAsmGenericFile *ef = asmFile->get_genericFile();
+        if (ef && CheckIsModifiedFlagSupport(ef))
+            retval = true;
+    }
+    
+    return retval;
 } 
 
--- src/frontend/SageIII/astPostProcessing/checkIsModifiedFlag.h
+++ src/frontend/SageIII/astPostProcessing/checkIsModifiedFlag.h
@@ -1,9 +1,9 @@
 #ifndef CHECK_ISMODIFIED_FLAG_H
 #define CHECK_ISMODIFIED_FLAG_H
 
-void checkIsModifiedFlag( SgNode *node);
+bool checkIsModifiedFlag( SgNode *node);
 
-void CheckIsModifiedFlagSupport( SgNode *node);
+bool CheckIsModifiedFlagSupport( SgNode *node);
 
 // endif for CHECK_ISMODIFIED_FLAG_H
 #endif
