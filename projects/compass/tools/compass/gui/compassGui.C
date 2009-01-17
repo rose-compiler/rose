f428b8552684c5025c1be2e6b999334d00ecd686
saebjornsen1 <saebjornsen1@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-01-17 01:45:48 +0000

    Fix to problem Dan reported
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@3991 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- projects/compass/tools/compass/gui/compassGui.C
+++ projects/compass/tools/compass/gui/compassGui.C
@@ -300,19 +300,20 @@ void CompassGui::run()
 #ifdef HAVE_SQLITE3
  std::vector<BuildLine> buildLines;
 
- //Make sure that the violations table exists and is empty
- try {
-   sqlite3x::sqlite3_command cmd(Compass::con, "drop table if exists violations" );
-   cmd.executenonquery();
- } catch (std::exception& e) {std::cerr << "Exception: " << e.what() << std::endl;}
- try {
-   Compass::con.executenonquery("create table IF NOT EXISTS violations( row_number INTEGER PRIMARY KEY, checker_name TEXT,  error_body TEXT, filename TEXT, line INTEGER, short_description TEXT )");
- } catch (std::exception& e) {std::cerr << "Exception: " << e.what() << std::endl;}
-
-
 
   if ( Compass::UseDbOutput == true )
   {
+
+    //Make sure that the violations table exists and is empty
+    try {
+      sqlite3x::sqlite3_command cmd(Compass::con, "drop table if exists violations" );
+      cmd.executenonquery();
+    } catch (std::exception& e) {std::cerr << "Exception: " << e.what() << std::endl;}
+    try {
+      Compass::con.executenonquery("create table IF NOT EXISTS violations( row_number INTEGER PRIMARY KEY, checker_name TEXT,  error_body TEXT, filename TEXT, line INTEGER, short_description TEXT )");
+    } catch (std::exception& e) {std::cerr << "Exception: " << e.what() << std::endl;}
+
+
 
     try { // Read each build_index from the database
       /* Read in from database here */
