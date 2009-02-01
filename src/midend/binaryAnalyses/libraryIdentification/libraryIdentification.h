65be6a00ad7978f7ec42eed152f5ad8f1ddc43e8
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-02-01 01:13:22 +0000

    Summary: Added more support for identification and recognition of library functions.
    Detailed changes in this commit:
       1) Added initial support to call interface agreed upon with Andreas.
       2) Added LibraryIdentification directory to src/midend/binaryAnalysis
       3) Added test code into new directory: developersScratchSpace/Dan/libraryIdentification_tests
    
    
    
    
    
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@4239 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/midend/binaryAnalyses/libraryIdentification/libraryIdentification.h
+++ src/midend/binaryAnalyses/libraryIdentification/libraryIdentification.h
@@ -4,4 +4,47 @@ namespace LibraryIdentification
   // This is an implementation of Fast Library Identification and Recognition Technology
      void generateLibraryIdentificationDataBase    ( std::string databaseName, SgProject* project );
      void matchAgainstLibraryIdentificationDataBase( std::string databaseName, SgProject* project );
+
+     struct library_handle
+        {
+          std::string filename;
+          std::string function_name;
+          size_t begin;
+          size_t end;
+        };
+
+  // Add an entry to store the pair <library_handle,string> in the database
+     void set_function_match( const library_handle & handle, const std::string & data );
+
+  // Return the library_handle matching string from the database. bool false
+  // is returned if no such match was found, true otherwise.
+     bool get_function_match( library_handle & handle, const std::string & data );
+
+     class FindFunctions: public AstSimpleProcessing
+        {
+          public:
+            // std::map<SgAsmGenericSymbol::addr_t,SgAsmGenericSymbol*> globalVariableMap;
+            // std::map<SgAsmGenericSymbol::addr_t,SgAsmGenericSymbol*> globalFunctionMap;
+
+            // Store the current statement as we traverse so that we can use it to call nextAsmStatement().
+            // We need the address of the next statement to evaluate references to global variables.
+               SgAsmStatement*   currentAsmFunction;
+               SgAsmInstruction* firstAsmInstruction;
+               SgAsmInstruction* lastAsmInstruction;
+
+               void visit(SgNode* n);
+        };
+
+     class FlattenAST: public AstSimpleProcessing
+        {
+          public:
+            // Save flattended AST in reference initialized at construction.
+               SgUnsignedCharList & data;
+
+               FlattenAST(SgUnsignedCharList & s) : data(s) {}
+
+               void visit(SgNode* n);
+        };
+
    }
+
--- src/midend/binaryAnalyses/libraryIdentification/libraryIdentification_writer.C
+++ src/midend/binaryAnalyses/libraryIdentification/libraryIdentification_writer.C
@@ -6,6 +6,7 @@
 #include <libraryIdentification.h>
 
 // Use the MD5 implementation that is in Linux.
+// I don't need this byt Andreas will...
 #include <openssl/md5.h>
 
 // Function prototype looks like:
@@ -19,5 +20,61 @@ LibraryIdentification::generateLibraryIdentificationDataBase( string databaseNam
   // DQ (9/1/2006): Introduce tracking of performance of ROSE at the top most level.
      TimingPerformance timer ("AST Library Identification reader : time (sec) = ",true);
 
-     printf ("Building LibraryIdentification database: %s from AST of project: %p \n",project,databaseName.c_str());
+     printf ("Building LibraryIdentification database: %s from AST of project: %p \n",databaseName.c_str(),project);
+
+     FindFunctions t;
+
+     printf ("Traverse the AST to file functions \n");
+     t.traverse(project,preorder);
+     printf ("DONE: Traverse the AST to file functions \n");
    }
+
+
+void
+LibraryIdentification::FindFunctions::visit(SgNode* n)
+   {
+     static int counter = 0;
+     if (isSgAsmFunctionDeclaration(n) != NULL)
+        {
+          SgUnsignedCharList s;
+          FlattenAST t(s);
+          printf ("Traverse the AST for this function to generate byte stream \n");
+          t.traverse(n,preorder);
+          printf ("DONE: Traverse the AST for this function to generate byte stream \n");
+
+       // set_function_match( const library_handle & handle, const std::string & data );
+
+       // TODO: 
+       //    1) fill in the library_handle with the filename, etc.
+       //    2) Compute the file offset from the instruction (is this easy).
+       //    3) Get Andreas' class in place.
+
+          library_handle handle;
+       // ANDREAS_CLASS::set_function_match(handle,t.data);
+
+          printf ("Function %d: ",counter++);
+          for (size_t i=0; i < s.size(); i++)
+             {
+               if (isprint(s[i]))
+                    printf("%c",s[i]);
+                 else
+                    printf(".");
+             }
+          printf("\n");
+        }
+   }
+
+void
+LibraryIdentification::FlattenAST::visit(SgNode* n)
+   {
+     SgAsmInstruction* asmInstruction = isSgAsmInstruction(n);
+     if (asmInstruction != NULL)
+        {
+          SgUnsignedCharList opCodeString = asmInstruction->get_raw_bytes();
+          for (size_t i=0; i < opCodeString.size(); i++)
+             {
+               data.push_back(opCodeString[i]);
+             }
+        }
+   }
+
