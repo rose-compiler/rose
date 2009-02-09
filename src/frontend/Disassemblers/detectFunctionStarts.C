0abf1f8a0b633906fd16cccfea4c74aaff1e2634
matzke <matzke@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-02-09 23:29:16 +0000

    Function detection tries harder to find names
    Any function that's a trampoline in the ELF .plt section will be given a
    name based on the symbol that points to the corresponding address in the
    .got.plt section.
    
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@4387 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/frontend/Disassemblers/detectFunctionStarts.C
+++ src/frontend/Disassemblers/detectFunctionStarts.C
@@ -167,6 +167,128 @@ mark_graph_edges(SgAsmInterpretation *interp,
     }
 }
 
+/* Returns the value of an SgAsmValueExpression by casting to each possible subtype since there's no virtual function to get
+ * the value. */
+static rose_addr_t
+value_of(SgAsmValueExpression *e)
+{
+    if (!e) {
+        return 0;
+    } else if (isSgAsmWordValueExpression(e)) {
+        return isSgAsmWordValueExpression(e)->get_value();
+    } else if (isSgAsmDoubleWordValueExpression(e)) {
+        return isSgAsmDoubleWordValueExpression(e)->get_value();
+    } else if (isSgAsmQuadWordValueExpression(e)) {
+        return isSgAsmQuadWordValueExpression(e)->get_value();
+    } else {
+        return 0;
+    }
+}
+
+/* Return the virtual address that holds the branch target for an indirect branch. For example, when called with these
+ * instructions:
+ *     jmp DWORD PTR ds:[0x80496b0]        -> (x86)   returns 80496b0
+ *     jmp QWORD PTR ds:[rip+0x200b52]     -> (amd64) returns 200b52 + address following instruction
+ *
+ * We only instructions that appear as the first instruction in an ELF .plt entry. */
+static rose_addr_t
+get_indirection_addr(SgAsmInstruction *g_insn)
+{
+    rose_addr_t retval = 0;
+
+    SgAsmx86Instruction *insn = isSgAsmx86Instruction(g_insn);
+    if (!insn ||
+        !x86InstructionIsUnconditionalBranch(insn) ||
+        1!=insn->get_operandList()->get_operands().size())
+        return retval;
+    
+    SgAsmMemoryReferenceExpression *mref = isSgAsmMemoryReferenceExpression(insn->get_operandList()->get_operands()[0]);
+    if (!mref)
+        return retval;
+
+    SgAsmExpression *mref_addr = mref->get_address();
+    if (isSgAsmBinaryExpression(mref_addr)) {
+        SgAsmBinaryExpression *mref_bin = isSgAsmBinaryExpression(mref_addr);
+        SgAsmx86RegisterReferenceExpression *reg = isSgAsmx86RegisterReferenceExpression(mref_bin->get_lhs());
+        SgAsmValueExpression *val = isSgAsmValueExpression(mref_bin->get_rhs());
+        if (reg->get_register_class()==x86_regclass_ip && val!=NULL) {
+            retval = value_of(val) + insn->get_address() + insn->get_raw_bytes().size();
+        }
+    } else if (isSgAsmValueExpression(mref_addr)) {
+        retval = value_of(isSgAsmValueExpression(mref_addr));
+    }
+
+    return retval; /*calculated value, or defaults to zero*/
+}
+
+/** Give names to the dynamic linking trampolines in the .plt section. This method must be called after function entry points
+ *  are detected since all it does is give names to functions that already exist. */
+static void
+name_plt_entries(SgAsmInterpretation *interp, 
+                 std::map<uint64_t, SgAsmInstruction*> &insns,
+                 FunctionStarts &func_starts)
+{
+    /* This function is ELF, x86 specific. [FIXME RPM 2009-02-06] */
+    SgAsmElfFileHeader *elf = isSgAsmElfFileHeader(interp->get_header());
+    if (!elf) return;
+
+    /* Find important sections */
+    SgAsmGenericSection *plt = elf->get_section_by_name(".plt");
+    if (!plt || !plt->is_mapped()) return;
+    SgAsmGenericSection *gotplt = elf->get_section_by_name(".got.plt");
+    if (!gotplt || !gotplt->is_mapped()) return;
+
+    /* Find all relocation sections */
+    std::set<SgAsmElfRelocSection*> rsects;
+    for (SgAsmGenericSectionPtrList::iterator si=elf->get_sections()->get_sections().begin();
+         si!=elf->get_sections()->get_sections().end(); 
+         si++) {
+        SgAsmElfRelocSection *reloc_section = isSgAsmElfRelocSection(*si);
+        if (reloc_section) {
+            ROSE_ASSERT(isSgAsmElfSymbolSection(reloc_section->get_linked_section()));
+            rsects.insert(reloc_section);
+        }
+    }
+    if (rsects.empty()) return;
+
+    /* Process each .plt trampoline */
+    for (FunctionStarts::iterator fi=func_starts.begin(); fi!=func_starts.end(); fi++) {
+        rose_addr_t func_addr = fi->first;
+
+        if (fi->second.name!="")
+            continue; /* function already has a name */
+
+        if (func_addr <  elf->get_base_va() + plt->get_mapped_rva() &&
+            func_addr >= elf->get_base_va() + plt->get_mapped_rva() + plt->get_mapped_size())
+            continue; /* function is not in the .plt section */
+            
+        /* The target in the ".plt" section will be an indirect (through the .got.plt section) jump to the actual dynamically
+         * linked function (or to the dynamic linker itself). The .got.plt address is what we're really interested in. */
+        SgAsmx86Instruction *insn = isSgAsmx86Instruction(insns[func_addr]);
+        rose_addr_t gotplt_va = get_indirection_addr(insn);
+
+        if (gotplt_va <  elf->get_base_va() + gotplt->get_mapped_rva() ||
+            gotplt_va >= elf->get_base_va() + gotplt->get_mapped_rva() + gotplt->get_mapped_size())
+            continue; /* PLT entry doesn't dereference a value in the .got.plt section */
+        
+        /* Find the relocation entry whose offset is the gotplt_rva and use that entries symbol for the function name. */
+        for (std::set<SgAsmElfRelocSection*>::iterator ri=rsects.begin(); ri!=rsects.end() && fi->second.name==""; ri++) {
+            SgAsmElfRelocEntryList *entries = (*ri)->get_entries();
+            SgAsmElfSymbolSection *symbol_section = isSgAsmElfSymbolSection((*ri)->get_linked_section());
+            SgAsmElfSymbolList *symbols = symbol_section->get_symbols();
+            for (size_t ei=0; ei<entries->get_entries().size() && fi->second.name==""; ei++) {
+                SgAsmElfRelocEntry *rel = entries->get_entries()[ei];
+                if (rel->get_r_offset()==gotplt_va) {
+                    unsigned long symbol_idx = rel->get_sym();
+                    ROSE_ASSERT(symbol_idx < symbols->get_symbols().size());
+                    SgAsmElfSymbol *symbol = symbols->get_symbols()[symbol_idx];
+                    fi->second.name = symbol->get_name()->get_string();
+                }
+            }
+        }
+    }
+}
+
 void
 detectFunctionStarts(SgAsmInterpretation *interp,
                      std::map<uint64_t, SgAsmInstruction*> &insns,
@@ -185,6 +307,9 @@ detectFunctionStarts(SgAsmInterpretation *interp,
 
     /* This one depends on basic block starts being consistent with function starts. */
     mark_graph_edges(interp, insns, basicBlockStarts, functionStarts);
+
+    /* This doesn't detect new functions, it just gives names to ELF .plt trampolines */
+    name_plt_entries(interp, insns, functionStarts);
 }
 
 
