e0d851de4c0c959bb95fdc0fb9eae221064aad91
matzke <matzke@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-02-09 23:29:09 +0000

    Minor changes and formatting
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@4386 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/frontend/SageIII/virtualCFG/virtualBinCFG.h
+++ src/frontend/SageIII/virtualCFG/virtualBinCFG.h
@@ -1,105 +1,121 @@
 #ifndef VIRTUAL_BIN_CFG_H
 #define VIRTUAL_BIN_CFG_H
 
-// #include "rose.h"
-// #include <string>
-// #include <vector>
-// #include <cassert>
-
 #include <stdint.h>
 
 namespace VirtualBinCFG {
 
-  class CFGEdge;
+    class CFGEdge;
 
-  enum EdgeConditionKind {
-    eckUnconditional, // Normal, unconditional edge
-    eckTrue,	      // True case of a two-way branch
-    eckFalse,	      // False case of a two-way branch
-    eckCaseLabel,     // Case label (constant is given by caseLabel())
-    eckDefault	      // Default label
-  };
+    enum EdgeConditionKind {
+        eckUnconditional,       // Normal, unconditional edge
+        eckTrue,                // True case of a two-way branch
+        eckFalse,               // False case of a two-way branch
+        eckCaseLabel,           // Case label (constant is given by caseLabel())
+        eckDefault              // Default label
+    };
+    
+    typedef std::set<rose_addr_t> AddressSet;
+    typedef std::map<rose_addr_t, SgAsmInstruction*> AddressToInstructionMap;
+    typedef std::map<SgAsmInstruction*, AddressSet> InstructionToAddressesMap;
+    typedef std::map<SgAsmStatement*, AddressSet> StatementToAddressesMap;
 
-  struct AuxiliaryInformation {
-    std::map<uint64_t, SgAsmInstruction*> addressToInstructionMap;
-    std::map<SgAsmInstruction*, std::set<uint64_t> > indirectJumpTargets;
-    std::map<SgAsmStatement*, std::set<uint64_t> > returnTargets; // Block or function
-    std::map<SgAsmInstruction*, std::set<uint64_t> > incomingEdges;
-
-    public:
-    AuxiliaryInformation() {}
-
-    AuxiliaryInformation(SgNode* top);
-
-    SgAsmInstruction* getInstructionAtAddress(uint64_t addr) const {
-      std::map<uint64_t, SgAsmInstruction*>::const_iterator i = addressToInstructionMap.find(addr);
-      if (i == addressToInstructionMap.end()) return NULL;
-      return i->second;
-    }
-
-    const std::set<uint64_t>& getPossibleSuccessors(SgAsmInstruction* insn) const;
-
-    const std::set<uint64_t>& getPossiblePredecessors(SgAsmInstruction* insn) const {
-      static const std::set<uint64_t> emptySet;
-      std::map<SgAsmInstruction*, std::set<uint64_t> >::const_iterator predsIter = incomingEdges.find(insn);
-      if (predsIter == incomingEdges.end()) {
-        return emptySet;
-      } else {
-        return predsIter->second;
-      }
-    }
-
-  };
-
-  class CFGNode {
-
-    SgAsmInstruction* node;
-    const AuxiliaryInformation* info;
+    struct AuxiliaryInformation {
+        AddressToInstructionMap    addressToInstructionMap;
+        InstructionToAddressesMap  indirectJumpTargets;
+        StatementToAddressesMap    returnTargets; /* statement is SgAsmBlock or SgAsmFunctionDeclaration */
+        InstructionToAddressesMap  incomingEdges;
 
     public:
-    explicit CFGNode(SgAsmInstruction* node, const AuxiliaryInformation* info = NULL): node(node), info(info) {ROSE_ASSERT (node);}
-    std::string toString() const;
-    // String for debugging graphs
-    std::string toStringForDebugging() const;
-    // ID to use for Dot, etc.
-    std::string id() const;
+        AuxiliaryInformation() {}
+        AuxiliaryInformation(SgNode* top);
 
-    SgAsmInstruction* getNode() const {return node;}
+        SgAsmInstruction *getInstructionAtAddress(rose_addr_t addr) const {
+            AddressToInstructionMap::const_iterator i = addressToInstructionMap.find(addr);
+            if (i == addressToInstructionMap.end()) return NULL;
+            return i->second;
+        }
+
+        /* NOTE: this is not the transpose of getPossiblePredecessors()! */
+        const AddressSet& getPossibleSuccessors(SgAsmInstruction* insn) const;
+
+        const AddressSet& getPossiblePredecessors(SgAsmInstruction* insn) const {
+            static const AddressSet emptySet;
+            InstructionToAddressesMap::const_iterator predsIter = incomingEdges.find(insn);
+            if (predsIter == incomingEdges.end()) {
+                return emptySet;
+            } else {
+                return predsIter->second;
+            }
+        }
+
+    };
+
+    class CFGNode {
+        SgAsmInstruction *node;
+        const AuxiliaryInformation *info;
+    public:
+        explicit CFGNode(SgAsmInstruction *node, const AuxiliaryInformation *info = NULL)
+            : node(node), info(info) {
+            ROSE_ASSERT(node);
+        }
+        std::string toString() const;
+        // String for debugging graphs
+        std::string toStringForDebugging() const;
+        // ID to use for Dot, etc.
+        std::string id() const;
+
+        SgAsmInstruction *getNode() const {
+            return node;
+        }
   
-    std::vector<CFGEdge> outEdges() const;
-    std::vector<CFGEdge> inEdges() const;
-    bool operator==(const CFGNode& o) const {return node == o.node ;}
-    bool operator!=(const CFGNode& o) const {return !(*this == o);}
-    bool operator<(const CFGNode& o) const {return node < o.node;}
+        std::vector<CFGEdge> outEdges() const;
+        std::vector<CFGEdge> inEdges() const;
+        bool operator==(const CFGNode& o) const {
+            return node == o.node;
+        }
+        bool operator!=(const CFGNode& o) const {
+            return !(*this == o);
+        }
+        bool operator<(const CFGNode& o) const {
+            return node < o.node;
+        }
+    };
 
-  };
-
-  class CFGEdge {
-    CFGNode src, tgt;
-    const AuxiliaryInformation* info;
+    class CFGEdge {
+        CFGNode src, tgt;
+        const AuxiliaryInformation *info;
     public:
-    CFGEdge(CFGNode src, CFGNode tgt, const AuxiliaryInformation* info = NULL): src(src), tgt(tgt), info(info) {}
-    // Pretty string for Dot node labels, etc.
-    std::string toString() const;
-    // String for debugging graphs
-    std::string toStringForDebugging() const;
-    // ID to use for Dot, etc.
-    std::string id() const;
-    CFGNode source() const {return src;}
-    CFGNode target() const {return tgt;}
-    EdgeConditionKind condition() const;
-    //SgExpression* caseLabel() const;
-    //SgExpression* conditionBasedOn() const;
-    //std::vector<SgInitializedName*> scopesBeingExited() const;
-    //std::vector<SgInitializedName*> scopesBeingEntered() const;
-    bool operator==(const CFGEdge& o) const {return src == o.src && tgt == o.tgt;}
-    bool operator!=(const CFGEdge& o) const {return src != o.src || tgt != o.tgt;}
-    bool operator<(const CFGEdge& o) const {return src < o.src || (src == o.src && tgt < o.tgt);}
-  };
+        CFGEdge(CFGNode src, CFGNode tgt, const AuxiliaryInformation *info = NULL)
+            : src(src), tgt(tgt), info(info)
+            {}
+        std::string toString() const;                   // Pretty string for Dot node labels, etc.
+        std::string toStringForDebugging() const;       // String for debugging graphs
+        std::string id() const;                         // ID to use for Dot, etc.
+        CFGNode source() const {
+            return src;
+        }
+        CFGNode target() const {
+            return tgt;
+        }
+        EdgeConditionKind condition() const;
+        //SgExpression* caseLabel() const;
+        //SgExpression* conditionBasedOn() const;
+        //std::vector<SgInitializedName*> scopesBeingExited() const;
+        //std::vector<SgInitializedName*> scopesBeingEntered() const;
+        bool operator==(const CFGEdge& o) const {
+            return src == o.src && tgt == o.tgt;
+        }
+        bool operator!=(const CFGEdge& o) const {
+            return src != o.src || tgt != o.tgt;
+        }
+        bool operator<(const CFGEdge& o) const {
+            return src < o.src || (src == o.src && tgt < o.tgt);
+        }
+    };
 
-    void makeEdge(SgAsmInstruction* from, SgAsmInstruction* to, const AuxiliaryInformation* info, std::vector<CFGEdge>& result); // Used in inEdges() and outEdges() methods
-
+    // Used in inEdges() and outEdges() methods
+    void makeEdge(SgAsmInstruction *from, SgAsmInstruction *to, const AuxiliaryInformation *info, std::vector<CFGEdge> &result);
 }
 
- // VIRTUAL_CFG_H
-#endif
+#endif /* VIRTUAL_CFG_H */
