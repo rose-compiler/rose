2f697270c79ee0ccbcb6a7602c57ad14074e2595
matzke <matzke@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-02-09 23:27:27 +0000

    Reformatted some code
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@4373 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/midend/binaryAnalyses/constantPropagation/flowEquations.h
+++ src/midend/binaryAnalyses/constantPropagation/flowEquations.h
@@ -6,61 +6,74 @@
 class Constraint;
 
 class Variable {
-  std::vector<Constraint*> users; // Constraints that use the value of this variable
-  void addUser(Constraint* c) {
-    users.push_back(c);
-  }
+    std::vector<Constraint*> users; // Constraints that use the value of this variable
+    void addUser(Constraint* c) {
+        users.push_back(c);
+    }
 
   protected:
-  Variable(): users() {}
-  // After updating the value of the variable (done in a subclass-specific
-  // way), this function will update all downstream variables, cycling as
-  // necessary to reach a fixpoint
-  void pushChanges() const;
+    Variable()
+        : users()
+        {}
+    // After updating the value of the variable (done in a subclass-specific
+    // way), this function will update all downstream variables, cycling as
+    // necessary to reach a fixpoint
+    void pushChanges() const;
 
   public:
-  virtual ~Variable() {}
-  friend class Constraint;
+    virtual ~Variable() {}
+    friend class Constraint;
 };
 
 class Constraint {
-  bool needsToRun;
-  bool getNeedsToRun() const {return needsToRun;}
-  void setNeedsToRun() {needsToRun = true;}
-  void resetNeedsToRun() {needsToRun = false;}
-  void runFull() { // Does changed checking and updating
-    if (!getNeedsToRun()) return;
-    resetNeedsToRun();
-    run();
-  }
+    bool needsToRun;
+    bool getNeedsToRun() const {
+        return needsToRun;
+    }
+    void setNeedsToRun() {
+        needsToRun = true;
+    }
+    void resetNeedsToRun() {
+        needsToRun = false;
+    }
+    void runFull() { // Does changed checking and updating
+        if (!getNeedsToRun())
+            return;
+        resetNeedsToRun();
+        run();
+    }
 
   protected:
-  Constraint(): needsToRun(true) {}
+    Constraint()
+        : needsToRun(true)
+        {}
 
-  virtual void run() const = 0;
-  virtual void markDependencies() = 0;
-  void addDependency(Variable* var) {
-    var->addUser(this);
-  }
+    virtual void run() const = 0;
+    virtual void markDependencies() = 0;
+    void addDependency(Variable* var) {
+        var->addUser(this);
+    }
 
   public:
-  void activate() {
-    markDependencies();
-    runFull();
-  }
+    void activate() {
+        markDependencies();
+        runFull();
+    }
 
-  virtual ~Constraint() {}
-  friend class Variable;
-  friend class FlowEquationSet;
+    virtual ~Constraint() {}
+    friend class Variable;
+    friend class FlowEquationSet;
 };
 
-inline void Variable::pushChanges() const {
-  for (size_t i = 0; i < users.size(); ++i) {
-    users[i]->setNeedsToRun();
-  }
-  for (size_t i = 0; i < users.size(); ++i) {
-    users[i]->run();
-  }
+inline void
+Variable::pushChanges() const
+{
+    for (size_t i = 0; i < users.size(); ++i) {
+        users[i]->setNeedsToRun();
+    }
+    for (size_t i = 0; i < users.size(); ++i) {
+        users[i]->run();
+    }
 }
 
-#endif // FLOWEQUATIONS_H
+#endif /* FLOWEQUATIONS_H */
