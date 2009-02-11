14488142af62ab6b654a0c4af972ff2cddf9b5c2
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-02-11 06:34:14 +0000

    Fixed boost path for OSX scripts.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@4417 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- scripts/roseFreshTestStub-Dan-OSX.sh
+++ scripts/roseFreshTestStub-Dan-OSX.sh
@@ -3,10 +3,10 @@
 # Stub file to include settings for roseFreshTest-common
 
 export PATH=/Users/dquinlan/local/libtool-install/bin:/Users/dquinlan/local/fontconfig-install/bin/:/Users/dquinlan/local/ghostscript-install/bin/:/Users/dquinlan/local/latex2html-install/bin/:/Users/dquinlan/local/texlive-install/2007/bin/i386-darwin/:/Users/dquinlan/local/graphviz-install/bin:/Users/dquinlan/local/doxygen-install/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin
-export DYLD_LIBRARY_PATH=/Users/dquinlan/local/boost-1.35-install/lib
+export DYLD_LIBRARY_PATH=/Users/dquinlan/local/boost_1_35_0_installTree-gxx-4.0.1/lib
 
 ROSE_TOP="/Users/dquinlan/ROSE/svn-test-rose/$$/"
-export BOOST_ROOTX="/Users/dquinlan/local/boost-1.35-install/"
+export BOOST_ROOTX="/Users/dquinlan/local/boost_1_35_0_installTree-gxx-4.0.1/"
 
 CONFIGURE_FLAGS=( --disable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-CXX_DEBUG=-g --with-C_DEBUG=-g --with-CXX_WARNINGS='-Wall' --with-C_WARNINGS='-Wall' --with-CXXFLAGS=-fPIC --with-CFLAGS=-fPIC --prefix=${ROSE_TOP}/install --with-boost=${BOOST_ROOTX} "$@")
 
