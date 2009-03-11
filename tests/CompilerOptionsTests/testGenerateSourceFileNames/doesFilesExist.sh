c8e8fceb90ad2f3a42f8c1e1145e35bd08dfb604
saebjornsen1 <saebjornsen1@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-03-11 19:11:46 +0000

    Added test for how ROSE parses filenames on the commmand line
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@4836 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ tests/CompilerOptionsTests/testGenerateSourceFileNames/doesFilesExist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -norc
+for FILE in "$@"
+do
+  if [ ! -f $FILE ]; then
+      echo "ERROR: expected output $FILE from ROSE (does not exist or is no regular file)"  ;
+      exit 1;
+  fi
+done
+
