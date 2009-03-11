90bfde15a3d32f862adeb913107c0a4393efd10f
saebjornsen1 <saebjornsen1@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-03-11 19:19:18 +0000

    Moved directory in compass to new much shorter name
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@4837 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ projects/compass/extensions/checkers/allocateAndFreeMemoryInTheSameModule/allocateAndFreeMemoryInTheSameModuleAtTheSameLevelOfAbstractionMain.C
@@ -0,0 +1,10 @@
+//
+// Do not modify this file
+//
+
+#include "compass.h"
+extern const Compass::Checker* const allocateAndFreeMemoryInTheSameModuleAtTheSameLevelOfAbstractionChecker;
+extern const Compass::Checker* const myChecker = allocateAndFreeMemoryInTheSameModuleAtTheSameLevelOfAbstractionChecker;
+
+#include "compass.C"
+#include "compassTestMain.C"
