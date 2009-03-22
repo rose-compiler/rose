434df130728ecfd899c15b45fe8f0f8b6f208d06
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-03-22 04:02:51 +0000

    Commented out code using dprintf under Cygwin (compile error under Cygwin).
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@4987 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- tests/CompileTests/Cxx_tests/test2006_146.C
+++ tests/CompileTests/Cxx_tests/test2006_146.C
@@ -3,4 +3,9 @@
 // This will cause C compilation to fail, but I think it should fail.
 // extern int dprintf (int __fd, __const char *__restrict __fmt, ...) __attribute__ ((__format__ (__printf__, 2, 3)));
 
+// DQ (3/21/2009): Cygwin defines this function differently (fails in virtualCFG tests).
+#if !defined(__CYGWIN__)
+
 void dprintf(int line, long level, char *prefix, ...);
+
+#endif
