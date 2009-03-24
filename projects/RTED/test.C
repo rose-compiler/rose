fe89d84d74a5460da7a87f0ff9c9be10cd509f7f
panas2 <panas2@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-03-24 18:27:00 +0000

    Improved Test for AST node deletion.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@5048 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- projects/RTED/test.C
+++ projects/RTED/test.C
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+
+class DeleteAST2 : public SgSimpleProcessing {
+public:
+  //! Required traversal function
+  void visit (SgNode* node) {
+    if (node) {
+//      if (!isSgAsmExecutableFileFormat(node)) {
+      	delete node;
+//      }
+    }
+  }
+};
+
+
 int main(int argc, char** argv) {
 
   SgProject* project = frontend(argc,argv);
@@ -11,18 +25,37 @@ int main(int argc, char** argv) {
 
   VariantVector vv1 = V_SgNode;
   vector<SgNode*> vec = NodeQuery::queryMemoryPool(vv1);
-  std::cout << "Number of nodes before deleting: " <<
+  std::cout << "Number of nodes before AST deleting: " <<
     vec.size() << std::endl;
 
+  DeleteAST2 deleteTree;
+   deleteTree.traverse(project,postorder);
+
+   vec = NodeQuery::queryMemoryPool(vv1);
+   std::cout << "Number of nodes after AST deletion: " <<
+     vec.size() << std::endl;
+
+  map<std::string, int> nodes;
   vector<SgNode*>::const_iterator it = vec.begin();
   for (;it!=vec.end();++it) {
     SgNode* node = *it;
     ROSE_ASSERT(node);
+    std::string name  = node->class_name();
+    nodes[name]++;
     delete node;
+  }
+
+  map<std::string, int>::const_iterator it2 = nodes.begin();
+  for (;it2!=nodes.end();++it2) {
+	  string name = it2->first;
+	  int amount = it2->second;
+	  cout << "  Could not delete : " << name << "  -  " << amount << endl;
   }
 
   std::cout << "Number of nodes after deleting in Memory pool: " <<
     NodeQuery::queryMemoryPool(vv1).size() << std::endl;
+
+  std::cout << ">> Rerunning frontend ..." << std::endl;
 
   project = frontend(argc,argv);
   ROSE_ASSERT(project);
