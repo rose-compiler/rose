26a8c2ca08b5443ae2f873e8fc5613d0e9aed508
saebjornsen1 <saebjornsen1@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-03-24 19:41:25 +0000

    Updated delete test
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@5054 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- tests/RunTests/AstDeleteTests/test.C
+++ tests/RunTests/AstDeleteTests/test.C
@@ -4,10 +4,42 @@
 
 using namespace std;
 
+class DeleteAST : public SgSimpleProcessing
+   {
+     public:
+      //! Required traversal function
+          void visit (SgNode* node);
+   };
+
+void
+DeleteAST::visit(SgNode* node)
+   {
+     delete node;
+   }
+
+void DeleteSgTree( SgNode* root)
+{
+  DeleteAST deleteTree;
+  deleteTree.traverse(root,postorder);
+}
+
+
 int main(int argc, char** argv) {
+
+
+  //DeleteAST
+  //QueryMemoryPool all nodes
+  //    num
 
   SgProject* project = frontend(argc,argv);
   ROSE_ASSERT(project);
+
+  DeleteSgTree(project);
+
+
+  std::cout << "Statistics" << std::endl;
+  std::cout << AstNodeStatistics::IRnodeUsageStatistics() << std::endl;
+  std::cout << "End Statistics" << std::endl;
 
   VariantVector vv1 = V_SgNode;
   vector<SgNode*> vec = NodeQuery::queryMemoryPool(vv1);
@@ -24,8 +56,8 @@ int main(int argc, char** argv) {
   std::cout << "Number of nodes after deleting in Memory pool: " <<
     NodeQuery::queryMemoryPool(vv1).size() << std::endl;
 
-  project = frontend(argc,argv);
-  ROSE_ASSERT(project);
+ // project = frontend(argc,argv);
+ // ROSE_ASSERT(project);
 
 
 }
