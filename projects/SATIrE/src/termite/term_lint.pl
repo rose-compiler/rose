1fe5d4d3b5366df0706720976789f6a85bd9fc5a
Gergo Barany <gergo@complang.tuwien.ac.at>
2009-05-05 06:07:31 -0700

    Added termite_lint program.
    Ignore-this: bf99e1ea462ae3e7b9a22d93e38ca136
    
    darcs-hash:20090505130731-0c742-4b827b83e71d2c5ff4009538d2ceabb09b1b7947.gz

---
+++ projects/SATIrE/src/termite/term_lint.pl
@@ -0,0 +1,294 @@
+% This is term_lint.pl, a small tool for checking terms against a tree
+% grammar (abstract syntax).
+% Author: Gergo Barany <gergo@complang.tuwien.ac.at>
+
+
+% operator declarations for great prettiness
+:- op(1200, xfx, ::=).      % grammar rule
+:- op(654, fy, atoms).      % enumeration of terminal atoms
+:- op(654, fy, functors).   % enumeration of terminal functors...
+:- op(653, yfy, with).      % ... with identical argument structure
+:- op(652, xfx, where).     % semantic constraints
+:- op(651, xf, ?).          % optional occurrence of nonterminal
+
+% TODO: @ operator for as-patterns to allow more complex semantic
+% constraints; example:  foo ::= foo(X@bar) where condition(X).
+
+
+% This is the grammar specification we use. Its name is hard-coded here,
+% which is fine for our single use case.
+:- consult('termite_spec.pl').
+
+
+% The tree grammar is a sequence of rules. This predicate defines what a
+% rule is. The predicate itself is not called by term_lint, but it's a nice
+% executable specification.
+is_rule(Nonterminal ::= Body) :-
+    atom(Nonterminal),
+    is_body(Body).
+
+is_body(Atom) :-
+    atom(Atom).
+is_body(A | B) :-
+    is_body(A),
+    is_body(B).
+is_body(Body where _Cond) :-
+    is_body(Body).  % and Cond is a prolog goal
+is_body({_}).  % unify body with term in braces
+is_body(atoms As) :-
+    maplist(atom, As).
+is_body(functors Fs with ArgTuple) :-
+    maplist(atom, Fs),
+    atomtuple_list(ArgTuple, Args),
+    maplist(is_arg, Args).
+is_body(Term) :-
+    Term =.. [_Functor | Args],
+    maplist(is_arg, Args).
+
+atomtuple_list(A, [A]) :-
+    atom(A).
+atomtuple_list((A,B), [A|Bs]) :-
+    atomtuple_list(B, Bs).
+
+is_arg(Atom) :-
+    atom(Atom).
+is_arg(Option?) :-
+    is_arg(Option).
+is_arg({_}).  % unify arg with term in braces
+is_arg([ElemKind]) :-
+    is_arg(ElemKind).
+
+
+% OK. Here is the semantics to the structures defined above:
+%
+% The grammar is a sequence of grammar rules. Each rule is of the form
+%   Nonterminal ::= Body.
+% There should be no two rules for the same nonterminal (use | instead), but
+% this is not checked.
+%
+% Body has the following meaning:
+%   atom      - a nonterminal, references another rule to be used
+%   A | B     - match A; if that fails, match B
+%   A where C - match A; if that succeeds, call Prolog goal C
+%   {A}       - unify with term A; {_} means "any term", {foo} is terminal foo
+%   atoms As  - match one of the atoms in As
+%   functors Fs with (A1,...,An)
+%             - match term F(A1,...,An) where F is a functor in Fs
+%   f(A1,...,An)
+%             - match a term with functor f, where arguments match A1,...,An
+%
+% Argument expressions (in argument tuples) can be:
+%   atom      - a nonterminal
+%   A?        - term is "missing" (see below) or matches argument expression A
+%   {A}       - unify with term A
+%   [A]       - list of terms matching argument expression A; as a special
+%               case, [_] means "list of any type"
+% Options (A?) are resolved as follows: If the term under consideration
+% is a solution of predicate missing/1 (to be defined along with the grammar
+% rules), there is a match; otherwise a term of type A must be matched.
+%
+% Predicates to be defined along with the grammar rules are:
+%   missing/1         - defines what A? can match except for A
+%   start_symbol/1    - single solution is the start symbol of the grammar
+
+
+% Here are a few example grammars to illustrate the explanations above.
+%
+%
+% --- arithmetic expressions, simple verbose version ---
+% var ::= {VarName} where atom(VarName).
+% num ::= {Number} where number(Number).
+% expr ::=
+%   var
+% | num
+% | expr + expr
+% | expr - expr
+% | expr * expr
+% | expr / expr.
+%
+%
+% --- arithmetic expressions, more condensed version ---
+% expr ::=
+%   {Leaf} where (atom(Leaf) ; number(Leaf))
+% | functors [+, -, *, /] with (expr, expr).
+%
+%
+% --- simple type system ---
+% type ::=
+%   atoms [number, character, string]
+% | function_type([type] /* argument types */, type? /* return type if any */).
+%
+% missing(none).    /* what to match if no return type is given */
+%
+%
+% --- partial specification (some parts are not constrained) ---
+% allowed ::=
+%   lst([_])        /* argument is list of some unknown things */
+% | opt(allowed?)   /* argument is missing, or of type allowed */
+% | any({_}).       /* argument is any term (anything unifies with _) */
+%
+% missing(nothing).
+%
+% In this last example, some allowed terms are:
+%   lst([]), lst([f(f)]), lst([x,y]), ...
+%   opt(nothing), opt(lst([])), opt(opt(nothing)), ...
+%   any(foo), any(g(f(x))), ...
+
+
+% One more thing, which might not be explicit from the stuff above: The
+% anonymous variable _ (free variables in general) may appear in the
+% grammar, but *only* inside [] or {}:
+%   [_] (list of any type)   or
+%   {_} (any term)   or
+%   {foo(_)} (any term with functor foo)
+% but:
+%   foo ::= foo(_)   is not allowed; use foo ::= foo({_})
+%   foo ::= X where cond(X)   is not allowed; use foo ::= {X} where cond(X)
+%
+% The interpreter does not check these things at the moment. Which means
+% that grammars containing variables in weird places will misbehave in weird
+% ways.
+
+
+% Obscure implementation predicates follow. No need to read these, I think.
+% (There is a main predicate at the very bottom.)
+
+% term_match(+Term, +NonterminalSymbol).
+term_match(Term, NonterminalSymbol) :-
+    nonterminal_body(NonterminalSymbol, Body),
+    catch(term_nt_bodymatch(Term, NonterminalSymbol, Body),
+          ErrorTerm,
+          term_match_error(ErrorTerm)).
+
+% nonterminal_body(+NonterminalSymbol, -Body).
+nonterminal_body(NonterminalSymbol, Body) :-
+    ( clause(NonterminalSymbol ::= Body, true)
+      -> true
+      ;  throw(error(no_such_nonterminal, nonterminal(NonterminalSymbol))) ).
+
+% wrapper for term_nt_bodymatch_; generates error if no case matches
+term_nt_bodymatch(Term, Nonterminal, Body) :-
+    term_nt_bodymatch_(Term, Nonterminal, Body),
+    !.
+term_nt_bodymatch(Term, Nonterminal, Body) :-
+    throw(error(nonterminal_does_not_match,
+                nt_body_term(Nonterminal, Body, Term))).
+
+% this does the real work of recursively checking the rule body against the
+% term; typically, we want this to fail silently, and the wrapper that calls
+% it will generate an error message
+term_nt_bodymatch_(Term, _NT, Nonterminal) :-
+    atom(Nonterminal),
+    nonterminal_body(Nonterminal, Body),
+    term_nt_bodymatch_(Term, Nonterminal, Body).
+term_nt_bodymatch_(Term, NT, A | _B) :-
+    term_nt_bodymatch_(Term, NT, A),
+    !.
+term_nt_bodymatch_(Term, NT, _A | B) :-
+    !,
+    term_nt_bodymatch_(Term, NT, B).
+term_nt_bodymatch_(Term, NT, T where Cond) :-
+    !,
+    nonvar(T), % grammar sanity check
+    term_nt_bodymatch_(Term, NT, T),
+    ( call(Cond)
+    ; throw(error(condition_failed, cond_nt_term(Cond, NT, Term))) ).
+term_nt_bodymatch_(Term, _NT, {T}) :-
+    Term = T,
+    !.
+term_nt_bodymatch_(Term, _NT, atoms Fs) :-
+    atom(Term),
+    member(Term, Fs).
+term_nt_bodymatch_(Term, _NT, functors Fs with ArgTuple) :-
+    functor(Term, F, _Arity),
+    member(F, Fs),
+    atomtuple_list(ArgTuple, ArgList),
+    CheckTerm =.. [F | ArgList],
+    term_terminalmatch(Term, CheckTerm).
+term_nt_bodymatch_(Term, _NT, T) :-
+    term_terminalmatch(Term, T).
+
+term_terminalmatch(Term, T) :-
+    Term =.. [Functor | TermArgs],
+    T =.. [Functor | ProductionArgs],
+    maplist(nonvar, ProductionArgs), % grammar sanity check
+    length(TermArgs, N),
+    length(ProductionArgs, N),
+    ( maplist(term_argumentmatch, TermArgs, ProductionArgs)
+    ; throw(error(argument_match_failure,
+                  functor_args_nts(Functor, TermArgs, ProductionArgs))) ).
+
+term_argumentmatch(Term, A) :-
+    atom(A),
+    nonterminal_body(A, Body),
+    term_nt_bodymatch(Term, A, Body).
+term_argumentmatch(Term, {A}) :-
+    term_nt_bodymatch(Term, '{}', {A}).
+term_argumentmatch(Term, _A?) :-
+    missing(Term),
+    !.
+term_argumentmatch(Term, A?) :-
+    term_argumentmatch(Term, A).
+term_argumentmatch(Term, [A]) :-
+    is_list(Term),
+    ( var(A) ; forall(member(T, Term), term_argumentmatch(T, A)) ).
+
+body_alternatives(A | B, FA/N | Alts) :-
+    !,
+    ( A = A1 where _C
+      -> functor(A1, FA, N)
+      ;  functor(A, FA, N) ),
+    body_alternatives(B, Alts).
+body_alternatives(X, FX/N) :-
+    functor(X, FX, N).
+
+term_match_error(error(nonterminal_does_not_match, nt_body_term(N, B, T))) :-
+    !,
+    body_alternatives(B, Alternatives),
+    functor(T, F, Arity),
+    die('nonterminal "~w" with alternatives "~w" does not match ~w term',
+        [N, Alternatives, F/Arity]).
+term_match_error(error(argument_match_failure,functor_args_nts(F,As,NTs))) :-
+    !,
+    maplist(functor, As, Args, _Arities),
+    die('~w term: could not match arguments ~w against ~w~n',
+        [F, Args, NTs]).
+term_match_error(error(no_such_nonterminal, nonterminal(NT))) :-
+    !,
+    die('internal error: nonterminal ~w is not defined', [NT]).
+term_match_error(error(condition_failed, cond_nt_term(C, NT, T))) :-
+    !,
+    functor(T, Functor, Arity),
+    die('for ~w ~w: condition ~w failed', [NT, Functor/Arity, C]).
+term_match_error(ErrorTerm) :-
+    !,
+    die('error term: ~w', [ErrorTerm]).
+
+die(Format, Args) :-
+    format(atom(A), Format, Args),
+    format('*** error: ~w~n', A),
+    !,
+    fail.
+
+
+
+
+termite_term(Filename, T) :-
+    catch(open(Filename, read, Stream, []),
+          Error,
+          (print_message(error, Error), !, fail)),
+    catch(read_term(Stream, T, [double_quotes(string)]),
+          Error,
+          (print_message(error, Error), !, fail)),
+    close(Stream).
+
+main :-
+    current_prolog_flag(argv, Argv),
+    ( append(_SystemArgs, [_ProgramName, '--', Filename], Argv)
+    ; format('* usage: need exactly one term file name argument~n'),
+      !,
+      fail ),
+    !,
+    termite_term(Filename, T),
+    start_symbol(StartSymbol),
+    catch(term_match(T, StartSymbol), _, ( !, fail ) ).
---
+++ projects/SATIrE/src/termite/termite_spec.pl
@@ -0,0 +1,402 @@
+% This is termite_spec.pl, a tree grammar (abstract syntax specification) to
+% be used with term_lint. It defines the structure of Termite terms.
+% Author: Gergo Barany <gergo@complang.tuwien.ac.at>
+
+
+% TODO with the grammar itself:
+% - see if there is anything missing (valid terms are not covered)
+% - see if there is anything wrong
+% - find uses of the nonterminal 'todo', which is a placeholder meaning "did
+%   not bother to define this yet", and replace them with stricter rules
+% - find other places where things could be stricter (for instance, the rhs
+%   of a dot or arrow expression is always a var_ref_exp, and we could
+%   express this in our grammar)
+% - maybe put the rules in some nice order
+
+% TODO with the grammar and term_lint.pl:
+% - add support for as-patterns to term_lint.pl (see comment there) and use
+%   them to check awesome properties like "the operands and result of a
+%   binary_op usually have sort of the same type"
+
+% TODO with the Termite term format in general:
+% - rename basic_block to compound_statement already
+% - introduce consistent naming of statements/expressions; e.g., use _expr
+%   and _stmt everywhere instead of the current mix
+% - find other things that should be fixed (the always-one-element list in
+%   variable declarations, for instance)
+
+
+% This is the start symbol (in case you didn't guess).
+start_symbol(termite).
+
+
+% Grammar rules.
+termite ::=
+    project.
+
+project ::=
+    project([file], default_annotation, analysis_info, file_info).
+
+file ::=
+    file(global, {null}, default_annotation, analysis_info, file_info).
+
+initialized_name ::=
+    initialized_name(initializer?, initialized_name_annotation,
+                     analysis_info, file_info).
+
+% --- statements ---
+statement ::=
+    break_stmt
+  | case_option_stmt
+  | continue_stmt
+  | declaration_statement
+  | default_option_stmt
+  | expr_statement
+  | for_init_statement
+  | goto_statement
+  | label_statement
+  | null_statement
+  | return_stmt
+  | scope_statement.
+
+break_stmt ::=
+    break_stmt(default_annotation, analysis_info, file_info).
+
+case_option_stmt ::=
+    case_option_stmt(expression, statement, expression? /* key_range_end */,
+                     default_annotation, analysis_info, file_info).
+
+continue_stmt ::=
+    continue_stmt(default_annotation, analysis_info, file_info).
+
+declaration_statement ::=
+    class_declaration
+  | enum_declaration
+  | function_declaration
+  | function_parameter_list
+  | pragma_declaration
+  | typedef_declaration
+  | variable_declaration
+  | variable_definition.
+
+class_declaration ::=
+    class_declaration(class_definition?, class_declaration_annotation,
+                      analysis_info, file_info).
+
+enum_declaration ::=
+    enum_declaration(todo).
+
+function_declaration ::=
+    function_declaration(function_parameter_list, function_definition?,
+                         function_declaration_annotation,
+                         analysis_info, file_info).
+
+function_parameter_list ::=
+    function_parameter_list([initialized_name],
+                            default_annotation, analysis_info, file_info).
+
+pragma_declaration ::=
+    pragma_declaration(todo).
+
+typedef_declaration ::=
+    typedef_declaration(class_declaration?, typedef_annotation,
+                        analysis_info, file_info).
+
+variable_declaration ::=
+    variable_declaration([initialized_name], variable_declaration_specific,
+                         analysis_info, file_info).
+
+variable_definition ::=
+    variable_definition(todo).
+
+
+default_option_stmt ::=
+    default_option_stmt(statement,
+                        default_annotation, analysis_info, file_info).
+
+expr_statement ::=
+    expr_statement(expression, default_annotation, analysis_info, file_info).
+
+for_init_statement ::=
+    for_init_statement([statement],
+                       default_annotation, analysis_info, file_info).
+
+goto_statement ::=
+    goto_statement(label_annotation, analysis_info, file_info).
+
+label_statement ::=
+    label_statement(label_annotation, analysis_info, file_info).
+
+null_statement ::=
+    null_statement(default_annotation, analysis_info, file_info). /* really? */
+
+return_stmt ::=
+    return_stmt(expression, default_annotation, analysis_info, file_info).
+
+scope_statement ::=
+    basic_block
+  | class_definition
+  | do_while_stmt
+  | for_statement
+  | function_definition
+  | global
+  | if_stmt
+  | switch_statement
+  | while_stmt.
+
+basic_block ::=
+    basic_block([statement], default_annotation, analysis_info, file_info).
+
+class_definition ::=
+    class_definition([variable_declaration], class_definition_annotation,
+                     analysis_info, file_info).
+
+do_while_stmt ::=
+    do_while_stmt(statement /* body */, statement /* condition */,
+                  default_annotation, analysis_info, file_info).
+
+for_statement ::=
+    for_statement(for_init_statement, statement /* test */,
+                  expression /* increment */, statement /* body */,
+                  default_annotation, analysis_info, file_info).
+
+function_definition ::=
+    function_definition(basic_block,
+                        default_annotation, analysis_info, file_info).
+
+global ::=
+    global([declaration_statement],
+           default_annotation, analysis_info, file_info).
+
+if_stmt ::=
+    if_stmt(statement /* condition */, statement /* true */,
+            statement? /* else */,
+            default_annotation, analysis_info, file_info).
+
+switch_statement ::=
+    switch_statement(statement /* key */, statement /* body */,
+                     default_annotation, analysis_info, file_info).
+
+while_stmt ::=
+    while_stmt(statement /* condition */, statement /* body */,
+               default_annotation, analysis_info, file_info).
+
+% --- expressions ---
+expression ::=
+    binary_op
+  | conditional_exp
+  | expr_list_exp
+  | function_call_exp
+  | function_ref_exp
+  | initializer
+  | null_expression
+  | size_of_op
+  | unary_op
+  | value_exp
+  | var_arg_copy_op
+  | var_arg_end_op
+  | var_arg_op
+  | var_arg_start_one_operand_op
+  | var_arg_start_op
+  | var_ref_exp.
+
+binary_op ::=
+    functors [add_op, and_assign_op, and_op, arrow_exp, assign_op,
+        bit_and_op, bit_or_op, bit_xor_op, comma_op_exp, div_assign_op,
+        divide_op, dot_exp, equality_op, greater_or_equal_op,
+        greater_than_op, ior_assign_op, less_or_equal_op, less_than_op,
+        lshift_assign_op, lshift_op, minus_assign_op, mod_assign_op, mod_op,
+        mult_assign_op, multiply_op, not_equal_op, or_op, plus_assign_op,
+        pntr_arr_ref_exp, rshift_assign_op, rshift_op, subtract_op,
+        xor_assign_op]
+    with (expression /* lhs */, expression /* rhs */,
+          binary_op_annotation, analysis_info, file_info).
+
+conditional_exp ::=
+    conditional_exp(expression /* condition */,
+                    expression /* true */, expression /* false */,
+                    conditional_exp_annotation, analysis_info, file_info).
+
+expr_list_exp ::=
+    expr_list_exp([expression], default_annotation, analysis_info, file_info).
+
+function_call_exp ::=
+    function_call_exp(expression /* function */, expr_list_exp /* args */,
+                      function_call_exp_annotation, analysis_info, file_info).
+
+function_ref_exp ::=
+    function_ref_exp(function_ref_exp_annotation, analysis_info, file_info).
+
+initializer ::=
+    aggregate_initializer
+  | assign_initializer.
+
+aggregate_initializer ::=
+    aggregate_initializer(expr_list_exp,
+                          default_annotation, analysis_info, file_info).
+
+assign_initializer ::=
+    assign_initializer(expression, assign_initializer_annotation,
+                       analysis_info, file_info).
+
+null_expression ::=
+    null_expression(default_annotation, analysis_info, file_info).
+
+size_of_op ::=
+    size_of_op(expression?, size_of_op_annotation, analysis_info, file_info).
+
+unary_op ::=
+    cast_exp(expression, expression? /* original expression tree */,
+             unary_op_annotation, analysis_info, file_info)
+  | functors [address_of_op, bit_complement_op, minus_minus_op,
+        minus_op, not_op, plus_plus_op, pointer_deref_exp, unary_add_op]
+    with (expression, unary_op_annotation, analysis_info, file_info).
+
+value_exp ::=
+    functors [long_long_int_val, unsigned_long_long_int_val, long_int_val,
+        unsigned_long_val, int_val, unsigned_int_val, short_val,
+        unsigned_short_val, char_val, unsigned_char_val, float_val,
+        double_val, long_double_val, string_val]
+    with (expression? /* original expression tree */,
+          value_annotation, analysis_info, file_info).
+
+var_arg_copy_op ::=
+    var_arg_copy_op(todo).
+
+var_arg_end_op ::=
+    var_arg_end_op(todo).
+
+var_arg_op ::=
+    var_arg_op(todo).
+
+var_arg_start_one_operand_op ::=
+    var_arg_start_one_operand_op(todo).
+
+var_arg_start_op ::=
+    var_arg_start_op(todo).
+
+var_ref_exp ::=
+    var_ref_exp(var_ref_exp_annotation, analysis_info, file_info).
+
+% --- annotations ---
+default_annotation ::=
+    default_annotation({null})
+  | default_annotation({null}, preprocessing_info).
+
+initialized_name_annotation ::=
+    initialized_name_annotation(type, name, todo /* storage modifier */,
+                                scope_name?).
+
+function_declaration_annotation ::=
+    function_declaration_annotation(type, name, declaration_modifier,
+                                    preprocessing_info).
+
+class_declaration_annotation ::=
+    class_declaration_annotation(name, todo /* class kind */, type,
+                                 preprocessing_info).
+
+class_definition_annotation ::=
+    class_definition_annotation(file_info, preprocessing_info).
+
+variable_declaration_specific ::=
+    variable_declaration_specific(todo /* declaration modifier */,
+                                  preprocessing_info).
+
+label_annotation ::=
+    label_annotation(name, preprocessing_info).
+
+size_of_op_annotation ::=
+    size_of_op_annotation(type? /* operand */, type /* sizeof expression */).
+
+value_annotation ::=
+    value_annotation(number_or_string, preprocessing_info).
+
+binary_op_annotation ::=
+    binary_op_annotation(type, preprocessing_info).
+
+unary_op_annotation ::=
+    unary_op_annotation(fixity, type, todo /* cast type */,
+                        todo /* throw kind */, preprocessing_info).
+
+var_ref_exp_annotation ::=
+    var_ref_exp_annotation(type, name, todo /* storage modifier */,
+                           scope_name?, preprocessing_info).
+
+typedef_annotation ::=
+    typedef_annotation(name, type, class_declaration?, preprocessing_info).
+
+function_ref_exp_annotation ::=
+    function_ref_exp_annotation(name, type, preprocessing_info).
+
+function_call_exp_annotation ::=
+    function_call_exp_annotation(type, preprocessing_info).
+
+assign_initializer_annotation ::=
+    assign_initializer_annotation(type, preprocessing_info).
+
+conditional_exp_annotation ::=
+    conditional_exp_annotation(type, preprocessing_info).
+
+% --- other stuff ---
+analysis_info ::=
+    analysis_info([_]).
+
+file_info ::=
+    file_info({_}, {_}, {_}).
+
+preprocessing_info ::=
+    preprocessing_info([_]).
+
+type ::=
+    basic_type
+  | array_type(type, expression?)
+  | function_type(type /* return */, todo /* ellipses */, [type] /* args */)
+  | modifier_type(type, type_modifier)
+  | named_type
+  | pointer_type(type).
+
+basic_type ::=
+    atoms [type_bool, type_char, type_double, type_ellipse, type_float,
+        type_int, type_long, type_long_double, type_long_long, type_short,
+        type_signed_char, type_string, type_unsigned_char,
+        type_unsigned_int, type_unsigned_long, type_unsigned_long_long,
+        type_unsigned_short, type_void].
+
+named_type ::=
+    class_type(name, todo, todo)
+  | enum_type(todo)
+  | typedef_type(name, type).
+
+type_modifier ::=
+    type_modifier([todo], todo, todo, todo).
+
+name ::=
+    {Name} where atom(Name).
+
+scope_name ::=  % name of a scope
+    {::}
+  | class_scope(scope_name, todo /* class kind (struct, union, etc.) */)
+  | name.
+
+number_or_string ::=
+    {It} where ( numberatom(It) ; number(It) ; string(It) ).
+
+fixity ::=  % fixity of unary operators
+    {prefix}
+  | {postfix}.
+
+declaration_modifier ::=
+    declaration_modifier(todo, todo, todo, todo).
+
+todo ::=
+    {_}.
+
+
+% For an optional argument  A? , this predicate is tried first.
+missing(null).
+
+
+% Test whether the atom A can be interpreted as a number.
+numberatom(A) :-
+    atom(A),
+    catch(atom_number(A, _N), _, fail).
