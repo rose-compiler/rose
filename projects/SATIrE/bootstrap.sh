d40c446fd30e17d18dbeb44974449e81379ec7a4
ebner <ebner@complang.tuwien.ac.at>
2009-05-18 05:44:24 -0700

    bugfix for bootstrap script; implement -disable-checks
    darcs-hash:20090518124424-e6709-7c29f3af61f1b2f42f10f9aa15ce560183b3a5be.gz

--- projects/SATIrE/bootstrap.sh
+++ projects/SATIrE/bootstrap.sh
@@ -6,12 +6,13 @@ PREFIX="`pwd`/install"
 
 ENABLE_CLANG="0"
 DISABLE_SATIRE="0"
+DISABLE_CHECKS="0"
 
 #parse command line options
 for i in $@; do
   case $i in
       -help | -h | --help)
-          echo "Usage: $0 <INSTALL_PREFIX> [-help] [-enable-clang] [-disable-satire] [-gcc-<ver>]" 
+          echo "Usage: $0 <INSTALL_PREFIX> [-help] [-enable-clang] [-disable-satire] [-disable-checks] [-gcc-<ver>]" 
           echo "  autoreconf, configure, test and install SATIrE to <INSTALL_PREFIX>"
           echo "  Paths are hardcoded for c8.complang.tuwien.ac.at"
           exit 1
@@ -27,6 +28,8 @@ for i in $@; do
           ;;
       -disable-satire) DISABLE_SATIRE="1" # build only CLANG frontend
           ;;
+      -disable-checks) DISABLE_CHECKS="1"
+          ;;
       -*) echo "invalid option $i"
           exit 1
           ;;
@@ -39,13 +42,23 @@ echo "using prefix $PREFIX"
 #configure and build satire
 if [ "$DISABLE_SATIRE" == "0" ] ; then
     echo "building satire"
-    libtoolize && \
-    autoreconf -i && \
-        ./configure --prefix=$PREFIX --with-rosedir=/usr/local/mstools/rose --with-pagdir=/usr/local/mstools/pag --with-boostdir=/usr/local/mstools/boost CXXFLAGS="-O2 -ggdb" $CCARGS && \
-        make $JOBS && \
-        make $JOBS distcheck && \
-        make install && \
-        make $JOBS installcheck
+    if [ "$DISABLE_CHECKS" ==  "1" ]; then
+        (libtoolize && \
+            autoreconf -i && \
+            ./configure --prefix=$PREFIX --with-rosedir=/usr/local/mstools/rose --with-pagdir=/usr/local/mstools/pag --with-boostdir=/usr/local/mstools/boost CXXFLAGS="-O2 -ggdb" $CCARGS && \
+            make $JOBS && \
+            make install) \
+            || exit 1
+    else
+        (libtoolize && \
+            autoreconf -i && \
+            ./configure --prefix=$PREFIX --with-rosedir=/usr/local/mstools/rose --with-pagdir=/usr/local/mstools/pag --with-boostdir=/usr/local/mstools/boost CXXFLAGS="-O2 -ggdb" $CCARGS && \
+            make $JOBS && \
+            make $JOBS distcheck && \
+            make install && \
+            make $JOBS installcheck ) \
+            || exit 1
+    fi
 fi
 
 #configure and build clang and llvm
