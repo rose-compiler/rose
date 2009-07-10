58323dbd7ba28779941e27c46675e1e52c27e9a6
saebjornsen1 <saebjornsen1@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-07-10 00:29:30 +0000

    Improved class hierarchy graph
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@6479 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/midend/programAnalysis/CallGraphAnalysis/ClassHierarchyGraph.h
+++ src/midend/programAnalysis/CallGraphAnalysis/ClassHierarchyGraph.h
@@ -1,36 +1,20 @@
 #ifndef CLASS_HIERARCHY_GRAPH_H
 #define CLASS_HIERARCHY_GRAPH_H
 
-#ifdef HAVE_MYSQL
-// TPS (01Dec2008): Enabled mysql and this fails.
-// seems like it is not supposed to be included
-#if 0
-   #include <GlobalDatabaseConnection.h>
-#endif
-#endif
-
-class ClassHierarchyNode : public MultiGraphElem
-   {
-     public:
-       SgClassDefinition *classDefinition;
-       ClassHierarchyNode( SgClassDefinition *cls ); 
-       virtual std::string toString() const; 
-   };
-
-
-typedef DAGCreate<ClassHierarchyNode, ClassHierarchyEdge> ClassHierarchy;
-
-typedef std::list<ClassHierarchyNode *> ClassHierarchyNodePtrList;
-
+typedef std::list<SgGraphNode *> ClassHierarchyNodePtrList;
 
 
 class ClassHierarchyWrapper
 {
     SgNode *root;
-    ClassHierarchy classGraph;
+    SgIncidenceDirectedGraph* classGraph;
     
   public:
 
+    enum EdgeDirection{
+        EdgeOut,
+        EdgeIn
+    } ;
     ClassHierarchyWrapper( SgNode *node );
     SgClassDefinitionPtrList getSubclasses( SgClassDefinition * );
     SgClassDefinitionPtrList getDirectSubclasses( SgClassDefinition * );
@@ -46,14 +30,11 @@ class ClassHierarchyWrapper
     std::list<std::string> getSubclasses( std::string className ,  sqlite3x::sqlite3_connection& gDB );
 #endif
 
-    ClassHierarchy* getClassHierarchyGraph();
-    ClassHierarchyNode* findNode(SgNode*);
-    ClassHierarchyEdge* findEdge(ClassHierarchyNode*,ClassHierarchyNode*);
-    //#endif
+    SgIncidenceDirectedGraph* getClassHierarchyGraph();
+    SgGraphNode* findNode(SgNode*);
 
    private:
-    //map<SgClassDefinition *, ClassHierarchyNode *> definition2node;
-    SgClassDefinitionPtrList getHierarchy ( SgClassDefinition *,ClassHierarchy::EdgeDirection );
+    SgClassDefinitionPtrList getHierarchy ( SgClassDefinition *, EdgeDirection );
 };
 
 
