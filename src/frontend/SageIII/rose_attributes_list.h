9972c6dc70a82b8a5cecca4865c15b23329cb6ac
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-07-15 17:56:12 +0000

    Various fixes and workaround to allow ROSE to be compiled by GCC 4.3 and 4.4
    * use different builtin function prototypes for different GCC versions
    * shadow new C++ extension type traits introduced since GCC 4.3 with macro definitions to sizeof
      http://gcc.gnu.org/onlinedocs/gcc-4.3.3/gcc/Type-Traits.html
      A full fix to this problem requires upgrading our EDG frontend to 4.0, that has full support
      for those new GNU C++ type traits extensions.
    * added some headers which are required to be explicitly included 
    * still work in progress due to bug 357: some unparsed C++ template code is no longer legal. 
    
    
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@6553 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/frontend/SageIII/rose_attributes_list.h
+++ src/frontend/SageIII/rose_attributes_list.h
@@ -10,9 +10,9 @@
 // Include the ROSE lex specific definitions of tokens
 #include "general_token_defs.h"
 
-// DQ (12/22/2008): I would appriciate it if this were a better name...
+// DQ (12/22/2008): I would appreciate it if this were a better name...
 #if CAN_NOT_COMPILE_WITH_ROSE != true
-
+#include <boost/preprocessor/iteration/iterate.hpp> // Liao, 7/10/2009, required by GCC 4.4.0 for a #define line of BOOST_PP_ITERATION_DEPTH
 #include <boost/wave/cpplexer/cpp_lex_token.hpp>    // token class
 #include <boost/wave/cpplexer/cpp_lex_iterator.hpp>   // lexer type
 
