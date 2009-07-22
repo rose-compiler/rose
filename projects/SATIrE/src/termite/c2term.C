f9aad99d4f40bd506502b0312aecd1f67b224c64
Adrian Prantl <adrian@complang.tuwien.ac.at>
2009-07-22 01:38:35 -0700

    c2term now takes an (optional) -o termfile.pl argument., just as term2c does. If it is missing, c2term writes to stdout.
    Ignore-this: c31205e84abd5048fcac7f7277413a51
    
    darcs-hash:20090722083835-3f1cf-86b262daabcd4422e5d967741c1a246bf45a58f4.gz

--- projects/SATIrE/src/termite/c2term.C
+++ projects/SATIrE/src/termite/c2term.C
@@ -5,10 +5,10 @@ Copyright 2006 Christoph Bonitz <christoph.bonitz@gmail.com>
  * Purpose: create a TERMITE representation of a given AST
  */
 
-
 #include <iostream>
 #include <fstream>
 #include <satire_rose.h>
+#include <getopt.h>
 
 // GB (2009-02-25): We want to build c2term without ICFG stuff to avoid
 // having to link against libsatire. Also, want to avoid PAG DFI stuff.
@@ -18,61 +18,102 @@ Copyright 2006 Christoph Bonitz <christoph.bonitz@gmail.com>
 
 using namespace std;
 
-int main ( int argc, char ** argv ) {
-  //frontend processing
-  if (argc < 3) {
-    cerr << "Usage: " << argv[0] 
-	 << " [frontend options] "/*[--strip-headers]*/" src1.c src2... termfile.pl"
-         << endl
-         << "       [frontend options] will be passed to the C/C++ frontend. " 
-	 << endl
-         << "       Default behaviour is to include header files in the term"
-	 << " representation." << endl;
+void usage(const char* me) 
+{
+  cerr << "Usage: " << me
+     << " [FRONTEND OPTIONS] [OPTIONS] src1.c src2.cpp ... [-o termfile.pl]\n"
+     << "  Parse one or more source files and convert them into a TERMITE file."
+       << "\n  Header files will be included in the term representation.\n\n"
+
+       << "Options:\n"
+       << "  [FRONTENT OPTIONS] will be passed to the C/C++ frontend.\n\n" 
+
+       << "  -o, --output <termfile.pl>\n"
+       << "    Write the output to <termifile.pl> instead of stdout.\n\n"
+
+       << "  --dot\n"
+       << "    Create a dotty graph of the syntax tree.\n\n"
+
+       << "  --pdf\n"
+       << "    Create a PDF printout of the syntax tree."
+       << endl;
+}
+
+int main(int argc, char** argv) {
+  const char* outfile = NULL;
+  int dot_flag = 0;
+  int pdf_flag = 0;
+
+  while (1) {
+    static struct option long_options[] = {
+      /* These options set a flag. */
+      {"dot", no_argument, &dot_flag, 1},
+      {"pdf", no_argument, &pdf_flag, 1},
+      {"output", required_argument, 0, 'o'},
+      {0, 0, 0, 0}
+    };
+    /* getopt_long stores the option index here. */
+    int option_index = 0;
+     
+    int c = getopt_long(argc, argv, "o:", long_options, &option_index);
+     
+    /* Detect the end of the options. */
+    if (c == -1)
+      break;
+     
+    switch (c) {
+    case 'o': outfile = optarg; break;
+     
+    default:
+      usage(argv[0]);
+      return 1; 
+    }
+  }
+  if (optind < argc) {
+    //infile = argv[optind];
+  } else {
+    usage(argv[0]);
     return 1;
   }
 
-  /* This option was ill-conceived to begin with: we would be missing
-     important declarations */
-  /*bool strip_headers = false;
-  for (int i = 0; i < argc; ++i)
-    if (string(argv[i]) == "--strip-headers") {
-      char empty[] = "";
-      argv[i] = empty;
-      strip_headers = true;
-      }*/
-
-  char *outputFileName = argv[argc-1];
-  // GB (2008-11-04): Now that this argument is unused, we can use it
-  // to turn off the frontend's warnings; they are distracting in the
+  // Turn off the frontend's warnings; they are distracting in the
   // automated test outputs.
-  char warningFlag[] = "-edg:w";
-  argv[argc-1] = warningFlag;
+  vector<char*> argv1;
+  char warningOpt[] = "-edg:w";
+  argv1.push_back(argv[0]);
+  argv1.push_back(warningOpt);
+  for (int i = 1; i < argc; ++i)
+    argv1.push_back(argv[i]);
 
-  SgProject * project = frontend(argc,argv);
 
-  //Create dot and pdf files
-  //DOT generation (numbering:preoder)
-  //AstDOTGeneration dotgen;
+  // Run the EDG frontend
+  SgProject* project = frontend(argc+1,&argv1[0]);
 
-  //dotgen.generateInputFiles(project,AstDOTGeneration::PREORDER);
-  //PDF generation
-  //AstPDFGeneration pdfgen;
-  //pdfgen.generateInputFiles(project);
+  if (dot_flag) {
+    //  Create dot and pdf files
+    //  DOT generation (numbering:preoder)
+    AstDOTGeneration dotgen;
+    dotgen.generateInputFiles(project, AstDOTGeneration::PREORDER);
+  }
+  if (pdf_flag) {
+    //  PDF generation
+    AstPDFGeneration pdfgen;
+    pdfgen.generateInputFiles(project);
+  }
 
   init_termite(argc, argv);
   
-  //create prolog term
+  // Create prolog term
   BasicTermPrinter tp;
-  //if (strip_headers) 
-  //  tp.traverseInputFiles(project); // Without headers
-  //else
-    tp.traverse(project); // With headers
+  tp.traverse(project); // With headers
 
   PrologTerm* genTerm = tp.getTerm();
-
-  ofstream ofile(outputFileName);
-  ofile << genTerm->getRepresentation() << "." << endl;
-  ofile.close();
+  
+  if (outfile) {
+    ofstream ofile(outfile);
+    ofile << genTerm->getRepresentation() << "." << endl;
+    ofile.close();
+  } else cout << genTerm->getRepresentation() << "." << endl;
 		
   return 0;
 }
