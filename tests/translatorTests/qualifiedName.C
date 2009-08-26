70554527046249773286f9d59601514c27890133
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-08-26 17:08:48 +0000

    A test case
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@7016 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ tests/translatorTests/qualifiedName.C
@@ -0,0 +1,23 @@
+/*
+ * a strange bug related to missing the scope of the 2nd parameter of __builtin_prefetch()
+ *  Liao
+ *  8/26/2009
+ */
+#include "rose.h"
+#include <iostream>
+using namespace std;
+
+int
+main (int argc, char *argv[])
+{
+  SgProject *project = frontend (argc, argv);
+  ROSE_ASSERT (project != NULL);
+
+  Rose_STL_Container<SgNode*> initNames = NodeQuery::querySubTree(project, V_SgInitializedName);
+  for (Rose_STL_Container<SgNode*>::const_iterator i = initNames.begin(); i != initNames.end(); ++i) {
+    SgInitializedName* iName = isSgInitializedName(*i);
+    string name = iName->get_qualified_name().str();
+  }
+
+  return backend (project);
+}
