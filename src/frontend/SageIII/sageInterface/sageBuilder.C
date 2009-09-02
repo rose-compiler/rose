698605bcab4bcfcda71ade17eef6badaba38f2b1
liao6 <liao6@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-09-02 16:24:10 +0000

    Fixed a bug related to duplicated building of a opaque type
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@7059 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

--- src/frontend/SageIII/sageInterface/sageBuilder.C
+++ src/frontend/SageIII/sageInterface/sageBuilder.C
@@ -411,36 +411,37 @@ SageBuilder::buildMemberFunctionType(SgType* return_type, SgFunctionParameterTyp
  //! Build an opaque type with a name, useful when a type's details are unknown during transformation, especially for a runtime library's internal type.
  SgType * SageBuilder::buildOpaqueType(std::string const name, SgScopeStatement * scope)
 {
+  // we require users to specify a target scope
   ROSE_ASSERT(scope);
-  SgTypedefDeclaration* type_decl = new SgTypedefDeclaration(name,buildIntType(),NULL, NULL, NULL);
-  ROSE_ASSERT(type_decl);
+  SgTypedefDeclaration* type_decl = NULL;
+  SgTypedefType* result = NULL;
+
   // Liao and Greg Bronevetsky , 8/27/2009
   // patch up the symbol  
-   // TODO  a function like fixTypeDeclaration() (similar to SageInterface::fixVariableDeclaration()) for this
-  if (scope != NULL)
+  // and avoid duplicated creation
+  // TODO  a function like fixTypeDeclaration() (similar to SageInterface::fixVariableDeclaration()) for this
+  SgTypedefSymbol* type_symbol = scope->lookup_typedef_symbol(name);
+  if (type_symbol == NULL)
   {
-    SgTypedefSymbol* type_symbol = scope->lookup_typedef_symbol(name);
-      if (type_symbol == NULL)
-      {
-        type_symbol = new SgTypedefSymbol(type_decl);
-        ROSE_ASSERT(type_symbol);
-        SgName n = name;
-        scope->get_symbol_table()->insert(n, type_symbol); 
-      } 
-     else
-     {
-       cout<<"Trying to build duplicated typedef declaration:"<<name<<endl;
-       ROSE_ASSERT(false);
-     }
+    type_decl =  new SgTypedefDeclaration(name,buildIntType(),NULL, NULL, NULL);
+    ROSE_ASSERT(type_decl);
+    type_symbol = new SgTypedefSymbol(type_decl);
+    ROSE_ASSERT(type_symbol);
+    SgName n = name;
+    scope->get_symbol_table()->insert(n, type_symbol); 
+    type_decl->set_firstNondefiningDeclaration (type_decl);
+    setOneSourcePositionForTransformation(type_decl);
+    prependStatement(type_decl,scope);
+    // Hide it from unparser
+    Sg_File_Info* file_info = type_decl->get_file_info();
+    file_info->unsetOutputInCodeGeneration ();
+    result = new SgTypedefType(type_decl);
+  } 
+  else
+  {
+    type_decl = type_symbol->get_declaration();
+    result = type_decl->get_type();
   }
-  type_decl->set_firstNondefiningDeclaration (type_decl);
-  setOneSourcePositionForTransformation(type_decl);
-  prependStatement(type_decl,scope);
-  // Hide it from unparser
-  Sg_File_Info* file_info = type_decl->get_file_info();
-  file_info->unsetOutputInCodeGeneration ();
-
-  SgTypedefType* result = new SgTypedefType(type_decl);
   ROSE_ASSERT(result);
   return result;
 }
