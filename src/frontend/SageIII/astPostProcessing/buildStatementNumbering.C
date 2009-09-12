134d774742bf3e431e8d3748fc81766d90d59dbc
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-09-12 18:13:28 +0000

    Initial support for better name qualification.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@7247 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ src/frontend/SageIII/astPostProcessing/buildStatementNumbering.C
@@ -0,0 +1,73 @@
+
+#include "rose.h"
+
+void
+SgSourceFile::
+// buildStatementNumbering( SgSourceFile::NumberToStatementType & numberToStatementContainer )
+buildStatementNumbering()
+   {
+  // DQ (9/11/2009): 
+
+  // typedef std::vector<SgNode*> NumberToStatementType;
+     typedef SgNodePtrList NumberToStatementType;
+  // NumberToStatementType numberToStatementContainer;
+
+     class NumberStatementsTraversal : public AstSimpleProcessing
+        {
+          public:
+               NumberToStatementType & local_numberToStatementContainer;
+
+               NumberStatementsTraversal(NumberToStatementType & x)
+                  : local_numberToStatementContainer(x) {}
+
+               void visit ( SgNode* astNode )
+                  {
+                    static int statementCounter = 0;
+                    SgStatement* statement = isSgStatement(astNode);
+                    if (statement != NULL)
+                       {
+                         local_numberToStatementContainer.push_back(statement);
+
+                         ROSE_ASSERT(local_numberToStatementContainer[statementCounter] == statement);
+
+                         statementCounter++;
+                       }
+                  }
+        };
+
+  // DQ (9/11/2009): Measure the cost of the renumbering to support the name qualification.
+     TimingPerformance timer ("Statement numbering (name qualification support):");
+
+     SgSourceFile* file = (SgSourceFile*) this;
+  // NumberStatementsTraversal t(numberToStatementContainer);
+     NumberStatementsTraversal t(p_statementNumberContainer);
+
+     t.traverse(file,preorder);
+
+#if 1
+  // This provides a count of the total number of statements.
+  // printf ("numberToStatementContainer.size() = %zu \n",numberToStatementContainer.size());
+     printf ("p_statementNumberContainer.size() = %zu \n",p_statementNumberContainer.size());
+#endif
+
+#if 0
+     printf ("Exiting in SgSourceFile::buildStatementNumbering() \n");
+     ROSE_ASSERT(false);
+#endif
+   }
+
+
+
+void
+// SgSourceFile::
+// buildStatementNumbering( SgSourceFile::NumberToStatementType & numberToStatementContainer )
+fixupUsingNamespaceDeclarations()
+   {
+  // DQ (9/11/2009): STart of support for processing using declarations.
+
+#if 0
+     printf ("Exiting in SgSourceFile::fixupUsingNamespaceDeclarations() \n");
+     ROSE_ASSERT(false);
+#endif
+   }
+
