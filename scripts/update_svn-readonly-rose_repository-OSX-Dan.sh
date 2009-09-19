9e3f198dc0299d336388d4ef3ddb2f193d81d721
dquinlan <dquinlan@a27c51de-2d2b-4d1b-a6e8-f7f87814f453>
2009-09-19 14:13:43 +0000

    Added scripts for automated update of svn-readonly-rose cached copy.
    git-svn-id: file:///usr/casc/overture/ROSE/svn/ROSE/trunk/ROSE@7392 a27c51de-2d2b-4d1b-a6e8-f7f87814f453

---
+++ scripts/update_svn-readonly-rose_repository-OSX-Dan.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+ROSE_TOP="/home/dquinlan/ROSE/svn-test-rose/svn-readonly-rose"
+
+# the right version of subversion is essential!!
+SVN=/nfs/apps/subversion/1.5.5/bin/svn
+
+# Check the EDG binaries already present
+ls -l ${ROSE_TOP}/src/frontend/CxxFrontend
+
+# Remove them so that we can cleanly update from the SVN repository
+rm ${ROSE_TOP}/src/frontend/CxxFrontend/*.gz
+
+# Update all of this checkedout version of ROSE
+${SVN} update ${ROSE_TOP}
+
+# Check the resulting binaries
+ls -l ${ROSE_TOP}/src/frontend/CxxFrontend
