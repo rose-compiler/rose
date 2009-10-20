a1003ec413ca4441f0cd2cdef1f75a63eefb01cd
Gergo Barany <gergo@complang.tuwien.ac.at>
2009-10-20 12:34:20 -0700

    bugfix
    Ignore-this: 739f342dbbfb12fb9c56ae5e0447f64e
    
    darcs-hash:20091020193420-0c742-644285b984bfda765bc3a4062903369e04ca024a.gz

--- projects/SATIrE/src/analyzer/support/satire_program_representation.C
+++ projects/SATIrE/src/analyzer/support/satire_program_representation.C
@@ -18,30 +18,8 @@ Program::Program(AnalyzerOptions *o)
  // for an annotated program.
     const std::vector<DataFlowAnalysis *> &analyzers
         = o->getDataFlowAnalyzers();
-    std::vector<DataFlowAnalysis *>::const_iterator a = analyzers.begin();
- // The first analyzer is special: It allows us to compute context
- // information and run the context-specific points-to analysis.
-    if (a != analyzers.end() && (*a) != NULL /* empty analyzer */)
-    {
-        (*a)->run(this);
-        a++;
-
-#if HAVE_PAG
-        icfg->contextInformation = new ContextInformation(icfg);
-        if (o->runPointsToAnalysis())
-        {
-            icfg->contextSensitivePointsToAnalysis->run(icfg);
-            if (o->outputPointsToGraph())
-            {
-                icfg->contextSensitivePointsToAnalysis->doDot(
-                        o->getPointsToGraphName());
-            }
-            o_Location_power = icfg->contextSensitivePointsToAnalysis
-                                    ->get_locations().size();
-        }
-#endif
-    }
-    for ( ; a != analyzers.end(); ++a)
+    std::vector<DataFlowAnalysis *>::const_iterator a;
+    for (a = analyzers.begin() ; a != analyzers.end(); ++a)
     {
       if (*a != NULL /* empty analyzer */) {
         (*a)->run(this);
