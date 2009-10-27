e7cb574938f133b8be3d8ea6a5120b7f525dfdcc
Robb P. Matzke <matzke@work.spizella.com>
2009-10-27 13:45:12 -0400

    binaryTests: improved cleanup rules; parallel comments
    The "make mostlyclean" rule was changed so that we don't have to list
    the sample output names in the makefile. Instead, the script that runs
    the test also knows how to clean up.

--- tests/roseTests/binaryTests/testSampleBinaries.sh
+++ tests/roseTests/binaryTests/testSampleBinaries.sh
@@ -2,17 +2,11 @@
 
 # Runs various tests on all the sample binaries
 # Test output, etc. is to stdout and stderr; progress info is to file descriptor 99
-SAMPLE_DIR="$1"    # Name of directory containing binary files to test
-CONFIG="$2"        # Name of config file. See testSampleBinaries.conf for details.
-DEFAULT_TESTS=skip # Tests to run if none are specified
-
-die() {
-    echo "$0: $@" >&2
-    exit 1
-}
 
 ########################################################################################################################
 #    Tests.  Their names (sans "test_" are listed in the config file).
+#    First argument is the name of the executable which should be tested. If a second argument is present then the
+#    test should clean up instead of run.
 ########################################################################################################################
 
 # A test that always passes; an alias for "true" that makes more sense in the output
@@ -28,26 +22,49 @@ test_fail() {
 # A test that just parses the binary container without disassembling
 test_parser() {
     local sample="$1"
-    ./execFormatsTest -rose:read_binary_file_format_only -rose:binary $sample
+    local cleanup="$2"
+    if [ -n "$cleanup" ]; then
+	local basename=$(basename $sample)
+	rm -f rose_$basename.s $basename.new $basename.dump
+    else
+        ./execFormatsTest -rose:read_binary_file_format_only -rose:binary $sample
+    fi
 }
 
 # A test of the unparser that tries to parse what was unparsed in the 'test_parser' test.
 test_unparser() {
     local sample="$1"
+    local cleanup="$2"
     sample="$(basename $sample).new"
-    ./execFormatsTest -rose:read_binary_file_format_only -rose:binary $sample
+    if [ -n "$cleanup" ]; then
+	rm -f rose_$sample.s $sample.new $sample.dump
+    else
+	./execFormatsTest -rose:read_binary_file_format_only -rose:binary $sample
+    fi
 }
 
 # A test that runs the disassembler with default settings
 test_disassembler() {
     local sample="$1"
-    ./disassemble -rose:binary $sample
+    local cleanup="$2"
+    if [ -n "$cleanup" ]; then
+	local basename=$(basename $sample)
+	rm -f rose_$basename.s $basename.new $basename.dump
+    else
+        ./disassemble -rose:binary $sample
+    fi
 }
 
 # A test that runs the disassembler and tries to reassemble to the same machine code bytes
 test_assembler() {
     local sample="$1"
-    ./disassemble -rose:binary --reassemble $sample
+    local cleanup="$2"
+    if [ -n "$cleanup" ]; then
+	local basename=$(basename $sample)
+	rm -f rose_$basename.s $basename.new $basename.dump
+    else
+	./disassemble -rose:binary --reassemble $sample
+    fi
 }
 
 
@@ -58,12 +75,42 @@ test_assembler() {
 
 
 
+die() {
+    echo "$0: $@" >&2
+    exit 1
+}
 
-
+# Parse command-line arguments
+do_cleanup=
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+	--cleanup)
+	    do_cleanup=yes
+	    shift
+  	    ;;
+	-*)
+	    die "unknown command-line option: $1"
+	    exit 1
+	    ;;
+	*)
+	    break
+	    ;;
+    esac
+done
+[ "$#" -gt 2 ] && die "usage: SAMPLE_DIR [CONFIG_FILE]"
+SAMPLE_DIR="$1"    # Name of directory containing binary files to test
+CONFIG="$2"        # Name of config file. See testSampleBinaries.conf for details.
+DEFAULT_TESTS=skip # Tests to run if none are specified
 [ -d "$SAMPLE_DIR" ] || die "no sample directory: $SAMPLE_DIR"
 [ -n "$CONFIG" ] || CONFIG=/dev/null
+
 failures= nfailures=0
-echo "Running binary tests on samples in $SAMPLE_DIR"
+if [ -n "$do_cleanup" ]; then
+    echo "Cleaning up test files for samples in $SAMPLE_DIR"
+else
+    echo "Running binary tests on samples in $SAMPLE_DIR"
+fi
+
 for sample in $(find $SAMPLE_DIR \( -type d -name .svn -prune -false \) -o -type f |sort); do
     sampleshort="${sample#$SAMPLE_DIR/}"
     samplebase="${sample##*/}"
@@ -87,37 +134,45 @@ for sample in $(find $SAMPLE_DIR \( -type d -name .svn -prune -false \) -o -type
     (echo -n "$sampleshort:" >&99) >/dev/null 2>&1
 
     for testname in $tests; do
-	(echo -n " $testname" >&99) >/dev/null 2>&1
-	echo
-	echo
-	echo
-	echo "==============================================================================================="
-	echo "===   $testname $sample"
-	echo "==============================================================================================="
-
-	if eval "test_$testname '$sample'"; then
-	    : ok
+	if [ -n "$do_cleanup" ]; then
+	    eval "test_$testname '$sample' cleanup"
 	else
-	    echo "FAILED: $testname $sample" >&2
-	    (echo -n "[FAILED]" >&99) >/dev/null 2>&1
-	    failures="$failures $sampleshort[$testname]"
-	    nfailures=$[nfailures+1]
+	    (echo -n " $testname" >&99) >/dev/null 2>&1
+	    echo
+	    echo
+	    echo
+	    echo "==============================================================================================="
+	    echo "===   $testname $sample"
+	    echo "==============================================================================================="
+
+	    if eval "test_$testname '$sample'"; then
+		: ok
+	    else
+		echo "FAILED: $testname $sample" >&2
+		(echo -n "[FAILED]" >&99) >/dev/null 2>&1
+		failures="$failures $sampleshort[$testname]"
+		nfailures=$[nfailures+1]
+	    fi
 	fi
     done
     (echo >&99) >/dev/null 2>&1
 done
 
 
-echo "==============================================================================================="
-echo "===   Summary"
-echo "==============================================================================================="
-(echo "Number of failed tests: $nfailures" >&99) >/dev/null 2>&1
-if [ $nfailures -eq 0 ]; then
-    echo "All tests passed!"
-    exit 0
+if [ -n "$do_cleanup" ]; then
+    :
 else
-    echo "Encountered $nfailures failures:"
-    echo "$failures" | tr ' ' '\n' |sed 's/^/    /'
-    echo "Test(s) failed!"
-    exit 1
+    echo "==============================================================================================="
+    echo "===   Summary"
+    echo "==============================================================================================="
+    (echo "Number of failed tests: $nfailures" >&99) >/dev/null 2>&1
+    if [ $nfailures -eq 0 ]; then
+	echo "All tests passed!"
+	exit 0
+    else
+	echo "Encountered $nfailures failures:"
+	echo "$failures" | tr ' ' '\n' |sed 's/^/    /'
+	echo "Test(s) failed!"
+	exit 1
+    fi
 fi
