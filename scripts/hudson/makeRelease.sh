1ad21faec5e160a0298fd90f62426f3a72253ce2
Chunhua (Leo) Liao <chunhualiao@gmail.com>
2009-12-14 18:38:57 -0800

    More scripting for automated releasing the git master branch

--- scripts/hudson/makeRelease.sh
+++ scripts/hudson/makeRelease.sh
@@ -3,22 +3,26 @@
 # Initial script to make external release packages
 # it assume 
 # Part of the work will be phased out once the external git repository is ready.
-
 # set up 32-bit environment
 #--------------------------------------
-# source /usr/apps/qt/4.5.1/setup.sh  # this causes the script to abort
+set +e
+source /usr/apps/qt/4.5.1/setup.sh  # this causes the script to abort if set +e is not used
 
 export JAVA_HOME=/usr/apps/java/jdk1.6.0_11
 export BOOST_ROOT="/home/liao6/opt/boost_1_35_0"
 export LD_LIBRARY_PATH="/usr/apps/java/jdk1.6.0_11/lib:${JAVA_HOME}/jre/lib/i386/server:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:${LD_LIBRARY_PATH}"
-export PATH="$JAVA_HOME/bin:/home/liao6/opt/git-1.6.5.2/bin:/usr/apps/automake/1.9.6/bin:$PATH"
+export PATH="$JAVA_HOME/bin:/home/liao6/opt/git-1.6.5.2/bin:/usr/apps/automake/1.9.6/bin:$PATH:/home/liao6/opt/ghc-6.10.4/bin"
 PROCESS_NUM=4
 
 # prepare the source tree 
 #--------------------------------------
-# git pull # this will cause problem since hudson test may run on a branch
+# git pull # this will cause problem since hudson test may run on a ghost branch
 
+set -e
+PSEUDO_REV_NUM=`git log --pretty=oneline |wc -l`
+
+ROSE_SRC_PATH=`pwd`
 echo `pwd`
 # always rebuild
 ./build
@@ -26,15 +30,28 @@ echo `pwd`
 # build tree:  configure and build
 #--------------------------------------
 # always build fresh
-rm -rf ROSE-build 
+if test -e ROSE-build ; then chmod -R u+w ROSE-build ; fi
+rm -rf ROSE-build
 mkdir ROSE-build
+
 cd ROSE-build
-# only reconfigure as needed
-../configure --with-boost=${BOOST_ROOT} --with-CXX_DEBUG=-g --with-CXX_WARNINGS=-Wall
+ROSE_BUILD_PATH=`pwd`
+
+# always reconfigure
+../configure --with-boost=${BOOST_ROOT} --with-CXX_DEBUG=-g --with-CXX_WARNINGS=-Wall --enable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-qt=/usr/apps/qt/4.5.1 --with-roseQt --with-haskell=/home/liao6/opt/ghc-6.10.4/bin --with-pch --with-gomp_omp_runtime_library=/home/liao6/opt/gcc-svn/lib/ --prefix=$HOME/.hudson/tempInstall
 
 make -j${PROCESS_NUM} && \
 make check -j${PROCESS_NUM} && \
-make docs -j${PROCESS_NUM} && \
-make dist -j${PROCESS_NUM} && \
+make docs -j${PROCESS_NUM} &&  \
+make dist -j${PROCESS_NUM} DOT_SVNREV=-$PSEUDO_REV_NUM && \
 make distcheck -j${PROCESS_NUM}
 
+# --------- update external svn repository
+/home/liao6/rose/scripts/hudson/importRoseGitDistributionToSVN $ROSE_SOURCE_PATH $ROSE_BUILD_PATH
+
+# --------- update external web site
+make -C docs/Rose copyWebPages
+
+# ---------update external file packages
+# SKIPPED here 
+# this is done off the external svn repository
