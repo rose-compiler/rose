e67ccd3e3a05aa1d667a1cd4cc22651b9ff8328b
Chunhua (Leo) Liao <chunhualiao@gmail.com>
2009-12-15 10:37:17 -0800

    Finalized scripts for releasing ROSE

--- scripts/hudson/releaseRoseFilePackages.sh
+++ scripts/hudson/releaseRoseFilePackages.sh
@@ -9,25 +9,59 @@
 #    as a distribution package. Makefile.in and other generated file should exist!!
 #  b) automatically fill out a web form to upload the package and release it.
 # by Liao
-# 12/14/2009
+# 12/15/2009
 
-if [ $# -ne 3 ]
+if [ $# -ne 2 ]
 then
-  echo This script needs three arguments to run.
-  echo Syntax: $0 ROSE_DIST_NO_EDG_PACKAGE_NAME ROSE_SOURCE_TREE ROSE_BUILD_TREE
-  echo Example: $0 rose-0.9.5a-8286.tar.gz /home/aaa/rose /home/aaa/buildrose
+  echo This script needs two arguments to run.
+  echo Syntax: $0 ROSE_SOURCE_PATH ROSE_BUILD_PATH
+  echo Example: $0 /home/aaa/rose /home/aaa/buildrose
   exit 1
 fi
 
-ROSE_DIST_NO_EDG_PACKAGE_NAME=$1
+# grab and verify the input parameters
+ROSE_SOURCE_PATH=$1
+ROSE_BUILD_PATH=$2
+
+if [ -d ${ROSE_SOURCE_PATH} ]; then
+  if [ -f ${ROSE_SOURCE_PATH}/rose_config.h.in ] ;
+   then
+     echo ${ROSE_SOURCE_PATH} is verified to be a rose source tree
+   else
+     echo Fatal error: ${ROSE_SOURCE_PATH} does not seem to be a rose source tree!
+     exit 3
+  fi
+else
+  echo Fatal error: ${ROSE_SOURCE_PATH} does not exist!
+  exit 3
+fi
+
+if [ -d ${ROSE_BUILD_PATH} ]; then
+  if [ -f ${ROSE_BUILD_PATH}/rose_config.h ] ;
+   then
+     echo ${ROSE_BUILD_PATH} is verified to be a rose build tree
+   else
+     echo Fatal error: ${ROSE_BUILD_PATH} does not seem to be a rose build tree!
+     exit 3
+  fi
+else
+  echo Fatal error: ${ROSE_BUILD_PATH} does not exist!
+  exit 3
+fi
+
+# ---------find the distribution package from the build tree
+# find the form of rose-version-rev.tar.gz
+# find the full path and name for the package to be uploaded
+cd ${ROSE_BUILD_PATH}
+ROSE_DIST_NO_EDG_PACKAGE_NAME=$(find . -maxdepth 1  -name  rose-\*-\*.tar.gz)
+if [ $? -ne 0 ]; then
+     echo "Fatal error: cannot find the distribution package!"
+     exit 1
+fi
+
 # we only need the base name
 ROSE_DIST_NO_EDG_PACKAGE_BASE_NAME=`basename $ROSE_DIST_NO_EDG_PACKAGE_NAME .tar.gz`
-ROSE_SOURCE_TREE=$2
-ROSE_BUILD_TREE=$3
-
-test -d $ROSE_SOURCE_TREE || exit 1
-test -d $ROSE_BUILD_TREE || exit 1
-test -f $ROSE_BUILD_TREE/$ROSE_DIST_NO_EDG_PACKAGE_NAME || exit 1
+test -f $ROSE_BUILD_PATH/$ROSE_DIST_NO_EDG_PACKAGE_NAME || exit 1
 
 # How many types of platforms to support
 # Let's try the lightweight release first!!
@@ -71,32 +105,23 @@ echo "The detected version number is $VERSION_NO"
 echo "The detected pseudo revision number is $REVISION_NO"
 
 #---------- upload the distribution package------------
-# find the full path and name for the package to be uploaded
-cd ${ROSE_BUILD_TREE}
 # find the distribution package from top/build
 
 # in case we want to use this script on both internal and external repository
 # they have different file names for the distributions
 #ROSE_DISTRIBUTION=$(find build -name  \*source-with-EDG-binary\*.tar.gz)
 #ROSE_DISTRIBUTION=$(find . -name rose-${VERSION_NO}-${REVISION_NO}.tar.gz)
-ROSE_DISTRIBUTION=$(find . -name rose-${VERSION_NO}-${REVISION_NO}.tar.gz)
-
-if [ "x${ROSE_DISTRIBUTION[0]}" != "x" ]; then
-    echo "Found rose distribution package:$ROSE_DISTRIBUTION"
-else  
-     echo "Fatal error: cannot find the distribution package!"
-     exit 1
+#ROSE_DISTRIBUTION=$(find . -name rose-${VERSION_NO}-${REVISION_NO}.tar.gz)
+ROSE_DISTRIBUTION=$ROSE_DIST_NO_EDG_PACKAGE_NAME
+if [ $? -ne 0 ]; then
+   echo "Fatal error: cannot find the distribution package!"
+   exit 1
 fi
-
-if [ "x${ROSE_DISTRIBUTION[1]}" != "x" ]; then
-     echo "Fatal error: found more than one distribution candidates!!"
-     exit 1
-fi  
 
 #------------ sanity check here!!------------------------
 #----------------------------------------------------
 # a temp place for sanity check
-UPLOAD_DIR=${ROSE_BUILD_TREE}/upload
+UPLOAD_DIR=${ROSE_BUILD_PATH}/upload
 echo "upload dir is $UPLOAD_DIR"
 
 rm -rf ${UPLOAD_DIR}
@@ -105,6 +130,10 @@ mkdir -p ${UPLOAD_DIR}
 # unpack the package to the work dir and get the root dir of the distribution
 #----------------------------------------------------
 tar xzvf ${ROSE_DISTRIBUTION} -C ${UPLOAD_DIR} &>/dev/null
+if [ $? -ne 0 ]; then
+   echo "Fatal error for: tar xzvf ${ROSE_DISTRIBUTION} -C ${UPLOAD_DIR}"
+   exit 1
+fi
 
 cd ${UPLOAD_DIR}
 
@@ -173,8 +202,8 @@ fi
 #-------------------- the actual uploading!!!---------------------
 # call the upload script 
 # VERSION_NO, REVISION_NO, FULL_PACKAGE_NAME
-#${SCRIPT_TOP}/makeRelease.sh ${VERSION_NO} ${REVISION_NO} ${FULL_PACKAGE_NAME} 
-#if [ $? -ne 0 ]; then
-#   echo "Fatal error: cannot upload the distribution package!"
-#   exit 1
-#fi
+${SCRIPT_TOP}/hudson/finalUploadFilePackages.sh ${VERSION_NO} ${REVISION_NO} ${FULL_PACKAGE_NAME} 
+if [ $? -ne 0 ]; then
+   echo "Fatal error: cannot upload the distribution package!"
+   exit 1
+fi
--- scripts/hudson/releaseRoseReposAndWeb.sh
+++ scripts/hudson/releaseRoseReposAndWeb.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
-# Liao 12/8/2009
-# Initial script to make external release packages
-# it assume 
-# Part of the work will be phased out once the external git repository is ready.
+#!/bin/bash -vx
+# A hudson-friendly script to 
+# 1) build a distribution package off the internal git repository's
+# master branch, 
+# 2) import the package to the external svn repository
+# 3) update rosecompiler.org website
+# 
+# Liao 12/15/2009
+
 # set up 32-bit environment
 #--------------------------------------
 set +e
@@ -20,6 +24,7 @@ PROCESS_NUM=4
 # git pull # this will cause problem since hudson test may run on a ghost branch
 
 set -e
+
 PSEUDO_REV_NUM=`git log --pretty=oneline |wc -l`
 
 ROSE_SRC_PATH=`pwd`
@@ -37,7 +42,7 @@ mkdir ROSE-build
 cd ROSE-build
 ROSE_BUILD_PATH=`pwd`
 
-# always reconfigure
+# only reconfigure as needed
 ../configure --with-boost=${BOOST_ROOT} --with-CXX_DEBUG=-g --with-CXX_WARNINGS=-Wall --enable-dq-developer-tests --with-ROSE_LONG_MAKE_CHECK_RULE=yes --with-qt=/usr/apps/qt/4.5.1 --with-roseQt --with-haskell=/home/liao6/opt/ghc-6.10.4/bin --with-pch --with-gomp_omp_runtime_library=/home/liao6/opt/gcc-svn/lib/ --prefix=$HOME/.hudson/tempInstall
 
 make -j${PROCESS_NUM} && \
@@ -54,4 +59,6 @@ make -C docs/Rose copyWebPages
 
 # ---------update external file packages
 # SKIPPED here 
-# this is done off the external svn repository
+# this is done by another weekly hudson job
+# /home/liao6/rose/scripts/hudson/releaseRoseFilePackages.sh $ROSE_SOURCE_PATH $ROSE_BUILD_PATH
+
