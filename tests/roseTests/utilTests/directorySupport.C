d9ebabad1f02fded22f76a140a42ea9a6e1f5cbf
Dan Quinlan <dquinlan@llnl.gov>
2010-01-23 17:33:07 -0800

    Added required directory tests files.

---
+++ tests/roseTests/utilTests/directorySupport.C
@@ -0,0 +1,26 @@
+// Example ROSE Translator
+// used for testing ROSE infrastructure
+
+#include "rose.h"
+
+int
+main( int argc, char * argv[] )
+   {
+     SgProject* project = frontend(argc,argv);
+     AstTests::runAllTests(project);
+
+  // Add a directory and unparse the code (to the new directory)
+     SgDirectory* directory = new SgDirectory("xxx");
+
+     directory->set_parent(project);
+
+#if 1
+  // Add a directory to the list in the SgProject node.
+     project->get_directoryList()->get_listOfDirectories().push_back(directory);
+
+     SgFile* file = project->get_fileList()[0];
+     directory->get_fileList()->get_listOfFiles().push_back(file);
+#endif
+
+     return backend(project); // only backend error code is reported
+   }
---
+++ tests/roseTests/utilTests/input_directorySupport.C
@@ -0,0 +1,6 @@
+// Input test problem so that we can test the new SgDirectory IR node.
+
+int main()
+   {
+     return 0;
+   }
