67cbc07dca130303e5445e2e322630526532229d
Thomas Panas <panas@llnl.gov>
2010-02-01 10:45:38 -0800

    changes to get this working under windows

--- src/frontend/SageIII/rosedefs.h
+++ src/frontend/SageIII/rosedefs.h
@@ -13,6 +13,10 @@
 
 // DQ (9/25/2007): Need to move this to here so that all of ROSE will see it.
 #define Rose_STL_Container std::vector
+#ifdef _MSC_VER
+#include "stdint.h"
+//typedef __int64 uint64_t;
+#endif
 typedef uint64_t rose_addr_t; /* address and size (file and memory) */
 #define ROSE_DEPRECATED_FUNCTION 
 #define __builtin_constant_p (exp) (0)
