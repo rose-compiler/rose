328978cef7f0437243ba17ad22d854d466cc257d
Thomas Panas <panas@llnl.gov>
2010-02-01 10:06:56 -0800

    small changes

--- src/frontend/SageIII/rosedefs.h
+++ src/frontend/SageIII/rosedefs.h
@@ -20,7 +20,7 @@ typedef uint64_t rose_addr_t; /* address and size (file and memory) */
 #include "roseInternal.h"
 
 
-#include "rose_attributes_list.h"
+//#include "rose_attributes_list.h"
 #include "rose_config.h"
 #include "virtualCFG.h" 
 #include "virtualBinCFG.h" 
