30123aacd830789fb41469bfef8c9de92fbcfe17
Adrian Prantl <adrian@complang.tuwien.ac.at>
2010-02-09 07:20:06 -0800

    More interval analysis improvements
    Ignore-this: 54da43f870c9b4d6c64956d39cad47aa
    
    darcs-hash:20100209152006-3f1cf-b78d80304d86e30392f3b20c7f7f26cb780856dc.gz

--- projects/SATIrE/tools/interval/arithmetic.hs
+++ projects/SATIrE/tools/interval/arithmetic.hs
@@ -1,10 +1,11 @@
 -- This is a test of the interval arithmetic functions used in interval.optla
 
+import Data.Bits
+
 plus (al,ah) (bl,bh) = (al+bl, ah+bh)
 minus (al,ah) (bl,bh) = (al-bh, ah-bl)
 times (al,ah) (bl,bh) = (min (min (al*bl) (al*bh)) (min (ah*bl) (ah*bh)),
                          max (max (al*bl) (al*bh)) (max (ah*bl) (ah*bh)))
---divide (al,ah) (bl,bh) = (min al bl, max ah bh)
 --divide (al,ah) (bl,bh) = (min (min (al `safediv` bh) (ah `safediv` bh))
 --                              (min (al `safediv` bminpos) (ah `safediv` bmaxneg)),
 --                          max (max (al `safediv` bminpos) (ah `safediv` bminpos))
@@ -17,9 +18,69 @@ times (al,ah) (bl,bh) = (min (min (al*bl) (al*bh)) (min (ah*bl) (ah*bh)),
 divide (al,ah) (bl,bh) = (-x, x)
                          where x = max (max (abs al) (abs bl))
                                        (max (abs ah) (abs bh))
+modulo a b@(bl,bh) = let bmax = max (abs bl) (abs bh)
+                     in if strict_equal_sign a b then
+                            if bl > 0 then (0, bmax -1)
+                            else (1-bmax, 0)
+                    else (1-bmax, bmax-1)
+
+lshift (al,ah) (bl,bh) = if al<0 || bl<0 then dontknow
+                         else (al `shiftL` bl, ah `shiftL` bh)
+
+-- according to ISO C99 right-shift of a negative number is compiler-specific
+rshift (al,ah) (bl,bh) = if al<0 || bl<0 then dontknow
+                         else (al `shiftR` bh, ah `shiftR` bl)
+
+bitand a@(al,ah) b@(bl,bh) = if (al==0 && ah==0) || (bl==0 && bh==0) 
+                             then (0, 0)
+                             else bitor a b
+
+bitor a@(al,ah) b@(bl,bh) =  let x = next_higher_power_of_2 
+                                       (max (max (abs al) (abs bl))
+                                            (max (abs ah) (abs bh)))
+                             in if strict_positive a b then
+                                    (0, x-1) 
+                                else (-x,x-1)
+
+bitxor a@(al,ah) b@(bl,bh) = let amax = max (abs al) (abs ah)
+                                 bmax = max (abs bl) (abs bh)
+                                 x = next_higher_power_of_2(amax .|. bmax)
+                             in if strict_equal_sign a b then
+                                    (0, x-1)
+                             else (-x,x-1)
+
+dontknow = (1-(2^63), 2^63-1)
+-- return True if all values in both intervals have the same sign
+strict_equal_sign (al,ah) (bl,bh) = (al > 0 && ah > 0 && bl > 0 && bh > 0) ||
+                                    (al < 0 && ah < 0 && bl < 0 && bh < 0)
+
+strict_positive (al,ah) (bl,bh) = (al > 0 && ah > 0 && bl > 0 && bh > 0)
+strict_negative (al,ah) (bl,bh) = (al < 0 && ah < 0 && bl < 0 && bh < 0)
+
+lowest_bit(0) = 0
+lowest_bit(n) =
+    if n == (1 `shiftL` 31) then 32 else  -- the critical negative case
+    if (n .&. 1) == 1       then 1
+                           else 1 + lowest_bit(n `shiftR` 1)
+
+highest_bit(0) = 0;
+highest_bit(n) =
+    if n < 0 then 32  -- sign bit is set
+             else 1 + highest_bit(n `shiftR` 1)
+
+lowest_power_of_2(0) = 0;
+lowest_power_of_2(n) = (1 `shiftL` (lowest_bit(n) - 1))
+
+next_higher_power_of_2(0) = 0;
+next_higher_power_of_2(n) = (1 `shiftL` highest_bit(n))
+
+
+------------------------------------------
 
 safediv a 0 = 0 -- ignore divide by zero
 safediv a b = a `div` b
+safemod a 0 = 0 -- ignore modide by zero
+safemod a b = a `mod` b
 
 test (f,op,name) a@(al,ah) b@(bl,bh) =
     foldl1 (&&) $ 
@@ -34,15 +95,23 @@ test (f,op,name) a@(al,ah) b@(bl,bh) =
                              ++"\n\tCounterexample: "++(show i)++name++(show j)
                              ++" = "++(show (i `op` j))
 
+r = runtest
 runtest = let -- We need to take at least one arbitrary +/- and the
               -- neutral element plus odd and even numbers for `div`.
               -- The rest should follow from monotonicity of the
               -- operators
-              vals = [-23,-8,-3,-1,0,1,7,4,17] 
+              vals = [-23,-8,-3,-1,0,1,7,4,17,21,32] 
               tuples = [(i,j) | i <- vals, j <- reverse vals, i<j]
               ops = [(plus,(+),"+"),
                      (minus,(-),"-"),
                      (times,(*),"*"),
-                     (divide,(safediv)," `div` ")]
+                     (divide,(safediv)," `div` "),
+                     (modulo,(safemod), " `mod` "),
+                     (lshift,shiftL," << "),
+                     (rshift,shiftR," >> "),
+                     (bitand,(.&.)," & "),
+                     (bitor,(.|.)," | "),
+                     (bitxor,xor," `xor` ")
+                    ]
           in
             foldl1 (&&) [test f a b | f <- ops, a <- tuples, b <- tuples]
