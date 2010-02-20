4651a4a4e1216fa94aac4bce79cf8564e13000f7
Dan Quinlan <dquinlan@llnl.gov>
2010-02-20 11:09:20 -0800

    Added more support for testing the AST File I/O mechanism in ROSE.    1) Added a Hudson test for this to operate on ROSE source code.    2) Added more aggressive tests of AST File I/O (using standard list       of tests code in tests/CompileTests/Cxx_tests/Makefile-pass.inc).    3) Removed test2005_56.C from standard list of test codes, but added       it explicitly to the test run in Cxx_tests/Makefile.am.    4) astFileIO_tests now uses the new tests/testAstFileIO translator.

--- tests/testAstFileIO.C
+++ tests/testAstFileIO.C
@@ -1,3 +1,12 @@
+// this test program reads the input file to generate an AST, and then:
+//   1) Generates an output source file (as the initial source file).
+//   2) Sets up the file I/O support
+//   3) Writes the AST to disk
+//   4) Clears the existing memory pools
+//   5) Reads the AST from disk
+//   6) Generates the source file from the AST (using backendGeneratesSourceCodeButCompilesUsingOriginalInputFile())
+//   7) Compares the initial source frile with the second source file using diff.
+
 #include "rose.h"
  
 using namespace std;
@@ -14,98 +23,78 @@ main ( int argc, char * argv[] )
 
      printf ("project_identity->get_outputFileName() = %s \n",project_identity->get_outputFileName().c_str());
 
-#if 1
+     printf ("Writing the initial file to disk... \n");
      backend(project_identity);
-     std::string fileName =  project_identity->get_outputFileName() ;
-#endif
-#if 1
-     std::string moving = "mv rose_" + fileName + ".C rose_" + fileName + "_identity.C" ;
+     string fileName =  project_identity->get_outputFileName() ;
 
+  // Save the generated source file to be the original file (to be used to compare against later).
+     string moving = "mv rose_" + fileName + ".C rose_" + fileName + "_identity.C" ;
      status = system ( moving.c_str() );
      ROSE_ASSERT(status == 0);
-#endif
 
 #if 0
+  // Debugging information
      AST_FILE_IO::printListOfPoolSizes() ;
 #endif
 
-#if 1
-     std :: cout  << std::endl<< "Here we call the AST_FILE_IO :: start_up ..." << std::endl;
-     AST_FILE_IO :: startUp( project_test ) ;
-     std :: cout  << std::endl<< "AST_FILE_IO :: start_up done ... " << std::endl;
-#endif
+     printf ("Calling File I/O initialization... \n");
+     AST_FILE_IO::startUp( project_test ) ;
 
-#if 1
   // DQ (12/12/2009): Allow output only when run in verbose mode to limit spew in testing.
      if (SgProject::get_verbose() > 0)
           AST_FILE_IO::printListOfPoolSizes() ;
-#endif
 
-
-#if 0 
+#if 0
   // This will move everything into the EasyStorageClasses and then reconstruct 
   // it from scratch. As a result it is a good test without and File I/O.
-     std :: cout  << std::endl<< "Here we call the AST_FILE_IO :: compressAstInMemoryPool() ..." << std::endl;
-     AST_FILE_IO :: compressAstInMemoryPool();
-     std :: cout  << std::endl<< "AST_FILE_IO :: compressAstInMemoryPool() done ... " << std::endl;
-#endif
- 
-#if 1 
-     std :: cout  << std::endl<< "Here we call the AST_FILE_IO :: writeASTToFile ..." << std::endl;
-     AST_FILE_IO :: writeASTToFile ( fileName + ".binary" );
-     std :: cout  << std::endl<< "AST_FILE_IO :: writeASTToFile done ... " << std::endl;
+     AST_FILE_IO::compressAstInMemoryPool();
 #endif
 
-  // DQ (6/24/2006): Test just this parts first
-  // return 0;
+     printf ("Writing the AST to disk... \n");
+     AST_FILE_IO::writeASTToFile ( fileName + ".binary" );
 
-#if 1
-     std :: cout  << std::endl<< "Here we delete the memroy pools and prepare them for reading back the ast the file IO " << std::endl;
+  // delete the memroy pools and prepare them for reading back the ast the file IO
+     printf ("Clear existing memory pools... \n");
+     AST_FILE_IO::clearAllMemoryPools( );
 
-     AST_FILE_IO :: clearAllMemoryPools( );
-
-     std :: cout  << std::endl<< "Here we call the AST_FILE_IO :: readASTFromFile ..." << std::endl;
+     printf ("Read in the AST from disk... \n");
      project_test = (SgProject*) (AST_FILE_IO :: readASTFromFile ( fileName + ".binary" ) );
-     std :: cout  << std::endl<< "AST_FILE_IO :: readASTFromFile done ... " << std::endl;
 
-  // AST_FILE_IO::printListOfPoolSizes() ;
+  // AST_FILE_IO::printListOfPoolSizes();
+
+#if 0
+     cout << "Resetting the memory pools to hold a valid linked list ... " << flush;
+     AST_FILE_IO::resetValidAstAfterWriting( );
+     cout << "done " << endl;
 #endif
 
 #if 0
-     std::cout << "Resetting the memory pools to hold a valid linked list ... " << std::flush;
-     AST_FILE_IO :: resetValidAstAfterWriting( );
-     std::cout << "done " << std::endl;
-#endif
-
-#if 0
-     std :: cout << "Staring AstTests ...... "   << flush;
+     cout << "Staring AstTests ...... " << flush;
      AstTests::runAllTests(project_test);
-     std :: cout << "done "   << std::endl;
+     cout << "done " << endl;
 #endif
  
 #if 0
-     std::cout << "Second try ... " << std::flush;
+     cout << "Second try ... " << flush;
      AST_FILE_IO::setStaticDataOfAst(0);
      AstTests::runAllTests(project_test);
-     std :: cout << "done "   << std::endl;
+     cout << "done " << endl;
 #endif
 
-#if 1
-     std :: cout  << std::endl<< " Writing file ......." << std::endl;
-     backend(project_test);
-     std :: cout  << std::endl<< " File to disk ! " << std::endl;
-#endif
+     printf ("Writing the second (generated source file from AST read from disk)... \n");
+  // backend(project_test);
+     backendGeneratesSourceCodeButCompilesUsingOriginalInputFile(project_test);
 
 #if 1
-     std::string diff = "diff  rose_"+ fileName + ".C rose_" + fileName + "_identity.C" ;
-  
+     printf ("Test initial source file against second source file using diff... \n");
+     string diff = "diff  rose_"+ fileName + ".C rose_" + fileName + "_identity.C" ;  
      if ( system ( diff.c_str() ) == 0 ) 
         {
-          std :: cout << "!!!!!!!!!!!!!  files are identical !!!!!!!!!!!!"   << std::endl;
+          cout << "!!!!!!!!!!!!!  files are identical !!!!!!!!!!!!"   << endl;
         }
        else 
         {
-          std :: cout << "********* Problem: Files seem not to match ****"   << std::endl;
+          cout << "********* Problem: Files seem not to match ****"   << endl;
           exit ( -1 );
         }
 #endif
