d834bd5d9a10914fb85e2aa54ee17d58ab562a78
Thomas Panas <panas@llnl.gov>
2010-02-24 10:28:49 -0800

    refactored CMakelist

--- src/frontend/SageIII/rosedll.h
+++ src/frontend/SageIII/rosedll.h
@@ -2,45 +2,42 @@
 #define __ROSEDLL__
 
 // tps (02/24/2010)
-// For Linux, ROSE_ROSETTA_API is defined as ROSE_HELPER_DLL_IMPORT
+// For Linux, ROSE_API is defined as ROSE_HELPER_DLL_IMPORT
 // which is always empty space
-// Actually, the default is empty space: ROSE_ROSETTA_EXPORTS is only defined
-// when using cmake. If not used, ROSE_ROSETTA_API = ROSE_HELPER_DLL_IMPORT = EMPTY SPACE
+// Actually, the default is empty space: ROSE_DLL_EXPORTS is only defined
+// when using cmake. If not used, ROSE_DLL_API = ROSE_DLL_HELPER_DLL_IMPORT = EMPTY SPACE
 
 
 // Generic helper definitions for shared library support
 #if defined _WIN32 || defined __CYGWIN__
-  #define ROSE_HELPER_DLL_IMPORT __declspec(dllimport)
-  #define ROSE_HELPER_DLL_EXPORT __declspec(dllexport)
-  #define ROSE_HELPER_DLL_LOCAL
+  #define ROSE_DLL_HELPER_DLL_IMPORT __declspec(dllimport)
+  #define ROSE_DLL_HELPER_DLL_EXPORT __declspec(dllexport)
+  #define ROSE_DLL_HELPER_DLL_LOCAL
 #else
   #if __GNUC__ >= 4
-//#define ROSE_HELPER_DLL_IMPORT __attribute__ ((visibility("default")))
-    #define ROSE_HELPER_DLL_IMPORT 
-    #define ROSE_HELPER_DLL_EXPORT __attribute__ ((visibility("default")))
-    #define ROSE_HELPER_DLL_LOCAL  __attribute__ ((visibility("hidden")))
+//    #define ROSE_DLL_HELPER_DLL_IMPORT __attribute__ ((visibility("default")))
+    #define ROSE_DLL_HELPER_DLL_IMPORT 
+//    #define ROSE_DLL_HELPER_DLL_EXPORT __attribute__ ((visibility("default")))
+    #define ROSE_DLL_HELPER_DLL_EXPORT 
+    #define ROSE_DLL_HELPER_DLL_LOCAL  __attribute__ ((visibility("hidden")))
   #else
-    #define ROSE_HELPER_DLL_IMPORT
-    #define ROSE_HELPER_DLL_EXPORT
-    #define ROSE_HELPER_DLL_LOCAL
+    #define ROSE_DLL_HELPER_DLL_IMPORT
+    #define ROSE_DLL_HELPER_DLL_EXPORT
+    #define ROSE_DLL_HELPER_DLL_LOCAL
   #endif
 #endif
 
 
-//#ifdef ROSE_ROSETTA // defined if ROSE_ROSETTA is compiled as a DLL
-  #ifdef ROSE_ROSETTA_EXPORTS // defined if we are building the ROSE_ROSETTA DLL (instead of using it)
-    #define ROSE_ROSETTA_API ROSE_HELPER_DLL_EXPORT
-//    #warning "ROSE_ROSETTA_API : ROSE_HELPER_DLL_EXPORT"
+// ROSE_DLL_EXPORTS is only defined for cmake
+  #ifdef ROSE_DLL_EXPORTS // defined if we are building the ROSE DLL (instead of using it)
+    #define ROSE_DLL_API ROSE_DLL_HELPER_DLL_EXPORT
+//    #warning "ROSE_DLL_API : ROSE_DLL_HELPER_DLL_EXPORT"
   #else
-    #define ROSE_ROSETTA_API ROSE_HELPER_DLL_IMPORT
-//    #warning "ROSE_ROSETTA_API : ROSE_HELPER_DLL_IMPORT"
-  #endif // ROSE_ROSETTA_DLL_EXPORTS
-  #define ROSE_ROSETTA_LOCAL ROSE_HELPER_DLL_LOCAL
-//#else // ROSE_ROSETTA_DLL is not defined: this means ROSE_ROSETTA is a static lib.
-//  #define ROSE_ROSETTA_API
-//  #define ROSE_ROSETTA_LOCAL
-//  #warning "ROSE_ROSETTA_API : NULL"
-//#endif // ROSE_ROSETTA_DLL
+// this is the default for automake
+    #define ROSE_DLL_API ROSE_DLL_HELPER_DLL_IMPORT
+//    #warning "ROSE_DLL_API : ROSE_DLL_HELPER_DLL_IMPORT"
+  #endif // ROSE_DLL_DLL_EXPORTS
+  #define ROSE_DLL_LOCAL ROSE_DLL_HELPER_DLL_LOCAL
 
 #endif
 
