35974b5cbae2acd12dc7185108af316d3176d175
Dan Quinlan <dquinlan@llnl.gov>
2010-04-07 17:00:21 -0700

    Fixed more of the Fortran support for linking, etc.

---
+++ tests/CompileTests/Fortran_tests/test2010_03.f90
@@ -0,0 +1,6 @@
+module precision
+! use mpif.h
+  include 'rose_mpif_error.h' ! include empty file...
+! DQ (4/7/2010): "Parser failed" error for OFP (version 0.7.2) but requires empty header file
+  integer, parameter :: doubleprec=selected_real_kind(12),singleprec=selected_real_kind(6),defaultprec=kind(0.0)
+end module precision
