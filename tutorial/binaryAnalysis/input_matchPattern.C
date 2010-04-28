7639ea3a7e5ef47286219e2c2bde44cc2fff94bb
Dan Quinlan <dquinlan@llnl.gov>
2010-04-27 18:22:12 -0700

    Added binary analysis tutorial example for NOP deteection.

---
+++ tutorial/binaryAnalysis/input_matchPattern.C
@@ -0,0 +1,16 @@
+
+int main()
+   {
+     asm ("nop");
+     asm ("nop");
+     asm ("nop");
+     asm ("nop");
+     asm ("nop");
+     asm ("nop");
+     asm ("nop");
+     asm ("nop");
+     asm ("nop");
+     asm ("nop");
+
+     return 0;
+   }
