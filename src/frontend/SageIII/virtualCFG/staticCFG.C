e6e2687c93e05e072863485016d6e66ec8f4d494
Michael Driscoll <driscoll6@llnl.gov>
2010-06-23 15:50:59 -0700

    fixed bug where certain calls weren't propogating "interprocedural" boolean to all CFGNode::{In,Out}Edges() calls

--- src/frontend/SageIII/virtualCFG/staticCFG.C
+++ src/frontend/SageIII/virtualCFG/staticCFG.C
@@ -174,7 +174,7 @@ void CFG::buildCFG(CFGNode n)
 #endif
 
 template <class NodeT, class EdgeT>
-void CFG::buildCFG(NodeT n, std::map<NodeT, SgGraphNode*>& all_nodes, std::set<NodeT>& explored)
+void CFG::buildCFG(NodeT n, std::map<NodeT, SgGraphNode*>& all_nodes, std::set<NodeT>& explored, bool interprocedural)
 {
     ROSE_ASSERT(n.getNode());
 
@@ -196,7 +196,7 @@ void CFG::buildCFG(NodeT n, std::map<NodeT, SgGraphNode*>& all_nodes, std::set<N
         graph_->addNode(from);
     }
 
-    std::vector<EdgeT> outEdges = n.outEdges();
+    std::vector<EdgeT> outEdges = n.outEdges(interprocedural);
     foreach (const EdgeT& edge, outEdges)
     {
         NodeT tar = edge.target();
@@ -225,7 +225,7 @@ void CFG::buildCFG(NodeT n, std::map<NodeT, SgGraphNode*>& all_nodes, std::set<N
         buildCFG<NodeT, EdgeT>(edge.target(), all_nodes, explored);
     }
 
-    std::vector<EdgeT> inEdges = n.inEdges();
+    std::vector<EdgeT> inEdges = n.inEdges(interprocedural);
     foreach (const EdgeT& edge, inEdges)
     {
         ROSE_ASSERT(edge.target() == n);
