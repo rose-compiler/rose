rose-compiler/rose

commit f8e61ae4f1d8d6691d7245a9d20e2129756715c0

Added missing headers to simulator makefile

src/midend/binaryAnalyses/instructionSemantics/YicesSolver.h
@@ -7,11 +7,19 @@
  *  for the satisfiable() virtual method to work. */
 class YicesSolver: public SMTSolver {
 public:
+    YicesSolver(): context(NULL) {}
     virtual void generate_file(std::ostream&, const InsnSemanticsExpr::TreeNode *expr, Definitions*);
     virtual std::string get_command(const std::string &config_name);
 
+    /** Determines if the specified expression is satisfiable.  Most solvers use the implementation in the base class, which
+     *  creates a text file (usually in SMT-LIB format) and then invokes an executable with that input, looking for a line of
+     *  output containing "sat" or "unsat". However, Yices provides a library that can optionally be linked into ROSE, and if
+     *  that library is present then we use it, otherwise we fall back to the implementation in the base class. */
+    virtual bool satisfiable(const InsnSemanticsExpr::TreeNode *expr);
+
 private:
-    /* These out*() functions convert a InsnSemanticsExpr expression into text which is suitable as input to yices. */
+    /* These out_*() functions convert a InsnSemanticsExpr expression into text which is suitable as input to "yices"
+     * executable. */
     void out_define(std::ostream&, const InsnSemanticsExpr::TreeNode*, Definitions *defns);
     void out_assert(std::ostream&, const InsnSemanticsExpr::TreeNode*);
     void out_number(std::ostream&, const InsnSemanticsExpr::TreeNode*);
@@ -28,6 +36,10 @@ private:
     void out_asr(std::ostream&, const InsnSemanticsExpr::InternalNode*);
     void out_zerop(std::ostream&, const InsnSemanticsExpr::InternalNode*);
     void out_mult(std::ostream &o, const InsnSemanticsExpr::InternalNode *in);
+
+    /* These ctx_*() functions build a Yices context object if Yices is linked into this executable. */
+    yices_context context;
+    
 };
 
 #endif

projects/simulator/Makefile.am
@@ -1,5 +1,7 @@
 include $(top_srcdir)/config/Makefile.for.ROSE.includes.and.libs
 
+include_HEADERS = memory.h simulate.h
+
 noinst_PROGRAMS = sandbox test1
 
 sandbox_SOURCES = emulator.C simulate.C linuxcalls.C elfload.C

commit 07fdcbbdb04c4d29b3864124ff5bf65fec264ca7

Partial revert of f8e61ae4f1d8d6691d7245a9d20e2129756715c0
Accidentally included a file from work in progress.

src/midend/binaryAnalyses/instructionSemantics/YicesSolver.h
@@ -7,19 +7,11 @@
  *  for the satisfiable() virtual method to work. */
 class YicesSolver: public SMTSolver {
 public:
-    YicesSolver(): context(NULL) {}
     virtual void generate_file(std::ostream&, const InsnSemanticsExpr::TreeNode *expr, Definitions*);
     virtual std::string get_command(const std::string &config_name);
 
-    /** Determines if the specified expression is satisfiable.  Most solvers use the implementation in the base class, which
-     *  creates a text file (usually in SMT-LIB format) and then invokes an executable with that input, looking for a line of
-     *  output containing "sat" or "unsat". However, Yices provides a library that can optionally be linked into ROSE, and if
-     *  that library is present then we use it, otherwise we fall back to the implementation in the base class. */
-    virtual bool satisfiable(const InsnSemanticsExpr::TreeNode *expr);
-
 private:
-    /* These out_*() functions convert a InsnSemanticsExpr expression into text which is suitable as input to "yices"
-     * executable. */
+    /* These out*() functions convert a InsnSemanticsExpr expression into text which is suitable as input to yices. */
     void out_define(std::ostream&, const InsnSemanticsExpr::TreeNode*, Definitions *defns);
     void out_assert(std::ostream&, const InsnSemanticsExpr::TreeNode*);
     void out_number(std::ostream&, const InsnSemanticsExpr::TreeNode*);
@@ -36,10 +28,6 @@ private:
     void out_asr(std::ostream&, const InsnSemanticsExpr::InternalNode*);
     void out_zerop(std::ostream&, const InsnSemanticsExpr::InternalNode*);
     void out_mult(std::ostream &o, const InsnSemanticsExpr::InternalNode *in);
-
-    /* These ctx_*() functions build a Yices context object if Yices is linked into this executable. */
-    yices_context context;
-    
 };
 
 #endif