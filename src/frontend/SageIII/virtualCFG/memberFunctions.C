be9e8d6608f944cecc9201404742faa61836b504
Michael Driscoll <driscoll6@llnl.gov>
2010-07-12 09:11:39 -0700

    added logic to handle constructor initializer lists in CFGs. The current implementation is hackish because ROSE doesn't distinguish between regular methods and constructors, so all methods have initialization lists.

--- src/frontend/SageIII/virtualCFG/memberFunctions.C
+++ src/frontend/SageIII/virtualCFG/memberFunctions.C
@@ -718,12 +718,13 @@ SgFunctionParameterList::cfgInEdges(unsigned int idx, bool interprocedural)
 
 unsigned int
 SgFunctionDefinition::cfgIndexForEnd() const {
-  return 2;
+  SgMemberFunctionDeclaration* memberDecl = isSgMemberFunctionDeclaration(get_declaration());
+  return (memberDecl) ? 3 : 2;
 }
 
 bool
 SgFunctionDefinition::cfgIsIndexInteresting(unsigned int idx) const {
-  return idx == 0 || idx == 2;
+  return idx == 0 || idx == 2 || idx == 3;
 }
 
 unsigned int 
@@ -752,11 +753,16 @@ SgFunctionDefinition::cfgFindChildIndex(SgNode* n)
 
 std::vector<CFGEdge> 
 SgFunctionDefinition::cfgOutEdges(unsigned int idx, bool interprocedural) {
+  SgMemberFunctionDeclaration* memberDecl = isSgMemberFunctionDeclaration(get_declaration());
+  int offset = 0;
+  if (! memberDecl && idx >= 1) offset = 1;
+
   std::vector<CFGEdge> result;
-  switch (idx) {
+  switch (idx + offset) {
     case 0: makeEdge(CFGNode(this, idx), this->get_declaration()->get_parameterList()->cfgForBeginning(), result); break;
-    case 1: makeEdge(CFGNode(this, idx), this->get_body()->cfgForBeginning(), result); break;
-    case 2: { 
+    case 1: makeEdge(CFGNode(this, idx), memberDecl->get_CtorInitializerList()->cfgForBeginning(), result); break;
+    case 2: makeEdge(CFGNode(this, idx), this->get_body()->cfgForBeginning(), result); break;
+    case 3: { 
      if (! interprocedural) break;
      SgFunctionDeclaration* thisdecl = this->get_declaration();
      SgFunctionDeclaration* thisdefdecl = isSgFunctionDeclaration(thisdecl->get_definingDeclaration());
@@ -793,9 +799,13 @@ SgFunctionDefinition::cfgOutEdges(unsigned int idx, bool interprocedural) {
 }
 
 std::vector<CFGEdge> SgFunctionDefinition::cfgInEdges(unsigned int idx, bool interprocedural) {
+  SgMemberFunctionDeclaration* memberDecl = isSgMemberFunctionDeclaration(get_declaration());
+  int offset = 0;
+  if (! memberDecl && idx >= 2) offset = 1;
+
   std::vector<CFGEdge> result;
   addIncomingFortranGotos(this, idx, result);
-  switch (idx) {
+  switch (idx + offset) {
     case 0: {
      if (! interprocedural) break;
      SgFunctionDeclaration* thisdecl = this->get_declaration();
@@ -828,7 +838,8 @@ std::vector<CFGEdge> SgFunctionDefinition::cfgInEdges(unsigned int idx, bool int
      break;
     }
     case 1: makeEdge(this->get_declaration()->get_parameterList()->cfgForEnd(), CFGNode(this, idx), result); break;
-    case 2: {
+    case 2: makeEdge(memberDecl->get_CtorInitializerList()->cfgForEnd(), CFGNode(this, idx), result); break;
+    case 3: {
       makeEdge(this->get_body()->cfgForEnd(), CFGNode(this, idx), result);
       // Liao, 5/21/2010. bad implementation since vectors are created/destroyed  multiple times
       //std::vector<SgReturnStmt*> returnStmts = SageInterface::findReturnStmts(this);
