3809c7eb6a2d2ad6dea62944ba5d40b3a9ac100f
vulov1 <vulov1@tux22.llnl.gov>
2010-07-29 14:29:15 -0700

    Run the function argument extraction normalization on all the C++ compile tests =D

--- projects/backstroke/tests/extractFunctionArgumentsTest/NormalizationTranslator.C
+++ projects/backstroke/tests/extractFunctionArgumentsTest/NormalizationTranslator.C
@@ -1,19 +1,15 @@
 #include "rose.h"
 #include "normalizations/ExtractFunctionArguments.h"
+#include "restrictedLanguage/LanguageRestrictions.h"
+#include "utilities/CPPDefinesAndNamespaces.h"
 
 int main(int argc, char** argv)
 {
 	SgProject* project = frontend(argc, argv);
+	ROSE_ASSERT(project != NULL);
 	AstTests::runAllTests(project);
 
-	//Generate a DOT file so we see what's going on
-	generateDOT(*project);
-	generatePDF(*project);
-	generateAstGraph(project, 2000);
-
-	//Normalize here
-	ExtractFunctionArguments e;
-	e.NormalizeTree(project);
+	ExtractFunctionArguments::NormalizeTree(project);
 
 	AstTests::runAllTests(project);
 	return backend(project);
