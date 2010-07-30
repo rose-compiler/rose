14076f0ef1bcbc5505e0317baf251de5d5d28fcf
Michael Driscoll <driscoll6@llnl.gov>
2010-07-30 08:31:00 -0700

    subclassed staticCFG to make interproceduralCFG. Hooked into makefile

---
+++ src/frontend/SageIII/virtualCFG/interproceduralCFG.C
@@ -0,0 +1,141 @@
+#include "sage3basic.h"
+#include "interproceduralCFG.h"
+#include <boost/foreach.hpp>
+
+#define foreach BOOST_FOREACH
+
+
+namespace StaticCFG
+{
+
+void InterproceduralCFG::buildFullCFG()
+{
+  CFG::buildFullCFG();
+#if 0
+    all_nodes_.clear();
+    clearNodesAndEdges();
+
+    std::set<VirtualCFG::CFGNode> explored;
+
+    graph_ = new SgIncidenceDirectedGraph;
+
+    if (SgProject* project = isSgProject(start_))
+    {
+        Rose_STL_Container<SgNode*> functions = NodeQuery::querySubTree(project, V_SgFunctionDefinition);
+        for (Rose_STL_Container<SgNode*>::const_iterator i = functions.begin(); i != functions.end(); ++i)
+        {
+            SgFunctionDefinition* proc = isSgFunctionDefinition(*i);
+            if (proc)
+            {
+                buildCFG<VirtualCFG::CFGNode, VirtualCFG::CFGEdge>
+                    (proc->cfgForBeginning(), all_nodes_, explored);
+            }
+        }
+    }
+    else
+        buildCFG<VirtualCFG::CFGNode, VirtualCFG::CFGEdge>
+            (start_->cfgForBeginning(), all_nodes_, explored);
+#endif
+}
+
+void InterproceduralCFG::buildFilteredCFG()
+{
+  CFG::buildFilteredCFG();
+#if 0
+    all_nodes_.clear();
+    clearNodesAndEdges();
+
+    std::set<VirtualCFG::InterestingNode> explored;
+    std::map<VirtualCFG::InterestingNode, SgGraphNode*> all_nodes;
+
+    graph_ = new SgIncidenceDirectedGraph;
+
+    if (SgProject* project = isSgProject(start_))
+    {
+        Rose_STL_Container<SgNode*> functions = NodeQuery::querySubTree(project, V_SgFunctionDefinition);
+        for (Rose_STL_Container<SgNode*>::const_iterator i = functions.begin(); i != functions.end(); ++i)
+        {
+            SgFunctionDefinition* proc = isSgFunctionDefinition(*i);
+            if (proc)
+            {
+                buildCFG<VirtualCFG::InterestingNode, VirtualCFG::InterestingEdge>
+                    (VirtualCFG::makeInterestingCfg(proc), all_nodes, explored);
+            }
+        }
+    }
+    else
+        buildCFG<VirtualCFG::InterestingNode, VirtualCFG::InterestingEdge>
+            (VirtualCFG::makeInterestingCfg(start_), all_nodes, explored);
+
+    typedef std::pair<VirtualCFG::InterestingNode, SgGraphNode*> pair_t;
+    foreach (const pair_t& p, all_nodes)
+        all_nodes_[VirtualCFG::CFGNode(p.first.getNode(), 0)] = p.second;
+#endif
+}
+
+template <class NodeT, class EdgeT>
+void InterproceduralCFG::buildCFG(NodeT n, std::map<NodeT, SgGraphNode*>& all_nodes, std::set<NodeT>& explored)
+{
+  CFG::buildCFG(n, all_nodes, explored);
+#if 0
+    ROSE_ASSERT(n.getNode());
+
+    if (explored.count(n) > 0)
+        return;
+    explored.insert(n);
+
+    SgGraphNode* from = NULL;
+    if (all_nodes.count(n) > 0)
+    {
+        from = all_nodes[n];
+    }
+    else
+    {
+        from = new SgGraphNode;
+        from->set_SgNode(n.getNode());
+        from->addNewAttribute("info", new CFGNodeAttribute(n.getIndex(), graph_));
+        all_nodes[n] = from;
+        graph_->addNode(from);
+    }
+
+    std::vector<EdgeT> outEdges = n.outEdges();
+    foreach (const EdgeT& edge, outEdges)
+    {
+        NodeT tar = edge.target();
+#if 1
+        std::cout << tar.toString() << std::endl;
+#endif
+
+        SgGraphNode* to = NULL;
+        if (all_nodes.count(tar) > 0)
+            to = all_nodes[tar];
+        else
+        {
+            to = new SgGraphNode;
+            to->set_SgNode(tar.getNode());
+            to->addNewAttribute("info", new CFGNodeAttribute(tar.getIndex(), graph_));
+            all_nodes[tar] = to;
+            graph_->addNode(to);
+        }
+
+        SgDirectedGraphEdge* new_edge = new SgDirectedGraphEdge(from, to);
+        new_edge->addNewAttribute("info", new CFGEdgeAttribute<EdgeT>(edge));
+        graph_->addDirectedEdge(new_edge);
+    }
+
+    foreach (const EdgeT& edge, outEdges)
+    {
+        ROSE_ASSERT(edge.source() == n);
+        buildCFG<NodeT, EdgeT>(edge.target(), all_nodes, explored);
+    }
+
+    std::vector<EdgeT> inEdges = n.inEdges();
+    foreach (const EdgeT& edge, inEdges)
+    {
+        ROSE_ASSERT(edge.target() == n);
+        buildCFG<NodeT, EdgeT>(edge.source(), all_nodes, explored);
+    }
+#endif
+}
+
+} // end of namespace StaticCFG
