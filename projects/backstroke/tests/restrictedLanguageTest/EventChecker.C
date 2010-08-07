b69e71d1f1547071e053849c3b7154fa30e85b85
vulov1 <vulov1@tux22.llnl.gov>
2010-08-06 21:21:04 -0700

    Revert "Make the language restriction tests run on all the compile tests (just checks for crashes, not necessarily correctness)"
    This reverts commit d9f3af34e8b6b598a92ce3d80ca96fe0a19e349a.

--- projects/backstroke/tests/restrictedLanguageTest/EventChecker.C
+++ projects/backstroke/tests/restrictedLanguageTest/EventChecker.C
@@ -1,22 +1,60 @@
 #include "restrictedLanguage/LanguageRestrictions.h"
-#include "utilities/CPPDefinesAndNamespaces.h"
 
 #include "rose.h"
 #include <vector>
 
 using namespace std;
 
+class visitorTraversal : public AstSimpleProcessing
+{
+public:
+	vector<SgFunctionDefinition*> eventList;
+
+	void visit(SgNode* n);
+
+};
+
+
+void visitorTraversal::visit(SgNode* node)
+{
+	SgFunctionDeclaration* functionDeclaration = isSgFunctionDeclaration(node);
+	if (functionDeclaration != NULL)
+	{
+		// Detect restricted language events
+		string functionName = functionDeclaration->get_name();
+		if (functionName.find("EVENT_", 0) != string::npos)
+		{
+			SgFunctionDefinition* functionDefinition = isSgFunctionDefinition(functionDeclaration->get_definition());
+			if (functionDefinition != NULL)
+			{
+				eventList.push_back(functionDefinition);
+			}
+		}
+	}
+}
+
+
+vector<SgFunctionDefinition*> buildEventList(SgProject* project)
+{
+	// This function collects the events in the input program.
+	// Events for our simple language are always marked by "EVENT_"
+	// as a prefix for the function names that correspond to and event.
+
+	// Build the traversal object
+	visitorTraversal restrictionTraversal;
+
+	// Call the traversal starting at the project node of the AST
+	restrictionTraversal.traverseInputFiles(project, preorder);
+
+	return restrictionTraversal.eventList;
+}
+
 
 int main(int argc, char * argv[])
 {
 	SgProject* project = frontend(argc, argv);
 
-	vector<SgFunctionDefinition*> eventList = SageInterface::querySubTree<SgFunctionDefinition>(project, V_SgFunctionDefinition);
+	vector<SgFunctionDefinition*> eventList = buildEventList(project);
 
-	foreach(SgFunctionDefinition* function, eventList)
-	{
-		LanguageRestrictions::violatesRestrictionsOnEventFunctions(function);
-	}
-
-	return 0;
+	return LanguageRestrictions::violatesRestrictionsOnEventFunctions(eventList);
 }
