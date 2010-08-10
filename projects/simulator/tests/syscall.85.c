ce1c87f0cadd46e059f5bfc79cb4e498bebec6fb
Jonathan Chu <chu24@llnl.gov>
2010-08-10 14:48:55 -0700

    Fix the readlink test to use symlink/unlink too

--- projects/simulator/tests/syscall.85.c
+++ projects/simulator/tests/syscall.85.c
@@ -1,9 +1,31 @@
 #include <unistd.h>
-#include "assert.h"
+
+#include <err.h>
+#include <errno.h>
+
+void setup() {
+  int result = symlink("/var/tmp","/tmp/vartmp");
+
+  if( result == -1 )
+    err(1, "symlink failed");
+}
+
+void cleanup() {
+  int result = unlink("/tmp/vartmp");
+
+  if( result == -1 )
+    err(1, "unlink failed");
+}
 
 int main() {
   char buf[100];
-  int count = readlink("/boot/vmlinuz", buf, sizeof(buf));
-  assert(count >= 0);
+
+  setup();
+
+  int count = readlink("/tmp/vartmp", buf, sizeof(buf));
+  if (count < 0)
+    err(1,"readlink failed");
+
+  cleanup();
   return 0;
 }
