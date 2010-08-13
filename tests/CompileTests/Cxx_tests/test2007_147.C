0214258651f02ee4b0c7744e9f444d27b6c9c9a4
Robb Matzke <matzke1@llnl.gov>
2010-08-13 09:42:36 -0400

    Fixing TABs in tests/CompileTests/Cxx_tests
    These changes do two things:
    
    1. They fix indentation where it was obviously messed up and not matching
       the predominant indentation of the rest of the file.  (Note: I did not
       fix indentation in files where it's royally screwed up or where I could
       not immediately discern the rules that were used.)
    
    2. They convert ASCII TAB characters to an appropriate number of SPC
       characters on certain lines of source files so that we can run an
       automated script to check tab-stop policy. (Note: for files that
       generated large numbers of warnings, rather than expand TABs on only the
       lines where a warning was reported, I expanded the entire file.)
    
    == The Tab-stop policy ==
    
    See the top of scripts/policies.d/EightColumnTabstops.pl for details. In
    short, since editors, terminals, printers, code formatters, etc. all have
    different capabilities when it comes to TAB characters, tab-stops, and the
    key labeled "Tab", we use the psuedo-standard that a TAB character is a
    compression method representing enough white space to advance to the next
    column which is a multiple of eight.
    
    The EightColumnTabstops.pl script looks at indentation deltas between
    adjacent lines of code, and any delta caused by TAB characters and which is
    not a multiple of the predominant indentation is flagged as a location that
    violates the eight-column tab-stops policy.  Thus, if the delta is correct
    then it can indicate so by indenting with SPC rather than TAB.

--- tests/CompileTests/Cxx_tests/test2007_147.C
+++ tests/CompileTests/Cxx_tests/test2007_147.C
@@ -1,31 +1,31 @@
 /*
-Bug name:	unparse-problem-with-operator-
+Bug name:       unparse-problem-with-operator-
 
-Reported by:	Brian White
+Reported by:    Brian White
 
-Date:		Sept 28, 2007
+Date:           Sept 28, 2007
 
 Date sent:      Sep 30, 2007
 
 E-mail subject: more SPEC 2006 bugs
 
-Occurs in:  	SPEC CPU 2006 	444.namd/src/dvector.cc
+Occurs in:      SPEC CPU 2006   444.namd/src/dvector.cc
 
-Brief description:  	ROSE backend triggers an assertion failure
+Brief description:      ROSE backend triggers an assertion failure
                         while attempting to unparse an invocation
                         of (friend) operator-.
 
 Files
 -----
-bug.cc		Invokes -w in main, where w is a DVector and
+bug.cc          Invokes -w in main, where w is a DVector and
                 operator- has been declared a friend of DVector.
 
-		This is the error returned when we try to compile
-		the file rose_bug.cc:
+                This is the error returned when we try to compile
+                the file rose_bug.cc:
 
 rosec: /fusion/research4/bwhite/ROSE-0.8.10e/ROSE/src/backend/unparser/unparse_expr.C:34: virtual void Unparser::unparseExpression(SgExpression*, SgUnparse_Info&): Assertion `expr != __null' failed.
 
-rose_bug.cc	The output from the backend for bug.cc.  ROSE unparses
+rose_bug.cc     The output from the backend for bug.cc.  ROSE unparses
                 the AST up to the point of the -w, at which point it aborts.
 */
 
