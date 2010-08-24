eac8e0b0c99b6919a0a411db573d8fae6f04ef50
unknown <panas2@.the-lab.llnl.gov>
2010-08-24 08:14:44 -0700

    debugging windows

--- tests/translatorTests/qualifiedName.C
+++ tests/translatorTests/qualifiedName.C
@@ -12,8 +12,31 @@ main (int argc, char *argv[])
 {
   SgProject *project = frontend (argc, argv);
   ROSE_ASSERT (project != NULL);
-
   printf("Running ROSE...");
+  string filename = project->get_outputFileName () ;
+  cout << "OUTPUTFILE : " << filename << endl;
+  std::vector<SgFile*> files = project->get_fileList_ptr()->get_listOfFiles();
+  std::vector<SgFile*>::const_iterator it = files.begin();
+  for (;it!=files.end();++it) {
+	SgFile* file = isSgFile(*it);
+	ROSE_ASSERT(file);
+	SgSourceFile* sfile = isSgSourceFile(file);
+	if (sfile) {
+		cout << "FILENAME : " << sfile->getFileName() << endl;
+		SgGlobal* global = isSgGlobal(sfile->get_globalScope());
+		 std::vector< SgNode * > nodes=	sfile->get_traversalSuccessorContainer ();
+		 std::vector< SgNode * >::const_iterator it2=nodes.begin();
+		for (;it2!=nodes.end();++it2) {
+			SgNode* node= isSgNode(*it2);
+			if (node) {
+				// right now this finds SgGlobal but it should find also
+				// SgVariableDeclaration and SgFunctionDeclaration
+				cout << "Found node : " << node->class_name() << endl;
+			}
+		}
+	}
+  }
+
   Rose_STL_Container<SgNode*> initNames = NodeQuery::querySubTree(project, V_SgInitializedName);
   for (Rose_STL_Container<SgNode*>::const_iterator i = initNames.begin(); i != initNames.end(); ++i) {
     SgInitializedName* iName = isSgInitializedName(*i);
