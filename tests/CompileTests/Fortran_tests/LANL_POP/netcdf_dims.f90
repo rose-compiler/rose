537a5e4327a4dc20292644d0b04e3431e76de610
Dan Quinlan <dquinlan@llnl.gov>
2010-08-24 20:52:09 -0700

    Fixed POP code tests using OFP and added test code to demonstrate OFP bug.

--- tests/CompileTests/Fortran_tests/LANL_POP/netcdf_dims.f90
+++ tests/CompileTests/Fortran_tests/LANL_POP/netcdf_dims.f90
@@ -2,14 +2,14 @@
   ! Dimension routines:
   !
   ! -----------
-  function nf90_def_dim(ncid, name, len1, dimid)
+  function nf90_def_dim(ncid, name, len, dimid)
     integer,             intent( in) :: ncid
     character (len = *), intent( in) :: name
-    integer,             intent( in) :: len1
+    integer,             intent( in) :: len
     integer,             intent(out) :: dimid
     integer                          :: nf90_def_dim
   
-    nf90_def_dim = nf_def_dim(ncid, name, len1, dimid)
+    nf90_def_dim = nf_def_dim(ncid, name, len, dimid)
   end function nf90_def_dim
   ! -----------
   function nf90_inq_dimid(ncid, name, dimid)
@@ -30,10 +30,10 @@
     nf90_rename_dim = nf_rename_dim(ncid, dimid, name)
   end function nf90_rename_dim
   ! -----------
-  function nf90_inquire_dimension(ncid, dimid, name, len2)
+  function nf90_inquire_dimension(ncid, dimid, name, len)
     integer,                       intent( in) :: ncid, dimid
     character (len = *), optional, intent(out) :: name
-    integer,             optional, intent(out) :: len2
+    integer,             optional, intent(out) :: len
     integer                                    :: nf90_inquire_dimension
     
     character (len = nf90_max_name) :: dimName
@@ -41,6 +41,6 @@
     
     nf90_inquire_dimension = nf_inq_dim(ncid, dimid, dimName, length)
     if(present(name)) name = trim(dimName)
-    if(present(len2 )) len2  = length
+    if(present(len )) len  = length
   end function nf90_inquire_dimension
   ! -----------
