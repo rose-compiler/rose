dc9838c7258fc26be020d3808a80801ad27ec89b
George Vulov <georgevulov@gatech.edu>
2010-09-02 16:09:41 -0400

    Clean up the pluggable reverser main. There is some mysterious segmentation fault in here

--- projects/backstroke/tests/pluggableReverserTest/new_pluggableReverserMain.C
+++ projects/backstroke/tests/pluggableReverserTest/new_pluggableReverserMain.C
@@ -16,121 +16,88 @@ using namespace SageInterface;
 using namespace SageBuilder;
 using namespace backstroke_util;
 
+
 int main(int argc, char * argv[])
 {
-    vector<string> args(argv, argv+argc);
-    SgProject* project = frontend(args);
+	vector<string> args(argv, argv + argc);
+	SgProject* project = frontend(args);
 
+	SgGlobal* global = getFirstGlobalScope(project);
 
-#if 1
-    //return 0;
-    //var_renaming.toDOT("temp.dot");
+	// Prepend includes to test files.
+	string includes = "#include <rctypes.h>\n"
+			"#include <stdio.h>\n"
+			"#include <stdlib.h>\n"
+			"#include <time.h>\n"
+			"#include <assert.h>\n"
+			"#include <memory.h>\n";
+	addTextForUnparser(global, includes, AstUnparseAttribute::e_before);
 
+	pushScopeStack(isSgScopeStatement(global));
 
-    SgGlobal* global = getFirstGlobalScope(project);
+	// Get every function declaration and identify if it's an event function.
+	vector<SgFunctionDeclaration*> func_decls = backstroke_util::querySubTree<SgFunctionDeclaration > (global);
+	foreach(SgFunctionDeclaration* decl, func_decls)
+	{
+		string func_name = decl->get_name();
+		if (!starts_with(func_name, "event") ||	ends_with(func_name, "reverse") || ends_with(func_name, "forward"))
+			continue;
 
-    // Prepend includes to test files.
-    string includes = "#include <rctypes.h>\n"
-        "#include <stdio.h>\n"
-        "#include <stdlib.h>\n"
-        "#include <time.h>\n"
-        "#include <assert.h>\n"
-        "#include <memory.h>\n";
-    addTextForUnparser(global, includes, AstUnparseAttribute::e_before);
+		//Normalize this event function.
+		backstroke_norm::normalizeEvent(decl);
+	}
 
+	VariableRenaming var_renaming(project);
+	var_renaming.run();
 
+	EventProcessor event_processor(NULL, &var_renaming);
 
-    pushScopeStack(isSgScopeStatement(global));
+	// Add all expression handlers to the expression pool.
+	event_processor.addExpressionProcessor(new NullExpressionProcessor);
+	event_processor.addExpressionProcessor(new IdentityExpressionProcessor);
+	event_processor.addExpressionProcessor(new StoreAndRestoreExpressionProcessor);
+	event_processor.addExpressionProcessor(new ConstructiveExpressionProcessor);
+	//event_processor.addExpressionProcessor(new ConstructiveAssignmentProcessor);
+	//event_processor.addExpressionProcessor(new AkgulStyleExpressionProcessor(project));
 
-    // Get every function declaration and identify if it's an event function.
-    vector<SgFunctionDeclaration*> func_decls = backstroke_util::querySubTree<SgFunctionDeclaration > (global);
+	// Add all statement handlers to the statement pool.
+	event_processor.addStatementProcessor(new ExprStatementProcessor);
+	event_processor.addStatementProcessor(new BasicBlockProcessor);
+	event_processor.addStatementProcessor(new IfStatementProcessor);
 
-    foreach(SgFunctionDeclaration* decl, func_decls)
-    {
-        string func_name = decl->get_name();
-        if (!starts_with(func_name, "event") ||
-                ends_with(func_name, "reverse") ||
-                ends_with(func_name, "forward"))
-            continue;
-        // First of all, normalize this event function.
-        //backstroke_norm::normalizeEvent(decl);
-    }
+	foreach(SgFunctionDeclaration* decl, func_decls)
+	{
+		string func_name = decl->get_name();
+		if (!starts_with(func_name, "event") || ends_with(func_name, "reverse") || ends_with(func_name, "forward"))
+			continue;
 
-    //generateGraphOfAST(project, "Graph");
+		timer t;
+		// Here reverse the event function into several versions.
+		FuncDeclPairs output = event_processor.processEvent(decl);
 
+		cout << "Time used: " << t.elapsed() << endl;
+		cout << "Event is processed successfully!\n";
 
-    VariableRenaming var_renaming(project);
-    var_renaming.run();
+		foreach(FuncDeclPair& func_decl_pair, output)
+		{
+			appendStatement(func_decl_pair.first);
+			appendStatement(func_decl_pair.second);
+		}
+	}
 
+	// Declare all stack variables on top of the generated file.
+	vector<SgVariableDeclaration*> stack_decls = event_processor.getAllStackDeclarations();
+	foreach(SgVariableDeclaration* decl, stack_decls)
+	{
+		prependStatement(decl);
+	}
 
-    EventProcessor event_processor(NULL, &var_renaming);
+	popScopeStack();
 
-    // Add all expression handlers to the expression pool.
-    event_processor.addExpressionProcessor(new NullExpressionProcessor);
-    event_processor.addExpressionProcessor(new IdentityExpressionProcessor);
-    event_processor.addExpressionProcessor(new StoreAndRestoreExpressionProcessor);
-    event_processor.addExpressionProcessor(new ConstructiveExpressionProcessor);
-    //event_processor.addExpressionProcessor(new ConstructiveAssignmentProcessor);
-    //event_processor.addExpressionProcessor(new AkgulStyleExpressionProcessor(project));
+	// Fix all variable references here.
+	cout << "VarRef fixed: " <<	fixVariableReferences(global) << endl;
+	AstTests::runAllTests(project);
+	cout << "Test Done!\n";
 
-    // Add all statement handlers to the statement pool.
-    event_processor.addStatementProcessor(new ExprStatementProcessor);
-    event_processor.addStatementProcessor(new BasicBlockProcessor);
-    event_processor.addStatementProcessor(new IfStatementProcessor);
-    
-
-    foreach(SgFunctionDeclaration* decl, func_decls)
-    {
-        string func_name = decl->get_name();
-        if (!starts_with(func_name, "event") ||
-                ends_with(func_name, "reverse") ||
-                ends_with(func_name, "forward"))
-            continue;
-
-        //var_renaming.run();
-
-        /*******************************************************/
-        // A small test here :)
-       // VariableRenaming var_renaming(project);
-        //var_renaming.run();
-
-        timer t;
-        // Here reverse the event function into several versions.
-        FuncDeclPairs output = event_processor.processEvent(decl);
-
-        cout << "Time used: " << t.elapsed() << endl;
-        cout << "Event is processed successfully!\n";
-        
-        foreach (FuncDeclPair& func_decl_pair, output)
-        {
-           appendStatement(func_decl_pair.first); 
-           appendStatement(func_decl_pair.second); 
-        }
-    }
-
-    // Declare all stack variables on top of the generated file.
-    vector<SgVariableDeclaration*> stack_decls = event_processor.getAllStackDeclarations();
-    foreach (SgVariableDeclaration* decl, stack_decls)
-        prependStatement(decl);
-
-    popScopeStack();
-
-    // Fix all variable references here.
-    cout << "VarRef fixed: " <<
-            fixVariableReferences(global) << endl;
-    
-
-
-    //generateWholeGraphOfAST("Cong");
-    //generateGraphOfAST(project, "Graph");
-    //cout << "Graph generated successfully!\n";
-
-#endif
-
-
-    AstTests::runAllTests(project);
-
-    cout << "Test Done!\n";
-
-    return backend(project);
+	return backend(project);
 }
