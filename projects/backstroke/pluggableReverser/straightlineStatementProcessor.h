113387c61d6eb57236bda80d52545d453c9205b1
George Vulov <georgevulov@gatech.edu>
2010-09-06 21:43:52 -0400

    Retrofit the straightline statement processor to work with the evaluateCost / generateAST split

--- projects/backstroke/pluggableReverser/straightlineStatementProcessor.h
+++ projects/backstroke/pluggableReverser/straightlineStatementProcessor.h
@@ -7,12 +7,17 @@
 class StraightlineStatementProcessor : public StatementProcessor
 {
 public:
-	virtual std::vector<StatementReversal> process(SgStatement* stmt, const VariableVersionTable& var_table);
+	virtual std::vector<EvaluationResult> evaluate(SgStatement* stmt, const VariableVersionTable& var_table);
+
+	virtual StatementReversal process(SgStatement* stmt, const EvaluationResult& evaluationResult);
+
 
 private:
 
 	/** Process an expression statement by using the first expression handler returning a valid result. */
-	std::vector<StatementReversal> processExpressionStatement(SgExprStatement* stmt, const VariableVersionTable& var_table);
+	std::vector<EvaluationResult> evaluateExpressionStatement(SgExprStatement* stmt, const VariableVersionTable& var_table);
 
-	std::vector<StatementReversal> processBasicBlock(SgBasicBlock* stmt, const VariableVersionTable& var_table);
+
+
+	std::vector<EvaluationResult> evaluateBasicBlock(SgBasicBlock* stmt, const VariableVersionTable& var_table);
 };
