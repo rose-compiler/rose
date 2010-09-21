b352b2ad0cd79f3268adecdb47befdebc35f4aa1
George Vulov <georgevulov@gatech.edu>
2010-09-21 15:48:08 -0400

    (Backstroke) Fix eight-space-tabstop policy checker false positives

--- projects/backstroke/pluggableReverser/eventProcessor.h
+++ projects/backstroke/pluggableReverser/eventProcessor.h
@@ -45,8 +45,8 @@ private:
 	//! Given a statement, return all evaluation results using all statement processors.
 	std::vector<EvaluationResult> evaluateStatement(SgStatement* stmt, const VariableVersionTable& var_table);
 
-	/** Given a set of results, if two results of them have the same variable table, we remove
-	 *  the one which has the higher cost. */
+	//! Given a set of results, if two results of them have the same variable table, we remove
+	//! the one which has the higher cost.
 	std::vector<EvaluationResult> filterResults(const std::vector<EvaluationResult>& results);
 
 	//! The following methods are for expression and statement processors for store and restore.
--- projects/backstroke/pluggableReverser/extractFromUseValueRestorer.h
+++ projects/backstroke/pluggableReverser/extractFromUseValueRestorer.h
@@ -1,25 +1,22 @@
 #ifndef EXTRACTFROMUSEVALUERESTORER_H
-#define	EXTRACTFROMUSEVALUERESTORER_H
+#define EXTRACTFROMUSEVALUERESTORER_H
 
 #include "handlerTypes.h"
 
 class ExtractFromUseValueRestorer : public VariableValueRestorer
 {
 public:
-	/**
-	* Given a variable and a version, returns an expression evaluating to the value of the variable
-	* at the given version.
-	*
-	* @param variable name of the variable to be restored
-	* @param availableVariables variables whos values are currently available
-	* @param definitions the version of the variable which should be restored
-	* @return expessions that when evaluated will produce the desired version of the variable
-	*/
+	//! Given a variable and a version, returns an expression evaluating to the value of the variable
+	//! at the given version.
+	//!
+	//! @param variable name of the variable to be restored
+	//! @param availableVariables variables whos values are currently available
+	//! @param definitions the version of the variable which should be restored
+	//! @return expessions that when evaluated will produce the desired version of the variable
 	virtual std::vector<SgExpression*> restoreVariable(VariableRenaming::VarName variable,
 			const VariableVersionTable& availableVariables, VariableRenaming::NumNodeRenameEntry definitions);
 
 private:
-
 	//! Given a variable used in an expression, and the value of that expresion, restore the value of the variable.
 	//! @param variable the variable whose value should be extracted
 	//! @param availableVariables versions of all the variables currently defined
@@ -30,4 +27,3 @@ private:
 };
 
 #endif	/* EXTRACTFROMUSEVALUERESTORER_H */
-
