ebc86def9baa7ba5c4e4843164419ed2f688f037
Chunhua (Leo) Liao <chunhualiao@gmail.com>
2010-09-27 18:27:29 -0700

    Replaced SgInitializedName with SgVarRefExp for variables used by OpenMP directives Added a test case for traversing variable references within OpenMP directives

---
+++ tests/translatorTests/input_ompVariableCollecting.C
@@ -0,0 +1,18 @@
+/*
+variable references within OpenMP pragma
+*/
+#include <stdio.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+
+void foo()
+{
+  float x;
+#pragma omp parallel private(x)
+  {
+     x=10;
+  }
+}
+
