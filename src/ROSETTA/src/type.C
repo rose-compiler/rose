f2acb1dc6a0451d2658503500f1c7454fd06f85e
Dan Quinlan <dquinlan@llnl.gov>
2010-10-03 10:08:55 -0700

    Added support for kind in SgType (fixed robustness issues via NO_DELETE in ROSETTA specification).

--- src/ROSETTA/src/type.C
+++ src/ROSETTA/src/type.C
@@ -201,11 +201,12 @@ Grammar::setUpTypes ()
      Type.setFunctionSource         ( "SOURCE_ATTRIBUTE_SUPPORT", "../Grammar/Support.code");
 #endif
 
-#if 0
+#if 1
+  // DQ (10/3/2010): Note that without the NO_DELETE the Fortran jacobi.f file will sometimes fail.
   // DQ (10/2/10): This is the better place for the Fortran kind mechanism (only meaningful for Fortran)
   // DQ (9/17/2007): Support for Fortran kind mechanism
-     Type.setDataPrototype("SgExpression*","type_kind","= NULL",
-          NO_CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, NO_TRAVERSAL || TYPE_TRAVERSAL, DEF_DELETE);
+  // Type.setDataPrototype("SgExpression*","type_kind","= NULL", NO_CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, NO_TRAVERSAL || TYPE_TRAVERSAL, DEF_DELETE);
+     Type.setDataPrototype("SgExpression*","type_kind","= NULL", NO_CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, NO_TRAVERSAL || TYPE_TRAVERSAL, NO_DELETE);
 #endif
      
 #ifdef BUILD_X_VERSION_TERMINALS
@@ -529,7 +530,7 @@ Grammar::setUpTypes ()
 					NO_CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, NO_TRAVERSAL, NO_DELETE);
 #endif
 
-#if 1
+#if 0
   // DQ (10/2/2010): The Fortran specific support for kind has been moved to the base class because so
   // many types in Fortran can have a kind parameter that is better to represent it in the base class.
   // DQ (12/1/2007): Support for Fortran kind mechanism (moved from SgType to SgModifierType)
