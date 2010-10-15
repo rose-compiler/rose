9297fa65f2af50dc157624825b36b145086f6236
Dan Quinlan <dquinlan@llnl.gov>
2010-10-14 20:05:24 -0700

    Removed rose_config.h from header file system and moved for required source files (avoids macro conflicts e.g. PACKAGE_BUGREPORT).

--- src/frontend/OpenFortranParser_SAGE_Connection/FortranParserActionROSE.C
+++ src/frontend/OpenFortranParser_SAGE_Connection/FortranParserActionROSE.C
@@ -1,23 +1,12 @@
-/*******************************************************************************
- * Copyright (c) 2005, 2006 Los Alamos National Security, LLC.
- * This material was produced under U.S. Government contract DE-AC52-06NA25396
- * for Los Alamos National Laboratory (LANL), which is operated by the Los Alamos
- * National Security, LLC (LANS) for the U.S. Department of Energy. The U.S. Government has
- * rights to use, reproduce, and distribute this software. NEITHER THE
- * GOVERNMENT NOR LANS MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
- * ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. If software is modified
- * to produce derivative works, such modified software should be clearly marked,
- * so as not to confuse it with the version available from LANL.
- *
- * Additionally, this program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
 
 #include "sage3basic.h"
 #include "fortran_support.h"
 #include "FortranParserState.h"
+
+// DQ (10/14/2010):  This should only be included by source files that require it.
+// This fixed a reported bug which caused conflicts with autoconf macros (e.g. PACKAGE_BUGREPORT).
+// Interestingly it must be at the top of the list of include files.
+#include "rose_config.h"
 
 #define SKIP_C_ACTION_IMPLEMENTATION 0
 
