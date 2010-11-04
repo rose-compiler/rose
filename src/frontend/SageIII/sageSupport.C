rose-compiler/rose

commit 0d6d719c224f6cee212961811b5becdc9746073d

(Windows) Commented out code dealing with archives
I recently modified this code to generate a temporary directory
based on the process ID. However, this code would not have worked
in its previous form either, so I replaced the whole thing with
an always-failing assertion on Windows.

src/frontend/SageIII/sageSupport.C
@@ -2885,6 +2885,14 @@ determineFileType ( vector<string> argv, int & nextErrorCode, SgProject* project
 
                                         if (isLibraryArchive == true)
                                            {
+#ifdef _MSC_VER
+                                               /* The following block of code deals with *.a library archives files found on
+                                                * Unix systems. I added better temporary file and directory names, but this
+                                                * block of code also has commands that likely won't run on Windows
+                                                * systems, so I'm commenting out the whole block. [RPM 2010-11-03] */
+                                               ROSE_ASSERT(!"Windows not supported");
+#else
+
                                           // This is the case of processing a library archive (*.a) file. We want to process these files so that 
                                           // we can test the library identification mechanism to build databases of the binary functions in 
                                           // libraries (so that we detect these in staticaly linked binaries).
@@ -2945,6 +2953,7 @@ determineFileType ( vector<string> argv, int & nextErrorCode, SgProject* project
                                           // ROSE_ASSERT(false);
 #endif
                                            }
+#endif /* _MSC_VER */
 #if 0
                                         printf ("Processed as a binary file! \n");
 #endif

commit c6dbc2214feb9450268138297c18e0684dc216db

(Windows) Fixed error introduced by 0d6d719c224f6cee212961811b5becdc9746073d

src/frontend/SageIII/sageSupport.C
@@ -2952,8 +2952,8 @@ determineFileType ( vector<string> argv, int & nextErrorCode, SgProject* project
                                              printf ("Exiting in processing a library archive file. \n");
                                           // ROSE_ASSERT(false);
 #endif
-                                           }
 #endif /* _MSC_VER */
+                                           }
 #if 0
                                         printf ("Processed as a binary file! \n");
 #endif