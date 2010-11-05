66a9d7c5c1449591cc7e79d53185d088efa86e0f
Dan Quinlan <dquinlan@llnl.gov>
2010-11-04 17:08:18 -0700

    Initial instruction trace reading support.

--- projects/traceAnalysis/trace.C
+++ projects/traceAnalysis/trace.C
@@ -20,6 +20,15 @@
 
  */
 
+// #define __STDC_FORMAT_MACROS
+#include <fcntl.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+
 using namespace std;
 using namespace SageInterface;
 using namespace SageBuilder;
@@ -74,11 +83,22 @@ struct trace_file
 }
 #endif
 
+
 // Read the trace file (using example).
-void
-readTraceFile ( const string & traceFileName )
+// void readTraceFile ( const string & traceFileName );
+// read_file(const string & traceFileName, size_t *file_sz)
+vector<unsigned char>
+read_file(const string & traceFileName)
    {
      const size_t MAX_INST_SIZE = 10;
+     uint64_t instructionAddress = 0;
+
+     SgAsmGenericFile *file = new SgAsmGenericFile();
+     SgAsmPEFileHeader *pe = new SgAsmPEFileHeader(file);
+     Disassembler *disassembler = Disassembler::lookup(pe)->clone();
+     ROSE_ASSERT(disassembler!=NULL);
+
+     vector<unsigned char> opCodeBuffer;
 
      printf ("Opening trace file = %s \n",traceFileName.c_str());
      FILE * tf = fopen(traceFileName.c_str(), "rb");
@@ -170,6 +190,20 @@ readTraceFile ( const string & traceFileName )
                     printf("%x ", inst_bytes[j]);
                   }
                printf("\n");
+
+            // insnData.second = isSgAsmx86Instruction(disassembler->disassembleOne(&insnBuf[0], addr, insnBuf.size(), addr, NULL));
+            // SgAsmx86Instruction* instruction = disassembler->disassembleOne(&insnBuf[0], addr, insnBuf.size(), addr, NULL);
+               SgAsmx86Instruction* instruction = isSgAsmx86Instruction(disassembler->disassembleOne(inst_bytes, instructionAddress, inst_size, instructionAddress, NULL));
+               ROSE_ASSERT(instruction != NULL);
+
+            // Increment the instruction address as though it were a linear stream of unique instructions (likely it is not but handle that detail later).
+               instructionAddress += inst_size;
+
+            // string stringifiedInstruction = instruction->unparseToString();
+               string stringifiedInstruction = instruction->get_mnemonic();
+
+               printf ("\tstringifiedInstruction = %s \n",stringifiedInstruction.c_str());
+
                fread(&eflags, 4, 1, tf);
                fread(&eax,4, 1, tf);
                fread(&ebx,4, 1, tf);
@@ -194,10 +228,58 @@ readTraceFile ( const string & traceFileName )
 
      printf ("Closing file: %s \n",traceFileName.c_str());
      fclose(tf);
+
+     return opCodeBuffer;
    }
 
+#if 0
+/* Read our example file into a memory buffer.  The file contains only the instructions and no PE file header or other
+ * container information. */
+static const unsigned char*
+OLD_read_file(const char* filename, size_t *file_sz)
+{
+    int fd = open(filename, O_RDONLY);
+    if (fd<0) {
+        perror(filename);
+        exit(1);
+    }
 
-int main( int argc, char * argv[] )
+    struct stat sb;
+    if (fstat(fd, &sb)<0) {
+        perror("fstat");
+        exit(1);
+    }
+
+    const unsigned char* buf = (const unsigned char*)mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (!buf) {
+        perror("mmap");
+        exit(1);
+    }
+
+    close(fd);
+    *file_sz = sb.st_size;
+    return buf;
+}
+#endif
+
+/* Define our own partitioner that doesn't do any real function detection analysis. It just takes all the instructions and
+ * stuffs them into a single function. */
+class MyPartitioner: public Partitioner
+   {
+     public:
+          SgAsmBlock* partition(const Disassembler::InstructionMap& insns, rose_addr_t entry_va, const std::string& name)
+             {
+               clear();
+               set_search(SgAsmFunctionDeclaration::FUNC_DEFAULT & ~SgAsmFunctionDeclaration::FUNC_LEFTOVERS);
+               add_instructions(insns);
+               add_function(entry_va, SgAsmFunctionDeclaration::FUNC_ENTRY_POINT, name);
+               analyze_cfg();
+               return build_ast();
+             }
+   };
+
+int
+main(int argc, char* argv[])
    {
      if (argc != 2)
         {
@@ -209,20 +291,80 @@ int main( int argc, char * argv[] )
      string traceFileName = argv[1];
 
   // Read the trace file...
-     readTraceFile(traceFileName);
+  // readTraceFile(traceFileName);
+     vector<unsigned char> opCodeBuffer = read_file(traceFileName);
 
+  // Code modified from disassembleBuffer.C
+#if 0
+     if (argc<3) {
+        fprintf(stderr, "usage: %s FILENAME START_ADDR [ENTRY_ADDR]\n", argv[0]);
+        exit(1);
+     }
+#endif
 
 #if 0
-  // Generate the ROSE AST.
-     SgProject* project = frontend(argc,argv);
+  // This is a demonstration of how to build the build the trace into an executable (ELF first an then later a PE executable).
 
-  // AST consistency tests (optional for users, but this enforces more of our tests)
-     AstTests::runAllTests(project);
+     const char* filename = argv[1];
+     uint64_t start_va = strtoll(argv[2], NULL, 0);
+     uint64_t entry_va = argc>3 ? strtol(argv[3], NULL, 0) : start_va;
 
-     NopTransform t;
-     t.traverse(project,preorder);
+  /* Read the supplied file into our memory, and then tell ROSE how that memory would have been mapped into
+   * the process' memory by the loader. In this case, we just map the entire buffer to a contiguous region
+   * of the process starting at the address specified on the command line. */
+     size_t buf_sz;
+  // const unsigned char *buf = read_file(filename, &buf_sz);
+     vector<unsigned char> opCodeBuffer = read_file(filename);
+     const unsigned char *buf = &(opCodeBuffer[0]);
 
-  // regenerate the original executable.
-     return backend(project);
+     MemoryMap::MapElement melmt(start_va, buf_sz, buf, 0, MemoryMap::MM_PROT_READ|MemoryMap::MM_PROT_EXEC);
+     melmt.set_name(filename);
+     MemoryMap mm;
+     mm.insert(melmt);
+
+  /* Create the SgAsmGenericFile object that will be needed later. We could have used its parse() method to read the
+   * contents of our file, but this example's purpose is to clearly demonstrate how to disassemble a buffer that might not
+   * have even come from a file. */
+     SgAsmGenericFile *file = new SgAsmGenericFile();
+
+  /* In order to disassemble, we need a Disassembler object.  The most convenient way to get one is to ask the Disassembler
+   * class to look up a suitable disassembler from those which have been registered.  We then clone that disassembler so
+   * that any changes we make (like changing its search heuristics) are local.  We need to provide information about
+   * what kind of instructions will be disassembled, which is done through the instruction set architecutre (InsSetArchecture)
+   * data member of an SgAsmGenericHeader object. Fortunately, the SgAsmPEFileHeader subclass' constructor fills in
+   * reasonable values for the instruction set (i.e., 32-bit x86). */
+     SgAsmPEFileHeader *pe = new SgAsmPEFileHeader(file);
+     Disassembler *d = Disassembler::lookup(pe)->clone();
+     d->set_search(Disassembler::SEARCH_FOLLOWING | Disassembler::SEARCH_DEADEND);
+    
+  /* Disassemble the mapped buffer. The last two arguments are optional, but we show how to use them here. */
+     Disassembler::BadMap errors;
+     Disassembler::AddressSet successors;
+     Disassembler::InstructionMap insns = d->disassembleBuffer(&mm, entry_va, &successors, &errors);
+     printf("Disassembled %zu instruction%s\n", insns.size(), 1==insns.size()?"":"s");
+
+  /* Report about any disassembly errors. */
+     for (Disassembler::BadMap::iterator ei=errors.begin(); ei!=errors.end(); ++ei)
+          printf("Error at 0x%08"PRIx64": %s\n", ei->second.ip, ei->second.mesg.c_str());
+
+  /* Report which additional addresses the disassembler would like to have seen. */
+     fputs("Successors:", stdout);
+     if (!successors.empty()) {
+         for (Disassembler::AddressSet::iterator si=successors.begin(); si!=successors.end(); ++si)
+             printf(" 0x%08"PRIx64, *si);
+         fputs("\n\n", stdout);
+     } else {
+         fputs(" buffer is self contained.\n\n", stdout);
+     }
+
+  /* A partitioner can reorganize the instructions into an AST if you desire.  This is necessary if you plan to use any
+   * ROSE's analysis or output functions since they operate exclusively on the tree representation. */
+     SgAsmBlock *block = MyPartitioner().partition(insns, entry_va, "entry_function");
+
+  /* Produce human-readable output.  The output can be customized by subclassing AsmUnparser (see disassemble.C for an
+   * example). This method of output is also more efficient than calling the old unparseAsmStatement() since there's no need
+   * to buffer the string representation in memory first. */
+     AsmUnparser().unparse(std::cout, block);
 #endif
    }
+
