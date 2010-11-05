c5551877191e58d59b1a3d91ebd26f3dc89c5dcd
Thomas Panas <panas@llnl.gov>
2010-11-04 17:32:36 -0700

    bugfix in RTED

--- projects/RTED/RtedTransformation.h
+++ projects/RTED/RtedTransformation.h
@@ -337,6 +337,7 @@ public:
    // PUBLIC FUNCTIONS ------------------------------------------------------------
    // Insert Header Files
    void insertProlog(SgProject* proj);
+   void visit (SgNode* node);
 
    // analyse file and apply necessary (call) transformations
    void transform(SgProject* project, std::set<std::string> &rtedfiles);
