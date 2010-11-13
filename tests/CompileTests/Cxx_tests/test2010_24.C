b2c7e6a0948e5b20a67e0244e582331d85427129
Chunhua (Leo) Liao <chunhualiao@gmail.com>
2010-11-12 17:08:17 -0800

    The fix to multiple alias namespace declarations is still not finished. Updated the test case to reflect the simple case which can pass for now.

--- tests/CompileTests/Cxx_tests/test2010_24.C
+++ tests/CompileTests/Cxx_tests/test2010_24.C
@@ -1,4 +1,15 @@
+/* 
+ * A test case from John Jacobson (etlverified@gmail.com)
+ *
+ * 11/12/2010
+ * */
+#if 0
+// Still fails 
 #include <vector>
-
+#else
+namespace std
+{
+};
+#endif
 namespace s = std;
-namespace s = std;
+   namespace s = std;
