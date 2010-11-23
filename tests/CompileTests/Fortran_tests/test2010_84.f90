af09561bb3a481d7569405e5dc0e6e4d1126bec6
Dan Quinlan <dquinlan@llnl.gov>
2010-11-23 07:43:46 -0800

    Force build on Hudson.

--- tests/CompileTests/Fortran_tests/test2010_84.f90
+++ tests/CompileTests/Fortran_tests/test2010_84.f90
@@ -1,10 +1,6 @@
+   integer mul
    integer multg
-   do mul = 1, multg
-      if( 1.eq.0 ) then
-          write(nfile(1),'(a23,i3,a3,100a1)') &
-     &      ' ---- multigrid level (', mul, ' ) ',('-',i=1,40)
-          write(nfile(2),'(a23,i3,a3,100a1)') &
-     &      ' ---- multigrid level (', mul, ' ) ',('-',i=1,40)
-      endif
-   enddo
+!  write(nfile(1),'(a23,i3,a3,100a1)') ' ---- multigrid level (', mul, ' ) ',('-',i=1,40)
+!  write(nfile(1),'(a23,i3,a3,100a1)') , mul, ' ) ',('-',i=1,40)
+   write(nfile(1),'(a23,i3,a3,100a1)') ' ---- multigrid level (', mul, ' ) ',('-',i=1,40)
 end
