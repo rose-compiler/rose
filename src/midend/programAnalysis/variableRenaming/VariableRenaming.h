93a80a453d8523fa2b8aca52a689e0a92b47bb87
George Vulov <georgevulov@gatech.edu>
2010-12-13 01:51:04 -0500

    (VariableRenaming) Big change in the way defs and uses are propagated 1. Defs no longer get converted to uses when propagated up the tree.    So, a statement such as (a = b) contains no uses of a 2. Uses propagate all the way up until a statement is reached. Previously,    uses were cut off at assignments.
    The new local def/use information correctly handles assignments to comma ops, nested assignments, etc. It also does not have spurious uses for variables that are only defined.

--- src/midend/programAnalysis/variableRenaming/VariableRenaming.h
+++ src/midend/programAnalysis/variableRenaming/VariableRenaming.h
@@ -326,7 +326,7 @@ private:
      * @param memberRefInserted Reference that indicates whether the function back-inserted a definition.
      * @return Whether the defs were different from those already on the node.
      */
-    bool mergeDefs(cfgNode curNode, bool *memberRefInserted);
+    bool mergeDefs(cfgNode curNode, bool *memberRefInserted, NodeVec &changedNodes);
 
     /** Called to update the uses on the current node.
      *
@@ -906,123 +906,6 @@ private:
         void setRefs(const std::vector<SgNode*>& newRefs) { refs.assign(newRefs.begin(), newRefs.end()); }
     };
 
-    /** Attribute that describes the variables modified by a given expression.
-     */
-    class VarDefUseSynthAttr
-    {
-    private:
-        /** Stores all of the varRefs that are defined in the current subtree.
-         */
-        std::vector<SgNode*> defs;
-
-        /** Stores all the varRefs that are used in the current subTree.
-         */
-        std::vector<SgNode*> uses;
-
-    public:
-        /** Create the attribute with no refs.
-         */
-        VarDefUseSynthAttr():defs(), uses(){}
-
-        /** Create the attribute with specified def/use.
-         *
-         * @param defNode The node to add to the list of defs, or NULL
-         * @param useNode The node to add to the list of uses, or NULL
-         */
-        VarDefUseSynthAttr(SgNode* defNode, SgNode* useNode)
-        {
-            if(defNode)
-                defs.push_back(defNode);
-
-            if(useNode)
-                uses.push_back(useNode);
-        }
-
-        /** Create the attribute with the list of defs and the use.
-         *
-         * @param defTree The vector of defs to add, or an empty vector.
-         * @param useNode The node to add to the list of uses, or NULL.
-         */
-        VarDefUseSynthAttr(const std::vector<SgNode*>& defTree, SgNode* useNode)
-        {
-            if(defTree.size() > 0)
-                defs.assign(defTree.begin(), defTree.end());
-
-            if(useNode)
-                uses.push_back(useNode);
-        }
-
-        /** Create the attribute with the def and list of uses.
-         *
-         * @param defNode The node to add to the list of defs, or NULL.
-         * @param useTree The vector of uses to add, or an empty vector.
-         */
-        VarDefUseSynthAttr(SgNode* defNode, const std::vector<SgNode*>& useTree)
-        {
-            if(useTree.size() > 0)
-                uses.assign(useTree.begin(), useTree.end());
-
-            if(defNode)
-                defs.push_back(defNode);
-        }
-
-        /** Create the attribute with the provided uses and defs.
-         *
-         * @param defTree The defs to use in this node, or empty vector.
-         * @param useTree The uses to use in this node, or empty vector.
-         */
-        VarDefUseSynthAttr(const std::vector<SgNode*>& defTree, const std::vector<SgNode*>& useTree)
-        {
-
-            if(defTree.size() > 0)
-                defs.assign(defTree.begin(), defTree.end());
-
-            if(useTree.size() > 0)
-                uses.assign(useTree.begin(), useTree.end());
-        }
-
-        /** Get the references for this node and below.
-         *
-         * @return A constant reference to the ref list.
-         */
-        std::vector<SgNode*>& getDefs() { return defs; }
-
-        /** Set the defs for this node and below.
-        *
-        * @param newDefs A constant reference to the defs to copy to this node.
-        */
-        void setDefs(const std::vector<SgNode*>& newDefs) { defs.assign(newDefs.begin(), newDefs.end()); }
-
-         /** Get the uses for this node and below.
-         *
-         * @return A constant reference to the use list.
-         */
-        std::vector<SgNode*>& getUses() { return uses; }
-
-        /** Set the uses for this node and below.
-        *
-        * @param newUses A constant reference to the uses to copy to this node.
-        */
-        void setUses(const std::vector<SgNode*>& newUses) { uses.assign(newUses.begin(), newUses.end()); }
-    };
-
-    class VarDefUseTraversal : public AstBottomUpProcessing<VariableRenaming::VarDefUseSynthAttr>
-    {
-        VariableRenaming* varRename;
-    public:
-        VarDefUseTraversal(VariableRenaming* varRenaming):varRename(varRenaming){}
-
-        /** Called to evaluate the synthesized attribute on every node.
-         *
-         * This function will handle passing all variables that are defined and used by a given operation.
-         *
-         * @param node The node being evaluated.
-         * @param attr The attributes from the child nodes.
-         * @return The attribute at this node.
-         */
-        virtual VariableRenaming::VarDefUseSynthAttr evaluateSynthesizedAttribute(SgNode* node, SynthesizedAttributesList attrs);
-    };
-
     /** Class to traverse the AST and assign unique names to every varRef.
      */
     class UniqueNameTraversal : public AstBottomUpProcessing<VariableRenaming::VarRefSynthAttr>
@@ -1052,7 +935,94 @@ private:
          */
         virtual VariableRenaming::VarRefSynthAttr evaluateSynthesizedAttribute(SgNode* node, SynthesizedAttributesList attrs);
     };
-    
+
+	/** Attribute that describes the variables used by a given expression. */
+	class ChildUses
+	{
+	private:
+		/** An assignment to the current expression in the AST would define this variable */
+		SgVarRefExp* currentVar;
+
+		/** Stores all the varRefs that are used in the current subTree. */
+		std::vector<SgNode*> uses;
+
+	public:
+
+		/** Create the attribute with no refs. 	 */
+		ChildUses() : currentVar(NULL)
+		{ }
+
+		ChildUses(SgNode* useNode, SgVarRefExp* var)
+		{
+			uses.push_back(useNode);
+			currentVar = var;
+		}
+
+		/** Create the attribute with the def and list of uses.
+		 *
+		 * @param useTree The vector of uses to add, or an empty vector.
+		 */
+		ChildUses(const std::vector<SgNode*>& useTree, SgVarRefExp* var = NULL)
+		{
+			if (useTree.size() > 0)
+				uses.assign(useTree.begin(), useTree.end());
+			currentVar = var;
+		}
+
+		/** Get the uses for this node and below.
+		 *
+		 * @return A constant reference to the use list.
+		 */
+		std::vector<SgNode*>& getUses()
+		{
+			return uses;
+		}
+
+		/** Set the uses for this node and below.
+		 *
+		 * @param newUses A constant reference to the uses to copy to this node.
+		 */
+		void setUses(const std::vector<SgNode*>& newUses)
+		{
+			uses.assign(newUses.begin(), newUses.end());
+		}
+
+		SgVarRefExp* getCurrentVar() const
+		{
+			return currentVar;
+		}
+	};
+
+	/** This class collects all the defs and uses associated with each node in the traversed CFG.
+	 * Note that this does not compute reachability information; it just records each instance of
+	 * a variable used or defined. */
+	class DefsAndUsesTraversal : public AstBottomUpProcessing<ChildUses>
+	{
+		VariableRenaming* ssa;
+
+	public:
+
+		DefsAndUsesTraversal(VariableRenaming* ssa) : ssa(ssa) { }
+
+		/** Called to evaluate the synthesized attribute on every node.
+		 *
+		 * This function will handle passing all variables that are defined and used by a given operation.
+		 *
+		 * @param node The node being evaluated.
+		 * @param attr The attributes from the child nodes.
+		 * @return The attribute at this node.
+		 */
+		virtual ChildUses evaluateSynthesizedAttribute(SgNode* node, SynthesizedAttributesList attrs);
+
+	private:
+
+		/** Mark all the uses as occurring at the specified node. */
+		void addUsesToNode(SgNode* node, std::vector<SgNode*> uses);
+
+		/** Mark the given variable as being defined at the node. */
+		void addDefForVarAtNode(SgVarRefExp* currentVar, SgNode* defNode);
+	};
+
 };
 
 #endif	/* SSAANALYSIS_H */
