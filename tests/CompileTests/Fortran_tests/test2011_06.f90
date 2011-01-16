a0fc21613157a45be4d9e1ec3fb90d08dfc23485
Dan Quinlan <dquinlan@llnl.gov>
2011-01-15 17:33:14 -0800

    Commit being forced by "smgit pull" (this is a problem)... Dan

---
+++ tests/CompileTests/Fortran_tests/test2011_06.f90
@@ -0,0 +1,6 @@
+! Pointer to array of real
+! real, pointer, dimension(:) :: g
+! Array of pointer to real (not unparsed correctly)
+! -- unparsed as: real, dimension(:) :: h
+  real, dimension(:), pointer :: h
+end
