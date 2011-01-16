892b7f0d95b14d509b237227dcceeeda4d7b728a
Dan Quinlan <dquinlan@llnl.gov>
2011-01-16 15:26:25 -0800

    Fixed specific test codes to define unique module names (to avoid colision in parallel testing).

--- tests/CompileTests/Fortran_tests/test2010_02.f90
+++ tests/CompileTests/Fortran_tests/test2010_02.f90
@@ -1,5 +1,5 @@
-module precision
+module precision_02
    ! Example of problem code from mpich2 mpif.h header file.
      integer*8 MPI_DISPLACEMENT_CURRENT
      REAL*8 MPI_WTIME, MPI_WTICK
-end module precision
+end module precision_02
