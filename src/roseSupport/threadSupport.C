e56610083a535627391f40f7271723d16ae711f0
Robb Matzke <matzke1@llnl.gov>
2011-02-25 19:37:30 -0500

    (Threads) Added multi-threaded messaging support

--- src/roseSupport/threadSupport.C
+++ src/roseSupport/threadSupport.C
@@ -1,5 +1,10 @@
 #include "threadSupport.h"
+#include <stdarg.h>
 #include <string.h>
+
+/******************************************************************************************************************************
+ *                                      Read-write locks
+ ******************************************************************************************************************************/
 
 int
 RTS_rwlock_init(RTS_rwlock_t *lock, pthread_rwlockattr_t *attrs)
@@ -88,4 +93,264 @@ RTS_rwlock_unlock(RTS_rwlock_t *lock)
     }
 
     return 0;
+}
+
+
+
+/******************************************************************************************************************************
+ *                                      Message output
+ ******************************************************************************************************************************/
+
+RTS_rwlock_t RTS_Message::rwlock = RTS_RWLOCK_INITIALIZER;
+RTS_Message *RTS_Message::in_multi = NULL;
+bool RTS_Message::sol = true;
+
+void
+RTS_Message::ctor()
+{}
+
+void
+RTS_Message::dtor()
+{
+    RTS_WRITE(rwlock) {
+        if (in_multi==this) {
+            if (f)
+                fputc('\n', f);
+            in_multi = NULL;
+            sol = true;
+        }
+    } RTS_WRITE_END;
+    delete[] buffer;
+}
+
+void
+RTS_Message::terminate()
+{
+    /* Internal functions assume that lock is already acquired. */
+    if (f && !sol) {
+        if (in_multi!=NULL) {
+            if (!in_multi->name.empty()) {
+                fprintf(f, " <%s continued below>\n", in_multi->name.c_str());
+            } else {
+                fputs(" <continued below>\n", f);
+            }
+            in_multi->interrupted = true;
+        } else {
+            fputc('\n', f);
+        }
+        in_multi = NULL;
+        sol = true;
+    }
+}
+
+void
+RTS_Message::prefix()
+{
+    /* Internal functions assume that lock is already acquired. */
+    if (f && p)
+        (*p)(f);
+}
+
+void
+RTS_Message::output_lines(const char *s)
+{
+    /* Internal functions assume that lock is already acquired. */
+    while (s && *s) {
+        if (sol)
+            prefix();
+        char *t = strchr(s, '\n');
+        if (t) {
+            fwrite(s, (t+1)-s, 1, f);
+            sol = true;
+        } else {
+            fputs(s, f);
+            sol = false;
+        }
+        s = t ? t+1 : NULL;
+    }
+    if (sol)
+        in_multi = false;
+}
+
+void
+RTS_Message::format(const char *fmt, va_list ap)
+{
+    if (!buffer) {
+        bufsz = 128;
+        buffer = new char[bufsz];
+    }
+    size_t nchars = vsnprintf(buffer, bufsz, fmt, ap);
+    if (nchars >= bufsz) {
+        bufsz = std::max(2*bufsz, nchars+1);
+        delete[] buffer;
+        buffer = new char[bufsz];
+        nchars = vsnprintf(buffer, bufsz, fmt, ap);
+        assert(nchars < bufsz);
+    }
+}
+
+RTS_Message &
+RTS_Message::multipart(const std::string &name, const char *fmt, ...)
+{
+    if (!f)
+        return *this;
+
+    va_list ap;
+    va_start(ap, fmt);
+    format(fmt, ap);
+    va_end(ap);
+    if (0==bufsz || !buffer[0])
+        return *this;
+
+    RTS_WRITE(rwlock) {
+        terminate();
+        output_lines(buffer);
+        this->name = name;
+        in_multi = sol ? NULL : this;
+        interrupted = false;
+    } RTS_WRITE_END;
+    return *this;
+}
+
+RTS_Message &
+RTS_Message::more(const char *fmt, ...)
+{
+    if (!f)
+        return *this;
+
+    va_list ap;
+    va_start(ap, fmt);
+    format(fmt, ap);
+    va_end(ap);
+    if (0==bufsz || !buffer[0])
+        return *this;
+
+    RTS_WRITE(rwlock) {
+        if (in_multi!=this)
+            terminate();
+        if (interrupted) {
+            prefix();
+            fprintf(f, "<%s%sresumed> ", name.c_str(), name.empty()?"":" ");
+            sol = false;
+        }
+        output_lines(buffer);
+        in_multi = sol ? NULL : this;
+    } RTS_WRITE_END;
+    return *this;
+}
+
+RTS_Message &
+RTS_Message::mesg(const char *fmt, ...)
+{
+    if (!f)
+        return *this;
+
+    va_list ap;
+    va_start(ap, fmt);
+    format(fmt, ap);
+    va_end(ap);
+    if (0==bufsz || !buffer[0])
+        return *this;
+
+    RTS_WRITE(rwlock) {
+        terminate();
+        output_lines(buffer);
+        terminate();
+        in_multi = false;
+    } RTS_WRITE_END;
+    return *this;
+}
+
+void
+RTS_Message::brief_begin(const char *fmt, ...)
+{
+    if (!f)
+        return;
+
+    va_list ap;
+    va_start(ap, fmt);
+    format(fmt, ap);
+    va_end(ap);
+
+    char *eol = buffer ? strchr(buffer, '\n') : NULL;
+    if (eol)
+        *eol = '\0';
+
+    /* Obtain lock; to be released by brief_end() */
+    int err = RTS_rwlock_wrlock(&rwlock);
+    assert(0==err);
+
+    fputc('[', f);
+    if (in_multi!=this)
+        prefix();
+    if (buffer)
+        fputs(buffer, f);
+    sol = false;
+}
+
+void
+RTS_Message::brief_end(const char *fmt, ...)
+{
+    if (!f)
+        return;
+    
+    va_list ap;
+    va_start(ap, fmt);
+    format(fmt, ap);
+    va_end(ap);
+
+    char *eol = buffer ? strchr(buffer, '\n') : NULL;
+    if (eol)
+        *eol = '\0';
+
+    /* Lock was acquired by brief_begin() */
+    if (buffer)
+        fputs(buffer, f);
+    fputc(']', f);
+    sol = false;
+
+    int err = RTS_rwlock_unlock(&rwlock);
+    assert(0==err);
+}
+
+RTS_Message &
+RTS_Message::brief(const char *fmt, ...)
+{
+    if (!f)
+        return *this;
+
+    va_list ap;
+    va_start(ap, fmt);
+    format(fmt, ap);
+    va_end(ap);
+    if (0==bufsz || !buffer[0])
+        return *this;
+
+    char *eol = strchr(buffer, '\n');
+    if (eol)
+        *eol = '\0';
+
+    RTS_WRITE(rwlock) {
+        fputc('[', f);
+        if (in_multi!=this)
+            prefix();
+        fputs(buffer, f);
+        fputc(']', f);
+        sol = false;
+    } RTS_WRITE_END;
+    return *this;
+}
+
+int
+RTS_Message::lock()
+{
+    return RTS_rwlock_wrlock(&rwlock);
+}
+
+int
+RTS_Message::unlock(bool sol)
+{
+    this->sol = sol;
+    in_multi = sol ? NULL : this;
+    return RTS_rwlock_unlock(&rwlock);
 }
