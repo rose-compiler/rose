9c9c9b6ceb630c3183543898ef21f0e06e87505c
Robb Matzke <matzke1@llnl.gov>
2011-03-10 11:08:58 -0500

    (BinaryLoader) Uses new get_base_va() functionality
    This allows remap() to operate on file headers.

--- src/frontend/BinaryLoader/BinaryLoader.C
+++ src/frontend/BinaryLoader/BinaryLoader.C
@@ -342,17 +342,17 @@ BinaryLoader::remap(MemoryMap *map, SgAsmGenericHeader *header)
 
             if (debug) {
                 fprintf(debug, "  mapping section [%d] \"%s\"", section->get_id(), section->get_name()->c_str());
-                if (section->get_header()->get_base_va()!=0)
-                    fprintf(debug, " with base va 0x%08"PRIx64, section->get_header()->get_base_va());
+                if (section->get_base_va()!=0)
+                    fprintf(debug, " with base va 0x%08"PRIx64, section->get_base_va());
                 fprintf(debug, "\n");
                 fprintf(debug, "    Specified RVA:       0x%08"PRIx64" + 0x%08"PRIx64" bytes = 0x%08"PRIx64"\n",
                         section->get_mapped_preferred_rva(), section->get_mapped_size(),
                         section->get_mapped_preferred_rva()+section->get_mapped_size());
-                if (section->get_header()->get_base_va()!=0) {
+                if (section->get_base_va()!=0) {
                     fprintf(debug, "    Specified  VA:       0x%08"PRIx64" + 0x%08"PRIx64" bytes = 0x%08"PRIx64"\n",
-                            section->get_header()->get_base_va() + section->get_mapped_preferred_rva(),
+                            section->get_base_va() + section->get_mapped_preferred_rva(),
                             section->get_mapped_size(),
-                            (section->get_header()->get_base_va() + section->get_mapped_preferred_rva() +
+                            (section->get_base_va() + section->get_mapped_preferred_rva() +
                              section->get_mapped_size()));
                 }
                 fprintf(debug, "    Specified offset:    0x%08"PRIx64" + 0x%08"PRIx64" bytes = 0x%08"PRIx64"\n",
@@ -383,7 +383,7 @@ BinaryLoader::remap(MemoryMap *map, SgAsmGenericHeader *header)
                             malign_lo, malign_hi, falign_lo, falign_hi);
                     fprintf(debug, "    Aligned VA:          0x%08"PRIx64" + 0x%08"PRIx64" bytes = 0x%08"PRIx64,
                             va, mem_size, va+mem_size);
-                    if (section->get_header()->get_base_va()+section->get_mapped_preferred_rva()==va &&
+                    if (section->get_base_va()+section->get_mapped_preferred_rva()==va &&
                         section->get_mapped_size()==mem_size) {
                         fputs(" (no change)\n", debug);
                     } else {
@@ -494,8 +494,9 @@ BinaryLoader::remap(MemoryMap *map, SgAsmGenericHeader *header)
                     a = va + total - offset;
                 }
                 if (debug) {
-                    fprintf(debug, "    %-41s va=0x%08"PRIx64" + 0x%08"PRIx64" = 0x%08"PRIx64"\n",
-                            "Mapping part beyond EOF:", a, n, a+n);
+                    fprintf(debug, "    Mapping part beyond EOF(0x%08"PRIx64"):      "
+                            "va=0x%08"PRIx64" + 0x%08"PRIx64" = 0x%08"PRIx64"\n",
+                            total, a, n, a+n);
                 }
                 MemoryMap::MapElement me(a, n, mapperms);
                 me.set_name(melmt_name);
@@ -691,7 +692,8 @@ BinaryLoader::align_values(SgAsmGenericSection *section, MemoryMap *map,
 {
     ROSE_ASSERT(section!=NULL);
     ROSE_ASSERT(section->is_mapped());
-    SgAsmGenericHeader *header = section->get_header();
+    SgAsmGenericHeader *header = isSgAsmGenericHeader(section);
+    if (!header) header = section->get_header();
     ROSE_ASSERT(header!=NULL);
     
     /* Initial guesses */
