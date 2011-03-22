a95c726e458da01358db85ababb97b98350878b5
Chunhua (Leo) Liao <chunhualiao@gmail.com>
2011-03-22 16:45:36 -0700

    (XOMP): runtime support for Fortran sections

--- tests/CompileTests/OpenMP_tests/section.c
+++ tests/CompileTests/OpenMP_tests/section.c
@@ -6,23 +6,27 @@ int main()
   int known_sum;
   int i;
   int j; 
+  int k = 111;
 #pragma omp parallel
   {
-#pragma omp sections reduction(+:sum) private(i) lastprivate(j)
+#pragma omp sections reduction(+:sum) private(i) lastprivate(j) firstprivate(k)
     {
 #pragma omp section
       {
+        assert (k == 111);
         for (i=1;i<400;i++){
           sum += i;
         }
       }
 #pragma omp section
       {
+        assert (k == 111);
         for(i=400;i<700;i++)
           sum += i;
       }
 #pragma omp section
       {
+        assert (k == 111);
         j = 888;
         for(i=700;i<1000;i++)
           sum += i;
