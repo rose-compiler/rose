f0545294e63ff657e533c8cc15cf83b3d10e6f3d
Robb Matzke <matzke1@llnl.gov>
2011-03-30 12:47:26 -0400

    (Disassembler) LabelMap arg is optional for unparseExpression()

--- src/frontend/SageIII/astVisualization/wholeAST.C
+++ src/frontend/SageIII/astVisualization/wholeAST.C
@@ -1666,7 +1666,7 @@ CustomMemoryPoolDOTGeneration::defaultColorFilter(SgNode* node)
           SgAsmExpression* asmExpression = isSgAsmExpression(node);
           if (asmExpression != NULL)
              {
-               string unparsedExpression = unparseExpression(asmExpression, NULL);
+               string unparsedExpression = unparseExpression(asmExpression);
 
                additionalNodeOptions = "shape=polygon,regular=0,URL=\"\\N\",tooltip=\"more info at \\N\",sides=8,peripheries=2,color=\"blue\",fillcolor=skyblue,fontname=\"7x13bold\",fontcolor=black,style=filled";
                labelWithSourceCode = "\\n  " + unparsedExpression + 
