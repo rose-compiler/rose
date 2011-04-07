8ef77f466e3ddb91f3d58359083b194bd849a77e
Justin Too <too1@llnl.gov>
2011-04-07 16:22:14 -0700

    Changed HUDSON_LDLIBS to retain '$(LIBS)' for poptest Makefile substitution rather than using the Autoconf LIBS in ROSE.

--- tests/RunTests/FortranTests/LANL_POP/hudson_example_setup.sh
+++ tests/RunTests/FortranTests/LANL_POP/hudson_example_setup.sh
@@ -1,3 +1,4 @@
+export TRANSLATOR="/home/too1/local/workspace/rose/osx/install/bin/testTranslator"
 export HUDSON_NUM_JOBS=8
 export HUDSON_FC="/usr/bin/gfortran"
 export HUDSON_FFLAGS="-ffree-line-length-0"
@@ -11,6 +12,8 @@ export HUDSON_LDLIBS="\$(LIBS) -lcurl"
 
 
 export LD_LIBRARY_PATH="/export/tmp.hudson-rose/opt/boost_1_40_0-inst/lib:/home/hudson-rose/programs/opengl/lib:/nfs/apps/java/jdk1.6.0_11/jre/lib/amd64/server:/nfs/apps/java/jdk1.6.0_11/jre/lib/amd64:/nfs/apps/java/jdk1.6.0_11/jre/../lib/amd64:/usr/apps/java/jdk1.6.0_11/jre/lib/amd64/server:$LD_LIBRARY_PATH"
+
+export LD_LIBRARY_PATH="`pwd`/poptest/netcdf-4.1.1/install/lib:$LD_LIBRARY_PATH"
 
 export PATH="/usr/apps/java/jdk1.6.0_11/bin/:/usr/apps/graphviz/2.20.3/bin:/export/tmp.hudson-rose/opt/cmake-2.6.4-inst/bin:/nfs/apps/git/1.6.4/bin:/nfs/apps/subversion/1.6.3/bin:/usr/apps/git/latest/bin:/usr/local/bin:/bin:/usr/bin:/home/hudson-rose/opt/ghc-6.10.4-inst-64bit/bin:$PATH"
 
