66af7c8aff56bdca5fdbdd55dbf73a81ab325154
George Vulov <georgevulov@gatech.edu>
2011-04-20 18:13:39 -0400

    Fix compile error

--- tutorial/classHierarchyGraph.C
+++ tutorial/classHierarchyGraph.C
@@ -1,22 +1,28 @@
-// Example ROSE Preprocessor
-// used for testing ROSE infrastructure
 #include "rose.h"
-#include <CallGraph.h>
+#include <boost/foreach.hpp>
+
+#define foreach BOOST_FOREACH
 
 using namespace std;
 
-int
-main( int argc, char * argv[] )
-   {
-     SgProject* project = new SgProject(argc, argv); 
+int main(int argc, char * argv[])
+{
+	SgProject* project = new SgProject(argc, argv);
 
-     //Construct class hierarchy graph
-     ClassHierarchyWrapper hier( project );
+	//Construct class hierarchy graph
+	ClassHierarchyWrapper hier(project);
 
-     //Output class hierarchy graph to dot
-     AstDOTGeneration dotgen;
-     dotgen.writeIncidenceGraphToDOTFile( hier.getClassHierarchyGraph() , "classHier.dot");
+	//Display the ancestors of each class
+	vector<SgClassDefinition*> allClasses = SageInterface::querySubTree<SgClassDefinition>(project, V_SgClassDefinition);
+	foreach(SgClassDefinition* classDef, allClasses)
+	{
+		printf("\n%s subclasses: ", classDef->get_declaration()->get_name().c_str());
+		foreach(SgClassDefinition* subclass, hier.getSubclasses())
+		{
+			printf("%s, ", subclass->get_declaration()->get_name().c_str());
+		}
+	}
 
-     return 0;
-   }
+	return 0;
+}
 
