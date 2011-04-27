45580c1fccc71fa345fd0168b3815926ea5e76e9
Justin Too <too1@llnl.gov>
2011-04-27 14:40:35 -0700

    Started cleaning up Hudson scripts.

---
+++ scripts/hudson/roseUtilities.sh
@@ -0,0 +1,191 @@
+#!/bin/bash -e
+
+# If non-empty, then trace execution
+debug=
+[ -n "$debug" ] && set -x
+
+###############################################################################################################################
+# Functions
+###############################################################################################################################
+
+# Convert number of seconds to HH:MM:SS
+sec_to_hms () {
+    local sec="$1"
+    printf "%02d:%02d:%02ds" $[sec/3600] $[sec%3600/60] $[sec%60]
+}
+
+# Make output more pretty. Reads standard input, indents and writes to standard output.
+filter () {
+    local start_time=$SECONDS prefix="    "
+    echo
+    echo "********************************************************************************"
+    echo "*** $*"
+    echo "********************************************************************************"
+    while read; do
+        echo "${prefix}$REPLY"
+    done
+}
+
+filterStep () {
+    local start_time=$SECONDS prefix="    "
+    ( echo ) 2>&1 |filter "Starting $* step"
+    while read; do
+        echo "${prefix}$REPLY"
+    done
+    ( echo "Duration: $(sec_to_hms $SECONDS)" ) 2>&1 |filter "Ending $* step"
+    echo "********************************************************************************"
+}
+
+# Clearly indicate death
+killStep () {
+    ( echo ) 2>&1 |filter "FAILED $* step"
+    exit 1
+}
+
+# Restrict 'rm -rf' to be executed only within Hudson's workspace or the MasterInstallTree
+safe_rm_rf () {
+    if [[ "$1" =~ "^/export/tmp.hudson-rose/hudson/workspace/" ]] || [[ "$1" =~ "${HOME}/MasterInstallTree/" ]]; then
+        echo "rm -rf $1"
+    else
+        echo "Error: unsafe rm -rf '$1'"
+        exit 1
+    fi
+}
+
+###############################################################################################################################
+# Functions
+###############################################################################################################################
+
+
+
+#########################################################################################
+#
+#  Dump Hudson environment
+#  This script should be sourced in other Hudson run test scripts to
+#  uniformly dump the Hudson environment.
+#
+
+#  TOO (3/16/2011): Centralized environment information to make it more
+#   useful for debugging
+#  DQ (1/14/2010): Support for dumping the enviroment so that it 
+#   can be easily sourced to permit Hudson tests to be reproduced.
+#
+#########################################################################################
+dumpHudsonEnvironment() {
+#########################################################################################
+set +x
+if test $# = 1 ; then
+  local env_dump_file=$1
+  rm $env_dump_file
+
+  (
+	echo ""
+	echo "This output is dumped in:"   
+	echo ""
+	echo "$machine_name@$env_dump_file"   
+	echo ""
+	echo ""
+	echo "-------------------------------------------------------------------"
+	echo "                                                          [General]"
+	echo ""
+	echo ""
+	echo "machine                           = $machine"
+	echo "machine_os                        = $machine_os"
+	echo "machine_name                      = $machine_name"
+	echo "start_time                        = $start_time"
+	echo "hudson_start_time_seconds         = $hudson_start_time_seconds"
+
+	echo ""
+	echo ""
+	echo "-------------------------------------------------------------------"
+	echo "                                                             [ROSE]"
+	echo ""
+	echo ""
+	set +e
+        env |grep "^ROSE_"
+	set -e
+
+	echo ""
+	echo ""
+	echo "-------------------------------------------------------------------"
+	echo "                                                              [Git]"
+	echo ""
+	echo ""
+	# tps (02/04/2010) : Checking the EDG submodule version
+	echo "git submodule status (EDG version) ="
+	echo "  `git submodule status`"
+
+	echo ""
+	echo "[Last (5) commits]"
+        git log -n5 --pretty=format:'
+          Committed %ar by %an%n
+          %s'
+	echo ""
+	echo "-------------------------------------------------------------------"
+	echo "                                                     [Library Path]"
+	echo ""
+	echo ""
+	if test "x$label" = "x64bit-macos-10.6" ; then
+	 echo "DYLD_LIBRARY_PATH ="
+	 for a_path in `echo $DYLD_LIBRARY_PATH | sed 's/:/ /g'` ; do
+	   echo "    $a_path"
+	 done
+	echo ""
+	 echo "Copy me: export DYLD_LIBRARY_PATH=\"$DYLD_LIBRARY_PATH:\$DYLD_LIBRARY_PATH\""
+	else
+	 echo "LD_LIBRARY_PATH ="
+	 for a_path in `echo $LD_LIBRARY_PATH | sed 's/:/ /g'` ; do
+	   echo "    $a_path"
+	 done
+	echo ""
+	echo "Copy me: export LD_LIBRARY_PATH=\"$LD_LIBRARY_PATH:\$LD_LIBRARY_PATH\""
+	fi
+
+	echo ""
+	echo ""
+	echo "-------------------------------------------------------------------"
+	echo "                                                  [Executable Path]"
+	echo ""
+	echo ""
+	echo "PATH ="
+	 for a_path in `echo $PATH | sed 's/:/ /g'` ; do
+	   echo "    $a_path"
+	 done
+	echo ""
+	echo "Copy me: export PATH=\"$PATH:\$PATH\""
+
+	echo ""
+	echo ""
+	echo "-------------------------------------------------------------------"
+	echo "                                                   [Configure line]"
+	echo ""
+	echo ""
+	echo "./configure"
+	 for an_option in ${CONFIGURE_FLAGS} ; do 
+	   echo "    $an_option"
+	 done
+	echo ""
+	echo "Copy me: ./configure ${CONFIGURE_FLAGS}"
+
+	echo ""
+	echo ""
+	echo "-------------------------------------------------------------------"
+	echo "                                                     [Install tree]"
+	echo ""
+	echo ""
+	echo "CONFIGURE_PREFIX_DIR (--prefix)=" 
+	echo ""
+	echo "    $CONFIGURE_PREFIX_DIR"
+	echo ""
+	echo ""
+
+	echo ""
+	echo ""
+  ) 2>&1 |tee $env_dump_file |filterStep "Hudson environment output"
+  [ ${PIPESTATUS[0]} -ne 0 ] && killStep "Hudson environment output"
+else
+    echo "[Error] Usage: dumpHudsonEnvironment <output-file-name>"
+    exit 1
+fi
+set -x
+}
--- scripts/hudson/runBuildStep.sh
+++ scripts/hudson/runBuildStep.sh
@@ -14,40 +14,31 @@ function runBuildStep {
          $1 == "make-installcheck" -o \
          $1 == "make-check" -o \
          $1 == "make-distcheck" -o \
-         $1 == "make-docs" \) ] ; then 
+         $1 == "make-docs" \) ]
+    then 
         echo "Usage: runBuildStep make|make-install|make-installcheck|make-check|make-distcheck|make-docs [spewAnalysis-outputFile]" 
         exit 1;
-    elif [ $# -eq 2 ]; then
-        outputFile=$2
     fi
     buildStep=`echo $1 | sed 's/-/ /'`
+    outputFile=$2
 
 ####################################################################################
 # EXECUTE BUILD STEP (with optional spew analysis)
 ####################################################################################
     
-    printBannerToStartStep "$buildStep"
-    local start_time_seconds="$(date +%s)"
+  (
 
+      ${buildStep} -j${NUM_PROCESS} 2>&1 | tee $outputFile
 
-    if [ $# -eq 2 ]; then
-        ${buildStep} -j${NUM_PROCESS} 2>&1 | tee $outputFile
-    else
-        ${buildStep} -j${NUM_PROCESS}
-    fi
-    # The pipestatus variable is an array that holds the exit status of your last
-    # foreground pipeline commands. we check to see if make failes through this command
-    if [ ${PIPESTATUS[0]} -ne 0 -o $? -ne 0 ]  ; then
-        printBanner "Fatal error during '${buildStep}'. Aborting..."
-       exit 3
-    fi
+      (
 
-    if [ $# -eq 2 ]; then
-        runSpewAnalysis $outputFile
-    fi
+          [ -n "$outputFile" ] && runSpewAnalysis $outputFile
 
+      ) 2>&1 |func_filterStep "${buildStep} spew analysis" 
+      [ ${PIPESTATUS[0]} -ne 0 -o $? -ne 0 ] && func_killStep "${buildStep} spew analysis"
 
-    printBannerToEndStep "$buildStep" $start_time_seconds 
+  ) 2>&1 |func_filterStep "${buildStep}" 
+  [ ${PIPESTATUS[0]} -ne 0 -o $? -ne 0 ] && func_killStep "${buildStep}"
 }
 
 
@@ -62,27 +53,22 @@ function runSpewAnalysis {
         exit 1;
     fi
 
-    printBannerToStartStep "Compile time spew"
-    local start_time_seconds="$(date +%s)"
-
-
     inputFile="$1"
 
     # Now process to count the number of lines of spew.
     echo "Computing compile time spew"
     if [ $inputFile == "make_check_output.txt" ]; then
-      grep -n "Making check in " $inputFile | ../scripts/checkMakeSpew.pl | tee makeSpewLineCount.txt
-    else      filteredFile="filtered_$inputFile"
+        grep -n "Making check in " $inputFile | ../scripts/checkMakeSpew.pl | tee makeSpewLineCount.txt
+    else
+        filteredFile="filtered_$inputFile"
 
-      # Filter out the legitimate lines specific to compilation and linking (filter compile, links, and Qt specific tools).
-      echo "Building the $filteredFile file"
-      grep -v "Entering directory" $inputFile | grep -v "Leaving directory" | grep -v "COMPILE" | grep -v "LINK" | grep -v "Qt-MOC" | grep -v "Qt-RCC" | grep -v "Qt-UIC" > $filteredFile
-      echo "Built $filteredFile"
-      grep -n "Making all in " $filteredFile | ../scripts/checkMakeSpew.pl | tee makeSpewLineCount.txt
-      # DQ (10/2/2010): Added test for parallelism of build system.
-      echo "Measure Build System Parallelism"
-      ../scripts/checkMakeParallelism.sh ${inputFile}
+        # Filter out the legitimate lines specific to compilation and linking (filter compile, links, and Qt specific tools).
+        echo "Building the $filteredFile file"
+        grep -v "Entering directory" $inputFile | grep -v "Leaving directory" | grep -v "COMPILE" | grep -v "LINK" | grep -v "Qt-MOC" | grep -v "Qt-RCC" | grep -v "Qt-UIC" > $filteredFile
+        echo "Built $filteredFile"
+        grep -n "Making all in " $filteredFile | ../scripts/checkMakeSpew.pl | tee makeSpewLineCount.txt
+        # DQ (10/2/2010): Added test for parallelism of build system.
+        echo "Measure Build System Parallelism"
+        ../scripts/checkMakeParallelism.sh ${inputFile}
     fi
-
-    printBannerToEndStep "Compile time spew" $start_time_seconds 
 }
