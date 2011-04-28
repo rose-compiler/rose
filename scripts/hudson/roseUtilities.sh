bc2f8ee9ae5ee2bdfbfcbca54374bbf55e7c71d9
Justin Too <too1@llnl.gov>
2011-04-27 17:30:08 -0700

    Simple fix.

--- scripts/hudson/roseUtilities.sh
+++ scripts/hudson/roseUtilities.sh
@@ -179,9 +179,6 @@ if test $# = 1 ; then
 	echo "    $ROSE_CONFIGURE_PREFIX"
 	echo ""
 	echo ""
-
-	echo ""
-	echo ""
   ) 2>&1 |tee $env_dump_file |filterStep "Hudson environment output"
 else
     echo "[Error] Usage: dumpHudsonEnvironment <output-file-name>"
