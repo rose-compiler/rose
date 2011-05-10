e0e9f189d977daacef6d214d7e990d4d0f6f090f
Robb Matzke <matzke1@llnl.gov>
2011-05-10 11:55:33 -0400

    (Binaries) Added SgAsmTarget node

--- src/ROSETTA/src/binaryInstruction.C
+++ src/ROSETTA/src/binaryInstruction.C
@@ -20,7 +20,7 @@ Grammar::setUpBinaryInstructions ()
 
      NEW_TERMINAL_MACRO ( AsmBlock        , "AsmBlock",        "AsmBlockTag" );
      NEW_TERMINAL_MACRO ( AsmOperandList  , "AsmOperandList",  "AsmOperandListTag" );
-
+     NEW_TERMINAL_MACRO(AsmTarget, "AsmTarget", "AsmTargetTag");
 
      NEW_TERMINAL_MACRO(AsmArmInstruction, "AsmArmInstruction", "AsmArmInstructionTag");
      AsmArmInstruction.setFunctionPrototype("HEADER_BINARY_ARM_INSTRUCTION", "../Grammar/BinaryInstruction.code");
@@ -463,7 +463,7 @@ Grammar::setUpBinaryInstructions ()
 
      NEW_NONTERMINAL_MACRO (AsmNode,
                             AsmStatement | AsmExpression | AsmInterpretation | AsmOperandList | AsmType |
-                            AsmExecutableFileFormat | AsmInterpretationList | AsmGenericFileList,
+                            AsmExecutableFileFormat | AsmInterpretationList | AsmGenericFileList | AsmTarget,
                             "AsmNode","AsmNodeTag", false);
 
   // DQ (3/15/2007): Added support forbinaries (along lines of suggestions by Thomas Dullien)
@@ -507,6 +507,14 @@ Grammar::setUpBinaryInstructions ()
      AsmInstruction.setDataPrototype("SgAsmStatementPtrList","sources","",
                            NO_CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, NO_TRAVERSAL, NO_DELETE);
 
+
+     // Link to a SgAsmBlock - This is more than a SgAsmBlockPtr.  SgAsmBlockPtr can only point to a block, but sometimes
+     // we need to represent an address that has no block.
+     AsmTarget.setFunctionPrototype("HEADER_BINARY_TARGET", "../Grammar/BinaryInstruction.code");
+     AsmTarget.setDataPrototype("rose_addr_t", "address", "= 0",        /*doxygen*/
+                                NO_CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, NO_TRAVERSAL, NO_DELETE);
+     AsmTarget.setDataPrototype("SgAsmBlock*", "block", "= NULL",       /*doxygen*/
+                                NO_CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, NO_TRAVERSAL, NO_DELETE);
 
 
      // Instruction basic block. One entry point (first instruction) and one exit point (last instruction).
@@ -2361,6 +2369,7 @@ Grammar::setUpBinaryInstructions ()
   // Non binary File IR node support
 
      AsmBlock.setFunctionSource                    ( "SOURCE_BINARY_BLOCK", "../Grammar/BinaryInstruction.code");
+     AsmTarget.setFunctionSource                   ( "SOURCE_BINARY_TARGET", "../Grammar/BinaryInstruction.code");
      AsmOperandList.setFunctionSource              ( "SOURCE_BINARY_OPERAND_LIST", "../Grammar/BinaryInstruction.code");
      AsmDataStructureDeclaration.setFunctionSource ( "SOURCE_BINARY_DATA_STRUCTURE", "../Grammar/BinaryInstruction.code");
      AsmInstruction.setFunctionSource              ( "SOURCE_BINARY_INSTRUCTION", "../Grammar/BinaryInstruction.code");
