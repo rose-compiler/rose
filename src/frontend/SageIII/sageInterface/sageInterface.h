06551fe1f062d804c100eeaa189fd5519acab2a1
George Vulov <georgevulov@gatech.edu>
2011-05-23 10:36:00 -0700

    (SageInterface) Fix compile error from last commit

--- src/frontend/SageIII/sageInterface/sageInterface.h
+++ src/frontend/SageIII/sageInterface/sageInterface.h
@@ -18,6 +18,7 @@ SgFile* determineFileType ( std::vector<std::string> argv, int& nextErrorCode, S
 
 #include "LivenessAnalysis.h"
 #include "abstract_handle.h"
+#include "ClassHierarchyGraph.h"
 
 // DQ (8/19/2004): Moved from ROSE/src/midend/astRewriteMechanism/rewrite.h
 //! A global function for getting the string associated with an enum (which is defined in global scope)
