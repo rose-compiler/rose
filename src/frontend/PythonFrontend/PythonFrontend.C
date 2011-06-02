649d6febf31f0354a2ae15abd8f06cbb0c4ce5c3
Michael Driscoll <driscoll6@llnl.gov>
2011-06-02 11:40:51 -0700

    added simple 'hello world' program to test embedded python interpreter

--- src/frontend/PythonFrontend/PythonFrontend.C
+++ src/frontend/PythonFrontend/PythonFrontend.C
@@ -3,12 +3,20 @@
 
 #ifndef USE_ROSE_PYTHON_SUPPORT // If Python is disabled then compile a stub
 
+#include <Python.h>
 
 #include <iostream>
 
 int python_main(std::string, SgFile*)
 {
-    std::cerr << "python_main was called" << std::endl;
+    std::cout << "python_main was called. Launching interpreter." << std::endl;
+
+    Py_Initialize();
+    PyRun_SimpleString("print 'Hello world!'");
+    Py_Finalize();
+
+    std::cout << "Interpreter terminated." << std::endl;
+
     return -1;
 }
 
