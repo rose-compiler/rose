ce621cee010ef6d2d6772c775e235767193e7701
Michael Driscoll <driscoll6@llnl.gov>
2011-06-03 11:29:03 -0700

    added preliminary buildNodeX methods

--- src/frontend/PythonFrontend/SagePythonInterface.h
+++ src/frontend/PythonFrontend/SagePythonInterface.h
@@ -4,7 +4,15 @@
 #include "Python.h"
 
 /* Return the number of arguments of the application command line */
-PyObject*
-emb_numargs(PyObject *self, PyObject *args);
+PyObject* sage_buildGlobal(PyObject *self, PyObject *args);
+PyObject* sage_buildPrintStmt(PyObject *self, PyObject *args);
+PyObject* sage_buildStringVal(PyObject *self, PyObject *args);
+
+static PyMethodDef SageBuilderMethods[] = {
+    {"buildGlobal", sage_buildGlobal, METH_KEYWORDS, "Builds an SgGlobal node."},
+    {"buildPrintStmt", sage_buildPrintStmt, METH_KEYWORDS, "Builds an SgPrintStmt node."},
+    {"buildStringVal", sage_buildStringVal, METH_KEYWORDS, "Builds an SgStringVal node."},
+    {NULL, NULL, 0, NULL}
+};
 
 #endif /* SAGE_PYTHON_INTERFACE_H_ */
