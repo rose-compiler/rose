9c70fe00149f409e69b8ff66887002e668fdaa5b
Michael Driscoll <driscoll6@llnl.gov>
2011-06-03 14:23:57 -0700

    working on the C++/Python interface. set up encapsulation

--- src/frontend/PythonFrontend/SagePythonInterface.C
+++ src/frontend/PythonFrontend/SagePythonInterface.C
@@ -11,11 +11,29 @@
 using namespace std;
 using namespace SageBuilder;
 
-PyObject* PyEncapsulate(void* ptr) {
-    return PyCapsule_New(ptr, /* name= */ NULL, /* destructor= */ NULL);
+/*
+ * Encapsulate the pointer in a Python object. 
+ * Use no name or destructor.
+ */
+PyObject* PyEncapsulate(void* ptr, char* name = NULL) {
+    return PyCapsule_New(ptr, name, /* destructor= */ NULL);
 }
 
-/* TODO doc */
+/*
+ * Build an SgAddOp node from the given Python statements.
+ *  - PyObject* args = (PyObject*, PyObject*)
+ */
+PyObject*
+sage_buildAddOp(PyObject *self, PyObject *args)
+{
+    cout << "Hello from buildAddOp()" << endl;
+    return Py_BuildValue("i", 0);
+}
+
+/*
+ * Build an SgGlobal node from the given list of Python statements.
+ *  - PyObject* args = ( [PyObject*, PyObject*, ...], )
+ */
 PyObject*
 sage_buildGlobal(PyObject *self, PyObject *args)
 {
@@ -23,18 +41,43 @@ sage_buildGlobal(PyObject *self, PyObject *args)
     return Py_BuildValue("i", 0);
 }
 
-/* TODO doc */
+/*
+ * Build an SgPrintStmt node from the given list of Python
+ * expressions.
+ *  - PyObject* args = ( [PyObject*, PyObject*, ... ], )
+ */
 PyObject*
 sage_buildPrintStmt(PyObject *self, PyObject *args)
 {
     cout << "Hello from buildPrintStmt()" << endl;
+    PyObject* arg1v = PyTuple_GetItem(args, 0);
+    Py_ssize_t arg1c = PyList_Size(arg1v);
+#if 0
+    SgPrintStmt* sg_print_statement = buildSgPrintStmt_nfi();
+#endif
+    for (int i = 0; i < arg1c; i++) {
+       PyObject* capsule = PyList_GetItem(arg1v, i);
+#if 0
+       SgExpression* exp = 
+           (SgExpression*) PyCapsule_GetPointer(capsule, NULL);
+       sg_print_statement->append_expression(exp);
+#endif
+    }
+#if 0
+    return PyEncapsulate(sg_print_statement);
+#else
     return Py_BuildValue("i", 0);
+#endif
 }
 
-/* TODO doc */
+/* 
+ * Build an SgStringVal node from the given Python String object.
+ *  - PyObject* args = ( PyStringObject*, ) 
+ */
 PyObject*
 sage_buildStringVal(PyObject *self, PyObject *args)
 {
+    cout << "Hello from buildStringVal()" << endl;
     char* cstr = NULL;
     PyArg_ParseTuple(args, "s", &cstr); // TODO error handling
     string str = string(cstr);
--- src/frontend/PythonFrontend/sageTranslator.py
+++ src/frontend/PythonFrontend/sageTranslator.py
@@ -2,24 +2,35 @@ import sys, ast
 
 import sage
 
+OPERATOR_BUILDFXN_MAP = {
+    ast.Add: sage.buildAddOp,
+}
+
 class SageTranslator(ast.NodeVisitor):
 
   def __call__(self, syntax_tree):
     return self.visit(syntax_tree)
 
   def generic_visit(self, node):
+    print "Generic: ", node.__class__.__name__
     return map(self.visit, ast.iter_child_nodes(node))
 
+  def visit_BinOp(self, node):
+    lhs = self.visit(node.left)
+    rhs = self.visit(node.right)
+    build_fxn = OPERATOR_BUILDFXN_MAP[node.op.__class__]
+    return build_fxn(lhs, rhs)
+
   def visit_Module(self, node):
-     subforest = self.generic_visit(node)
-     return sage.buildGlobal(subforest)
+    subforest = self.generic_visit(node)
+    return sage.buildGlobal(subforest)
 
   def visit_Print(self, node):
-     subforest = self.generic_visit(node)
-     return sage.buildPrintStmt(subforest)
+    subforest = self.generic_visit(node)
+    return sage.buildPrintStmt(subforest)
 
   def visit_Str(self, node):
-     return sage.buildStringVal(node.s)
+    return sage.buildStringVal(node.s)
 
 
 def translate(infilename):
@@ -31,9 +42,7 @@ def translate(infilename):
     print >>sys.stderr, "IO error when reading file: %s" % infilename
     exit(1)
   syntax_tree = ast.parse(contents)
-  sageTree = SageTranslator()(syntax_tree)
-  print sageTree
-  return sageTree
+  return SageTranslator().visit(syntax_tree)
 
 def main(argv):
   map(translate, argv[1:])
