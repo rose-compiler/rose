7666017aebdf9c5b80ecaec6035ac2df0979dd74
Michael Driscoll <driscoll6@llnl.gov>
2011-06-04 20:01:13 -0700

    added support for FunctionDef nodes. started work on tracking scopes

--- src/frontend/PythonFrontend/SagePythonInterface.h
+++ src/frontend/PythonFrontend/SagePythonInterface.h
@@ -5,17 +5,22 @@
 
 /* Return the number of arguments of the application command line */
 PyObject* sage_buildAddOp(PyObject *self, PyObject *args);
+PyObject* sage_buildFunctionDef(PyObject *self, PyObject *args);
 PyObject* sage_buildGlobal(PyObject *self, PyObject *args);
 PyObject* sage_buildLongIntVal(PyObject *self, PyObject *args);
 PyObject* sage_buildPrintStmt(PyObject *self, PyObject *args);
 PyObject* sage_buildStringVal(PyObject *self, PyObject *args);
 
+PyObject* sage_addChildrenToNode(PyObject *self, PyObject *args);
+
 static PyMethodDef SageBuilderMethods[] = {
     {"buildAddOp", sage_buildAddOp, METH_VARARGS, "Builds an SgAddOp node."},
+    {"buildFunctionDef", sage_buildFunctionDef, METH_VARARGS, "Builds an SgFunctionDeclaration node."},
     {"buildGlobal", sage_buildGlobal, METH_VARARGS, "Builds an SgGlobal node."},
     {"buildLongIntVal", sage_buildLongIntVal, METH_VARARGS, "Builds an SgIntVal node."},
     {"buildPrintStmt", sage_buildPrintStmt, METH_VARARGS, "Builds an SgPrintStmt node."},
     {"buildStringVal", sage_buildStringVal, METH_VARARGS, "Builds an SgStringVal node."},
+    {"addChildrenToNode", sage_addChildrenToNode, METH_VARARGS, "Add children to a given SgNode."},
     {NULL, NULL, 0, NULL}
 };
 
