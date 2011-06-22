eef8d166c80cd779e67191dcb3de5b5d161a8e28
Michael Driscoll <driscoll6@llnl.gov>
2011-06-21 17:17:44 -0700

    disable compilation of python frontend via cmake. Linking the frontend requires knowledge of the python include and library paths, which I have yet to set up for cmake

--- src/backend/unparser/unparser.C
+++ src/backend/unparser/unparser.C
@@ -347,8 +347,12 @@ Unparser::unparseFile ( SgSourceFile* file, SgUnparse_Info& info )
                        {
                            if (file->get_Python_only())
                               {
+#if ROSE_USE_PYTHON
                                   Unparse_Python unparser(this,file->get_unparse_output_filename());
                                   unparser.unparseStatement(globalScope, info);
+#else
+                                  ROSE_ABORT("unparsing Python requires ROSE_USE_PYTHON be set");
+#endif
                               }
                            else
                              {
