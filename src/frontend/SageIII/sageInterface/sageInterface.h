1c11895718ad04d8e7f0f630ccfa7dc036b39806
Michael Driscoll <driscoll6@llnl.gov>
2011-07-07 12:56:56 -0700

    [Python] added SageBuilder methods for constructing SgTupleExps

--- src/frontend/SageIII/sageInterface/sageInterface.h
+++ src/frontend/SageIII/sageInterface/sageInterface.h
@@ -1223,6 +1223,7 @@ SgVariableSymbol* prependArg(SgFunctionParameterList *, SgInitializedName*);
 
 //! Append an expression to a SgExprListExp, set the parent pointer also
 void appendExpression(SgExprListExp *, SgExpression*);
+void appendExpression(SgTupleExp*, SgExpression*);
 
 //! Append an expression list to a SgExprListExp, set the parent pointers also
 void appendExpressionList(SgExprListExp *, const std::vector<SgExpression*>&);
