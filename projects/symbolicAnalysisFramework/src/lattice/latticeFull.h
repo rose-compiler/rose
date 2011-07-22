34a3ca7952e4dad2b8e0cb863c89f59a58588286
Phil Miller <miller256@llnl.gov>
2011-07-22 11:33:32 -0700

    SAF: Remove uninitBotLattice

--- projects/symbolicAnalysisFramework/src/lattice/latticeFull.h
+++ projects/symbolicAnalysisFramework/src/lattice/latticeFull.h
@@ -13,67 +13,6 @@ using namespace std;
  *** Commonly used lattices ***
  ******************************/
  
-/* Generic lattice wrapper that adds an uninitialized state below all the regular
-   lattice states and assumes a single bottom state that corresponds to no information. * /
-class uninitBotLattice : public Lattice
-{
-	public:
-	typedef enum infContent {uninitalized, bottom, value};
-	
-	private:
-	infContent level;
-	
-	public:
-	
-	uninitBotLattice();
-	
-	uninitBotLattice(const uninitLattice &that);
-	
-	// initializes this Lattice to its bottom state, if it it not already initialized
-	void initialize();
-	
-	// called by derived classes to indicate that this lattice has been set to a specific value
-	void setToVal();
-	
-	// Returns this lattice's level in the uninitialized <= bottom <= value hierarchy
-	infContent getUninitBotLevel();
-	
-	// returns a copy of this lattice
-	//Lattice* copy();
-	
-	// overwrites the state of this Lattice with that of that Lattice
-	void copy(Lattice* that);
-	
-	// Called by analyses to create a copy of this lattice. However, if this lattice maintains any 
-	//    information on a per-variable basis, these per-variable mappings must be converted from 
-	//    the current set of variables to another set. This may be needed during function calls, 
-	//    when dataflow information from the caller/callee needs to be transferred to the callee/calleer.
-	// We do not force child classes to define their own versions of this function since not all
-	//    Lattices have per-variable information.
-	// varNameMap - maps all variable names that have changed, in each mapping pair, pair->first is the 
-	//              old variable and pair->second is the new variable
-	// func - the function that the copy Lattice will now be associated with
-	void remapVars(const map<varID, varID>& varNameMap, const Function& newFunc);
-	
-	// Called by analyses to copy over from the that Lattice dataflow information into this Lattice.
-	// that contains data for a set of variables and incorporateVars must overwrite the state of just
-	// those variables, while leaving its state for other variables alone.
-	// We do not force child classes to define their own versions of this function since not all
-	//    Lattices have per-variable information.
-	void incorporateVars(Lattice* that);
-	
-	// computes the meet of this and that and saves the result in this
-	// returns true if this causes this to change and false otherwise
-	bool meetUpdate(Lattice* that);
-	
-	virtual bool operator==(Lattice* that);
-		
-	// The string that represents this object
-	// If indent!="", every line of this string must be prefixed by indent
-	// The last character of the returned string should not be '\n', even if it is a multi-line string.
-	string str(string indent="");
-};*/
- 
 class BoolAndLattice : public FiniteLattice
 {
 	// state can be:
