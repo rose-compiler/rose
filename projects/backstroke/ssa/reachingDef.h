df5380c863744bc3a83d4d1bc7d206f9c1f85033
George Vulov <georgevulov@gatech.edu>
2011-07-22 14:23:53 -0700

    Fix some pedantic compiler warnings (extra semicolons mostly)

--- projects/backstroke/ssa/reachingDef.h
+++ projects/backstroke/ssa/reachingDef.h
@@ -25,7 +25,7 @@ public:
 		
 		/** This is not a real definition; this variable is external to the scope being analyzed and
 		 * this def represents the existing value of the variable at the beginning of the scope. */
-		EXTERNAL_DEF,
+		EXTERNAL_DEF
 	};
 	
 	typedef boost::shared_ptr<ReachingDef> ReachingDefPtr;
