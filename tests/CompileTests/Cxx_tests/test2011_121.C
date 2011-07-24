1d05b518d5402e1f25fe499840ea9a59d7314a15
Dan Quinlan <dquinlan@llnl.gov>
2011-07-24 06:32:31 -0700

    Fixed problem with Thrifty release test.

---
+++ tests/CompileTests/Cxx_tests/test2011_121.C
@@ -0,0 +1,15 @@
+
+// Skip version 4.x gnu compilers
+// #if ( __GNUC__ == 3 )
+
+#include <complex>
+
+// #else
+//  #warning "Not tested on gnu 4.0 or greater versions"
+// #endif
+
+int main()
+   {
+     return 0;
+   }
+
