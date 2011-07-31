708e907d3ab67ee2e20dbbc0448d65c11ec326f5
Dan Quinlan <dquinlan@llnl.gov>
2011-07-31 10:50:39 -0700

    Fixed statement handling to use the statements stack more precisely and added SgForStatement, SgBreakStatement, and SgContinueStatement support.

---
+++ tests/CompileTests/Java_tests/test2011_31.java
@@ -0,0 +1,17 @@
+// Simplest java example (note that class name must match filename).
+
+public class test2011_31
+   {
+  // This is the required "main" function.
+     public static void main(String args[])
+        {
+          int x = 7;
+       // int y = 8;
+
+       // Case of for statement.
+          for (x = 2; x < 1; x++)
+               break;
+            // continue;
+        }
+   }
+
