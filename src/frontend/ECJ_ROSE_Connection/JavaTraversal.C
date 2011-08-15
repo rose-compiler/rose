66fba84cb90842de90c9caf5e57e2081ed7687b0
Dan Quinlan <dquinlan@llnl.gov>
2011-08-15 11:08:59 -0700

    Removed generation of astC.dot files caught by distcheck rule.

--- src/frontend/ECJ_ROSE_Connection/JavaTraversal.C
+++ src/frontend/ECJ_ROSE_Connection/JavaTraversal.C
@@ -9,44 +9,56 @@
  #include <iostream>
  #include <fstream> 
 
- std::ofstream myfile ("astC.dot");
+// #error "This JavaTraversal.C file is no longer used."
+
+#if 1
+// std::ofstream myfile ("astC.dot");
 
  JNIEXPORT void JNICALL 
  Java_JavaTraversal_invokeINIT(JNIEnv *env, jobject obj)
  {
+#if 0
    if (myfile.is_open())
      {
        myfile << "Digraph G {\n";
      }
+#endif
  }
 
  JNIEXPORT void JNICALL 
  Java_JavaTraversal_invokeEND(JNIEnv *env, jobject obj)
  {
+#if 0
   if (myfile.is_open())
      {
        myfile << "}\n";
        myfile.close();
      }
+#endif
   }
 
  JNIEXPORT void JNICALL 
  Java_JavaTraversal_invokeNODE(JNIEnv *env, jobject obj, jstring str, jint nr)
  {
+#if 0
    jboolean iscopy;
    const char *classname = (env)->GetStringUTFChars( str, &iscopy);
    //printf("     C-SIDE: Found Node %s (%d)\n",classname,(int)nr);
    myfile << "\"" <<nr << classname << "\" [label=\"" << classname << "\"];\n";
+#endif
    return;
  }
 
  JNIEXPORT void JNICALL 
  Java_JavaTraversal_invokeEDGE(JNIEnv *env, jobject obj, jstring str1, jint nr1,jstring str2, jint nr2)
  {
+#if 0
    jboolean iscopy;
    const char *classname1 = (env)->GetStringUTFChars( str1, &iscopy);
    const char *classname2 = (env)->GetStringUTFChars( str2, &iscopy);
    //printf("     C-SIDE: Found Edge %s(%d)->%s(%d)  \n",classname1,(int)nr1,classname2,(int)nr2);
    myfile << "\"" << nr1 << classname1 << "\" -> \"" << nr2 << classname2 << "\";\n";
+#endif
    return; 
  }
+#endif
