b3fc187d10ad14bc98eda00041766c517efa651e
Robb Matzke <matzke1@llnl.gov>
2011-08-19 12:55:47 -0400

    (x86sim) Debugging code checkpoint

--- projects/simulator/x86robb.C
+++ projects/simulator/x86robb.C
@@ -23,32 +23,46 @@ main(int argc, char *argv[], char *envp[])
     rose_argv[3] = NULL;
     SgProject *project = frontend(3, rose_argv);
 
-    /* Disassemble when we hit memcpy() the first time */
+    /**************************************************************************************************************************
+     *                                  Debugging callbacks...
+     **************************************************************************************************************************/
+
+
+#if 0
+    /* Do we ever write to mem[gs-0x28=080c87d8]?  The native execution writes 0x080c5f00 to this address, but the simulation
+     * for some reason doesn't. */
+    sim.install_callback(new RSIM_Tools::MemoryAccessWatcher(0x080c87d8, 4));
+
+
+    /* Disassemble when we hit memcpy() the first time, and also show stack traces */
     rose_addr_t memcpy_va = RSIM_Tools::FunctionFinder().address(project, "memcpy");
     std::cerr <<"memcpy() at " <<StringUtility::addrToString(memcpy_va) <<"\n";
     sim.install_callback(new RSIM_Tools::MemoryDisassembler(memcpy_va));
+    sim.install_callback(new RSIM_Tools::FunctionReporter(true));
 
-    /* Check the memcpy() behavior. */
-    struct CheckMemcpy: public RSIM_Callbacks::InsnCallback {
+
+    /* Show memcpy arguments */
+    struct MemcpyCall: public RSIM_Callbacks::InsnCallback {
         uint32_t memcpy_va;
-        CheckMemcpy(uint32_t memcpy_va): memcpy_va(memcpy_va) {}
-        virtual CheckMemcpy *clone() { return this; }
+        MemcpyCall(uint32_t memcpy_va): memcpy_va(memcpy_va) {}
+        virtual MemcpyCall *clone() { return this; }
         virtual bool operator()(bool enabled, const Args &args) {
             if (!enabled) return false;
             RTS_Message *m = args.thread->tracing(TRACE_MISC);
             if (args.insn->get_address()==memcpy_va) {
+                /* Show arguments */
                 uint32_t sp = args.thread->policy.readGPR(x86_gpr_sp).known_value();
                 uint32_t actual[3];
                 args.thread->get_process()->mem_read(actual, sp+4, 12);
-                m->mesg("CheckMemcpy triggered: entering memcpy(0x%08"PRIx32", 0x%08"PRIx32", 0x%08"PRIx32")",
+                m->mesg("MemcpyCall triggered: entering memcpy(0x%08"PRIx32", 0x%08"PRIx32", 0x%08"PRIx32")",
                         actual[0], actual[1], actual[2]);
                 args.thread->report_stack_frames(m, "", true);
             }
             return true;
         }
     };
-    sim.install_callback(new CheckMemcpy(memcpy_va));
-
+    sim.install_callback(new MemcpyCall(memcpy_va));
+#endif
     
 
     /***************************************************************************************************************************
