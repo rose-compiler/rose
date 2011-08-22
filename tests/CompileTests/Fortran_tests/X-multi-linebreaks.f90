f294752dce7912d1e524bfd7c9144ed26bb75d11
Scott K Warren <scott@rice.edu>
2011-08-22 11:51:13 -0500

    Really fixed the conflict in Fortran test X-multi-linebreaks.f90.

--- tests/CompileTests/Fortran_tests/X-multi-linebreaks.f90
+++ tests/CompileTests/Fortran_tests/X-multi-linebreaks.f90
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ! On the following multiline statement, OFP 0.8.3 failed to handle continuations correctly.
 
 module multi_line_breaks
@@ -8,15 +7,3 @@ character, parameter :: p1*(*) = &
 &(*) = "y"
 
 end module
-=======
-! OFP 0.8.3 failed with the following message:
-! multi-linebreaks.f90 line 5:53 no viable alternative at input ''
-MODULE multi_line_breaks
-
-CHARACTER, PARAMETER :: out_1*(*) = &
-&"x", out_2*&
-&(*) = "y"
-
-END MODULE
-
->>>>>>> zung
