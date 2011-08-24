1dbe509edcf514a0619520d447c97bcfc45dbf99
Dan Quinlan <dquinlan@llnl.gov>
2011-08-23 20:12:16 -0700

    Added Java generics examples and implemented symbol table handling for implicit statements.

--- tests/CompileTests/Java_tests/test2011_43.java
+++ tests/CompileTests/Java_tests/test2011_43.java
@@ -7,7 +7,8 @@ public class test2011_43
      public void foo()
         {
 //        List<String> v;
-          java.util.List<String> v;
+          java.util.List<String> w;
+
 //        java.util.List v;
 //        List v;
 //        String w;
