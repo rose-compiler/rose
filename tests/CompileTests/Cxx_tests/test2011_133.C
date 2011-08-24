4aaf8d9bd38b97aadf23c5bff85a35a043d0658e
Dan Quinlan <dquinlan@llnl.gov>
2011-08-24 09:57:41 -0700

    Added SSE support for gnu 4.4.x compiler.

--- tests/CompileTests/Cxx_tests/test2011_133.C
+++ tests/CompileTests/Cxx_tests/test2011_133.C
@@ -2,7 +2,24 @@
 // You need to add -mmmx and -msse to the compiler 
 // configuration (for gcc to enable these).
 
-#include<emmintrin.h>
+#if 0
+// Builtin functions specific to GNU 4.4.1 (likely 4.4.x)
+int __builtin_ia32_psllwi(short,int);
+int __builtin_ia32_pslldi(int,int);
+int __builtin_ia32_psllqi(long long,int);
+int __builtin_ia32_psrawi(short,int);
+int __builtin_ia32_psradi(int,int);
+int __builtin_ia32_psrlwi(int,int);
+int __builtin_ia32_psrldi(int,int);
+int __builtin_ia32_psrlqi(long long,int);
+#endif
+
+
+
+
+#ifdef __MMX__
+  #include<emmintrin.h>
+#endif
 
 int main()
    {
