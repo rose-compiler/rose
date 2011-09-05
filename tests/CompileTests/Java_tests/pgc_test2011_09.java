b8f8f7b66356e2e4e102a8a4956f539a3d900379
Philippe Charles <pgc1@rice.edu>
2011-09-05 11:28:53 -0400

    check in non-passing test for ForEach statement

---
+++ tests/CompileTests/Java_tests/pgc_test2011_09.java
@@ -0,0 +1,10 @@
+//
+// Test Try statement with finally block.
+//
+public class pgc_test2011_09 {
+    public static void main(String args[]) {
+    	for (String s : args) {
+            ;
+    	}
+    }
+}
