87e486db1cadc9b8c93e343b4c4bfb5fee7bb561
Philippe Charles <pgc1@rice.edu>
2011-09-09 14:21:18 -0400

    use the new SgJavaLabelStatement IR node instead of the C++ SgLabelStatement node; Complete translation of SgJavaForEachStatement; add new test pgc_test2011_09.java

--- tests/CompileTests/Java_tests/pgc_test2011_09.java
+++ tests/CompileTests/Java_tests/pgc_test2011_09.java
@@ -1,10 +1,12 @@
 //
 // Test Try statement with finally block.
 //
+import java.util.ArrayList;
 public class pgc_test2011_09 {
-    public static void main(String args[]) {
-    	for (String s : args) {
+    void test() {
+        ArrayList<String> args = new ArrayList<String>();
+        for (String s : args){
             ;
-    	}
+        }
     }
 }
