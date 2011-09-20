d5cd0e85300ca94ebc075344e3a04987c0466607
vc8 <vc8@rice.edu>
2011-09-20 16:24:51 -0500

    add cave3's java tests

---
+++ tests/CompileTests/Java_tests/cave3_dowhile0.java
@@ -0,0 +1,8 @@
+public class cave3_dowhile0 {
+	public void loop() {
+		int i = 0;
+		do {
+			i++;
+		} while (i < 10);
+	}
+}
---
+++ tests/CompileTests/Java_tests/cave3_nameResolution0.java
@@ -0,0 +1,7 @@
+public class cave3_nameResolution0 { 
+	void test(int num) {
+		// Mixing up classnames with variable declaration
+		cave3_nameResolution0 cave3_nameResolution0 = new cave3_nameResolution0();
+		cave3_nameResolution0.test(3); 
+	} 
+}
