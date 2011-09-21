d707a69c535a6676f5a2242b1f5cb66cae0ac025
Robb Matzke <matzke1@llnl.gov>
2011-09-21 13:20:09 -0400

    (Warnings) Added more compiler checks and cleaned up some warnings
    * Deprecated Partitioner functions are marked as deprecated
    
    * Disassembler and Partitioner printf-like functions have their
      format and arguments checked by GCC.
    
    * Function-local enums in the Java parser are marked with NO_STRINGIFY
      since there's no need to generate the functions that turn the enum
      constants into strings.

--- src/frontend/ECJ_ROSE_Connection/JavaParserActionROSE.C
+++ src/frontend/ECJ_ROSE_Connection/JavaParserActionROSE.C
@@ -1884,7 +1884,7 @@ JNIEXPORT void JNICALL Java_JavaParser_cactionCompoundAssignmentEnd(JNIEnv *env,
      outputJavaState("At TOP of cactionCompoundAssignmentEnd");
 
   // These are the operator code values directly from ECJ.
-     enum ops
+     enum ops // NO_STRINGIFY
         {
           ERROR_OPERATOR       = 0, // This is not a ECJ value 
           AND                  = 2,
@@ -2047,7 +2047,7 @@ JNIEXPORT void JNICALL Java_JavaParser_cactionEqualExpressionEnd(JNIEnv *env, jo
      outputJavaState("At TOP of cactionEqualExpressionEnd");
 
   // These are the operator code values directly from ECJ.
-     enum ops
+     enum ops // NO_STRINGIFY
         {
           ERROR_OPERATOR = 0, // This is not a ECJ value 
           EQUAL_EQUAL    = 18,
@@ -3233,7 +3233,7 @@ JNIEXPORT void JNICALL Java_JavaParser_cactionPostfixExpressionEnd(JNIEnv *env, 
      outputJavaState("At TOP of cactionPostfixExpressionEnd");
 
   // These are the operator code values directly from ECJ.
-     enum ops
+     enum ops // NO_STRINGIFY
         {
           ERROR_OPERATOR = 0, // This is not a ECJ value 
           MINUS          = 13,
@@ -3281,7 +3281,7 @@ JNIEXPORT void JNICALL Java_JavaParser_cactionPrefixExpressionEnd(JNIEnv *env, j
      outputJavaState("At TOP of cactionPrefixExpressionEnd");
 
   // These are the operator code values directly from ECJ.
-     enum ops
+     enum ops // NO_STRINGIFY
         {
           ERROR_OPERATOR = 0, // This is not a ECJ value 
           MINUS          = 13,
@@ -3699,7 +3699,7 @@ JNIEXPORT void JNICALL Java_JavaParser_cactionUnaryExpressionEnd(JNIEnv *env, jo
      outputJavaState("At TOP of cactionUnaryExpressionEnd");
 
   // These are the operator code values directly from ECJ.
-     enum ops
+     enum ops // NO_STRINGIFY
         {
           ERROR_OPERATOR = 0, // This is not a ECJ value 
           NOT            = 11,
