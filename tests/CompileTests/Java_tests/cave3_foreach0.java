02a61eb9748a595aa80ed78412165fd140d7ec8d
’cave3’ <vcave@rice.edu>
2011-09-28 10:59:34 -0500

    Add foreach cfg support

---
+++ tests/CompileTests/Java_tests/cave3_foreach0.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class cave3_foreach0 {
+	public static void main(String[] args) {
+		List l = new ArrayList();
+		for (Object object : l) {
+			
+		}
+	}
+}
