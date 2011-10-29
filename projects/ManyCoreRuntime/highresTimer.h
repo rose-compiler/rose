03d725d05f5b27bbe98087c009337a046e2758ad
Dan Quinlan <dquinlan@llnl.gov>
2011-10-29 13:37:47 -0700

    Commit new work on compiler-independent many-core runtime system.

---
+++ projects/ManyCoreRuntime/highresTimer.h
@@ -0,0 +1,8 @@
+// Support for timing
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+// Moderate resolution timer code (from the web)
+timespec diff(timespec start, timespec end);
