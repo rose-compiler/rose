bc93cbeabf6b3fa6cc4f0cc1e1663318fe36f7f2
George Vulov <georgevulov@gatech.edu>
2011-12-09 21:29:46 -0500

    (Backstroke) Fix usage of SageInterface::createTempVarForExpression after the signature was changed

--- projects/backstroke/pluggableReverser/stateSavingStatementHandler.C
+++ projects/backstroke/pluggableReverser/stateSavingStatementHandler.C
@@ -351,10 +351,8 @@ void StateSavingStatementHandler::saveOneVariable(const VariableRenaming::VarNam
 			//
 
 			//Declare the temporary variable to store the popped value
-			boost::tie(tempVarDecl, reassignTempVar, tempVarRef) = 
-					SageInterface::createTempVariableForExpression(popVal_front(varType), commitBody, true);
-			SageInterface::deepDelete(reassignTempVar);
-			reassignTempVar = NULL;
+			boost::tie(tempVarDecl, tempVarRef) = 
+					SageInterface::createTempVariableForExpression(popVal_front(varType), commitBody, true, NULL);
 
 			//Build if-statments that check for the actual type of the popped value
 			vector<SgIfStmt*> commitIfStatements;
