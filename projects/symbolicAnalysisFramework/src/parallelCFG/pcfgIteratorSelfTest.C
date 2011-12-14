e6cb2ed1f54a00372b6ae2b51b55e38484f3ec85
Sriram <aananthakris1@llnl.gov>
2011-12-13 16:32:50 -0800

    adding mpi pcfg work

---
+++ projects/symbolicAnalysisFramework/src/parallelCFG/pcfgIteratorSelfTest.C
@@ -0,0 +1,57 @@
+#include "rose.h"
+#include <list>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// dataflow analysis includes
+#include "common.h"
+#include "variables.h"
+#include "analysisCommon.h"
+#include "functionState.h"
+#include "latticeFull.h"
+#include "analysis.h"
+#include "dataflow.h"
+#include "liveDeadVarAnalysis.h"
+#include "divAnalysis.h"
+#include "ConstrGraphAnalysis.h"
+
+// pcfg includes
+#include "pCFG.h"
+#include "MPIDepAnalysis.h"
+#include "pcfgAnnotations.h"
+#include "pcfgIterator.h"
+
+
+int main(int argc, char* argv[])
+{
+    printf("================ S T A R T =================\n");
+
+    SgProject* project = frontend(argc, argv);
+    initAnalysis(project);
+    Dbg::init("PCFGIterator",".","index.html");
+
+    PragmaParse pragmaparser;
+    pragmaparser.traverseInputFiles(project, preorder);
+
+    // PragmaParseTest pragmaparsetest;
+    // pragmaparsetest.traverseInputFiles(project, preorder);
+
+    analysisDebugLevel = 2;
+
+    LiveDeadVarsAnalysis ldva(project);
+    UnstructuredPassInterDataflow upid_ldva(&ldva);
+    upid_ldva.runAnalysis();
+
+    MPIDepAnalysis mda(&ldva);
+    UnstructuredPassInterDataflow upid_mda(&mda);
+    upid_mda.runAnalysis();
+
+    pCFGIterator pcfgiterator(&ldva, &mda);
+    UnstructuredPassInterDataflow upid_pcfg(&pcfgiterator);
+    upid_pcfg.runAnalysis();
+    pcfgiterator.writeToDot();
+    
+    return 0;
+}
