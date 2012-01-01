0ad660fa01538937b84999f25b3ae23e8ded98fd
Markus Schordan <schordan@technikum-wien.at>
2012-01-01 13:02:27 +0100

    Aral: small fix to make tests succeed again.

--- projects/SATIrE/src/aral/src/aral.C
+++ projects/SATIrE/src/aral/src/aral.C
@@ -3,7 +3,6 @@
 int main()
 {
 	Aral::AralFile* root=Aral::Translator::frontEnd();
-	std::cout << "BACKEND:" << std::endl;
 	if(root) std::cout << Aral::Translator::backEnd(root) << std::endl;
 	return 0;
 }
