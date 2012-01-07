f7c41372cd89368b3b6aec43d988daaa011769f6
Markus Schordan <schordan@technikum-wien.at>
2012-01-07 20:10:01 +0100

    removed option -disable-satire

--- projects/SATIrE/bootstrap.sh
+++ projects/SATIrE/bootstrap.sh
@@ -6,30 +6,27 @@ PREFIX="`pwd`/install"
 BUILDDIR=build
 
 ENABLE_CLANG="0"
-DISABLE_SATIRE="0"
 DISABLE_CHECKS="0"
 
 #parse command line options
 for i in $@; do
   case $i in
-      -help | -h | --help)
-          echo "Usage: $0 <INSTALL_PREFIX> [-help] [-enable-clang] [-disable-satire] [-disable-checks] [-gcc-<ver>]" 
+       -h | --help)
+          echo "Usage: $0 <INSTALL_PREFIX> [--help] [--enable-clang] [--disable-checks] [-gcc-<ver>]" 
           echo "  autoreconf, configure, test and install SATIrE to <INSTALL_PREFIX>"
           echo "  Paths are hardcoded for c8.complang.tuwien.ac.at"
           exit 1
           ;;
-      -gcc-*)
+      --gcc-*)
           CC="gcc-${i:5}"
           CXX="g++-${i:5}"
           CCARGS="CC=$CC CXX=$CXX"
           export CC=$CC
           export CXX=$CXX
           ;;
-      -enable-clang) ENABLE_CLANG="1"
+      --enable-clang) ENABLE_CLANG="1"
           ;;
-      -disable-satire) DISABLE_SATIRE="1" # build only CLANG frontend
-          ;;
-      -disable-checks) DISABLE_CHECKS="1"
+      --disable-checks) DISABLE_CHECKS="1"
           ;;
       -*) echo "invalid option $i"
           exit 1
@@ -41,32 +38,28 @@ done;
 echo "using prefix $PREFIX"
 
 #configure and build satire
-if [ "$DISABLE_SATIRE" == "0" ] ; then
-    echo "building satire"
-    if [ "$DISABLE_CHECKS" ==  "1" ]; then
-        (libtoolize && \
-            autoreconf -i && \
-            mkdir -p $BUILDDIR && cd $BUILDDIR && \
-            ../configure --prefix=$PREFIX --with-rosedir=/usr/local/mstools/rose --with-pagdir=/usr/local/mstools/pag --with-boostdir=/usr/local/mstools/boost --with-boost-compiler-string= PKG_CONFIG_PATH=/usr/local/mstools/lib/pkgconfig:$PKG_CONFIG_PATH CXXFLAGS="-O2 -ggdb -Wno-deprecated" $CCARGS && \
-            make $JOBS && \
-            make install) \
-            || exit 1
-    else
-        (libtoolize && \
-            autoreconf -i && \
-            mkdir -p $BUILDDIR && cd $BUILDDIR && \
-            ../configure --prefix=$PREFIX --with-rosedir=/usr/local/mstools/rose --with-pagdir=/usr/local/mstools/pag --with-boostdir=/usr/local/mstools/boost --with-boost-compiler-string= PKG_CONFIG_PATH=/usr/local/mstools/lib/pkgconfig:$PKG_CONFIG_PATH CXXFLAGS="-O2 -ggdb -Wno-deprecated" $CCARGS && \
-            make $JOBS && \
-            make $JOBS distcheck && \
-            make install && \
-            make $JOBS installcheck ) \
-            || exit 1
-    fi
+echo "building satire"
+if [ "$DISABLE_CHECKS" ==  "1" ]; then
+    (libtoolize && \
+    autoreconf -i && \
+    mkdir -p $BUILDDIR && cd $BUILDDIR && \
+    ../configure --prefix=$PREFIX --with-rosedir=/usr/local/mstools/rose --with-pagdir=/usr/local/mstools/pag --with-boostdir=/usr/local/mstools/boost --with-boost-compiler-string= PKG_CONFIG_PATH=/usr/local/mstools/lib/pkgconfig:$PKG_CONFIG_PATH CXXFLAGS="-O2 -ggdb -Wno-deprecated" $CCARGS && \
+    make $JOBS && \
+    make install) \
+    || exit 1
+else
+    (libtoolize && \
+    autoreconf -i && \
+    mkdir -p $BUILDDIR && cd $BUILDDIR && \
+    ../configure --prefix=$PREFIX --with-rosedir=/usr/local/mstools/rose --with-pagdir=/usr/local/mstools/pag --with-boostdir=/usr/local/mstools/boost --with-boost-compiler-string= PKG_CONFIG_PATH=/usr/local/mstools/lib/pkgconfig:$PKG_CONFIG_PATH CXXFLAGS="-O2 -ggdb -Wno-deprecated" $CCARGS && \
+    make $JOBS && \
+    make $JOBS distcheck && \
+    make install && \
+    make $JOBS installcheck ) \
+    || exit 1
 fi
 
 #configure and build clang and llvm
 if [ "$ENABLE_CLANG" == "1" ] ; then
     make clang
 fi
-
-
