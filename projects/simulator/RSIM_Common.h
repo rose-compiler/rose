4b59ee5d2ce540eccb28c416cfc09503916f11e4
Robb Matzke <matzke1@llnl.gov>
2012-01-25 09:39:55 -0500

    (Binary Simulator) Conditional compilation for O_CLOEXEC
    Fixes portability problem introduced by commit a94a9c2c since
    O_CLOEXEC isn't defined on OS X.

--- projects/simulator/RSIM_Common.h
+++ projects/simulator/RSIM_Common.h
@@ -568,7 +568,9 @@ static const Translate open_flags[] = { TF(O_RDWR), TF(O_RDONLY), TF(O_WRONLY),
                                         TF(O_CREAT), TF(O_EXCL), TF(O_NONBLOCK), TF(O_NOCTTY), TF(O_TRUNC),
                                         TF(O_APPEND), TF(O_NDELAY), TF(O_ASYNC), TF(O_FSYNC), TF(O_SYNC), TF(O_NOATIME),
                                         TF(O_LARGEFILE), /*0x8000*/
+#ifdef O_CLOEXEC
                                         TF(O_CLOEXEC),
+#endif
                                         T_END };
 
 /* Types for getdents syscalls */
