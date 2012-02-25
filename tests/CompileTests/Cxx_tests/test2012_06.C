ced2abc20810d86f2793295219800b58c31e43c9
Dan Quinlan <dquinlan@llnl.gov>
2012-02-25 13:54:35 -0800

    Committing work while on travel.

---
+++ tests/CompileTests/Cxx_tests/test2012_06.C
@@ -0,0 +1,13 @@
+#if 1
+namespace my_namespace_X 
+   {
+  // typedef int my_integerType;
+   }
+#endif
+
+#if 1
+namespace my_namespace_X 
+   {
+  // typedef int my_floatType;
+   }
+#endif
---
+++ tests/CompileTests/Cxx_tests/test2012_07.C
@@ -0,0 +1,4 @@
+#include<list>
+
+#include<vector>
+// #include<map>
