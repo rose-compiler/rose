3b2e887331c5ab2caad60a1287a45c0fa2f2c179
Robb Matzke <matzke1@llnl.gov>
2012-03-29 14:32:51 -0400

    (Binary Analysis) Added missing header file

--- src/frontend/Disassemblers/dummyDisassembler.C
+++ src/frontend/Disassemblers/dummyDisassembler.C
@@ -1,6 +1,7 @@
 // ROSETTA has no way to exclude certain IR node definitions based on the user's choice of languages.  Therefore, we need to
 // provide (stub) definitions for those nodes' virtual methods in order to get things to compile when those nodes aren't
 // actually being used anywhere.
+#include "sage3basic.h"
 
 SgAsmType *SgAsmIntegerValueExpression::get_type() { abort(); }
 SgAsmType *SgAsmByteValueExpression::get_type() { abort(); }
