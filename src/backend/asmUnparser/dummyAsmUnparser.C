d450a55baad696409a4a674a2ac79e446dae3409
Robb Matzke <matzke1@llnl.gov>
2012-04-03 10:03:18 -0400

    (Build) Non-binary support needs to like with dummy libraries
    When building ROSE without binary support, we need to link against
    the dummy versions of virtual functions because these functions
    are referenced by the ROSETTA-generated nodes.  ROSETTA generates
    IR node types for binary analysis even when binary analysis is not
    enabled.

---
+++ src/backend/asmUnparser/dummyAsmUnparser.C
@@ -0,0 +1,5 @@
+// dummy function definitions to use when binary support is disabled.  ROSETTA still generates the binary IR node types, so
+// we have to still define some of the functions they use.
+#include "sage3basic.h"
+
+std::string unparseExpression(SgAsmExpression*, const AsmUnparser::LabelMap*) { abort(); }
