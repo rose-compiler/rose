a3a0aea27c3c984bb44e796a450fecfa8952d97f
Michael Hoffman <hoffman34@llnl.gov>
2012-04-04 14:39:47 -0700

    added a tutorial example and removed the yices implementation from its interface

---
+++ tutorial/yicesTutorialExample.C
@@ -0,0 +1,10 @@
+#include <yicesParserLib.C>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    std::cout << "yices check" << std::endl;
+    int i = yicesCheck(argc, argv);
+    std::cout << "end yices check" << std::endl;
+}
