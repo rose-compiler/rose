5fc5bd996046d704687da1e44391338fc75bd14b
Michael Hoffman <hoffman34@llnl.gov>
2012-04-06 16:26:54 -0700

    Adding the yices interface for SMT Solving

--- tests/roseTests/astProcessingTests/yicesParser.C
+++ tests/roseTests/astProcessingTests/yicesParser.C
@@ -1,9 +1,12 @@
-#include <yicesParserLib.C>
-#include <stdio.h>
+#include "yicesParserLib.C"
+//#include <yicesParserLib.h>
+//#include <stdio.h>
+//#include <rose.h>
 
+using namespace std;
 
 int main(int argc, char* argv[]) {
-    std::cout << "yices check" << std::endl;
+//    std::cout << "yices check" << std::endl;
     int i = yicesCheck(argc, argv);
-    std::cout << "end yices check" << std::endl;
+//    std::cout << "end yices check" << std::endl;
 }
