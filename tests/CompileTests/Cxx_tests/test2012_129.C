8ee82f923d2cd7ef5178a0e64a35b95525a7b6d3
Dan Quinlan <dquinlan@llnl.gov>
2012-07-20 19:04:48 -0700

    Newest C++11 support...(942 passing out of 972 tests)

--- tests/CompileTests/Cxx_tests/test2012_129.C
+++ tests/CompileTests/Cxx_tests/test2012_129.C
@@ -8,7 +8,10 @@ class X
                return valueArray[0];
              }
 #endif
+//        static const int arraySize = 16;
           static const int arraySize = 16;
           static const int valueArray[arraySize+10];
    };
 
+// const int X::arraySize = 16;
+const int X::arraySize;
