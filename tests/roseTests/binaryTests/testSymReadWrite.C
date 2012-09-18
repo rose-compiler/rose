06268f56570b0cf395a76bae1a301e8ca9a5f525
Robb Matzke <matzke1@llnl.gov>
2012-09-18 08:47:26 -0400

    (Binary Analysis) SymReadWrite test does not require an SMT solver
    (cherry picked from commit 958425a92c5edec5f187eb50cce78a057fe89fad)

--- tests/roseTests/binaryTests/testSymReadWrite.C
+++ tests/roseTests/binaryTests/testSymReadWrite.C
@@ -1,21 +1,15 @@
 #include "rose.h"
-
 #include "SymbolicSemantics.h"
-#include "YicesSolver.h"
 
 struct Analysis: public AstSimpleProcessing {
     void visit(SgNode *node) {
         SgAsmBlock *block = isSgAsmBlock(node);
         if (block && block->has_instructions()) {
-            
-            YicesSolver smt_solver;
-            smt_solver.set_linkage(YicesSolver::LM_EXECUTABLE); // more debugging capabilities
-
             using namespace BinaryAnalysis::InstructionSemantics;
             typedef SymbolicSemantics::Policy<SymbolicSemantics::State, SymbolicSemantics::ValueType> Policy;
             typedef X86InstructionSemantics<Policy, SymbolicSemantics::ValueType> Semantics;
 
-            Policy policy(&smt_solver);
+            Policy policy(NULL/*no SMT solver*/);
             Semantics semantics(policy);
 
             const SgAsmStatementPtrList &stmts = block->get_statementList();
