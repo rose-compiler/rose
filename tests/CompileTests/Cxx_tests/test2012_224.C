31e57c7470317270d289f24e661f1ebcf84e5f4c
Dan Quinlan <dquinlan@llnl.gov>
2012-09-29 09:07:51 -0700

    [EDG 4.4 and C11] Fix C issues for nginx 100K test application (added new test codes).

---
+++ tests/CompileTests/Cxx_tests/test2012_224.C
@@ -0,0 +1,14 @@
+// Simplest version of failing code:
+struct X
+   {
+  // int x1;
+
+  // This must be a variable declaration for this code to fail.
+     struct Y
+        {
+       // int x2;
+        } B;
+   };
+
+// Y yyy;
+
