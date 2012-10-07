600f1dc98f43b19e1b0de2e16c8e8582f8c3fccb
Dan Quinlan <dquinlan@llnl.gov>
2012-10-07 15:03:38 -0700

    [EDG 4.4 and C11] Fixed static function specification plus more C issues: working on errors in zsh and cherokee files.

---
+++ tests/CompileTests/Cxx_tests/test2012_225.C
@@ -0,0 +1,12 @@
+void foo (int x)
+   {
+     class A
+        {
+          public:
+             A () {}
+             int foo (int x) { return x; }
+
+          // A (int) {}
+          // operator bool () { return false; }
+        } x1;
+   }
