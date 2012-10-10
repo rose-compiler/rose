393ab5c200d7c0dfde61d253270242cffda7cd60
Dan Quinlan <dquinlan@llnl.gov>
2012-10-09 18:58:53 -0700

    [EDG 4.4 and C11] Fixed issues suggested by Robb as part of getting other directories in ROSE to pass internal testing.

--- src/backend/unparser/CxxCodeGeneration/unparseCxx.h
+++ src/backend/unparser/CxxCodeGeneration/unparseCxx.h
@@ -435,11 +435,11 @@ class Unparse_ExprStmt : public UnparseLanguageIndependentConstructs
        // TV (05/06/2010): SgCudaKernelCallExp
           virtual void unparseCudaKernelCall(SgExpression* expr, SgUnparse_Info& info);
 
-      // DQ (7/21/2012): New IR node (only seen in C++11 so far).
-         void unparseTemplateParameterValue(SgExpression* expr, SgUnparse_Info& info);
+       // DQ (7/21/2012): New IR node (only seen in C++11 so far).
+          void unparseTemplateParameterValue(SgExpression* expr, SgUnparse_Info& info);
 
-      // DQ (10/5/2012): Added support for GNU type attributes.
-         void unparseTypeAttributes ( SgDeclarationStatement* declaration );
+       // DQ (10/5/2012): Added support for GNU type attributes.
+          void unparseTypeAttributes ( SgDeclarationStatement* declaration );
    };
 
 #endif
