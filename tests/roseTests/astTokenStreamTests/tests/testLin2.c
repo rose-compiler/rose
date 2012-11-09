56c58dea9fbaf77fbd54031b83a308df0310a404
Robb Matzke <matzke1@llnl.gov>
2012-11-09 12:42:24 -0500

    (Token Stream) Changed tests to avoid differences between EDG3 and 4
    * Renamed specimen functions from "main" to "foo" and added explicit
      return statements to avoid differences with how EDG adds implicit
      return statements to functions named "main".
    
    * Renamed existing *.out files to *.edg3.ans" files to better
      document that these are expected answers.
    
    * Added *.edg4.ans files to be used when ROSE is compiled against
      EDG 4.x.  The edg4x version of ROSE produces fewer extraneous
      paretheses.
    
    (cherry picked from commit cf7a1c7ec15482276a3efdcd403dff09432b325b)

--- tests/roseTests/astTokenStreamTests/tests/testLin2.c
+++ tests/roseTests/astTokenStreamTests/tests/testLin2.c
@@ -1,9 +1,9 @@
 #include <math.h>
 
-int main()
+int foo()
 {
     double A4, MmP, lP, Lambdasun;
       A4 = ((Lambdasun - 0.1114041 * A4 - Lambdasun) - 360.0 * (floor((Lambdasun - 0.1114041 * A4 - Lambdasun) / 360.0)));
-
+    return 0;
 };
 
