38c137f2e03e56d8881d4af7609412085c470c63
Sara Royuela Alcazar <royuelaalcaz1@llnl.gov>
2012-11-12 14:12:41 -0800

    Add compiler tests for OpenMP clauses containing macro definitions
    See #db95a183b20fac50ea2931072a2c47fd6ba46949

---
+++ tests/CompileTests/OpenMP_tests/macroIds.c
@@ -0,0 +1,23 @@
+#include "macroIdsDef.h"
+#include "macroIds.h"
+#include <omp.h>
+
+#define THREADS_1 2
+
+int tmp()
+{
+    return 0;
+}
+
+void tmp2()
+{
+#pragma omp parallel num_threads( THREADS_1 )
+    {
+        i++;
+    }
+#pragma omp parallel num_threads( THREADS_2 + THREADS_3 )
+    {
+        i++;
+    }
+
+}
