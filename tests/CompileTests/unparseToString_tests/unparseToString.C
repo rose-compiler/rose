92538269d96a3bac69e07b97271b4c94f17a2943
Robb Matzke <matzke1@llnl.gov>
2012-12-27 08:56:40 -0500

    (Unparse to string) Updating the test

--- tests/CompileTests/unparseToString_tests/unparseToString.C
+++ tests/CompileTests/unparseToString_tests/unparseToString.C
@@ -1,48 +1,32 @@
 #include <rose.h>
 #include <iostream>
 
-using namespace std;
-
 bool unparse_all = false;
 
-class TypeUnparser : public ROSE_VisitTraversal {
+class TypeUnparser: public ROSE_VisitTraversal {
 public:
-    void visit( SgNode *n );
+    void visit(SgNode *n) {
+        if (SgType *type= isSgType(n))
+            std::cout << type->unparseToString() << std::endl;
+    }
 };
 
-void TypeUnparser::visit( SgNode *n )
-{
-    SgType *type= dynamic_cast<SgType*>(n);
-    if (!type)
-        return;
-    cout << type->unparseToString() << endl;
-}
-
-class TypeUnparserOnAST : public AstSimpleProcessing {
+class TypeUnparserOnAST: public AstSimpleProcessing {
 public:
-    void visit( SgNode *n );
+    void visit(SgNode *n) {
+        if (unparse_all) {
+            if (SgFunctionDeclaration *d = isSgFunctionDeclaration(n))
+                std::cout << "function declaration, name = " << d->get_name().getString() << std::endl;
+            std::cout <<n->unparseToString() <<std::endl;
+        }
+
+        if (SgVariableDeclaration* variableDeclaration = isSgVariableDeclaration(n)) {
+            SgInitializedNamePtrList &varList = variableDeclaration->get_variables();
+            for (SgInitializedNamePtrList::iterator i=varList.begin(); i!=varList.end(); ++i)
+                std::cout << (*i)->get_typeptr()->unparseToString() << std::endl;
+        }
+    }
 };
-
-void TypeUnparserOnAST::visit( SgNode *n )
-{
-    if (unparse_all) {
-        if (SgFunctionDeclaration *d = isSgFunctionDeclaration(n)) {
-            cout << "function declaration, name = " << d->get_name().getString() << endl;
-        }
-        string s = n->unparseToString();
-        cout << s << endl;
-    }
-
-    SgVariableDeclaration* variableDeclaration = isSgVariableDeclaration(n);
-    if (variableDeclaration != NULL) {
-        SgInitializedNamePtrList & varList = variableDeclaration->get_variables();
-        SgInitializedNamePtrList::iterator i = varList.begin();
-        while (i != varList.end()) {
-            cout << (*i)->get_typeptr()->unparseToString() << endl;
-            i++;
-        }
-    }
-}
 
 int main(int argc, char **argv)
 {
