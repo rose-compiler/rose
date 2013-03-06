036c50c11cfba695425e49f53fd45322e418ca84
Markus Schordan <schordan@technikum-wien.at>
2013-03-06 16:40:51 +0100

    updated for-loop test case.

--- projects/CodeThorn/src/tests/test_for4.c
+++ projects/CodeThorn/src/tests/test_for4.c
@@ -1,13 +1,12 @@
+#include <stdio.h>
 // we intentionally avoid using ++,-- in this test
 int main() {
   int sum=0;
-  for(int i=1;i<6;i++) {
-	for(int j=1,k=j;j<3;j++) {
-	  int s; // int s=j*k computes to top
-	  s=j*k;
-	  sum=sum+s;
+  for(int i=1;i<3;i++) {
+	for(int j=1;j<3;j++) {
+	  sum=sum+j;
 	}
-	i++;
   }
+  printf("sum:%d\n",sum);
   return 0;
 }
