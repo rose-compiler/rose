df8b2e32ea5b8010b08b0d63146c46963f167a45
Markus Schordan <schordan@technikum-wien.at>
2013-03-06 16:52:30 +0100

    updated for-loop test case.

--- projects/CodeThorn/src/tests/test_for4.c
+++ projects/CodeThorn/src/tests/test_for4.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-// we intentionally avoid using ++,-- in this test
 int main() {
   int sum=0;
-  for(int i=1;i<3;i++) {
-	for(int j=1;j<3;j++) {
+  for(int i=0;i<3;i++) {
+	for(int j=0;j<3;j++) {
 	  sum=sum+j;
 	}
   }
