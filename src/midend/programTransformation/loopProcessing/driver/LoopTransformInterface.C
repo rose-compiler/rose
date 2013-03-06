c627162055855cb29fb6e6fca12df5ab43df366b
Zack Galbreath <zack.galbreath@kitware.com>
2013-03-06 15:36:39 -0500

    (CMAKE ROSE) replace __out with __outstream
    __out is not defined on Visual Studio
    
    (cherry picked from commit (EDG3) 0ed1af3613399e95d3b3d2ac269b5e0789367fcc)

--- src/midend/programTransformation/loopProcessing/driver/LoopTransformInterface.C
+++ src/midend/programTransformation/loopProcessing/driver/LoopTransformInterface.C
@@ -231,7 +231,7 @@ TransformTraverse(AstInterfaceImpl& scope,const AstNodePtr& head)
 }
 
 void LoopTransformInterface::
-PrintTransformUsage(std::ostream& __out)
+PrintTransformUsage(std::ostream& __outstream)
 {
   std::cerr << "-debugloop: print debugging information for loop transformations; \n"
             << "-debugdep: print debugging information for dependence analysis; \n"
@@ -240,7 +240,7 @@ PrintTransformUsage(std::ostream& __out)
             << "opt <level=0>: the level of loop optimizations to apply; by default, only the outermost level is optimized;\n"
             << LoopUnrolling::cmdline_help() << std::endl
             << BreakupStatement::cmdline_help() << std::endl;
-  LoopTransformOptions::GetInstance()->PrintUsage(__out);
+  LoopTransformOptions::GetInstance()->PrintUsage(__outstream);
 }
 
 //////////////////////////////////
