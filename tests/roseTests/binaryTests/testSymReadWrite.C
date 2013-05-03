e5fae30329ea08db39043b13589d89d5f63725ba
Robb Matzke <matzke1@llnl.gov>
2013-05-02 23:50:56 -0400

    (Binary Analysis) Updated test answers
    Answers changed because of better symbolic simplification.

--- tests/roseTests/binaryTests/testSymReadWrite.C
+++ tests/roseTests/binaryTests/testSymReadWrite.C
@@ -1,6 +1,8 @@
 #include "rose.h"
-#include "SymbolicSemantics.h"
 
+#if 0 // OLD API
+
+#include "SymbolicSemantics.h"
 struct Analysis: public AstSimpleProcessing {
     void visit(SgNode *node) {
         SgAsmBlock *block = isSgAsmBlock(node);
@@ -24,6 +26,33 @@ struct Analysis: public AstSimpleProcessing {
         }
     }
 };
+
+#else // NEW API
+
+#include "SymbolicSemantics2.h"
+#include "DispatcherX86.h"
+struct Analysis: public AstSimpleProcessing {
+    void visit(SgNode *node) {
+        SgAsmBlock *block = isSgAsmBlock(node);
+        if (block && block->has_instructions()) {
+            using namespace BinaryAnalysis::InstructionSemantics2;
+            const RegisterDictionary *regdict = RegisterDictionary::dictionary_i386();
+            BaseSemantics::RiscOperatorsPtr ops = SymbolicSemantics::RiscOperators::instance(regdict);
+            BaseSemantics::DispatcherPtr dispatcher = DispatcherX86::instance(ops);
+            const SgAsmStatementPtrList &stmts = block->get_statementList();
+            for (SgAsmStatementPtrList::const_iterator si=stmts.begin(); si!=stmts.end(); ++si) {
+                SgAsmx86Instruction *insn = isSgAsmx86Instruction(*si);
+                if (insn) {
+                    std::cout <<unparseInstructionWithAddress(insn) <<"\n";
+                    dispatcher->processInstruction(insn);
+                    std::cout <<*ops <<"\n";
+                }
+            }
+        }
+    }
+};
+            
+#endif
 
 int
 main(int argc, char *argv[])
