e0b8970209aaed57fe036e12780e7b42e984956c
Sara Royuela Alcazar <sara.royuela@gmail.com>
2013-05-10 19:10:47 -0700

    Differenciate dinamic and static arrays in OMP outlining
    Dinamically allocated arrays can be passed as a void pointer, whereas
    statically allocated arrays need to keep the array form in the outlined
    parameters struct.
    
    This still has to be tested deeply.

--- src/midend/programTransformation/astOutlining/Transform.cc
+++ src/midend/programTransformation/astOutlining/Transform.cc
@@ -22,86 +22,106 @@ using namespace std;
 using namespace SageBuilder;
 using namespace SageInterface;
 // =====================================================================
+
+static SgType * buildArrayMemberType( SgType * member_type )
+{
+    SgType * ret;
+    SgType * defined_type = member_type->stripType( SgType::STRIP_TYPEDEF_TYPE );
+    if( isSgArrayType( defined_type ) )
+        ret = buildArrayType( buildArrayMemberType( isSgArrayType( defined_type )->get_base_type( ) ),
+                              isSgArrayType( member_type )->get_index( ) );
+    else
+        ret = member_type;
+    return ret;
+}
+
 // ! create a struct to contain data members for variables to be passed as parameters
 // A wrapper struct for variables passed to the outlined function
 // Each variable (e.g a) has two choices
 //   1. store the value of a:  the same type representation in the struct
 //   2. store the address of a:  pointer type of a
 SgClassDeclaration* Outliner::generateParameterStructureDeclaration(
-                               SgBasicBlock* s, // the outlining target
-                               const std::string& func_name_str, // the name for the outlined function, we generate the name of struct based on this.
-                               const ASTtools::VarSymSet_t& syms, // variables to be passed as parameters
-                               ASTtools::VarSymSet_t& symsUsingAddress, // variables whose addresses are stored into the struct 
-                               SgScopeStatement* func_scope ) // the scope of the outlined function, could be different from s's global scope
+        SgBasicBlock* s, // the outlining target
+        const std::string& func_name_str, // the name for the outlined function, we generate the name of struct based on this.
+        const ASTtools::VarSymSet_t& syms, // variables to be passed as parameters
+        ASTtools::VarSymSet_t& symsUsingAddress, // variables whose addresses are stored into the struct 
+        SgScopeStatement* func_scope ) // the scope of the outlined function, could be different from s's global scope
 {
-  SgClassDeclaration* result = NULL;
-  // no need to generate the declaration if no variables are to be passed
-  if (syms.empty()) 
-    return result;
+    SgClassDeclaration* result = NULL;
+    // no need to generate the declaration if no variables are to be passed
+    if( syms.empty( ) ) 
+        return result;
+    
+    ROSE_ASSERT( s != NULL );
+    ROSE_ASSERT( func_scope != NULL );
+    // this declaration will later on be inserted right before the outlining target calling the outlined function
+    ROSE_ASSERT( isSgGlobal( func_scope ) != NULL );
+    string decl_name = func_name_str+"_data";
+    
+    result = buildStructDeclaration( decl_name, getGlobalScope( s ) );
+    //  result ->setForward(); // cannot do this!! it becomes prototype
+    //  if (result->get_firstNondefiningDeclaration()  )
+    //   ROSE_ASSERT(isSgClassDeclaration(result->get_firstNondefiningDeclaration())->isForward() == true);
+    //   cout<<"Debug Outliner::generateParameterStructureDeclaration(): struct address ="<<result <<" firstNondefining address ="<<result->get_firstNondefiningDeclaration()<<endl;
 
-  ROSE_ASSERT (s != NULL);
-  ROSE_ASSERT (func_scope != NULL);
-  // this declaration will later on be inserted right before the outlining target calling the outlined function
-  ROSE_ASSERT (isSgGlobal(func_scope) != NULL);
-  string decl_name = func_name_str+"_data";
-  
-  result = buildStructDeclaration(decl_name, getGlobalScope(s));
-//  result ->setForward(); // cannot do this!! it becomes prototype
-//  if (result->get_firstNondefiningDeclaration()  )
-//   ROSE_ASSERT(isSgClassDeclaration(result->get_firstNondefiningDeclaration())->isForward() == true);
-//   cout<<"Debug Outliner::generateParameterStructureDeclaration(): struct address ="<<result <<" firstNondefining address ="<<result->get_firstNondefiningDeclaration()<<endl;
-
-  // insert member variable declarations to it
-  SgClassDefinition *def = result->get_definition();
-  ROSE_ASSERT (def != NULL); 
-  SgScopeStatement* def_scope = isSgScopeStatement (def);
-  ROSE_ASSERT (def_scope != NULL); 
-  for (ASTtools::VarSymSet_t::const_iterator i = syms.begin ();
-      i != syms.end (); ++i)
-  { 
-    const SgInitializedName* i_name = (*i)->get_declaration ();
-    ROSE_ASSERT (i_name);
-    const SgVariableSymbol* i_symbol  = isSgVariableSymbol(i_name->get_symbol_from_symbol_table ());
-    ROSE_ASSERT (i_symbol != NULL);
-    string member_name= i_name->get_name ().str ();
-    SgType* member_type = i_name->get_type() ;
-    // use pointer type or its original type?
-    if (symsUsingAddress.find(i_symbol) != symsUsingAddress.end())
-    {
-       member_name = member_name+"_p";
-       // member_type = buildPointerType(member_type);
-       // Liao, 10/26/2009
-       // We use void* instead of type* to ease the handling of C++ class pointers wrapped into the data structure
-       // Using void* can avoid adding a forward class declaration  which is needed for classA *
-       // It also simplifies unparsing: unparsing the use of classA* has some complications. 
-       // The downside is that type casting is needed for setting and using the pointer typed values
-       member_type = buildPointerType(buildVoidType());
+    // insert member variable declarations to it
+    SgClassDefinition *def = result->get_definition( );
+    ROSE_ASSERT( def != NULL ); 
+    SgScopeStatement* def_scope = isSgScopeStatement( def );
+    ROSE_ASSERT( def_scope != NULL ); 
+    for( ASTtools::VarSymSet_t::const_iterator i = syms.begin( ); i != syms.end( ); ++i )
+    { 
+        const SgInitializedName* i_name = (*i)->get_declaration( );
+        ROSE_ASSERT( i_name );
+        const SgVariableSymbol* i_symbol = isSgVariableSymbol( i_name->get_symbol_from_symbol_table( ) );
+        ROSE_ASSERT( i_symbol != NULL );
+        string member_name = i_name->get_name( ).str( );
+        SgType* member_type = i_name->get_type( ) ;
+        // use pointer type or its original type?
+        if (symsUsingAddress.find(i_symbol) != symsUsingAddress.end())
+        {
+            member_name = member_name+"_p";
+            
+            // member_type = buildPointerType(member_type);
+            // Liao, 10/26/2009
+            // We use void* instead of type* to ease the handling of C++ class pointers wrapped into the data structure
+            // Using void* can avoid adding a forward class declaration  which is needed for classA *
+            // It also simplifies unparsing: unparsing the use of classA* has some complications. 
+            // The downside is that type casting is needed for setting and using the pointer typed values
+            if( isSgArrayType( member_type->stripType( SgType::STRIP_TYPEDEF_TYPE ) ) )
+            {   // Sara, 05/10/2013
+                // An array type here means that the memory was statically allocated.
+                // In this case we need the array to be allocated in the struct
+                member_type = buildPointerType( buildArrayMemberType( member_type ) );
+            }
+            else
+            {
+                member_type = buildPointerType( buildVoidType( ) );
+            }
+        }
+        SgVariableDeclaration *member_decl = buildVariableDeclaration( member_name, member_type, NULL, def_scope );
+        appendStatement( member_decl, def_scope );
     }
 
-    SgVariableDeclaration *member_decl = buildVariableDeclaration(member_name, member_type, NULL, def_scope);
-    appendStatement (member_decl, def_scope);
-  }
-
-  // insert it before the s, but must be in a global scope
-  // s might be within a class, namespace, etc. we need to find its ancestor scope
-  SgNode* global_scoped_ancestor = getEnclosingFunctionDefinition(s,false); 
-  while (!isSgGlobal(global_scoped_ancestor->get_parent())) 
- // use get_parent() instead of get_scope() since a function definition node's scope is global while its parent is its function declaration
-  {
-    global_scoped_ancestor = global_scoped_ancestor->get_parent();
-   }
-//  cout<<"global_scoped_ancestor class_name: "<<global_scoped_ancestor->class_name()<<endl; 
-  ROSE_ASSERT (isSgStatement(global_scoped_ancestor));
-  insertStatementBefore(isSgStatement(global_scoped_ancestor), result); 
-  moveUpPreprocessingInfo(result,isSgStatement(global_scoped_ancestor));
-
-  if (global_scoped_ancestor->get_parent() != func_scope )
-  {
-  //TODO 
-   cout<<"Outliner::generateParameterStructureDeclaration() separated file case is not yet handled."<<endl;
-   ROSE_ASSERT (false);
-  }
-  return result;
+    // insert it before the s, but must be in a global scope
+    // s might be within a class, namespace, etc. we need to find its ancestor scope
+    SgNode* global_scoped_ancestor = getEnclosingFunctionDefinition( s, false ); 
+    while( !isSgGlobal( global_scoped_ancestor->get_parent( ) ) ) 
+    // use get_parent() instead of get_scope() since a function definition node's scope is global while its parent is its function declaration
+    {
+        global_scoped_ancestor = global_scoped_ancestor->get_parent( );
+    }
+    //  cout<<"global_scoped_ancestor class_name: "<<global_scoped_ancestor->class_name()<<endl; 
+    ROSE_ASSERT( isSgStatement( global_scoped_ancestor ) );
+    insertStatementBefore( isSgStatement( global_scoped_ancestor ), result ); 
+    moveUpPreprocessingInfo( result, isSgStatement( global_scoped_ancestor ) );
+    
+    if( global_scoped_ancestor->get_parent( ) != func_scope )
+    {   //TODO 
+        cout << "Outliner::generateParameterStructureDeclaration() separated file case is not yet handled." << endl;
+        ROSE_ASSERT( false );
+    }
+    return result;
 }
 
 //!  A helper function to decide if some variables need to be restored from their clones in the end of the outlined function
@@ -459,11 +479,7 @@ static SgStatement* build_array_packing_statement( SgExpression * lhs, SgExpress
     SgStatement * loop_init = buildAssignStatement( buildVarRefExp( loop_index_name, scope ), buildIntVal( 0 ) );
     
     // Get the real type of the LHS
-    SgType * lhs_type = lhs->get_type();
-    while( isSgTypedefType( lhs_type ) )
-    {
-        lhs_type = isSgTypedefType( lhs_type )->get_base_type( );
-    }
+    SgType * lhs_type = lhs->get_type()->stripType( SgType::STRIP_TYPEDEF_TYPE );
     ROSE_ASSERT( isSgArrayType( lhs_type ) );
     
     // Loop test
@@ -477,18 +493,14 @@ static SgStatement* build_array_packing_statement( SgExpression * lhs, SgExpress
     SgExpression * assign_lhs = buildPntrArrRefExp( lhs, buildVarRefExp( loop_index_name, scope ) );
     SgExpression * assign_rhs = buildPntrArrRefExp( rhs, buildVarRefExp( loop_index_name, scope ) );
     SgStatement * loop_body = NULL;
-    SgType * assign_lhs_type = assign_lhs->get_type();
-    while( isSgTypedefType( assign_lhs_type ) )
-    {
-        assign_lhs_type = isSgTypedefType( assign_lhs_type )->get_base_type( );
-    }
+    SgType * assign_lhs_type = assign_lhs->get_type()->stripType( SgType::STRIP_TYPEDEF_TYPE );
     if( isSgArrayType( assign_lhs_type ) )
     {
         loop_body = build_array_packing_statement( assign_lhs, assign_rhs, target );
     }
     else
     {
-        loop_body = buildAssignStatement(assign_lhs, assign_rhs);
+        loop_body = buildAssignStatement( assign_lhs, assign_rhs );
     }
     
     // Loop satement
@@ -578,13 +590,9 @@ std::string Outliner::generatePackingStatements(SgStatement* target, ASTtools::V
       ROSE_ASSERT (class_def != NULL);
       lhs = buildDotExp ( buildVarRefExp(out_argv), buildVarRefExp (member_name, class_def));
       
-      SgType * lhs_type = lhs->get_type();
-      while( isSgTypedefType( lhs_type ) )
-      {
-          lhs_type = isSgTypedefType( lhs_type )->get_base_type( );
-      }
-      if( isSgArrayType( lhs_type ) )
-      {// Copy each position of the array
+      SgType * lhs_type = lhs->get_type()->stripType( SgType::STRIP_TYPEDEF_TYPE );
+      if( pdsyms.find(i_symbol) == pdsyms.end() && isSgArrayType( lhs_type ) )
+      {   // Copy each position of the array
           assignment = build_array_packing_statement( lhs, rhs, target );
       }
       else
