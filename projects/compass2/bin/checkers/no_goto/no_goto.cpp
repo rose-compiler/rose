b2e9850fc574d4d38e10a4e46588ac577a2a70d8
Sam Kelly <kelly64@llnl.gov>
2013-07-02 16:28:44 -0700

    (Compass2) Add checker: NoGoto

---
+++ projects/compass2/bin/checkers/no_goto/no_goto.cpp
@@ -0,0 +1,132 @@
+/**
+ * \file    no_goto.cpp
+ * \author  Sam Kelly <kelly64@llnl.gov>
+ * \date    July 2, 2013
+ */
+
+#include "rose.h"
+#include "compass2/compass.h"
+#include <boost/foreach.hpp>
+#include "CodeThorn/src/AstMatching.h"
+#include "CodeThorn/src/AstTerm.h"
+
+
+using std::string;
+using namespace StringUtility;
+using namespace CodeThorn;
+
+extern const Compass::Checker* const noGotoChecker;
+
+/*-----------------------------------------------------------------------------
+ * Interface
+ *---------------------------------------------------------------------------*/
+
+#ifndef COMPASS_NO_GOTO_H
+#define COMPASS_NO_GOTO_H
+
+namespace CompassAnalyses
+{
+/**
+ * \brief Finds Goto Statements
+ */
+namespace NoGoto
+{
+  extern const string checker_name;
+  extern const string short_description;
+  extern const string long_description;
+  extern       string source_directory;
+
+  class CheckerOutput: public Compass::OutputViolationBase {
+   public:
+    explicit CheckerOutput(SgNode *const node);
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(CheckerOutput);
+  };
+
+  bool IsNodeNotInUserLocation(const SgNode* node)
+  {
+      const SgLocatedNode* located_node = isSgLocatedNode(node);
+      if (located_node != NULL)
+      {
+          return ! Compass::IsNodeInUserLocation(
+                      located_node,
+                      NoGoto::source_directory);
+      }
+      else
+      {
+          return true;
+      }
+  };
+
+} // ::CompassAnalyses
+} // ::NoGoto
+#endif // COMPASS_NO_GOTO_H
+
+/*-----------------------------------------------------------------------------
+ * Implementation
+ *---------------------------------------------------------------------------*/
+
+namespace CompassAnalyses
+{
+ namespace NoGoto
+ {
+  const string checker_name      = "NoGoto";
+  const string short_description = "GOTO Statement Found";
+  const string long_description  = "Finds Goto Statements";
+  string source_directory = "/";
+ }
+}
+
+CompassAnalyses::NoGoto::
+CheckerOutput::CheckerOutput(SgNode *const node)
+    : OutputViolationBase(node,
+                          ::noGotoChecker->checkerName,
+                          ::noGotoChecker->shortDescription) {}
+
+static void
+run(Compass::Parameters parameters, Compass::OutputObject* output)
+  {
+      // We only care about source code in the user's space, not,
+      // for example, Boost or system files.
+      string target_directory =
+          parameters["general::target_directory"].front();
+      CompassAnalyses::NoGoto::source_directory.assign(target_directory);
+      
+      // Use the pre-built ROSE AST
+      SgProject* sageProject = Compass::projectPrerequisite.getProject();
+      SgNode* root_node = (SgNode*) sageProject;
+      
+      // search for SgGotoStatements
+      AstMatching matcher;
+      MatchResult matches = matcher
+          .performMatching("$r = SgGotoStatement", root_node);
+
+      BOOST_FOREACH(SingleMatchVarBindings match, matches)
+      {
+        SgGotoStatement *goto_statement = (SgGotoStatement *)match["$r"];
+        output->addOutput(
+            new CompassAnalyses::NoGoto::
+            CheckerOutput(goto_statement));
+      }
+
+  }
+
+// Remove this function if your checker is not an AST traversal
+static Compass::AstSimpleProcessingWithRunFunction*
+createTraversal(Compass::Parameters params, Compass::OutputObject* output)
+  {
+    return NULL;
+  }
+
+extern const Compass::Checker* const noGotoChecker =
+  new Compass::CheckerUsingAstSimpleProcessing(
+      CompassAnalyses::NoGoto::checker_name,
+    // Descriptions should not include the newline character "\n".
+      CompassAnalyses::NoGoto::short_description,
+      CompassAnalyses::NoGoto::long_description,
+      Compass::C | Compass::Cpp,
+      Compass::PrerequisiteList(1, &Compass::projectPrerequisite),
+      run,
+      createTraversal);
+
