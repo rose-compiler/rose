40f1554f5ccac81bef2aaad28d26c1f105285d2e
Sam Kelly <kelly64@llnl.gov>
2013-07-03 08:22:31 -0700

    (Compass2) Add templates for NoRand and BooleanIsHas checkers

--- projects/compass2/bin/checkers/boolean_is_has/boolean_is_has.cpp
+++ projects/compass2/bin/checkers/boolean_is_has/boolean_is_has.cpp
@@ -1,41 +1,39 @@
 /**
- * \file    no_goto.cpp
- * \author  Sam Kelly <kelly64@llnl.gov> <kellys@dickinson.edu>
- * \date    July 2, 2013
+ * \file
+ * \author
+ * \date
  */
 
 #include "rose.h"
 #include "compass2/compass.h"
-#include <boost/foreach.hpp>
-#include "CodeThorn/src/AstMatching.h"
-#include "CodeThorn/src/AstTerm.h"
-
 
 using std::string;
 using namespace StringUtility;
-using namespace CodeThorn;
 
-extern const Compass::Checker* const noGotoChecker;
+extern const Compass::Checker* const booleanIsHasChecker;
 
 /*-----------------------------------------------------------------------------
  * Interface
  *---------------------------------------------------------------------------*/
 
-#ifndef COMPASS_NO_GOTO_H
-#define COMPASS_NO_GOTO_H
+#ifndef COMPASS_BOOLEAN_IS_HAS_H
+#define COMPASS_BOOLEAN_IS_HAS_H
 
 namespace CompassAnalyses
 {
 /**
- * \brief Finds Goto Statements
+ * \brief Description of checker
  */
-namespace NoGoto
+namespace BooleanIsHas
 {
   extern const string checker_name;
   extern const string short_description;
   extern const string long_description;
   extern       string source_directory;
 
+  /**
+   * \brief Specificaiton of checker results.
+   */
   class CheckerOutput: public Compass::OutputViolationBase {
    public:
     explicit CheckerOutput(SgNode *const node);
@@ -51,7 +49,7 @@ namespace NoGoto
       {
           return ! Compass::IsNodeInUserLocation(
                       located_node,
-                      NoGoto::source_directory);
+                      BooleanIsHas::source_directory);
       }
       else
       {
@@ -60,8 +58,8 @@ namespace NoGoto
   };
 
 } // ::CompassAnalyses
-} // ::NoGoto
-#endif // COMPASS_NO_GOTO_H
+} // ::BooleanIsHas
+#endif // COMPASS_BOOLEAN_IS_HAS_H
 
 /*-----------------------------------------------------------------------------
  * Implementation
@@ -69,20 +67,20 @@ namespace NoGoto
 
 namespace CompassAnalyses
 {
- namespace NoGoto
+ namespace BooleanIsHas
  {
-  const string checker_name      = "NoGoto";
-  const string short_description = "GOTO Statement Found";
-  const string long_description  = "Finds Goto Statements";
+  const string checker_name      = "BooleanIsHas";
+  const string short_description = "";
+  const string long_description  = "";
   string source_directory = "/";
  }
 }
 
-CompassAnalyses::NoGoto::
+CompassAnalyses::BooleanIsHas::
 CheckerOutput::CheckerOutput(SgNode *const node)
     : OutputViolationBase(node,
-                          ::noGotoChecker->checkerName,
-                          ::noGotoChecker->shortDescription) {}
+                          ::booleanIsHasChecker->checkerName,
+                          ::booleanIsHasChecker->shortDescription) {}
 
 static void
 run(Compass::Parameters parameters, Compass::OutputObject* output)
@@ -91,25 +89,14 @@ run(Compass::Parameters parameters, Compass::OutputObject* output)
       // for example, Boost or system files.
       string target_directory =
           parameters["general::target_directory"].front();
-      CompassAnalyses::NoGoto::source_directory.assign(target_directory);
+      CompassAnalyses::BooleanIsHas::source_directory.assign(target_directory);
       
       // Use the pre-built ROSE AST
       SgProject* sageProject = Compass::projectPrerequisite.getProject();
       SgNode* root_node = (SgNode*) sageProject;
       
-      // search for SgGotoStatements
-      AstMatching matcher;
-      MatchResult matches = matcher
-          .performMatching("$r = SgGotoStatement", root_node);
-
-      BOOST_FOREACH(SingleMatchVarBindings match, matches)
-      {
-        SgGotoStatement *goto_statement = (SgGotoStatement *)match["$r"];
-        output->addOutput(
-            new CompassAnalyses::NoGoto::
-            CheckerOutput(goto_statement));
-      }
-
+      // perform AST matching here
+      
   }
 
 // Remove this function if your checker is not an AST traversal
@@ -119,12 +106,12 @@ createTraversal(Compass::Parameters params, Compass::OutputObject* output)
     return NULL;
   }
 
-extern const Compass::Checker* const noGotoChecker =
+extern const Compass::Checker* const booleanIsHasChecker =
   new Compass::CheckerUsingAstSimpleProcessing(
-      CompassAnalyses::NoGoto::checker_name,
+      CompassAnalyses::BooleanIsHas::checker_name,
     // Descriptions should not include the newline character "\n".
-      CompassAnalyses::NoGoto::short_description,
-      CompassAnalyses::NoGoto::long_description,
+      CompassAnalyses::BooleanIsHas::short_description,
+      CompassAnalyses::BooleanIsHas::long_description,
       Compass::C | Compass::Cpp,
       Compass::PrerequisiteList(1, &Compass::projectPrerequisite),
       run,
