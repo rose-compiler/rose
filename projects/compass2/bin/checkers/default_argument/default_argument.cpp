80d111f038fbe224f0481a09f2a4d697ab235092
Michael Roup <roup.michael@gmail.com>
2013-07-17 11:06:06 -0700

    (Compass2) Add checker: default argument

--- projects/compass2/bin/checkers/default_argument/default_argument.cpp
+++ projects/compass2/bin/checkers/default_argument/default_argument.cpp
@@ -1,15 +1,15 @@
 /**
- * \file    default_argument.cpp
- * \author  Justin Too <too1@llnl.gov>
- * \date    August 15, 2012
+ * \file   default_argument.cpp
+ * \author Mike Roup <roup1@llnl.gov>
+ * \date   July 17, 2013
  */
-
-#include "rose.h"
-#include "string_functions.h"
 
 #include <boost/foreach.hpp>
 
+#include "rose.h"
 #include "compass2/compass.h"
+#include "CodeThorn/src/AstMatching.h"
+#include "CodeThorn/src/AstTerm.h"
 
 using std::string;
 using namespace StringUtility;
@@ -26,13 +26,14 @@ extern const Compass::Checker* const defaultArgumentChecker;
 namespace CompassAnalyses
 {
 /**
- * \brief Detect default arguments in function parameter lists.
+ * \brief Description of checker
  */
 namespace DefaultArgument
 {
   extern const string checker_name;
   extern const string short_description;
   extern const string long_description;
+  extern       string source_directory;
 
   /**
    * \brief Specificaiton of checker results.
@@ -45,64 +46,21 @@ namespace DefaultArgument
     DISALLOW_COPY_AND_ASSIGN(CheckerOutput);
   };
 
-  /**
-   * \brief Specification of AST traversal.
-   */
-  class Traversal : public Compass::AstSimpleProcessingWithRunFunction {
-   public:
-    Traversal(Compass::Parameters inputParameters,
-              Compass::OutputObject *output);
-
-    /**
-     * \brief Get inherited attribute for traversal.
-     *
-     * We are not using an inherited attribute for this checker.
-     *
-     * \returns NULL
-     */
-    void* initialInheritedAttribute() const
+  bool IsNodeNotInUserLocation(const SgNode* node)
+  {
+      const SgLocatedNode* located_node = isSgLocatedNode(node);
+      if (located_node != NULL)
       {
-        return NULL;
+          return ! Compass::IsNodeInUserLocation(
+                      located_node,
+                      DefaultArgument::source_directory);
       }
-
-    void run(SgNode *n)
+      else
       {
-        this->traverse(n, preorder);
+          return true;
       }
-
-    void visit(SgNode *n);
-
-    /*-----------------------------------------------------------------------
-     * Utilities
-     *---------------------------------------------------------------------*/
-
-    /*-----------------------------------------------------------------------
-     * Accessors / Mutators
-     *---------------------------------------------------------------------*/
-    string source_directory() const { return source_directory_; }
-    void set_source_directory(const string &source_directory)
-      {
-        source_directory_ = source_directory;
-      }
-
-    Compass::OutputObject* output() const { return output_; }
-    void set_output(Compass::OutputObject *const output)
-      {
-        output_ = output;
-      }
-
-   private:
-    /*-----------------------------------------------------------------------
-     * Attributes
-     *---------------------------------------------------------------------*/
-    string source_directory_; ///< Restrict analysis to user input files.
-    Compass::OutputObject* output_;
-
-    /*-----------------------------------------------------------------------
-     * Utilities
-     *---------------------------------------------------------------------*/
-    DISALLOW_COPY_AND_ASSIGN(Traversal);
   };
+
 } // ::CompassAnalyses
 } // ::DefaultArgument
 #endif // COMPASS_DEFAULT_ARGUMENT_H
@@ -116,9 +74,9 @@ namespace CompassAnalyses
  namespace DefaultArgument
  {
   const string checker_name      = "DefaultArgument";
-  const string short_description = "Default function argument detected.";
-  const string long_description  = "This analysis looks for functions \
-      with default arguments in their parameter list.";
+  const string short_description = "Default funciton argument detected.";
+  const string long_description  = "This analysis looks for functions with default arguments in their parameter list.";
+  string source_directory = "/";
  }
 }
 
@@ -128,91 +86,40 @@ CheckerOutput::CheckerOutput(SgNode *const node)
                           ::defaultArgumentChecker->checkerName,
                           ::defaultArgumentChecker->shortDescription) {}
 
-CompassAnalyses::DefaultArgument::Traversal::
-Traversal(Compass::Parameters a_parameters, Compass::OutputObject* output)
-    : output_(output)
-  {
-    try
-    {
-        string target_directory = a_parameters["general::target_directory"].front();
-        source_directory_.assign(target_directory);
-    }
-    catch (Compass::ParameterNotFoundException e)
-    {
-        std::cout << "ParameterNotFoundException: " << e.what() << std::endl;
-        homeDir(source_directory_);
-    }
-  }
-
-//////////////////////////////////////////////////////////////////////////////
-
-/** visit(SgNode *node)
- *
- *  Purpose
- *  ========
- *
- *  Search for function parameters that are assigned
- *  default values.  For example:
- *
- *      // Here, `x` is given a default value of `7`
- *      void foobar(int y, int x=7);
- *
- *  Target AST
- *  ==========
- *                (1)                           (2)
- *      SgFunctionParameterList ------> `(int y, int x=7)`
- *                                               '___'
- *                                                 |
- *                                      ___________|___________
- *                                     |           |           |
- * (3) Variable is being assigned to:  |  SgInitializedName    |
- *                                     |           |           |
- *                                     |  SgAssignInitializer  |
- *                                     |_______________________|
- */
-void
-CompassAnalyses::DefaultArgument::Traversal::
-visit(SgNode *node)
-{
-    SgLocatedNode* located_node = isSgLocatedNode(node);
-    if (located_node != NULL &&
-        Compass::IsNodeInUserLocation(located_node, source_directory_))
-    {
-        // (1)
-        SgFunctionParameterList* parameters = isSgFunctionParameterList(node);
-        if (parameters != NULL)
-        {
-            // (2)
-            SgInitializedNamePtrList args = parameters->get_args();
-            BOOST_FOREACH(SgInitializedName* name, args)
-            {
-                // (3)
-                Rose_STL_Container<SgNode*> default_assignment =
-                    NodeQuery::querySubTree(name, V_SgAssignInitializer);
-                if (default_assignment.size() > 0)
-                {
-                    output_->addOutput(
-                      new CheckerOutput(node));
-                }
-            }
-        }
-    }
-}// end of the visit function.
-
-// Checker main run function and metadata
-
 static void
-run(Compass::Parameters params, Compass::OutputObject* output)
+run(Compass::Parameters parameters, Compass::OutputObject* output)
   {
-    CompassAnalyses::DefaultArgument::Traversal(params, output).run(
-      Compass::projectPrerequisite.getProject());
+      // We only care about source code in the user's space, not,
+      // for example, Boost or system files.
+      string target_directory =
+          parameters["general::target_directory"].front();
+      CompassAnalyses::DefaultArgument::source_directory.assign(target_directory);
+      
+      // Use the pre-built ROSE AST
+      SgProject* sageProject = Compass::projectPrerequisite.getProject();
+      
+      // perform AST matching here
+      CodeThorn::AstMatching match_defarg;
+      MatchResult result_defarg = match_defarg.performMatching
+	("$r = SgFunctionParameterList", sageProject);
+      BOOST_FOREACH(SingleMatchVarBindings match, result_defarg)
+	{
+	  SgFunctionParameterList* fpl = (SgFunctionParameterList*)match["$r"];
+	  CodeThorn::AstMatching match_assign;
+	  MatchResult result_assign = match_assign.performMatching
+	    ("$s = SgAssignInitializer", fpl);
+	  BOOST_FOREACH(SingleMatchVarBindings new_match, result_assign)
+	    {
+	      output->addOutput(new CompassAnalyses::DefaultArgument::CheckerOutput(new_match["$s"]));
+	    }
+	}
   }
 
 // Remove this function if your checker is not an AST traversal
 static Compass::AstSimpleProcessingWithRunFunction*
 createTraversal(Compass::Parameters params, Compass::OutputObject* output)
   {
-    return new CompassAnalyses::DefaultArgument::Traversal(params, output);
+    return NULL;
   }
 
 extern const Compass::Checker* const defaultArgumentChecker =
