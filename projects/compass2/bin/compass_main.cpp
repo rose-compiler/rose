c31b621286eb9bffa4d7e53a0ac3d2aae57a205b
Sam Kelly <kelly64@llnl.gov>
2013-07-22 11:00:07 -0700

    (Compass2) Adds completed checker AsynchronousSignalHandler
    Conflicts:
    
    	projects/compass2/Makefile.am

--- projects/compass2/bin/compass_main.cpp
+++ projects/compass2/bin/compass_main.cpp
@@ -52,6 +52,7 @@ extern const Compass::Checker* const discardAssignmentChecker;
 extern const Compass::Checker* const noVforkChecker;
 extern const Compass::Checker* const pointerComparisonChecker;
 extern const Compass::Checker* const noVariadicFunctionsChecker;
+extern const Compass::Checker* const asynchronousSignalHandlerChecker;
 //PLACEHOLDER1
 /*-----------------------------------------------------------------------------
  * Main program
@@ -200,6 +201,8 @@ int main (int argc, char** argv)
                 traversals.push_back (pointerComparisonChecker);
             else if ("noVariadicFunctions" == checker_name)
                 traversals.push_back (noVariadicFunctionsChecker);
+            else if ("asynchronousSignalHandler" == checker_name)
+                traversals.push_back (asynchronousSignalHandlerChecker);
             //PLACEHOLDER2
             else
             {
