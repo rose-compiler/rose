1a30475696a99519d057c63ec561c9c47d721d43
Markus Schordan <schordan1@llnl.gov>
2013-08-05 19:56:26 -0700

    Added a default implementation for RD AST attribute.

--- projects/CodeThorn/src/RDAnalysisAstAttribute.C
+++ projects/CodeThorn/src/RDAnalysisAstAttribute.C
@@ -7,20 +7,33 @@ bool RDAnalysisAstAttribute::isBottomElement() {
 }
 VariableIdSet RDAnalysisAstAttribute::allVariableIds() {
   VariableIdSet set;
-  
+  for(RDAnalysisAstAttribute::iterator i=begin();i!=end();++i) {
+	set.insert((*i).second);
+  }
   return set;
 }
 LabelSet RDAnalysisAstAttribute::allLabels() {
   LabelSet ls;
-  return ls;
+  for(RDAnalysisAstAttribute::iterator i=begin();i!=end();++i) {
+	ls.insert((*i).first);
   }
+  return ls;
+}
 LabelSet RDAnalysisAstAttribute::definitionsOfVariableId(VariableId varId) {
   LabelSet ls;
-	return ls;
+  for(RDAnalysisAstAttribute::iterator i=begin();i!=end();++i) {
+	if(varId==(*i).second)
+	  ls.insert((*i).first);
+  }
+  return ls;
 }
 VariableIdSet RDAnalysisAstAttribute::variableIdsOfDefinition(Label def) {
-  VariableIdSet set;
-  return set;
+  VariableIdSet vset;
+  for(RDAnalysisAstAttribute::iterator i=begin();i!=end();++i) {
+	if(def==(*i).first)
+	  vset.insert((*i).second);
+  }
+  return vset;
 }
 RDAnalysisAstAttribute::iterator
 RDAnalysisAstAttribute::begin() {
