00a6ddd606900a6d17d7bfe24970f460ee6c2e4c
Andreas Saebjoernsen <andreas.saebjoernsen@gmail.com>
2013-08-16 00:19:53 +0000

    new experimental heuristic for syntactic only using instructions with semantic implementation

--- projects/BinaryCloneDetection/syntactic/createSignatureVectors.C
+++ projects/BinaryCloneDetection/syntactic/createSignatureVectors.C
@@ -75,10 +75,260 @@ unparseAndIntern(SgAsmExpression* e)
         return i->second;
     }
 }
+
+/*
+
+
+*/
+
+inline size_t
+kindToInteger(size_t kind){
+        int vk = -1;
+	switch (kind) {
+		case x86_mov:                     
+		case x86_movapd:                  
+		case x86_movaps:                  
+		case x86_movbe:                   
+		case x86_movd:                    
+		case x86_movddup:                 
+		case x86_movdq2q:                 
+		case x86_movdqa:                  
+		case x86_movdqu:                  
+		case x86_movhlps:                 
+		case x86_movhpd:                  
+		case x86_movhps:                  
+		case x86_movlhps:                 
+		case x86_movlpd:                  
+		case x86_movlps:                  
+		case x86_movmskpd:                
+		case x86_movmskps:                
+		case x86_movntdq:                 
+		case x86_movntdqa:                
+		case x86_movnti:                  
+		case x86_movntpd:                 
+		case x86_movntps:                 
+		case x86_movntq:                  
+		case x86_movntsd:                 
+		case x86_movntss:                 
+		case x86_movq:                    
+		case x86_movq2dq:                 
+		case x86_movsb:                   
+		case x86_movsd:                   
+		case x86_movsd_sse:               
+		case x86_movshdup:                
+		case x86_movsldup:                
+		case x86_movsq:                   
+		case x86_movss:                   
+		case x86_movsw:                   
+		case x86_movsx:                   
+		case x86_movsxd:                  
+		case x86_movupd:                  
+		case x86_movups:                  
+		case x86_movzx:                   
+		case x86_xor:
+		case x86_push:
+		case x86_pop:
+		case x86_lea: 
+			vk = 0; break;
+		case x86_add:
+		case x86_inc:
+			vk = 1; break;
+		case x86_sub:
+		case x86_dec:
+			vk = 2; break;
+		case x86_ja:              
+		case x86_jae:             
+		case x86_jb:             
+		case x86_jbe:            
+		case x86_jcxz:           
+		case x86_je:             
+		case x86_jecxz:          
+		case x86_jg:             
+		case x86_jge:            
+		case x86_jl:             
+		case x86_jle:            
+		case x86_jmpe:           
+		case x86_jne:            
+		case x86_jno:            
+		case x86_jns:            
+		case x86_jo:             
+		case x86_jpe:            
+		case x86_jpo:            
+		case x86_jrcxz:          
+		case x86_js: 
+			vk = 3; break;
+
+		case x86_ret:
+		case x86_retf:
+			vk = 4; break;
+			break;
+
+		case x86_test:
+		case x86_neg:
+		case x86_cmpxchg: 
+		case x86_cmpsb: 
+		case x86_cmpsw: 
+		case x86_cmpsd:
+		case x86_cmp:
+			vk = 5; break;
+
+		case x86_xchg: 
+                        vk = 6; break;
+
+		case x86_cbw: 
+		case x86_cwde: 
+		case x86_cwd: 
+		case x86_cdq: 
+		case x86_and: 
+		case x86_or: 
+		case x86_not: 
+		case x86_xadd: 
+		case x86_adc: 
+		case x86_sbb: 
+                       vk = 7; break;
+
+
+		case x86_shl:       // fall through
+		case x86_sar:       // fall through
+		case x86_shr: 
+			      vk = 8; break;
+		case x86_rol: vk = 9; break;
+		case x86_ror: vk = 10; break;
+		case x86_shrd: vk = 11; break;
+		case x86_bsf: vk = 12; break;
+		case x86_bt: vk = 13; break;
+		case x86_btr: vk = 14; break;
+		case x86_bts: vk = 15; break;
+		case x86_imul:vk = 16; break;
+
+		case x86_mul: vk = 17; break;
+
+		case x86_idiv: vk = 18; break;
+
+		case x86_div: vk = 19; break;
+
+		case x86_aaa: vk = 20; break;
+
+		case x86_aas: vk = 21; break;
+
+		case x86_aam: vk = 22; break;
+
+		case x86_aad: vk = 23; break;
+
+
+		case x86_leave: vk = 24; break;
+
+		case x86_call: vk = 25; break;
+
+
+		case x86_loop: vk = 26; break;
+		case x86_loopnz: vk = 27; break;
+
+		case x86_jmp: vk = 28; break;
+
+		case x86_setne: 
+		case x86_sete:  
+		case x86_setno: 
+		case x86_seto:  
+		case x86_setpo: 
+		case x86_setpe: 
+		case x86_setns: 
+		case x86_sets:  
+		case x86_setae: 
+		case x86_setb:  
+		case x86_setbe: 
+		case x86_seta:  
+		case x86_setle: 
+		case x86_setg:  
+		case x86_setge: 
+		case x86_setl:  
+			      vk = 29;
+			      break;
+		case x86_cmovne:  
+		case x86_cmove:     
+		case x86_cmovno:   
+		case x86_cmovo:     
+		case x86_cmovpo:   
+		case x86_cmovpe:    
+		case x86_cmovns:  
+		case x86_cmovs:    
+		case x86_cmovae:    
+		case x86_cmovb:    
+		case x86_cmovbe:   
+		case x86_cmova:     
+		case x86_cmovle: 
+		case x86_cmovg:   
+		case x86_cmovge:   
+		case x86_cmovl:     
+			      vk = 30; break;
+		case x86_cld: vk = 31; break; 
+
+		case x86_std: vk = 32; break;
+
+		case x86_clc: vk = 33; break;
+
+		case x86_stc: vk = 34; break;
+
+		case x86_cmc: vk = 35; break;
+
+		case x86_repne_scasb: 
+		case x86_repne_scasw: 
+		case x86_repne_scasd: 
+		case x86_repe_scasb:  
+		case x86_repe_scasw:  
+		case x86_repe_scasd:  
+			      vk = 36; break;
+		case x86_scasb: 
+		case x86_scasw: 
+		case x86_scasd: 
+			      vk = 37; break;
+		case x86_repne_cmpsb: 
+		case x86_repne_cmpsw: 
+		case x86_repne_cmpsd: 
+		case x86_repe_cmpsb:  
+		case x86_repe_cmpsw:  
+		case x86_repe_cmpsd:  
+			      vk = 38; break;
+		case x86_rep_movsb: 
+		case x86_rep_movsw: 
+		case x86_rep_movsd: 
+			      vk = 39; break;
+		case x86_stosb:
+		case x86_stosw: 
+		case x86_stosd: 
+			      vk = 40; break;
+		case x86_rep_stosb: 
+		case x86_rep_stosw: 
+		case x86_rep_stosd: 
+			      vk = 41; break;
+		case x86_lodsb: 
+		case x86_lodsw: 
+		case x86_lodsd: 
+			      vk = 42; break;
+		case x86_hlt: vk = 43; break;
+
+		case x86_cpuid: vk = 44; break;
+
+		case x86_rdtsc: vk = 45; break;
+
+		case x86_int: vk = 46; break;
+
+		case x86_fnstcw: vk = 48; break;
+
+		case x86_fldcw: vk = 49; break;
+
+		case x86_sysenter: vk = 50; break;
+
+		default: {
+				 vk = -1; break;
+			 }
+	}
+        return vk;
+}
  
 class SignatureVector {
 public:
-    static const size_t Size = numberOfInstructionKinds ;
+    static const size_t Size = 51 ;
     typedef uint16_t ElementType;
 
 private:
@@ -100,7 +350,7 @@ public:
     }
 
     ElementType& totalForVariant(size_t var) {
-        assert(var < numberOfInstructionKinds);
+        assert(var < Size);
         return values[var];
     }
 
@@ -203,99 +453,7 @@ createVectorsForAllInstructions(SgNode* top, const std::string& filename, const 
     for (size_t insnNumber = 0; insnNumber < insnCount; ++insnNumber) {
       SgAsmx86Instruction* insn = insns[insnNumber];
       size_t var = getInstructionKind(insn);
-
-      // Add to total for this variant
-      
-      switch(var){
-	      case x86_mov:                     
-	      case x86_movapd:                  
-	      case x86_movaps:                  
-	      case x86_movbe:                   
-	      case x86_movd:                    
-	      case x86_movddup:                 
-	      case x86_movdq2q:                 
-	      case x86_movdqa:                  
-	      case x86_movdqu:                  
-	      case x86_movhlps:                 
-	      case x86_movhpd:                  
-	      case x86_movhps:                  
-	      case x86_movlhps:                 
-	      case x86_movlpd:                  
-	      case x86_movlps:                  
-	      case x86_movmskpd:                
-	      case x86_movmskps:                
-	      case x86_movntdq:                 
-	      case x86_movntdqa:                
-	      case x86_movnti:                  
-	      case x86_movntpd:                 
-	      case x86_movntps:                 
-	      case x86_movntq:                  
-	      case x86_movntsd:                 
-	      case x86_movntss:                 
-	      case x86_movq:                    
-	      case x86_movq2dq:                 
-	      case x86_movsb:                   
-	      case x86_movsd:                   
-	      case x86_movsd_sse:               
-	      case x86_movshdup:                
-	      case x86_movsldup:                
-	      case x86_movsq:                   
-	      case x86_movss:                   
-	      case x86_movsw:                   
-	      case x86_movsx:                   
-	      case x86_movsxd:                  
-	      case x86_movupd:                  
-	      case x86_movups:                  
-	      case x86_movzx:                   
-
-
-	      case x86_xor:
-	      case x86_push:
-	      case x86_pop:
-	      case x86_lea:
-		      var = x86_mov;
-		      break;
-	      case x86_inc:
-		      var = x86_add;
-		      break;
-	      case x86_dec:
-		      var = x86_sub;
-		      break;
-	      case x86_ja:              
-	      case x86_jae:             
-	      case x86_jb:             
-	      case x86_jbe:            
-	      case x86_jcxz:           
-	      case x86_je:             
-	      case x86_jecxz:          
-	      case x86_jg:             
-	      case x86_jge:            
-	      case x86_jl:             
-	      case x86_jle:            
-	      case x86_jmpe:           
-	      case x86_jne:            
-	      case x86_jno:            
-	      case x86_jns:            
-	      case x86_jo:             
-	      case x86_jpe:            
-	      case x86_jpo:            
-	      case x86_jrcxz:          
-	      case x86_js: 
-		      var = x86_jb;
-		      break;
-	      case x86_ret:
-	      case x86_retf:
-		      var = x86_ret;
-                      break;
-	      case x86_test:
-	      case x86_neg:
-		      //case x86_comp:
-		      var = x86_cmp;  
-		      break;       
-	      default:
-		      break;
-
-      };
+      var = kindToInteger(var);
 
       if(var != x86_nop) ++vec.totalForVariant(var);
     }
