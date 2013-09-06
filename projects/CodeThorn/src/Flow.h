c7c8f7923df541bd8dcb78c08c8917671580c744
Markus Schordan <schordan1@llnl.gov>
2013-09-06 12:03:19 -0700

    Moved flow-computation classes to new separate file Flow.[hC]

---
+++ projects/CodeThorn/src/Flow.h
@@ -0,0 +1,126 @@
+#ifndef FLOW_H
+#define FLOW_H
+
+namespace CodeThorn {
+
+#include "SgNodeHelper.h"
+#include "Labeler.h"
+#include "CommandLineOptions.h"
+
+  enum EdgeType { EDGE_UNKNOWN=0, EDGE_FORWARD, EDGE_BACKWARD, EDGE_TRUE, EDGE_FALSE, EDGE_LOCAL, EDGE_CALL, EDGE_CALLRETURN, EDGE_EXTERNAL, EDGE_PATH };
+
+  class Edge;
+  typedef set<Edge> EdgeSet;
+
+/*! 
+  * \author Markus Schordan
+  * \date 2012.
+ */
+class Edge {
+ public:
+  Edge();
+  Edge(Label source0,Label target0);
+  Edge(Label source0,EdgeType type0,Label target0);
+  Edge(Label source0,set<EdgeType> type0,Label target0);
+  string toString() const;
+  string toStringNoType() const;
+  string toDotColored() const;
+  string toDotFixedColor(string) const;
+  //string typeToString() const;
+  string typesToString() const;
+  static string typeToString(EdgeType et);
+  Label source;
+  //EdgeType type;
+  Label target;
+  bool isType(EdgeType et) const;
+  void addType(EdgeType et);
+  void addTypes(set<EdgeType> ets);
+  void removeType(EdgeType et);
+  set<EdgeType> types() const;
+  long typesCode() const;
+  string color() const;
+  string dotEdgeStyle() const;
+  long hash() const;
+ private:
+  set<EdgeType> _types;
+};
+
+ bool operator==(const Edge& e1, const Edge& e2);
+ bool operator!=(const Edge& e1, const Edge& e2);
+ bool operator<(const Edge& e1, const Edge& e2);
+
+/*! 
+  * \author Markus Schordan
+  * \date 2012.
+ */
+ class Flow : public set<Edge> {
+ public:  
+  Flow();
+  Flow operator+(Flow& s2);
+  Flow& operator+=(Flow& s2);
+  LabelSet nodeLabels();
+  LabelSet sourceLabels();
+  LabelSet targetLabels();
+  LabelSet pred(Label label);
+  LabelSet succ(Label label);
+  LabelSet reachableNodes(Label start);
+
+  //LabelSet reachableNodesOnPath(Label start, Label target);
+  /* computes all nodes which are reachable in the graph from the start node. A path is terminated by either the target node 
+	or by a node without successors. The start node is not included in the result set, except
+     it is reachable on some path starting from the start node.
+  */
+  LabelSet reachableNodesButNotBeyondTargetNode(Label start, Label target);
+
+  Flow inEdges(Label label);
+  Flow outEdges(Label label);
+  Flow edgesOfType(EdgeType edgeType);
+  Flow outEdgesOfType(Label label, EdgeType edgeType);
+  void setDotOptionDisplayLabel(bool opt);
+  void setDotOptionDisplayStmt(bool opt);
+  void setDotOptionFixedColor(bool opt);
+  void setDotFixedColor(string color);
+  void setDotOptionHeaderFooter(bool opt);
+  string toDot(Labeler *labeler);
+  void setTextOptionPrintType(bool opt);
+  void resetDotOptions();
+  string toString();
+  // deletes all Edges of type edgeType. The return value is the number of deleted edges.
+  size_t deleteEdges(EdgeType edgeType);
+  size_t deleteEdges(Flow& flow);
+  void establishBoostGraph();
+ private:
+  bool _dotOptionDisplayLabel;
+  bool _dotOptionDisplayStmt;
+  bool _stringNoType;
+  bool _dotOptionFixedColor;
+  string _fixedColor;
+  bool _dotOptionHeaderFooter;
+};
+
+class InterEdge {
+ public:
+  InterEdge(Label call, Label entry, Label exit, Label callReturn);
+  string toString() const;
+  Label call;
+  Label entry;
+  Label exit;
+  Label callReturn;
+};
+
+bool operator<(const InterEdge& e1, const InterEdge& e2);
+bool operator==(const InterEdge& e1, const InterEdge& e2);
+bool operator!=(const InterEdge& e1, const InterEdge& e2);
+
+/*! 
+  * \author Markus Schordan
+  * \date 2012.
+ */
+class InterFlow : public set<InterEdge> {
+ public:
+  string toString() const;
+};
+
+} // end namespace CodeThorn
+
+#endif
