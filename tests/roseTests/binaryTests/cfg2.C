d02a162fc21cdac44dbe0fb3386a21f2167f7e30
Robb Matzke <matzke1@llnl.gov>
2013-09-10 08:36:49 -0400

    (Binary Analysis) Moved CFG vertex writer into library
    The instruction-based CFG GraphViz vertex writer that was defined
    in the cfg2.C test is useful enough that it should be part of ROSE,
    so I've moved it.

--- tests/roseTests/binaryTests/cfg2.C
+++ tests/roseTests/binaryTests/cfg2.C
@@ -4,28 +4,7 @@
 //   * function return vertices point to the fall through instruction of all of their known call sites
 #include "sage3basic.h"
 #include "BinaryControlFlow.h"
-#include "AsmUnparser_compat.h"
-
-// Label the graphviz vertices with the address and instruction.  Include function name for function entry instructions.
-// This is used by boost::write_graphviz()
-template<class Graph>
-struct GraphvizVertexWriter {
-    const Graph &cfg;
-    GraphvizVertexWriter(Graph &cfg): cfg(cfg) {}
-    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
-    void operator()(std::ostream &output, const Vertex &v) const {
-        SgAsmInstruction *insn = get(boost::vertex_name, cfg, v);
-        SgAsmx86Instruction *x86 = isSgAsmx86Instruction(insn);
-        SgAsmFunction *func = SageInterface::getEnclosingNode<SgAsmFunction>(insn);
-        output <<"[ label=\"[" <<v << "] " <<unparseInstructionWithAddress(insn) <<"\"";
-        if (insn->get_address()==func->get_entry_va()) {
-            output <<", style=filled, color=\"#cd853f\"";
-        } else if (x86 && x86_ret==x86->get_kind()) {
-            output <<", style=filled, color=\"#fed3a7\"";
-        }
-        output <<" ]";
-    }
-};
+#include "BinaryAnalysisUtils.h"
 
 int main(int argc, char *argv[])
 {
@@ -39,5 +18,5 @@ int main(int argc, char *argv[])
     typedef BinaryAnalysis::ControlFlow::InsnGraph CFG;
     BinaryAnalysis::ControlFlow analyzer;
     CFG cfg = analyzer.build_insn_cfg_from_ast<CFG>(interp);
-    analyzer.write_graphviz(std::cout, cfg, GraphvizVertexWriter<CFG>(cfg));
+    analyzer.write_graphviz(std::cout, cfg, BinaryAnalysis::InsnCFGVertexWriter<CFG>(cfg));
 }
