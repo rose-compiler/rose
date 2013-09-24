dec78f33aa5c597fb9b454fa379f82cc88f01008
Andreas Saebjoernsen <andreas.saebjoernsen@gmail.com>
2013-09-24 00:50:45 +0000

    (BinaryCloneDetection) made lsh syntactic clone detection separate from path syntactic, and reverted coupling

--- projects/BinaryCloneDetection/syntactic/failure-rate.sql
+++ projects/BinaryCloneDetection/syntactic/failure-rate.sql
@@ -3,25 +3,18 @@
 begin transaction;
 
 drop table if exists fr_results;
-drop table if exists fr_results_precision_recall;
 drop table if exists fr_false_positives;
 drop table if exists fr_false_negatives;
-drop table if exists fr_true_positives;
-drop table if exists fr_true_negatives;
 drop table if exists fr_clone_pairs;
 drop table if exists fr_negative_pairs;
 drop table if exists fr_positive_pairs;
-drop table if exists fr_true_negatives;
-drop table if exists fr_true_positives;
 drop table if exists fr_function_pairs;
-drop index if exists fr_fio_func_id;
-drop table if exists fr_fio;
+
+drop table if exists fr_cluster_pairs;
 drop table if exists fr_functions;
 drop table if exists fr_funcnames;
 drop table if exists fr_specimens;
 drop table if exists fr_settings;
-
-
 
 create table fr_settings as
     select 0.75 as similarity_threshold;
@@ -40,7 +33,7 @@ create table fr_funcnames as
         join semantic_functions as func1 on func1.file_id = file.id
         left join semantic_functions as func2
             on func1.id<>func2.id and func1.name <> '' and func1.name=func2.name and func1.file_id=func2.file_id
-        where func2.id is null and func1.ninsns >= 20                                            -- !!FIXME
+        where func2.id is null and func1.ninsns >= 2                                            -- !!FIXME
         group by func1.name
         having count(*) = (select count(*) from fr_specimens);
 
@@ -102,50 +95,21 @@ create table fr_false_negatives as
 create table fr_false_positives as
     select * from fr_negative_pairs intersect select func1_id, func2_id from fr_clone_pairs;
 
-create table fr_true_positives as
-    select * from fr_positive_pairs intersect select func1_id, func2_id from fr_clone_pairs;
-
-create table fr_true_negatives as
-    select * from fr_negative_pairs except select func1_id, func2_id from fr_clone_pairs;
-
-select func1.name, func2.name
- from ipt_combined_pairs as pair
- join semantic_functions as func1 on pair.func1_id=func1.id
- join semantic_functions as func2 on pair.func2_id=func2.id
-;
-
-
 -- False negative rate is the ratio of false negatives to expected positives
 -- False positive rate is the ratio of false positives to expected negatives
 create table fr_results as
     select
-        (select count(*) from fr_positive_pairs)  as expected_positives,
-        (select count(*) from fr_negative_pairs)  as expected_negatives,
-        ((select count(*) from fr_true_positives ) + (select count(*) from fr_false_positives) ) as positives,
-        ((select count(*) from fr_true_negatives ) + (select count(*) from fr_false_negatives) ) as negatives,
-        (select count(*) from fr_true_positives ) as true_positives,
-        (select count(*) from fr_true_negatives ) as true_negatives,
-        (select count(*) from fr_false_positives) as false_positives,
-        (select count(*) from fr_false_negatives) as false_negatives,
-        ((select 100.0*count(*) from fr_true_positives ) / (0.001+ ((select count(*) from fr_true_positives ) + (select count(*) from fr_false_positives)))) as tp_percent,
-        (100.0*((select 1.0*count(*) from fr_negative_pairs ) - (select 1.0*count(*) from fr_false_negatives) ) /(0.001+ (select count(*) from fr_negative_pairs))) as tn_percent,
-        ((select 100.0*count(*) from fr_false_positives) /(0.001+ ((select count(*) from fr_true_positives ) + (select count(*) from fr_false_positives) ))) as fp_percent,
-        ((select 100.0*count(*) from fr_false_negatives) /(0.001+ (select count(*) from fr_negative_pairs))) as fn_percent
-;
- 
-create table fr_results_precision_recall as
-    select
-        ( (select 100.0*true_positives from fr_results limit 1) /(0.001+ ( ( select true_positives from fr_results  limit 1 ) + (select false_negatives from fr_results limit 1) )  )) as recall,
-        ( (select 100.0*true_negatives from fr_results limit 1) /(0.001+ ( ( select true_negatives from fr_results  limit 1) + (select false_positives from fr_results limit 1)) )) as specificity,
-        ( (select 100.0*true_positives from fr_results limit 1) /(0.001+ ( ( select true_positives from fr_results  limit 1) + (select false_positives from fr_results  limit 1 )))) as precision
-;
+        (select count(*) from fr_positive_pairs) as "Expected Positives",
+        (select count(*) from fr_negative_pairs) as "Expected Negatives",
+        (select count(*) from fr_false_negatives) as "False Negatives",
+        (select count(*) from fr_false_positives) as "False Positives",
+        ((select 100.0*count(*) from fr_false_negatives) / (select count(*) from fr_positive_pairs)) as "FN Percent",
+        ((select 100.0*count(*) from fr_false_positives) / (select count(*) from fr_negative_pairs)) as "FP Percent";
 
 -------------------------------------------------------------------------------------------------------------------------------
 -- Some queries to show the results
 
 select * from fr_results;
-select * from fr_results_precision_recall;
-
 
 -- select 'The following table shows the false negative function pairs.
 -- Both functions of the pair always have the same name.' as "Notice";
--- projects/BinaryCloneDetection/syntactic/iptheft.sql
+++ projects/BinaryCloneDetection/syntactic/iptheft.sql
@@ -8,7 +8,7 @@ drop table if exists ipt_parameters;
 
 -- Change these values to whatever you want
 create table ipt_parameters as select
-  0.3 as semantic_similarity,		-- min similarity for two functions to be considered semantic clones
+  0.7 as semantic_similarity,		-- min similarity for two functions to be considered semantic clones
   0.1 as syntactic_overlap;             -- min vector overlap for two functions to be considered syntactic clones
 
 -- Pairs of syntactic clones (func1_id < func2_id)
@@ -31,43 +31,21 @@ create table ipt_combined_pairs as
   select * from ipt_semantic_pairs;
 
 
-select func1.name, func2.name
- from ipt_combined_pairs as pair
- join semantic_functions as func1 on pair.func1_id=func1.id
- join semantic_functions as func2 on pair.func2_id=func2.id
-;
-
-select func1.name, func2.name
- from ipt_semantic_pairs as pair
- join semantic_functions as func1 on pair.func1_id=func1.id
- join semantic_functions as func2 on pair.func2_id=func2.id
-;
-
 -- False negative/positive rates (cut-n-pasted from failure-rate.sql and changed the name of a table
 
 begin transaction;
 
 drop table if exists fr_results;
-drop table if exists fr_results_precision_recall;
 drop table if exists fr_false_positives;
 drop table if exists fr_false_negatives;
-drop table if exists fr_true_positives;
-drop table if exists fr_true_negatives;
 drop table if exists fr_clone_pairs;
 drop table if exists fr_negative_pairs;
 drop table if exists fr_positive_pairs;
-drop table if exists fr_true_negatives;
-drop table if exists fr_true_positives;
 drop table if exists fr_function_pairs;
-drop index if exists fr_fio_func_id;
-drop table if exists fr_fio;
+drop table if exists fr_cluster_pairs;
 drop table if exists fr_functions;
 drop table if exists fr_funcnames;
 drop table if exists fr_specimens;
-drop table if exists fr_settings;
-
-
-
 
 -- Files that are binary specimens; i.e., not shared libraries, etc.
 create table fr_specimens as
@@ -83,7 +61,7 @@ create table fr_funcnames as
         join semantic_functions as func1 on func1.file_id = file.id
         left join semantic_functions as func2
             on func1.id<>func2.id and func1.name <> '' and func1.name=func2.name and func1.file_id=func2.file_id
-        where func2.id is null and func1.ninsns >= 20                                            -- !!FIXME
+        where func2.id is null and func1.ninsns >= 2                                            -- !!FIXME
         group by func1.name
         having count(*) = (select count(*) from fr_specimens);
 
@@ -141,104 +119,21 @@ create table fr_false_negatives as
 create table fr_false_positives as
     select * from fr_negative_pairs intersect select func1_id, func2_id from fr_clone_pairs;
 
-create table fr_true_positives as
-    select * from fr_positive_pairs intersect select func1_id, func2_id from fr_clone_pairs;
-
-create table fr_true_negatives as
-    select * from fr_negative_pairs except select func1_id, func2_id from fr_clone_pairs;
-
-
 -- False negative rate is the ratio of false negatives to expected positives
 -- False positive rate is the ratio of false positives to expected negatives
 create table fr_results as
     select
-        (select count(*) from fr_positive_pairs)  as expected_positives,
-        (select count(*) from fr_negative_pairs)  as expected_negatives,
-        ((select count(*) from fr_true_positives ) + (select count(*) from fr_false_positives) ) as positives,
-        ((select count(*) from fr_true_negatives ) + (select count(*) from fr_false_negatives) ) as negatives,
-        (select count(*) from fr_true_positives ) as true_positives,
-        (select count(*) from fr_true_negatives ) as true_negatives,
-        (select count(*) from fr_false_positives) as false_positives,
-        (select count(*) from fr_false_negatives) as false_negatives,
-        ((select 100.0*count(*) from fr_true_positives ) / (0.001+ ((select count(*) from fr_true_positives ) + (select count(*) from fr_false_positives)))) as tp_percent,
-        (100.0*((select 1.0*count(*) from fr_negative_pairs ) - (select 1.0*count(*) from fr_false_negatives) ) / (0.001+ (select count(*) from fr_negative_pairs))) as tn_percent,
-        ((select 100.0*count(*) from fr_false_positives) / (0.001+ ((select count(*) from fr_true_positives ) + (select count(*) from fr_false_positives) ))) as fp_percent,
-        ((select 100.0*count(*) from fr_false_negatives) /(0.001+ (select count(*) from fr_negative_pairs))) as fn_percent
-;
- 
-create table fr_results_precision_recall as
-    select
-        ( (select 100.0*true_positives from fr_results limit 1) /(0.001+ ( ( select true_positives from fr_results  limit 1 ) + (select false_negatives from fr_results limit 1) )  )) as recall,
-        ( (select 100.0*true_negatives from fr_results limit 1) /(0.001+ ( ( select true_negatives from fr_results  limit 1) + (select false_positives from fr_results limit 1)) )) as specificity,
-        ( (select 100.0*true_positives from fr_results limit 1) / (0.001+ ( ( select true_positives from fr_results  limit 1) + (select false_positives from fr_results  limit 1 )))) as precision
-;
+        (select count(*) from fr_positive_pairs) as "Expected Positives",
+        (select count(*) from fr_negative_pairs) as "Expected Negatives",
+        (select count(*) from fr_false_negatives) as "False Negatives",
+        (select count(*) from fr_false_positives) as "False Positives",
+        ((select 100.0*count(*) from fr_false_negatives) / (select count(*) from fr_positive_pairs)) as "FN Percent",
+        ((select 100.0*count(*) from fr_false_positives) / (select count(*) from fr_negative_pairs)) as "FP Percent";
 
 -------------------------------------------------------------------------------------------------------------------------------
 -- Some queries to show the results
 
 select * from fr_results;
-select * from fr_results_precision_recall;
-
-
-select func1.name, func2.name
- from ipt_semantic_pairs as pair
- join semantic_functions as func1 on pair.func1_id=func1.id
- join semantic_functions as func2 on pair.func2_id=func2.id
-;
-
-
--- Tests that we want to consider
-create table fr_fio as
-    select fio.*
-        from fr_functions as func
-        join semantic_fio as fio on func.id = fio.func_id;
-create index fr_fio_func_id on fr_fio(func_id);
-
-
-
-select 'The following table shows a list of function names that are
-true positives, and the average number of instructions executed by
-tests on this name.' as "Notice";
-select
-        func.name, func.id, func.file_id,
-        count(*) as npass,
-        sum(fio.instructions_executed)/count(*) as ave_insns_exec
-    from fr_fio as fio
-    join fr_functions as func on fio.func_id = func.id
-    join fr_true_positives as neg on fio.func_id=neg.func1_id or fio.func_id=neg.func2_id
-    where fio.status = 0
-    group by func.name, func.id, func.file_id
-    order by ave_insns_exec;
-
-select 'The following table shows a list of function names that are
-false positives, and the average number of instructions executed by
-tests on this name.' as "Notice";
-select
-        func.name, func.id, func.file_id,
-        count(*) as npass,
-        sum(fio.instructions_executed)/count(*) as ave_insns_exec
-    from fr_fio as fio
-    join fr_functions as func on fio.func_id = func.id
-    join fr_false_positives as neg on fio.func_id=neg.func1_id or fio.func_id=neg.func2_id
-    where fio.status = 0
-    group by func.name, func.id, func.file_id
-    order by ave_insns_exec;
-
-
-select 'The following table shows a list of function names that are
-false negatives, and the average number of instructions executed by
-tests on this name.' as "Notice";
-select
-        func.name, func.id, func.file_id,
-        count(*) as npass,
-        sum(fio.instructions_executed)/count(*) as ave_insns_exec
-    from fr_fio as fio
-    join fr_functions as func on fio.func_id = func.id
-    join fr_false_negatives as neg on fio.func_id=neg.func1_id or fio.func_id=neg.func2_id
-    where fio.status = 0
-    group by func.name, func.id, func.file_id
-    order by ave_insns_exec;
-
 
 -- select 'The following table shows the false negative function pairs.
 -- Both functions of the pair always have the same name.' as "Notice";
