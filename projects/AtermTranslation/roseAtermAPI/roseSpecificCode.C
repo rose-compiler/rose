84b1020eda179b548f16afc443451c66ead8544c
Dan Quinlan <dquinlan@llnl.gov>
2013-10-21 09:22:16 -0700

    Fixed added assertion for token stream handling (caught by Fortran example *.F files) and added more ATerm support.

---
+++ projects/AtermTranslation/roseAtermAPI/roseSpecificCode.C
@@ -0,0 +1,13 @@
+#include "rose.h"
+
+#include "rose_aterm_api.h"
+
+ATerm convertNodeToAterm(SgNode* project)
+   {
+  // This function likely does not make sense because we will not be converting ROSE AST's to Aterms via a serialization.
+
+     ROSE_ASSERT(!"convertNodeToAterm(): not implemented!");
+   }
+
+
+
