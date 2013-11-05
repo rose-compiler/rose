6119d16c707e3dbd11c7dbea43ef681896789c6a
Robb Matzke <matzke1@llnl.gov>
2013-11-05 12:55:13 -0500

    (Clone Detection) Very minor tweak to a debugging message
    This debugging message is not disabled because Andreas might
    need to to see where 25-run tests has problems.

--- projects/BinaryCloneDetection/semantic/25-run-tests.C
+++ projects/BinaryCloneDetection/semantic/25-run-tests.C
@@ -1203,7 +1203,7 @@ main(int argc, char *argv[])
 
         // Find the function to test
 #if 1 /*DEBUGGING [Robb P. Matzke 2013-10-29]*/
-        std::cerr <<"func_id=" <<work.func_id <<"\n";
+        std::cerr <<"func_id=" <<work.func_id <<"\r";
 #endif
         IdFunctionMap::iterator func_found = functions.find(work.func_id);
         assert(func_found!=functions.end());
