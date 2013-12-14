7e1d311094c1597fbfcc2af03071d01ef3d1c064
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2013-12-14 00:55:14 -0500

    Minor aesthetic change.

--- projects/openacc-to-opencl/openacc-to-opencl.cpp
+++ projects/openacc-to-opencl/openacc-to-opencl.cpp
@@ -27,7 +27,7 @@ int main(int argc, char ** argv) {
   DLX::Frontend::Frontend<DLX::OpenACC::language_t> frontend;
   Handles::TransformationHandler transformation_handler;
   DLX::OpenACC::language_t::compiler_module_handlers_t compiler_module_handlers;
-//  DLX::Compiler::Compiler<DLX::OpenACC::language_t, Handles::TransformationHandler> compiler(transformation_handler, compiler_module_handlers);
+//DLX::Compiler::Compiler<DLX::OpenACC::language_t, Handles::TransformationHandler> compiler(transformation_handler, compiler_module_handlers);
 
   SgProject * project = new SgProject(argc, argv);
 
