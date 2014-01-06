e7bb8722d1d173ae04bc8586ccf9d06be6df79fb
Justin Too <doubleotoo@gmail.com>
2014-01-06 11:37:21 -0800

    (KeepGoing) Disable on Windows; currently unsupported

--- src/frontend/SageIII/sage_support/keep_going.h
+++ src/frontend/SageIII/sage_support/keep_going.h
@@ -19,30 +19,44 @@ extern bool g_keep_going;
 extern struct sigaction SignalAction;
 typedef void(*SignalHandlerFunction)(int);
 
+
 //-----------------------------------------------------------------------------
 // Public API
 //-----------------------------------------------------------------------------
-#define KEEP_GOING_CAUGHT_FRONTEND_SIGNAL                         \
-  (                                                               \
-      ROSE::KeepGoing::g_keep_going &&                            \
-      ROSE::KeepGoing::set_signal_handler(                        \
-          &ROSE::KeepGoing::Frontend::SignalHandler) &&           \
-      sigsetjmp(ROSE::KeepGoing::Frontend::jmp_target, 0) != 0    \
-  )
-#define KEEP_GOING_CAUGHT_FRONTEND_SECONDARY_PASS_SIGNAL                        \
-  (                                                                             \
-      ROSE::KeepGoing::g_keep_going &&                                          \
-      ROSE::KeepGoing::set_signal_handler(                                      \
-          &ROSE::KeepGoing::Frontend::SecondaryPass::SignalHandler) &&          \
-      sigsetjmp(ROSE::KeepGoing::Frontend::SecondaryPass::jmp_target, 0) != 0   \
-  )
-#define KEEP_GOING_CAUGHT_BACKEND_UNPARSER_SIGNAL                       \
-  (                                                                     \
-      ROSE::KeepGoing::g_keep_going &&                                  \
-      ROSE::KeepGoing::set_signal_handler(                              \
-          &ROSE::KeepGoing::Backend::Unparser::SignalHandler) &&        \
-      sigsetjmp(ROSE::KeepGoing::Backend::Unparser::jmp_target, 0) != 0 \
-  )
+#ifndef _MSC_VER
+    #define KEEP_GOING_CAUGHT_FRONTEND_SIGNAL                         \
+      (                                                               \
+          ROSE::KeepGoing::g_keep_going &&                            \
+          ROSE::KeepGoing::set_signal_handler(                        \
+              &ROSE::KeepGoing::Frontend::SignalHandler) &&           \
+          sigsetjmp(ROSE::KeepGoing::Frontend::jmp_target, 0) != 0    \
+      )
+    #define KEEP_GOING_CAUGHT_FRONTEND_SECONDARY_PASS_SIGNAL                        \
+      (                                                                             \
+          ROSE::KeepGoing::g_keep_going &&                                          \
+          ROSE::KeepGoing::set_signal_handler(                                      \
+              &ROSE::KeepGoing::Frontend::SecondaryPass::SignalHandler) &&          \
+          sigsetjmp(ROSE::KeepGoing::Frontend::SecondaryPass::jmp_target, 0) != 0   \
+      )
+    #define KEEP_GOING_CAUGHT_BACKEND_UNPARSER_SIGNAL                       \
+      (                                                                     \
+          ROSE::KeepGoing::g_keep_going &&                                  \
+          ROSE::KeepGoing::set_signal_handler(                              \
+              &ROSE::KeepGoing::Backend::Unparser::SignalHandler) &&        \
+          sigsetjmp(ROSE::KeepGoing::Backend::Unparser::jmp_target, 0) != 0 \
+      )
+#else //_MSC_VER
+    // TOO1 (2013/12/30): See setjmp/longjmp for implementation details:
+    //
+    //                     http://msdn.microsoft.com/en-us/library/xe7acxfb.aspx
+    //                     http://msdn.microsoft.com/en-us/library/3ye15wsy.aspx
+    //    See:
+    //                     http://stackoverflow.com/questions/8934879/how-to-handle-sigabrt-signal
+    //
+    #define KEEP_GOING_CAUGHT_FRONTEND_SIGNAL false
+    #define KEEP_GOING_CAUGHT_FRONTEND_SECONDARY_PASS_SIGNAL false
+    #define KEEP_GOING_CAUGHT_BACKEND_UNPARSER_SIGNAL false
+#endif //_MSC_VER
 
 //-----------------------------------------------------------------------------
 // Private API
@@ -50,18 +64,24 @@ typedef void(*SignalHandlerFunction)(int);
 bool set_signal_handler(SignalHandlerFunction handler);
 
 namespace Frontend {
+#ifndef _MSC_VER
   extern sigjmp_buf jmp_target;
+#endif //_MSC_VER
   void SignalHandler(int sig);
 
   namespace SecondaryPass {
-    extern sigjmp_buf jmp_target;
+    #ifndef _MSC_VER
+      extern sigjmp_buf jmp_target;
+    #endif //_MSC_VER
     void SignalHandler(int sig);
   }// ::ROSE::KeepGoing::Frontend::SecondaryPass
 }// ::ROSE::KeepGoing::Frontend
 
 namespace Backend {
 namespace Unparser {
-  extern sigjmp_buf jmp_target;
+  #ifndef _MSC_VER
+    extern sigjmp_buf jmp_target;
+  #endif //_MSC_VER
   void SignalHandler(int sig);
 }// ::ROSE::KeepGoing::Backend::Unparser
 }// ::ROSE::KeepGoing::Backend
