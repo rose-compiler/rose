3f46406a237ee9eece9b338776541812fef1ce8a
Robb Matzke <matzke1@llnl.gov>
2014-03-05 15:10:39 -0500

    (Tutorial) Restored the identityTranslator I accidently wiped out

--- tutorial/identityTranslator.C
+++ tutorial/identityTranslator.C
@@ -1,38 +1,18 @@
 // Example ROSE Translator: used for testing ROSE infrastructure
 
 #include "rose.h"
-#include <boost/foreach.hpp>
 
-static SgInitializedName *
-findInitializedName(SgNode *ast, const std::string &name)
-{
-    std::vector<SgInitializedName*> names = SageInterface::querySubTree<SgInitializedName>(ast);
-    BOOST_FOREACH (SgInitializedName *iname, names) {
-        if (0==iname->get_name().getString().compare(name))
-            return iname;
-    }
-    return NULL;
-}
+int main( int argc, char * argv[] ) 
+   {
+  // Build the AST used by ROSE
+     SgProject* project = frontend(argc,argv);
 
+  // Run internal consistency tests on AST
+     AstTests::runAllTests(project);
 
-// tests for Unparse_Java::unparseInitializedName
-    
+  // Insert your own manipulation of the AST here...
 
-int main(int argc, char * argv[]) {
-    SgProject* project = frontend(argc,argv);
-    AstTests::runAllTests(project);
+  // Generate source code from AST and call the vendor's compiler
+     return backend(project);
+   }
 
-    // AST returned by frontend() is ok.
-    SgInitializedName *a = findInitializedName(project, "a");
-    ROSE_ASSERT(a->attributeExists("type") || !"a must have a type attribute");
-    (void) a->getAttribute("type");
-
-    // Copy part of the AST
-    SgTreeCopy deep;
-    SgInitializedName *b = isSgInitializedName(a->copy(deep));
-    ROSE_ASSERT(b || !"copy failed");
-
-    // Check the copy
-    ROSE_ASSERT(b->attributeExists("type") || !"copy has no type attribute");
-    (void) b->getAttribute("type");                     // fails assertion
-}
