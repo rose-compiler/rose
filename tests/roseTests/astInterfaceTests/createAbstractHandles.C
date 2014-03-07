d2180a62ef7ede924386f59108428b5e4ccf4bfa
Chunhua (Leo) Liao <liaoch@llnl.gov>
2014-03-07 12:09:28 -0800

    (abstract handle) test translator and input file

---
+++ tests/roseTests/astInterfaceTests/createAbstractHandles.C
@@ -0,0 +1,64 @@
+/*
+test code for creating abstract handles with qualified name style
+This is useful to uniquely identify AST nodes during transformations.
+
+by Liao, 3/6/2014
+*/
+#include "rose.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+using namespace AbstractHandle;
+
+
+// a global handle for the current file
+static abstract_handle* file_handle = NULL;
+
+class visitorTraversal : public AstSimpleProcessing
+{
+  protected:
+    virtual void visit(SgNode* n);
+};
+
+void visitorTraversal::visit(SgNode* n)
+{
+  SgForStatement* forloop = isSgForStatement(n);
+
+  if (forloop)
+  {
+    cout<<"Creating handles for a loop construct..."<<endl;
+#if 0    
+    //Create an abstract node
+    abstract_node* anode= buildroseNode(forloop);
+
+    //Create an abstract handle from the abstract node
+    //Using source position specifiers by default
+    abstract_handle * ahandle = new abstract_handle(anode);
+    cout<<ahandle->toString()<<endl;
+
+    // Create handles based on numbering specifiers within the file
+    abstract_handle * bhandle = new abstract_handle(anode,e_numbering,file_handle);
+#endif
+    abstract_handle * bhandle = buildAbstractHandle(forloop);
+    cout<<bhandle->toString()<<endl<<endl;
+  }
+}
+
+int main(int argc, char * argv[])
+{
+  SgProject *project = frontend (argc, argv);
+
+  //Generate a file handle
+#if 0
+  abstract_node * file_node = buildroseNode((project->get_fileList())[0]);
+  file_handle = new abstract_handle(file_node);
+#endif
+  //Generate handles for language constructs
+  visitorTraversal myvisitor;
+  myvisitor.traverseInputFiles(project,preorder);
+
+// Generate source code from AST and call the vendor's compiler
+  return backend(project);
+}
+
