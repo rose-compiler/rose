608a5fb1a3067772fe2564fd175f3d34de15edd4
Dan Quinlan <dquinlan@llnl.gov>
2014-03-16 16:40:46 -0700

    Added more test codes (small enough to support vizualization of the whole AST).

---
+++ tests/roseTests/astSnippetTests/SmallSpecimensC/snippet2014_07.c
@@ -0,0 +1,15 @@
+/* Snippets with #include directives */
+
+// #include <stdio.h>                                      // fputs
+// This function declaration must be present in order to insert a function call that would reference this function. 
+// void fputs(v1, stderr);
+// typedef int FILE;
+struct __FILE {};
+typedef struct __FILE FILE;
+extern int fputs(const char *s, FILE *stream);
+extern int *stderr;
+
+void print(const char *v1)
+{
+    fputs(v1, stderr);
+}
