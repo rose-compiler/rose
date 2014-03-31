86d0f2b458e692ffee285d01edce3fcb085ee2e4
Robb Matzke <matzke1@llnl.gov>
2014-03-31 11:37:43 -0400

    (Clone Detection) Moved most of 25-run-tests into a separate .C file
    This is in preparation for adding 25-run-tests-fork so that the two
    programs can share most of the same code.

---
+++ projects/BinaryCloneDetection/semantic/RunTests.h
@@ -0,0 +1,73 @@
+#ifndef ROSE_CloneDetection_RunTests_H
+#define ROSE_CloneDetection_RunTests_H
+
+#include "sage3basic.h"
+#include "CloneDetectionLib.h"
+#include "rose_getline.h"
+
+namespace CloneDetection {
+namespace RunTests {
+
+typedef CloneDetection::Policy<State, PartialSymbolicSemantics::ValueType> ClonePolicy;
+typedef X86InstructionSemantics<ClonePolicy, PartialSymbolicSemantics::ValueType> CloneSemantics;
+typedef std::set<std::string> NameSet;
+
+struct Switches {
+    Switches()
+        : verbosity(SILENT), progress(false), pointers(false), interactive(false), trace_events(0), dry_run(false),
+          save_coverage(false), save_callgraph(false), save_consumed_inputs(false) {
+        checkpoint = 300 + LinearCongruentialGenerator()()%600;
+    }
+    Verbosity verbosity;                        // semantic policy has a separate verbosity
+    bool progress;
+    bool pointers;
+    time_t checkpoint;
+    bool interactive;
+    unsigned trace_events;
+    bool dry_run;
+    std::string input_file_name;
+    bool save_coverage;
+    bool save_callgraph;
+    bool save_consumed_inputs;
+    PolicyParams params;
+};
+
+struct WorkItem {
+    int specimen_id, func_id, igroup_id;
+    WorkItem(): specimen_id(-1), func_id(-1), igroup_id(-1) {}
+    WorkItem(int specimen_id, int func_id, int igroup_id): specimen_id(specimen_id), func_id(func_id), igroup_id(igroup_id) {}
+    bool operator<(const WorkItem&) const;
+    bool operator==(const WorkItem&) const;
+};
+
+
+extern Switches opt;
+extern std::string argv0;
+extern int interrupted;
+
+void usage(int exit_status);
+int parse_commandline(int argc, char *argv[]);
+WorkList<WorkItem> load_worklist(const std::string &filename, FILE*);
+CloneDetection::PointerDetector* detect_pointers(SgAsmFunction*, const CloneDetection::FunctionIdMap&);
+void sig_handler(int signo);
+void add_builtin_functions(NameSet&);
+SgAsmGenericHeader* header_for_va(SgAsmInterpretation*, rose_addr_t va);
+Disassembler::AddressSet get_import_addresses(SgAsmInterpretation*, const NameSet &whitelist_names);
+void overmap_dynlink_addresses(SgAsmInterpretation*, const InstructionProvidor&, FollowCalls, MemoryMap *ro_map /*in,out*/,
+                               rose_addr_t special_value, const Disassembler::AddressSet &whitelist_imports,
+                               Disassembler::AddressSet &whitelist /*out*/);
+OutputGroup fuzz_test(SgAsmInterpretation *interp, SgAsmFunction *function, InputGroup &inputs, Tracer &tracer,
+                      const InstructionProvidor &insns, MemoryMap *ro_map, const PointerDetector *pointers,
+                      const AddressIdMap &entry2id, const Disassembler::AddressSet &whitelist_exports, FuncAnalyses &funcinfo,
+                      InsnCoverage &insn_coverage, DynamicCallGraph &dynamic_cg, ConsumedInputs &consumed_inputs);
+SqlDatabase::TransactionPtr checkpoint(const SqlDatabase::TransactionPtr &tx, OutputGroups &ogroups, Tracer &tracer,
+                                       InsnCoverage &insn_coverage, DynamicCallGraph &dynamic_cg, ConsumedInputs &consumed_inputs,
+                                       Progress &progress, size_t ntests_ran, int64_t cmd_id);
+
+
+
+
+} // namespace
+} // namespace
+
+#endif
