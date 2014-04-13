6888ef7f9a419ccbed5f1cb2fb27b7434b9b3e04
Dan Quinlan <dquinlan@llnl.gov>
2014-04-13 14:24:53 -0700

    Defined work around for handling of un-nmed enum declarations.

--- tests/roseTests/astSnippetTests/SmallSpecimensC/snippets8.c
+++ tests/roseTests/astSnippetTests/SmallSpecimensC/snippets8.c
@@ -5,9 +5,11 @@
 
 void handleFiles(char * otherFileName) {
     int value = 0;
+ // Note that this will become: value = ((*__ctype_b_loc ())[(int) ((*otherFileName))] & (unsigned short int) _ISupper);
     value = isupper(*otherFileName);
 }
 
 void weakness(char * taintedValue) {
+ // Note that this will become: int inSnippet = ((*__ctype_b_loc ())[(int) ((*taintedValue))] & (unsigned short int) _ISupper);
     int inSnippet = isupper(*taintedValue);
 }
