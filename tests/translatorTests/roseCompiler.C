bf71efcd8584171f5434614d980b3767a2179bb0
Chunhua (Leo) Liao <liaoch@llnl.gov>
2014-05-08 11:55:17 -0700

    (ir mapping) whole AST display support

--- tests/translatorTests/roseCompiler.C
+++ tests/translatorTests/roseCompiler.C
@@ -5,11 +5,22 @@
 // This provides a quick way to build src and install it for testing
 // without building projects, tests directories
 #include "rose.h"
+#include "transformationTracking.h"
 
 int
 main (int argc, char *argv[])
 {
   SgProject *sageProject = frontend (argc, argv);
   AstTests::runAllTests (sageProject);
+
+#if 0
+// Test unique ID assignment and dot graph generation
+  TransformationTracking::registerAstSubtreeIds (sageProject);
+  generateWholeGraphOfAST("wholeAST");
+
+// simple DOT graph
+  AstDOTGeneration astdotgen;
+  astdotgen.generateInputFiles(sageProject);
+#endif 
   return backend (sageProject);	// only backend error code is reported
 }
