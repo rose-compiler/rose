59649fdbe8af9066c36891af16091ffd25a9770c
Zack Galbreath <zack.galbreath@kitware.com>
2014-05-08 12:33:28 -0700

    get testTranslator running on Windows again
    It still is not generating a rose_ source file, but at
    least it doesn't segfault anymore.

--- src/frontend/SageIII/sage_support/sage_support.cpp
+++ src/frontend/SageIII/sage_support/sage_support.cpp
@@ -1687,7 +1687,10 @@ SgSourceFile::SgSourceFile ( vector<string> & argv , SgProject* project )
 // : SgFile (argv,errorCode,fileNameIndex,project)
    {
   // printf ("In the SgSourceFile constructor \n");
-
+     this->p_package = NULL;
+     this->p_import_list = NULL;
+     this->p_class_list = NULL;
+     
      set_globalScope(NULL);
 
   // DQ (6/15/2011): Added scope to hold unhandled declarations (see test2011_80.C).
