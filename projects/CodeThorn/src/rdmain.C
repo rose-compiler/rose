dccf7bdc8bb068913de9f17f96cd3e8fa9f46238
Markus Schordan <schordan1@llnl.gov>
2014-05-09 17:04:10 -0700

    CT: experimental substitution rdmain.

--- projects/CodeThorn/src/rdmain.C
+++ projects/CodeThorn/src/rdmain.C
@@ -37,6 +37,19 @@ void printAttributes(Labeler* labeler, VariableIdMapping* vim, string attributeN
   }
 }
 
+list<SgExpression*> varRefExpOfusedVar(SgNode* root, VariableId varId, VariableIdMapping* variableIdMapping ) {
+  list<SgExpression*> varRefList;
+  RoseAst ast(root);
+  for(RoseAst::iterator i=ast.begin();i!=ast.end();++i) {
+    if(SgVarRefExp* varRefExp=isSgVarRefExp(*i)) {
+      if(varId==variableIdMapping->variableId(varRefExp)) {
+        varRefList.push_back(varRefExp);
+      }
+    }
+  }
+  return varRefList;
+}
+
 // requires available expressions analysis
 void substituteUsesWithAvailableExpRhsOfDef(string udAttributeName, SgNode* root, Labeler* labeler, VariableIdMapping* variableIdMapping) {
   RoseAst ast(root);
@@ -48,7 +61,7 @@ void substituteUsesWithAvailableExpRhsOfDef(string udAttributeName, SgNode* root
     if(udAttr) {
       VariableIdSet usedVars=udAttr->useVariables(*variableIdMapping);
       if(usedVars.size()>0) {
-        cout<<"Found used vars."<<endl;
+        //cout<<"Found used vars."<<endl;
         // foreach uvar in usedVars do if(def(uvar)==1) replace(ast(uvar),rhs(def(uvar))) od
         for(VariableIdSet::iterator i=usedVars.begin();i!=usedVars.end();++i) {
           LabelSet varDefs=udAttr->definitionsOfVariable(*i);
@@ -58,9 +71,32 @@ void substituteUsesWithAvailableExpRhsOfDef(string udAttributeName, SgNode* root
             SgNode* defRootNode=labeler->getNode(def);
             // only substitute variables
             if(variableIdMapping->hasIntegerType(*i)||variableIdMapping->hasFloatingPointType(*i)) {
-              cout<<"Found UD Attribute with one def. variable:"<<variableIdMapping->uniqueShortVariableName(*i)<<" ";
-              cout<<"DEF:"<<defRootNode->unparseToString()<<endl;
-              //SgNodeHelper::replaceExpression(useRef,SageInterface::copyExpression(defRhs),true); // must be true (otherwise internal error)
+              //cout<<"Found UD Attribute with one def. variable:"<<variableIdMapping->uniqueShortVariableName(*i)<<" ";
+              //cout<<"DEF:"<<defRootNode->unparseToString()<<endl;
+              // somewhat combersome to determime the rhs of the def
+              // 1) declaration initializer
+              // 2) assignment
+              SgExpression* rhsExp=0;
+              if(isSgExprStatement(defRootNode)) {
+                defRootNode=SgNodeHelper::getExprStmtChild(defRootNode);
+              }
+              if(SgVariableDeclaration* decl=isSgVariableDeclaration(defRootNode)) {
+                SgExpression* exp=SgNodeHelper::getInitializerExpressionOfVariableDeclaration(decl);
+                if(exp) {
+                  rhsExp=exp;
+                }
+              }
+              if(SgAssignOp* assignOp=isSgAssignOp(defRootNode)) {
+                rhsExp=isSgExpression(SgNodeHelper::getRhs(assignOp));
+              }
+              if(rhsExp) {
+                list<SgExpression*> uses=varRefExpOfusedVar(node, *i, variableIdMapping);
+                for(list<SgExpression*>::iterator i=uses.begin();i!=uses.end();++i) {
+                  cout<<"Substituting:"<<(*i)->unparseToString()<<" by "<<rhsExp->unparseToString()<<endl;
+                  SgNodeHelper::replaceExpression(*i,SageInterface::copyExpression(rhsExp),true); // must be true (otherwise internal error)
+                }
+              }
+              // determine rhs of assignment
             }
           }
         }
@@ -92,7 +128,7 @@ int main(int argc, char* argv[]) {
   cout << "INFO: generating and attaching UD-data to AST."<<endl;
   createUDAstAttributeFromRDAttribute(rdAnalyzer->getLabeler(),"rd-analysis-in", "ud-analysis");
 
-  substituteUsesWithReadOnlyRhsOfDef("ud-analysis", root, rdAnalyzer->getLabeler(), rdAnalyzer->getVariableIdMapping());
+  substituteUsesWithAvailableExpRhsOfDef("ud-analysis", root, rdAnalyzer->getLabeler(), rdAnalyzer->getVariableIdMapping());
 
 #if 0
   Flow* flow=rdAnalyzer->getFlow();
