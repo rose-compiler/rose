286d195d1a091668a5c58e158b82be521806f2b0
Robb Matzke <matzke1@llnl.gov>
2014-06-11 21:43:11 -0400

    (Support) Temporary use of Sawyer headers to satisfy policies

---
+++ src/util/sawyer/useHeaders.C
@@ -0,0 +1,8 @@
+// Temporary C file that uses Sawyer header files.  This will go away as soon as other
+// branch start using sawyer. It's only here for now to keep "make enforce_policies" from
+// complaining about header files that aren't used anywhere. [Robb P. Matzke 2014-06-11]
+
+#include <sawyer/BitVector.h>
+#include <sawyer/GraphBoost.h>
+#include <sawyer/IntervalSet.h>
+#include <sawyer/PoolAllocator.h>
