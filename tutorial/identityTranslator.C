f485f45ef0fe66fbabecd7bd5e8e62e1c97a471c
Robb Matzke <matzke1@llnl.gov>
2014-07-11 14:47:39 -0400

    (Tutorial) Changed some comments to mprintf calls
    This is mainly to demonstrate how to perform logging.

--- tutorial/identityTranslator.C
+++ tutorial/identityTranslator.C
@@ -1,18 +1,22 @@
 // Example ROSE Translator: used for testing ROSE infrastructure
 
 #include "rose.h"
+#include "Diagnostics.h"
+using namespace rose::Diagnostics;
 
 int main( int argc, char * argv[] ) 
    {
-  // Build the AST used by ROSE
+  // Parse the command line and parse source files to build the AST used by ROSE
      SgProject* project = frontend(argc,argv);
 
-  // Run internal consistency tests on AST
+     mprintf("running consistency tests\n");
      AstTests::runAllTests(project);
 
   // Insert your own manipulation of the AST here...
 
-  // Generate source code from AST and call the vendor's compiler
-     return backend(project);
+     mprintf("generating source code from the AST and calling the vendor's compiler\n");
+     int status = backend(project);
+     mprintf("backend exit status is %d\n", status);
+     return status;
    }
 
