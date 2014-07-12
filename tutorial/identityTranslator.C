d86e93473907fdd7d98796a82e1c8fb24a15f1f9
Robb Matzke <matzke1@llnl.gov>
2014-07-12 14:46:31 -0400

    Revert "(Tutorial) Changed some comments to mprintf calls"
    This reverts commit 698d43f3c1472d68ac3fbcdc9f6b6c9749f3efb3.
    
    Does not link with Microsoft compiler:
    
    105>identityTranslator.obj : error LNK2019: unresolved external symbol "public: int __cdecl rose::Diagnostics::StreamPrintf::operator()(char const *,...)" (??RStreamPrintf@Diagnostics@rose@@QAAHPBDZZ) referenced in function _main
    
    105>identityTranslator.obj : error LNK2019: unresolved external symbol "class rose::Diagnostics::StreamPrintf __cdecl rose::Diagnostics::mfprintf(class std::basic_ostream<char,struct std::char_traits<char> > &)" (?mfprintf@Diagnostics@rose@@YA?AVStreamPrintf@12@AAV?$basic_ostream@DU?$char_traits@D@std@@@std@@@Z) referenced in function _main
    
    !105>identityTranslator.obj : error LNK2001: unresolved external symbol "class Sawyer::Message::Facility rose::Diagnostics::mlog" (?mlog@Diagnostics@rose@@3VFacility@Message@Sawyer@@A)

--- tutorial/identityTranslator.C
+++ tutorial/identityTranslator.C
@@ -1,22 +1,18 @@
 // Example ROSE Translator: used for testing ROSE infrastructure
 
 #include "rose.h"
-#include "Diagnostics.h"
-using namespace rose::Diagnostics;
 
 int main( int argc, char * argv[] ) 
    {
-  // Parse the command line and parse source files to build the AST used by ROSE
+  // Build the AST used by ROSE
      SgProject* project = frontend(argc,argv);
 
-     mprintf("running consistency tests\n");
+  // Run internal consistency tests on AST
      AstTests::runAllTests(project);
 
   // Insert your own manipulation of the AST here...
 
-     mprintf("generating source code from the AST and calling the vendor's compiler\n");
-     int status = backend(project);
-     mprintf("backend exit status is %d\n", status);
-     return status;
+  // Generate source code from AST and call the vendor's compiler
+     return backend(project);
    }
 
