517894c6a80e49abb60e32a9eb072b5d1434c94f
Robb Matzke <matzke1@llnl.gov>
2014-08-11 18:02:23 -0400

    (Clone Detection) Replaced cut-n-pasted code with a function call

--- projects/BinaryCloneDetection/compression/00-split-file-into-functions.C
+++ projects/BinaryCloneDetection/compression/00-split-file-into-functions.C
@@ -1,165 +1,15 @@
 // Adds functions to the database.
-#include "sage3basic.h"
+#include "rose.h"
 #include "DwarfLineMapper.h"
 #include "BinaryFunctionCall.h"
 #include "BinaryLoader.h"
 #include "Partitioner.h"
-#include "rose.h"
+#include "CloneDetectionLib.h"
 
 #include <boost/lexical_cast.hpp>
 
 
 std::string argv0;
-
-SgAsmInterpretation *
-open_specimen(const std::string &specimen_name, const std::string &argv0, bool do_link)
-{
-    // Parse the binary container (ELF, PE, etc) but do not disassemble yet.
-    std::cerr <<argv0 <<": parsing binary container: " <<specimen_name <<"\n";
-    std::string arg = "-rose:read_executable_file_format_only";
-    char *argv[4];
-    argv[0] = strdup(argv0.c_str());
-    argv[1] = strdup(arg.c_str());
-    argv[2] = strdup(specimen_name.c_str());
-    argv[3] = NULL;
-    int argc = 3;
-    SgProject *project = frontend(argc, argv);
-    free(argv[0]);
-    free(argv[1]);
-    free(argv[2]);
-
-    // Find the primary interpretation (e.g., the PE, not DOS, interpretation in PE files).
-    std::cerr <<argv0 <<": finding primary interpretation\n";
-    std::vector<SgAsmInterpretation*> interps = SageInterface::querySubTree<SgAsmInterpretation>(project);
-    if (interps.empty()) {
-        std::cerr <<argv0 <<": ERROR: no binary specimen given\n";
-        return NULL;
-    }
-    SgAsmInterpretation *interp = interps.back();
-    SgAsmGenericHeader *spec_header = interp->get_headers()->get_headers().back();
-
-    // If the specimen is a shared library then the .text section (and most of the others) are mapped starting at virtual
-    // address zero.  This will interfere with tests where the specimen reads from a memory address that was randomly
-    // generated--because the randomly generated address will be a low number. When the test tries to read from that low
-    // address it will read an instruction rather than a value from an input queue. The way we avoid this is to pre-map the low
-    // addresses to force BinaryLoader::remap() to move the specimen to a higher address. [Robb P. Matzke 2013-11-26]
-    Extent exclusion_area(0, 0x03000000); // size is arbitrary, but something recognizable
-    SgAsmGenericSection *text = spec_header->get_section_by_name(".text");
-    bool added_exclusion_area = false;
-    if (text!=NULL && text->is_mapped() && text->get_mapped_preferred_extent().overlaps(exclusion_area)) {
-        std::cerr <<argv0 <<": specimen is a shared object; remapping it to a higher virtual address\n";
-        MemoryMap *map = interp->get_map();
-        if (!map)
-            interp->set_map(map = new MemoryMap);
-        map->insert(exclusion_area,
-                    MemoryMap::Segment(MemoryMap::AnonymousBuffer::create(exclusion_area.size()),
-                                       0, MemoryMap::MM_PROT_NONE, "temporary exclusion area"));
-        added_exclusion_area = true;
-    }
-
-    // Get the shared libraries, map them, and apply relocation fixups. We have to do the mapping step even if we're not
-    // linking with shared libraries, because that's what gets the various file sections lined up in memory for the
-    // disassembler.
-    SgAsmGenericHeader *builtin_header = NULL;
-    if (do_link)
-        std::cerr <<argv0 <<": loading shared libraries\n";
-    if (BinaryLoader *loader = BinaryLoader::lookup(interp)) {
-        try {
-            loader = loader->clone(); // so our settings are private
-            if (do_link) {
-                // Link with the standard libraries
-                loader->add_directory("/lib32");
-                loader->add_directory("/usr/lib32");
-                loader->add_directory("/lib");
-                loader->add_directory("/usr/lib");
-                if (char *ld_library_path = getenv("LD_LIBRARY_PATH")) {
-                    std::vector<std::string> paths;
-                    StringUtility::splitStringIntoStrings(ld_library_path, ':', paths/*out*/);
-                    loader->add_directories(paths);
-                }
-                loader->link(interp);
-            } else {
-                // If we didn't link with the standard C library, then link with our own library.  Our own library is much
-                // smaller and is intended to provide the same semantics as the C library for those few functions that GCC
-                // occassionally inlines because the function is built into GCC.  This allows us to compare non-optimized
-                // code (without these functions having been inlined) with optimized code (where these functions are inlined)
-                // because the unoptimized code will traverse into the definitions we provide.
-                //
-                // Note: Using BinaryLoader to link our builtins.so does not actually resolve the imports in the specimen.
-                // I'm not sure why, but we work around this by calling link_builtins() below. [Robb P. Matzke 2013-07-11]
-                std::string subdir = "/projects/BinaryCloneDetection/semantic";
-                loader->add_directory(ROSE_AUTOMAKE_TOP_BUILDDIR + subdir);
-                loader->add_directory(ROSE_AUTOMAKE_TOP_SRCDIR + subdir);
-                loader->add_directory(ROSE_AUTOMAKE_LIBDIR);
-                std::string builtin_name = loader->find_so_file("builtins.so");
-                SgBinaryComposite *binary = SageInterface::getEnclosingNode<SgBinaryComposite>(interp);
-                assert(binary!=NULL);
-                SgAsmGenericFile *builtin_file = loader->createAsmAST(binary, builtin_name);
-                assert(builtin_file!=NULL);
-                builtin_header = builtin_file->get_headers()->get_headers().back();
-            }
-            loader->remap(interp);
-            BinaryLoader::FixupErrors fixup_errors;
-            loader->fixup(interp, &fixup_errors);
-            if (!fixup_errors.empty()) {
-                std::cerr <<argv0 <<": warning: " <<fixup_errors.size()
-                          <<" relocation fixup error" <<(1==fixup_errors.size()?"":"s") <<" encountered\n";
-            }
-            if (SageInterface::querySubTree<SgAsmInterpretation>(project).size() != interps.size())
-                std::cerr <<argv0 <<": warning: new interpretations created by the linker; mixed 32- and 64-bit libraries?\n";
-        } catch (const BinaryLoader::Exception &e) {
-            std::cerr <<argv0 <<": BinaryLoader error: " <<e.mesg <<"\n";
-            return NULL;
-        }
-    } else {
-        std::cerr <<argv0 <<": ERROR: no suitable loader/linker found\n";
-        return NULL;
-    }
-    assert(interp->get_map()!=NULL);
-    if (added_exclusion_area)
-        interp->get_map()->erase(exclusion_area);
-    MemoryMap map = *interp->get_map();
-
-    // Figure out what to disassemble.  If we did dynamic linking then we can mark the .got and .got.plt sections as read-only
-    // because we've already filled them in with the addresses of the dynamically linked entities.  This will allow the
-    // disassembler to know the successors for the indirect JMP instruction in the .plt section (the dynamic function thunks).
-    if (do_link) {
-        const SgAsmGenericHeaderPtrList &headers = interp->get_headers()->get_headers();
-        for (SgAsmGenericHeaderPtrList::const_iterator hi=headers.begin(); hi!=headers.end(); ++hi) {
-            SgAsmGenericSectionPtrList sections = (*hi)->get_sections_by_name(".got.plt");      // ELF
-            SgAsmGenericSectionPtrList s2 = (*hi)->get_sections_by_name(".got");                // ELF
-            SgAsmGenericSectionPtrList s3 = (*hi)->get_sections_by_name(".import");             // PE
-            sections.insert(sections.end(), s2.begin(), s2.end());
-            sections.insert(sections.end(), s3.begin(), s3.end());
-            for (SgAsmGenericSectionPtrList::iterator si=sections.begin(); si!=sections.end(); ++si) {
-                if ((*si)->is_mapped()) {
-                    Extent mapped_va((*si)->get_mapped_actual_va(), (*si)->get_mapped_size());
-                    map.mprotect(mapped_va, MemoryMap::MM_PROT_READ, true/*relax*/);
-                }
-            }
-        }
-    }
-
-    // Disassemble the executable
-    std::cerr <<argv0 <<": disassembling and partitioning\n";
-    if (Disassembler *disassembler = Disassembler::lookup(interp)) {
-        disassembler = disassembler->clone(); // so our settings are private
-#if 1 // FIXME [Robb P. Matzke 2013-05-14]
-        // We need to handle -rose:disassembler_search, -rose:partitioner_search, and -rose:partitioner_config
-        // command-line switches.
-#endif
-        Partitioner *partitioner = new Partitioner();
-        SgAsmBlock *gblk = partitioner->partition(interp, disassembler, &map);
-        interp->set_global_block(gblk);
-        gblk->set_parent(interp);
-    } else {
-        std::cerr <<argv0 <<": unable to disassemble this specimen\n";
-        return NULL;
-    }
-    return interp;
-}
-
-
 
 static void
 usage(int exit_status)
@@ -223,7 +73,7 @@ main(int argc, char *argv[])
 
     std::cout << "Specimen name is: " << specimen_name << std::endl;
 
-    SgAsmInterpretation *interp = open_specimen(specimen_name, argv0, false);
+    SgAsmInterpretation *interp = CloneDetection::open_specimen(specimen_name, argv0, false);
     SgBinaryComposite *binfile = SageInterface::getEnclosingNode<SgBinaryComposite>(interp);
     assert(interp!=NULL && binfile!=NULL);
  
