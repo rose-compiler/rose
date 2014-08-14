860f9a004efe51b41e87b3ae5ddb0ec696b17cfc
Robb Matzke <matzke1@llnl.gov>
2014-08-14 19:47:02 -0400

    (Binary Analysis) InstructionProvider creates unknown instructions w/data

--- projects/BinaryAnalysisTools/InstructionProvider.C
+++ projects/BinaryAnalysisTools/InstructionProvider.C
@@ -5,18 +5,24 @@ namespace BinaryAnalysis {
 
 SgAsmInstruction*
 InstructionProvider::operator[](rose_addr_t va) {
-    SgAsmInstruction *retval = NULL;
-    if (!insnMap_.getOptional(va).assignTo(retval)) {
+    SgAsmInstruction *insn = NULL;
+    if (!insnMap_.getOptional(va).assignTo(insn)) {
         if (useDisassembler_ && memMap_.exists(va, MemoryMap::MM_PROT_EXEC)) {
             try {
-                retval = disassembler_->disassembleOne(&memMap_, va);
+                insn = disassembler_->disassembleOne(&memMap_, va);
             } catch (const Disassembler::Exception &e) {
-                retval = disassembler_->make_unknown_instruction(e);
+                insn = disassembler_->make_unknown_instruction(e);
+                ASSERT_not_null(insn);
+                uint8_t byte;
+                if (1==memMap_.read(&byte, va, 1, MemoryMap::MM_PROT_EXEC))
+                    insn->set_raw_bytes(SgUnsignedCharList(1, byte));
+                ASSERT_require(insn->get_address()==va);
+                ASSERT_require(insn->get_size()==1);
             }
         }
-        insnMap_.insert(va, retval);
+        insnMap_.insert(va, insn);
     }
-    return retval;
+    return insn;
 }
 
 } // namespace
