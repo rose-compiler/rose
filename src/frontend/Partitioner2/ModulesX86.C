e0b9100a7f78613808e057cffd2a90908cd4eeb4
Robb Matzke <matzke1@llnl.gov>
2014-08-22 21:13:27 -0400

    (Binary Analysis) x86 function prologue detectors for new partitioner

--- src/frontend/Partitioner2/ModulesX86.C
+++ src/frontend/Partitioner2/ModulesX86.C
@@ -1,10 +1,87 @@
 #include "sage3basic.h"
 #include <Partitioner2/ModulesX86.h>
+#include <Partitioner2/Partitioner.h>
 
 namespace rose {
 namespace BinaryAnalysis {
 namespace Partitioner2 {
 namespace ModulesX86 {
+
+bool
+MatchStandardPrologue::match(Partitioner *partitioner, rose_addr_t anchor) {
+    const RegisterDescriptor bp(x86_regclass_gpr, x86_gpr_bp, 0,
+                                partitioner->instructionProvider().instructionPointerRegister().get_nbits());
+    const RegisterDescriptor sp(x86_regclass_gpr, x86_gpr_sp, 0, bp.get_nbits());
+
+    // Look for PUSH EBP
+    SgAsmx86Instruction *insn = NULL; 
+    {
+        rose_addr_t pushVa = anchor;
+        insn = isSgAsmx86Instruction(partitioner->discoverInstruction(pushVa));
+        if (partitioner->instructionExists(pushVa))
+            return false;                               // already in the CFG/AUM
+        if (!insn || insn->get_kind()!=x86_push)
+            return false;
+        const SgAsmExpressionPtrList &opands = insn->get_operandList()->get_operands();
+        if (opands.size()!=1)
+            return false;                               // crazy operands!
+        SgAsmRegisterReferenceExpression *rre = isSgAsmRegisterReferenceExpression(opands[0]);
+        if (!rre || rre->get_descriptor()!=bp)
+            return false;
+    }
+
+    // Look for MOV RBP,RSP following the PUSH.
+    {
+        rose_addr_t moveVa = insn->get_address() + insn->get_size();
+        if (partitioner->instructionExists(moveVa))
+            return false;                               // already in the CFG/AUM
+        insn = isSgAsmx86Instruction(partitioner->discoverInstruction(moveVa));
+        if (!insn || insn->get_kind()!=x86_mov)
+            return false;
+        const SgAsmExpressionPtrList &opands = insn->get_operandList()->get_operands();
+        if (opands.size()!=2)
+            return false;                               // crazy operands!
+        SgAsmRegisterReferenceExpression *rre = isSgAsmRegisterReferenceExpression(opands[0]);
+        if (!rre || rre->get_descriptor()!=bp)
+            return false;
+        rre = isSgAsmRegisterReferenceExpression(opands[1]);
+        if (!rre || rre->get_descriptor()!=sp)
+            return false;
+    }
+
+    function_ = Function::instance(anchor);
+    return true;
+}
+
+
+bool
+MatchHotPatchPrologue::match(Partitioner *partitioner, rose_addr_t anchor) {
+    // Look for optional MOV EDI, EDI
+    rose_addr_t moveVa = anchor;
+    if (partitioner->instructionExists(moveVa))
+        return false;                               // already in the CFG/AUM
+    SgAsmx86Instruction *insn = isSgAsmx86Instruction(partitioner->discoverInstruction(moveVa));
+    if (!insn || insn->get_kind()!=x86_mov)
+        return false;
+    const SgAsmExpressionPtrList &opands = insn->get_operandList()->get_operands();
+    if (opands.size()!=2)
+        return false;
+    SgAsmDirectRegisterExpression *dst = isSgAsmDirectRegisterExpression(opands[0]);
+    if (!dst ||
+        dst->get_descriptor().get_major()!=x86_regclass_gpr ||
+        dst->get_descriptor().get_minor()!=x86_gpr_di)
+        return false;
+    SgAsmDirectRegisterExpression *src = isSgAsmDirectRegisterExpression(opands[1]);
+    if (!src || dst->get_descriptor()!=src->get_descriptor())
+        return false;
+
+    // Match a standard prologue immediately following the hot-patch
+    if (!MatchStandardPrologue::match(partitioner, insn->get_address()+insn->get_size()))
+        return false;
+
+    function_ = Function::instance(anchor);
+    return true;
+}
 
 } // namespace
 } // namespace
--- src/frontend/Partitioner2/ModulesX86.h
+++ src/frontend/Partitioner2/ModulesX86.h
@@ -1,10 +1,42 @@
 #ifndef ROSE_Partitioner2_ModulesX86_H
 #define ROSE_Partitioner2_ModulesX86_H
 
+#include <Partitioner2/Modules.h>
+
 namespace rose {
 namespace BinaryAnalysis {
 namespace Partitioner2 {
 namespace ModulesX86 {
+
+/** Matches an x86 function prologue.
+ *
+ *  The standard x86 function prologue is:
+ *
+ * @code
+ *  push ebp
+ *  mov ebp, esp
+ * @endcode
+ *
+ *  The width of ebp and esp must match the word size for the architecture (i.e., they must be EBP and ESP for the i386 family,
+ *  and RBP, RSP for the amd64 family). */
+class MatchStandardPrologue: public FunctionPrologueMatcher {
+protected:
+    Function::Ptr function_;
+public:
+    static Ptr instance() { return Ptr(new MatchStandardPrologue); } /**< Allocating constructor. */
+    virtual Function::Ptr function() const /*override*/ { return function_; }
+    virtual bool match(Partitioner *partitioner, rose_addr_t anchor) /*override*/;
+};
+
+/** Matches an x86 function prologue with hot patch.
+ *
+ *  A hot-patch prologue is a MOV EDX, EDX instruction followed by a standard prologue. */
+class MatchHotPatchPrologue: public MatchStandardPrologue {
+public:
+    static Ptr instance() { return Ptr(new MatchHotPatchPrologue); } /**< Allocating constructor. */
+    virtual Function::Ptr function() const /*override*/ { return function_; }
+    virtual bool match(Partitioner *partitioner, rose_addr_t anchor) /*override*/;
+};
 
 } // namespace
 } // namespace
