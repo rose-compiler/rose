f265d8c94cfdc7ef11abd67dcb394d0391eec7a0
Dan Quinlan <dquinlan@llnl.gov>
2014-08-25 09:39:44 -0700

    Fixed more C++ support (and failing test code in unparseToString directory).

--- tests/CompileTests/Cxx_tests/test2014_157.C
+++ tests/CompileTests/Cxx_tests/test2014_157.C
@@ -1,29 +1,35 @@
 #include <vector>
 
-namespace XXX {
+namespace XXX 
+   {
      void foobar(const int def[3]);
-} // namespace XXX
+   } // namespace XXX
 
-namespace XXX {
-namespace {
-
-const unsigned size = 3;
-
-struct AAA
+namespace XXX 
    {
-     bool isPeriodic;
-   };
+     namespace 
+        {
 
-struct YYY;
-std::vector<YYY*> boundary_data;
+          const unsigned size = 3;
 
-} // unnamed namespace, back to XXX
+          struct AAA
+             {
+               bool isPeriodic;
+             };
 
-void foobar( const int def[size] )
-   {
-     AAA abc;
+       // struct YYY;
+          struct YYY {};
+          std::vector<YYY*> boundary_data;
 
-     abc.isPeriodic = false;
-   }
+      } // unnamed namespace, back to XXX
 
-} // namespace XXX
+#if 1
+     void foobar( const int def[size] )
+        {
+#if 0
+          AAA abc;
+          abc.isPeriodic = false;
+#endif
+        }
+#endif
+   } // namespace XXX
