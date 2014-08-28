dd0700d9ce3008ed28f2b5642ac7adfc6ff26e59
Dan Quinlan <dquinlan@llnl.gov>
2014-08-28 10:01:30 -0700

    Fixed more C++ support for template declaration vs. instantiation classifications (added more test codes).

--- tests/CompileTests/Cxx_tests/test2014_163.C
+++ tests/CompileTests/Cxx_tests/test2014_163.C
@@ -2,22 +2,21 @@ template <typename T>
 class HistVarSet 
    {
      public:
-#if 1
+#if 0
        // HistVarSet (int x) {}
           HistVarSet (int x);
 #endif
 #if 1
           void foo( HistVarSet<bool> flags );
-       // HistVarSet<bool> flags;
 #endif
    };
 
-HistVarSet<bool> log_flags(7);
-
 #if 1
-void foobar()
-   {
-     HistVarSet<bool> log_flags(7);
-   }
+void foobar( HistVarSet<bool> flags );
+#endif
+
+#if 0
+HistVarSet<bool> bool_flags;
+HistVarSet<float> float_flags;
 #endif
 
