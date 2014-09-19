8cce85c79d1c2e83c98f6b1ab9af3b214589478e
Robb Matzke <matzke1@llnl.gov>
2014-09-19 12:55:08 -0400

    (Edit Distance) Cleaned up compiler warnings in projects/EditDistanceMetric
    Also added calls to the version of TreeEditDistance that's in librose.

--- projects/EditDistanceMetric/astDiff.C
+++ projects/EditDistanceMetric/astDiff.C
@@ -8,28 +8,31 @@
 // the integer order of visit).
 
 #include <rose.h>
+#include <EditDistance/TreeEditDistance.h>
+#include <sawyer/CommandLine.h>
+#include <sawyer/Stopwatch.h>
 
 using namespace std;
+using namespace rose;
 
 // #include "labelTrace.h"
 #include "treeIsomorphism.h"
 #include "treeDiff.h"
 #include "sequenceGeneration.h"
 #include "treeEditDistance.h"
+#include "commandLine.h"
 
 int
 main(int argc, char** argv)
    {
-  // ios::sync_with_stdio();     // Syncs C++ and C I/O subsystems!
+     ios::sync_with_stdio();                            // Syncs C++ and C I/O subsystems!
+     rose::Diagnostics::initialize();                   // because librose doesn't initialize itself until frontend()
 
-  // DQ (9/1/2006): Introduce tracking of performance of ROSE at the top most level.
-     TimingPerformance timer ("AST equivalence test (main): time (sec) = ",true);
-
-     SgProject* project = frontend(argc,argv);
+     Settings settings;
+     std::vector<std::string> roseArgs = parseCommandLine(argc, argv, settings).unreachedArgs();
+     roseArgs.insert(roseArgs.begin(), std::string(argv[0])); // not sure why frontend needs this, but it does.
+     SgProject* project = frontend(roseArgs);
      ROSE_ASSERT (project != NULL);
-
-  // Just set the project, the report will be generated upon calling the destructor for "timer"
-     timer.set_project(project);
 
   // Internal AST consistancy tests.
      AstTests::runAllTests(project);
@@ -53,11 +56,28 @@ main(int argc, char** argv)
      printf ("Done with call to sequenceAlignment() \n");
 #endif
 
+     // This method simply prints the edits as a side effect and doesn't return anything useful.
+     Sawyer::Stopwatch tedLocalTimer;
      tree_edit_distance (project->get_fileList()[0],project->get_fileList()[1]);
+     std::cout <<"local implementation of tree edit distance took " <<tedLocalTimer <<" seconds\n";
 
-     outputAST(project);
-
-  // Unparse the binary executable (as a binary, as an assembler text file, 
-  // and as a dump of the binary executable file format details (sections)).
-     return backend(project);
+     // This stuff uses the new edit distance analysis in librose
+     std::cout <<"Using TreeEditDistance from librose:\n";
+     TreeEditDistance analysis;
+     Sawyer::Stopwatch tedRoseTimer;
+     analysis.compute(project->get_fileList()[0], project->get_fileList()[1],  // the subtrees to compare
+                      project->get_fileList()[0], project->get_fileList()[1]); // their file restrictions
+     std::cout <<"librose implementation of tree edit distance took " <<tedRoseTimer <<" seconds\n";
+     TreeEditDistance::Edits edits = analysis.edits();
+     std::cout <<"  Nodes in source tree: " <<analysis.sourceTreeNodes().size() <<"\n"
+               <<"  Nodes in target tree: " <<analysis.targetTreeNodes().size() <<"\n"
+               <<"  Number of edits:      " <<edits.size() <<"\n"
+               <<"  Total cost of edits:  " <<analysis.cost() <<"\n"
+               <<"  Relative cost:        " <<analysis.relativeCost() <<"\n"
+               <<"  Individual edits:\n";
+     BOOST_FOREACH (const TreeEditDistance::Edit &edit, edits)
+         std::cout <<"    " <<edit <<"\n";
+     std::ofstream graphViz("output-rose.dot");
+     analysis.emitGraphViz(graphViz);
+     std::cout <<"librose implementation of tree edit distance + output took " <<tedRoseTimer <<" seconds\n";
    }
