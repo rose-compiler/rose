b8a0f55b15bc0cf5668969ee77c985dd1cfc061a
Robb Matzke <matzke1@llnl.gov>
2014-09-19 10:33:38 -0400

    (Support) Update to latest Sawyer
    In particular, we're now using Sawyer's Stopwatch class instead of
    the one that was in ROSE. Sawyer's version has an API that is almost
    identical but adds additional things like operator<<. It also works
    with recent versions of boost (>=1.47 using boost::chrono) and falls
    back to POSIX and Windows APIs for older versions.

--- src/util/sawyer/Stopwatch.h
+++ src/util/sawyer/Stopwatch.h
@@ -1,8 +1,14 @@
-// Simple timer for real elapsed time.  Operates similar to a stopwatch.
-#ifndef ROSE_Stopwatch_H
-#define ROSE_Stopwatch_H
+#ifndef Sawyer_Stopwatch_H
+#define Sawyer_Stopwatch_H
 
-namespace rose {
+#include <sawyer/Sawyer.h>
+
+#ifdef SAWYER_HAVE_BOOST_CHRONO
+# include <boost/chrono/duration.hpp>
+# include <boost/chrono/system_clocks.hpp>
+#endif
+
+namespace Sawyer {
 
 /** Simple elapsed time.
  *
@@ -12,8 +18,8 @@ namespace rose {
  *  std::cerr <<"that took " <<stopwatch.stop() <<" seconds.\n";
  * @endcode
  *
- *  All times are returned as floating point number of seconds.  The underlying data structure has microsecond resolution
- *  although the system call might not be capable of reporting at that resolution.
+ *  All times are returned as floating point number of seconds.  The underlying data structure has nanosecond resolution
+ *  although accuracy is probably nowhere near that.
  *
  *  A stopwatch can be started and stopped as often as desired and it will accumulate the total amount of time it has been in
  *  the running state (the clear() method resets the stopwatch by stopping it and zeroing the total).  Starting an already
@@ -24,34 +30,29 @@ namespace rose {
  *
  *  Thread safety: These functions are not thread-safe, although multiple threads can invoke the methods concurrently on
  *  different objects.  It is permissible for different threads to invoke methods on the same object provided that
- *  synchronization occurs above the method calls.
- *
- *  See also, AstPerformance
- */
-class Stopwatch {
-    // Cached value from last call to gettimeofday(). We store the values as integer seconds and microseconds rather than
-    // floating point to avoid the loss of precision when subtracting to very similar floating point values.  We use our
-    // own struct rather than struct timeval to avoid having OS-specific conditional compilation in this header.
-    struct Timeval {
-        unsigned long seconds, useconds;
-    };
-    mutable Timeval begin_;
+ *  synchronization occurs above the method calls. */
+class SAWYER_EXPORT Stopwatch {
+public:
+#ifdef SAWYER_HAVE_BOOST_CHRONO
+    typedef boost::chrono::high_resolution_clock::time_point TimePoint;
+    typedef boost::chrono::duration<double> Duration;
+#else
+    typedef double TimePoint;
+    typedef double Duration;
+#endif
 
-    // Sum of elapsed run time time in seconds
-    mutable double elapsed_;
-
-    // State of the stopwatch: running or not.
-    bool running_;
-
-    // Similar to gettimeofday, but portable
-    static int getCurrentTime(Timeval*);
+private:
+#include <sawyer/WarningsOff.h>
+    mutable TimePoint begin_;                           // time that this stopwatch (re)started
+    mutable Duration elapsed_;                          // sum of elapsed run time in seconds
+    bool running_;                                      // state of the stopwatch: running or not
+#include <sawyer/WarningsRestore.h>
 
 public:
     /** Construct and optionally start a timer.
      *
      *  The timer is started immediately unless the constructor is invoked with a false argument. */
-    explicit Stopwatch(bool start=true)
-        : elapsed_(0.0), running_(false) {
+    explicit Stopwatch(bool start=true): elapsed_(0), running_(false) {
         start && this->start();
     }
 
@@ -84,6 +85,9 @@ public:
      *  Returns true if and only if the stopwatch is running. */
     bool isRunning() const { return running_; }
 };
+
+
+SAWYER_EXPORT std::ostream& operator<<(std::ostream&, const Stopwatch&);
 
 } // namespace
 #endif
