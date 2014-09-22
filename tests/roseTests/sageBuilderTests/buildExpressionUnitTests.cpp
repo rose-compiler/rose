ef5a1c21e36aef208d80ae6d9010239999514338
Jan Patrick Lehr <jp.lehr@gmail.com>
2014-09-22 13:09:09 -0700

    Fixed an error in the buildExpression unit tests.

--- tests/roseTests/sageBuilderTests/buildExpressionUnitTests.cpp
+++ tests/roseTests/sageBuilderTests/buildExpressionUnitTests.cpp
@@ -6,17 +6,19 @@
 #include "gtest/gtest.h"
 
 TEST(BuildIntValTest, HandlesDefaultParameter){
-  ::SgIntVal* _nullptr = NULL;
+  ::SgIntVal* _valnullptr = NULL;
+  ::SgTypeInt* _typenullptr = NULL;
   ::SgIntVal* t = SageBuilder::buildIntVal();
-  ASSERT_NE(_nullptr, t);
-  EXPECT_NE(_nullptr, isSgTypeInt(t));
+  ASSERT_NE(_valnullptr, t);
+  EXPECT_NE(_typenullptr, isSgTypeInt(t));
   EXPECT_EQ(0, t->get_value());
 }
 
 TEST(BuildIntValTest, HandlesParameter){
-  ::SgIntVal* _nullptr = NULL;
+  ::SgIntVal* _valnullptr = NULL;
+  ::SgTypeInt* _typenullptr = NULL;
   ::SgIntVal* t = SageBuilder::buildIntVal(42);
-  ASSERT_NE(_nullptr, t);
-  EXPECT_NE(_nullptr, isSgTypeInt(t));
+  ASSERT_NE(_valnullptr, t);
+  EXPECT_NE(_typenullptr, isSgTypeInt(t));
   EXPECT_EQ(42, t->get_value());
 }
