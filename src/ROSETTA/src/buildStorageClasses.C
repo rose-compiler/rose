f7367338ac7754557b4aa71be525a07ebb3e1027
Robb Matzke <matzke1@llnl.gov>
2014-10-07 17:24:37 -0400

    (Binary Analysis) AddressIntervalSet can be stored in Sage nodes

--- src/ROSETTA/src/buildStorageClasses.C
+++ src/ROSETTA/src/buildStorageClasses.C
@@ -747,6 +747,10 @@ Terminal::evaluateType(std::string& varTypeString)
         {
           returnType = STL_MAP;
         }
+     else if (varTypeString == "AddressIntervalSet")
+        {
+          returnType = STL_SET;
+        }
      else if ( 15 < length && varTypeString.substr(0,14) == "std::multimap<" && varTypeString.rfind(">" ) == length-1 )
         {
           returnType = STL_MULTIMAP;
