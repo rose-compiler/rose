c88d3e503533a0a12c7a00bf7f92a92ff7156f19
Chunhua (Leo) Liao <liaoch@llnl.gov>
2014-10-17 17:45:39 -0700

    (decl move) * declararation rescoping : skip compiler generated code from system headers. * handle single statement for-loop body, and similar bodies of while, if, etc. * Relax getLoopIndexVariable(), return NULL instead assert false * SageInterface::ensureBasicBlockAsFalseBodyOfIf() and changeAllBodiesToBlocks(): add a flag to allow avoiding creation of empty body

--- src/frontend/SageIII/sageInterface/sageInterface.C
+++ src/frontend/SageIII/sageInterface/sageInterface.C
@@ -8928,7 +8928,8 @@ SgInitializedName* SageInterface::getLoopIndexVariable(SgNode* loop)
   if (init.size() !=1)
   {
     cerr<<"SageInterface::getLoopIndexVariable(), no or more than one initialization statements are encountered. Not supported yet "<<endl;
-    ROSE_ASSERT(false);
+    //ROSE_ASSERT(false);
+    return NULL; 
   }
   SgStatement* init1 = init.front();
   SgExpression* ivarast=NULL;
@@ -8973,10 +8974,11 @@ SgInitializedName* SageInterface::getLoopIndexVariable(SgNode* loop)
   }
   else
   {
-    cerr<<"Error. SageInterface::getLoopIndexVariable(). Unhandled init_stmt type of SgForStatement"<<endl;
+    cerr<<"Warning: SageInterface::getLoopIndexVariable(). Unhandled init_stmt type of SgForStatement"<<endl;
     cerr<<"Init statement is :"<<init1->class_name() <<" " <<init1->unparseToString()<<endl;
     init1->get_file_info()->display("Debug");
-    ROSE_ASSERT (false);
+    return NULL; 
+    //ROSE_ASSERT (false);
   }
   // Cannot be both true
  // ROSE_ASSERT(!(isCase1&&isCase2));
@@ -12508,8 +12510,11 @@ SgBasicBlock* SageInterface::ensureBasicBlockAsBodyOfUpcForAll(SgUpcForAllStatem
     return isSgBasicBlock(b);
   }
 
-  SgBasicBlock* SageInterface::ensureBasicBlockAsFalseBodyOfIf(SgIfStmt* fs) {
+  SgBasicBlock* SageInterface::ensureBasicBlockAsFalseBodyOfIf(SgIfStmt* fs , bool createEmptyBody /* = true*/) {
     SgStatement* b = fs->get_false_body();
+    // if no false body at all AND no-create-empty-body
+    if (!createEmptyBody && (b == NULL || isSgNullStatement(b)))
+      return NULL;
     if (!isSgBasicBlock(b)) {
       b = SageBuilder::buildBasicBlock(b); // This works if b is NULL as well (producing an empty block)
       fs->set_false_body(b);
@@ -12817,8 +12822,11 @@ SgLocatedNode* SageInterface::ensureBasicBlockAsParent(SgStatement* s)
         changeAllBodiesToBlocks(top) ;
   }
 
-  void SageInterface::changeAllBodiesToBlocks(SgNode* top) {
-    struct Visitor: public AstSimpleProcessing {
+  void SageInterface::changeAllBodiesToBlocks(SgNode* top, bool createEmptyBody /*= true*/ ) {
+    class Visitor: public AstSimpleProcessing {
+      public: 
+      bool allowEmptyBody; 
+      Visitor (bool flag):allowEmptyBody(flag) {}
       virtual void visit(SgNode* n) {
         switch (n->variantT()) {
           case V_SgForStatement: {
@@ -12839,7 +12847,7 @@ SgLocatedNode* SageInterface::ensureBasicBlockAsParent(SgStatement* s)
           }
           case V_SgIfStmt: {
             ensureBasicBlockAsTrueBodyOfIf(isSgIfStmt(n));
-            ensureBasicBlockAsFalseBodyOfIf(isSgIfStmt(n));
+            ensureBasicBlockAsFalseBodyOfIf(isSgIfStmt(n), allowEmptyBody);
             break;
           }
           case V_SgCatchOptionStmt: {
@@ -12860,7 +12868,7 @@ SgLocatedNode* SageInterface::ensureBasicBlockAsParent(SgStatement* s)
         }
       }
     };
-    Visitor().traverse(top, postorder);
+    Visitor(createEmptyBody).traverse(top, postorder);
   }
 
 
@@ -18855,6 +18863,11 @@ void moveVariableDeclaration(SgVariableDeclaration* decl, std::vector <SgScopeSt
           // target scope is a for loop and the declaration declares its index variable.
           if (i_name == SageInterface::getLoopIndexVariable (stmt))
           {
+            if (i_name == NULL)
+            {
+              cerr<<"Warning: in moveVariableDeclaration(): targe_scope is a for loop with unrecognized index variable. Skipping it ..."<<endl;
+              break;
+            }
             // we move int i; to be for (int i=0; ...);
             SgStatementPtrList& stmt_list = stmt->get_init_stmt();
             // Try to match a pattern like for (i=0; ...) here
