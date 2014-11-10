04718697b3cb110a6c8107c32be7a8ed46905d99
Andreas Saebjoernsen <saebjornsen1@llnl.gov>
2014-11-10 10:41:13 -0800

    (Binary and SourceCloneDetection) base size of vector off V_SgNumVariants

--- projects/BinaryCloneDetection/semantic/compute_signature_vector.h
+++ projects/BinaryCloneDetection/semantic/compute_signature_vector.h
@@ -8,7 +8,7 @@ enum ExpressionCategory {ec_reg = 0, ec_mem = 1, ec_val = 2};
 
 class SignatureVector {
 public:
-    static const size_t Size = x86_last_instruction * 4 + 300 + 9 + 3;
+    static const size_t Size = V_SgNumVariants * 4 + 300 + 9 + 3;
     typedef uint16_t ElementType;
 
 private:
@@ -30,30 +30,30 @@ public:
     }
 
     ElementType& totalForVariant(size_t var) {
-        assert(var < x86_last_instruction);
+        assert(var < V_SgNumVariants);
         return values[var * 4];
     }
 
     ElementType& opsForVariant(ExpressionCategory cat, size_t var) {
-        assert(var < x86_last_instruction);
+        assert(var < V_SgNumVariants);
         return values[var * 4 + (int)cat + 1];
     }
 
     ElementType& specificOp(ExpressionCategory cat, size_t num) {
 	static ElementType dummyVariable = 0;
 	if (num < 100) {
-            return values[x86_last_instruction * 4 + 100 * (int)cat + num];
+            return values[V_SgNumVariants * 4 + 100 * (int)cat + num];
 	} else {
             return dummyVariable;
         }
     }
 
     ElementType& operandPair(ExpressionCategory a, ExpressionCategory b) {
-        return values[x86_last_instruction * 4 + 300 + (int)a * 3 + (int)b];
+        return values[V_SgNumVariants * 4 + 300 + (int)a * 3 + (int)b];
     }
 
     ElementType& operandTotal(ExpressionCategory a) {
-        return values[x86_last_instruction * 4 + 300 + 9 + (int)a];
+        return values[V_SgNumVariants * 4 + 300 + 9 + (int)a];
     }
 
     const ElementType* getBase() const {
