88295aac113574bdcdabfda6534f7b627f725099
Markus Schordan <schordan1@llnl.gov>
2014-11-14 19:15:39 -0800

    CT: updated RDAnalyzer.

--- projects/CodeThorn/src/RDAnalyzer.h
+++ projects/CodeThorn/src/RDAnalyzer.h
@@ -35,16 +35,6 @@ class RDAnalyzer : public DFAnalyzer<RDLattice> {
 
  private:
   void attachInfoToAst(string attributeName,bool isInInfo);
-#if 0
-  void transferExpression(Label label, SgExpression* expr, RDLattice& element);
-  void transferDeclaration(Label label, SgVariableDeclaration* decl, RDLattice& element);
-  void transferFunctionCall(Label lab, SgFunctionCallExp* callExp, SgExpressionPtrList& arguments, RDLattice& element);
-  void transferFunctionCallReturn(Label lab, SgFunctionCallExp* callExp, RDLattice& element);
-  void transferFunctionEntry(Label lab, SgFunctionDefinition* funDef,SgInitializedNamePtrList& formalParameters, RDLattice& element);
-  void transferFunctionExit(Label lab, SgFunctionDefinition* funDef, VariableIdSet& localVariablesInFunction, RDLattice& element);
-#else
-  //RDTransferFunctions* _rdTransferFunctions;
-#endif
 };
 
 #endif
