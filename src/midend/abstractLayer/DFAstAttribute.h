8cfd9ec5e1d116bd0b1f355e224561e136cb7bd2
Markus Schordan <schordan1@llnl.gov>
2014-11-19 11:15:46 -0800

    AL: added toStream method to AstAnnotator.

--- src/midend/abstractLayer/DFAstAttribute.h
+++ src/midend/abstractLayer/DFAstAttribute.h
@@ -1,8 +1,11 @@
 #ifndef DFASTATTRIBUTE_H
 #define DFASTATTRIBUTE_H
 
+#include <iostream>
 #include <string>
 using std::string;
+
+#include "VariableIdMapping.h"
 
 /*! 
   * \author Markus Schordan
@@ -12,6 +15,7 @@ class DFAstAttribute : public AstAttribute {
  public:
   DFAstAttribute();
   string virtual toString();
+  void virtual toStream(std::ostream& os,VariableIdMapping*);
   ~DFAstAttribute();
 };
 
