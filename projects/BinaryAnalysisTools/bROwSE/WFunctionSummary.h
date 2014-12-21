1ac7ac31e5be3439b53c1fbef72999147d533ada
Robb Matzke <matzke1@llnl.gov>
2014-12-20 20:47:42 -0500

    (Binary Analysis) More work on bROwSE server
    * Function analyzers have tool tips.
    
    * Function summary tab uses same analyzers as the function list tab.

--- projects/BinaryAnalysisTools/bROwSE/WFunctionSummary.h
+++ projects/BinaryAnalysisTools/bROwSE/WFunctionSummary.h
@@ -2,6 +2,7 @@
 #define bROwSE_WFunctionSummary_H
 
 #include <bROwSE/bROwSE.h>
+#include <bROwSE/FunctionUtil.h>
 #include <Partitioner2/Function.h>
 #include <Wt/WContainerWidget>
 
@@ -10,18 +11,24 @@ namespace bROwSE {
 // Presents function summary information
 class WFunctionSummary: public Wt::WContainerWidget {
     Context &ctx_;
-    P2::Function::Ptr function_;
-    Wt::WTable *table_;
-    Wt::WText *wName_, *wEntry_, *wBBlocks_, *wDBlocks_, *wInsns_, *wBytes_, *wDiscontig_, *wCallers_, *wCallsInto_;
-    Wt::WText *wCallees_, *wCallsOut_, *wRecursive_, *wMayReturn_, *wStackDelta_;
-public:
+    P2::Function::Ptr function_;                        // current function
+    std::vector<FunctionAnalyzer::Ptr> analyzers_;
+    Wt::WTable *wAnalysisResultTable_;
+    std::vector<Wt::WText*> analyzerResults_;           // one per analyzer
 
+public:
     WFunctionSummary(Context &ctx, Wt::WContainerWidget *parent=NULL)
-        : Wt::WContainerWidget(parent), ctx_(ctx), table_(NULL), wName_(NULL), wEntry_(NULL), wBBlocks_(NULL), wDBlocks_(NULL),
-          wInsns_(NULL), wBytes_(NULL), wDiscontig_(NULL), wCallers_(NULL), wCallsInto_(NULL), wCallees_(NULL),
-          wCallsOut_(NULL), wRecursive_(NULL), wMayReturn_(NULL), wStackDelta_(NULL) {
+        : Wt::WContainerWidget(parent), ctx_(ctx), wAnalysisResultTable_(NULL) {
         init();
     }
+
+    /** Vector of analyzers whose results should be displayed.  The array should be initialized during construction and not
+     * modified thereafter.
+     *
+     * @{ */
+    const std::vector<FunctionAnalyzer::Ptr>& analyzers() const { return analyzers_; }
+    std::vector<FunctionAnalyzer::Ptr>& analyzers() { return analyzers_; }
+    /** @} */
 
     void changeFunction(const P2::Function::Ptr &function);
 
