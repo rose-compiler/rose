cc60453ab28a89e69673d5aadbffc19696eed8c1
Markus Schordan <schordan1@llnl.gov>
2014-12-23 13:25:31 -0800

    CT: added missing PointerAnalysisInterface.

---
+++ projects/CodeThorn/src/PointerAnalysisInterface.C
@@ -0,0 +1,27 @@
+#include "PointerAnalysisInterface.h"
+
+SPRAY::PointerAnalysisInterface::PointerAnalysisInterface() {
+}
+
+SPRAY::PointerAnalysisInterface::~PointerAnalysisInterface() {
+}
+
+SPRAY::PointerAnalysisEmptyImplementation::PointerAnalysisEmptyImplementation(VariableIdMapping* vim) {
+  _variableIdMapping=vim;
+}
+
+void
+SPRAY::PointerAnalysisEmptyImplementation::initialize() {
+  // nothing to be done
+}
+
+void 
+SPRAY::PointerAnalysisEmptyImplementation::run() {
+  ROSE_ASSERT(_variableIdMapping);
+  // nothing to be done
+}
+
+VariableIdSet SPRAY::PointerAnalysisEmptyImplementation::getModByPointer() {
+  ROSE_ASSERT(_variableIdMapping);
+  return _variableIdMapping->getVariableIdSet();
+}
