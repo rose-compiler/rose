d0b7f1194eea6043e9ba13cfc0270da1dc4cc0c6
Dan Quinlan <dquinlan@llnl.gov>
2015-01-24 16:07:11 -0800

    Added fixes for for(;;) special case (added test code).

---
+++ tests/roseTests/astInterfaceTests/inputmoveDeclarationToInnermostScope_test2015_94.C
@@ -0,0 +1,17 @@
+
+void foobar()
+   {
+     int a;
+     int i;
+
+     switch(a) 
+        {
+          case 1:
+          case 2:
+             {
+               i = 42;
+            /* something here */
+               break ;
+             }
+        } 
+   } 
