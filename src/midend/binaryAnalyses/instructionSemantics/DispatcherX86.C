4f0a2c3726ccb5a57de001651b9b3214c85a2e7e
Robb Matzke <matzke1@llnl.gov>
2015-03-10 13:16:07 -0400

    (Binary Analysis) x86-64 semantics for MOVDQU

--- src/midend/binaryAnalyses/instructionSemantics/DispatcherX86.C
+++ src/midend/binaryAnalyses/instructionSemantics/DispatcherX86.C
@@ -1236,8 +1236,8 @@ struct IP_move_extend: P {
     }
 };
 
-// Move aligned double quadword
-struct IP_movdqa: P {
+// Move aligned or unaligned double quadword
+struct IP_movdq: P {
     void p(D d, Ops ops, I insn, A args) {
         assert_args(insn, args, 2);
         if (insn->get_lockPrefix()) {
@@ -1930,7 +1930,8 @@ DispatcherX86::iproc_init()
     iproc_set(x86_loopz,        new X86::IP_loop(x86_loopz));
     iproc_set(x86_mov,          new X86::IP_mov);
     iproc_set(x86_movd,         new X86::IP_move_extend);
-    iproc_set(x86_movdqa,       new X86::IP_movdqa);
+    iproc_set(x86_movdqa,       new X86::IP_movdq);
+    iproc_set(x86_movdqu,       new X86::IP_movdq);
     iproc_set(x86_movq,         new X86::IP_move_extend);
     iproc_set(x86_movsb,        new X86::IP_movestring(x86_repeat_none, 8));
     iproc_set(x86_movsw,        new X86::IP_movestring(x86_repeat_none, 16));
