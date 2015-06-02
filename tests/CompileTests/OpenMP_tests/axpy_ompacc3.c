d606d6b5477f66db3a7b4d79302d746049b628d8
Chunhua (Leo) Liao <liaoch@llnl.gov>
2015-06-02 16:45:51 -0700

    (XOMP) axpy multiple GPU version using standard directives

---
+++ tests/CompileTests/OpenMP_tests/axpy_ompacc3.c
@@ -0,0 +1,127 @@
+//AXPY multiple GPU version, using OpenMP 4.0 standard directives
+// vector = vector + vector * scalar
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <assert.h>
+#include <omp.h>
+
+#define REAL double
+#define VEC_LEN 1024000 //use a fixed number for now
+#define MAX_GPU_COUNT 4 // maximum GPU numbers in computation
+
+/* zero out the entire vector */
+void zero(REAL *A, int n)
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        A[i] = 0.0;
+    }
+}
+
+/* initialize a vector with random floating point numbers */
+void init(REAL *A, int n)
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        A[i] = (REAL)drand48();
+    }
+}
+
+REAL check(REAL*A, REAL*B, int n)
+{
+    int i;
+    REAL diffsum =0.0, sum = 0.0;
+    for (i = 0; i < n; i++) {
+        diffsum += fabs(A[i] - B[i]);
+        sum += fabs(B[i]);
+    }
+    return diffsum/sum;
+}
+
+/*serial version */
+void axpy(REAL* x, REAL* y, long n, REAL a) {
+  int i;
+  for (i = 0; i < n; ++i)
+  {
+    y[i] += a * x[i];
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  int n,i;
+  REAL *y_ref, *y_ompacc, *x;
+  REAL a = 123.456f;
+
+  n = VEC_LEN;
+  if (argc >= 2)
+    n = atoi(argv[1]);
+
+  y_ref = (REAL *) malloc(n * sizeof(REAL));
+  y_ompacc = (REAL *) malloc(n * sizeof(REAL));
+  x = (REAL *) malloc(n * sizeof(REAL));
+
+  srand48(1<<12);
+  init(x, n);
+  init(y_ref, n);
+  memcpy(y_ompacc, y_ref, n*sizeof(REAL));
+
+  int GPU_N = 0;
+  cudaGetDeviceCount(&GPU_N);
+  if (GPU_N > MAX_GPU_COUNT)
+  {
+    GPU_N = MAX_GPU_COUNT;
+  }
+  printf("CUDA-capable device count: %i\n", GPU_N);
+
+  // preparation for multiple GPUs
+
+  omp_set_num_threads(GPU_N); 
+#pragma omp parallel shared (GPU_N,x , y_ompacc, n) private(i)
+  {
+    int tid = omp_get_thread_num();
+    cudaSetDevice(tid);
+
+    int size = n / GPU_N;
+    int offset = size * tid;
+    if(tid < n%GPU_N)
+    {
+      size++; 
+    }
+    if(tid >= n%GPU_N)
+      offset += n%GPU_N;
+    else
+      offset += tid;
+
+    printf("thread %d working on GPU devices %d with size %d copying data from y_ompacc with offset %d\n",tid, tid, size,offset);
+    int j;
+#pragma omp target device (tid) map(tofrom: y_ompacc[offset:size]) map(to: x[offset:size],a,size, offset)
+#pragma omp parallel for shared(size, a)  private(j)
+    for (j = offset; j < offset+size; ++j)
+    {
+      y_ompacc[j] += a * x[j];
+    }
+  }
+
+  int num_threads;
+#pragma omp parallel shared (num_threads)
+  {
+    if (omp_get_thread_num() == 0)
+      num_threads = omp_get_num_threads();
+  }
+  // serial version
+  axpy(x, y_ref, n, a); 
+
+  REAL checksum = check(y_ref, y_ompacc, n);
+  printf("axpy(%d): checksum: %g\n", n, checksum);
+  assert (checksum < 1.0e-10);
+
+  free(y_ref);
+  free(y_ompacc);
+  free(x);
+  return 0;
+}
+
+
