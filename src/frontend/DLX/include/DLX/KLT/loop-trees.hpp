2fc4d2e9894c2feaba6781da67c46e4140568dbb
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2015-07-06 14:59:57 -0700

    [DLX][KLT][TileK] Made loop-trees extraction generic. Created a KLT interface section in DLX::language_t.

--- src/frontend/DLX/include/DLX/KLT/loop-trees.hpp
+++ src/frontend/DLX/include/DLX/KLT/loop-trees.hpp
@@ -7,69 +7,75 @@
 
 namespace DLX {
 
-template <class Language> 
+template <class Language>
+KLT::Data<KLT_Annotation<Language> > * convertData(typename Language::data_clause_t * data_clause, const DLX::Frontend::data_sections_t & data_section) {
+  typedef KLT_Annotation<Language> Annotation;
+
+  SgVariableSymbol * data_sym = data_section.first;
+
+  SgType * base_type = data_sym->get_type();
+  std::vector<DLX::Frontend::section_t>::const_iterator it_section;
+  for (it_section = data_section.second.begin(); it_section != data_section.second.end(); it_section++) {
+         if (isSgPointerType(base_type)) base_type = ((SgPointerType *)base_type)->get_base_type();
+    else if (isSgArrayType  (base_type)) base_type = ((SgArrayType   *)base_type)->get_base_type();
+    else assert(false);
+    assert(base_type != NULL);
+  }
+  KLT::Data<Annotation> * data = new KLT::Data<Annotation>(data_sym, base_type);
+  for (it_section = data_section.second.begin(); it_section != data_section.second.end(); it_section++) {
+    typename KLT::Data<Annotation>::section_t section;
+      section.lower_bound = it_section->lower_bound;
+      section.size = it_section->size;
+      section.stride = it_section->stride;
+    data->addSection(section);
+  }
+  data->annotations.push_back(Annotation(data_clause));
+  return data;
+}
+
+template <class Language>
+void extractData(const std::vector<DLX::Directives::generic_clause_t<Language> *> & clauses, KLT::LoopTrees< KLT_Annotation<Language> > * loop_tree) {
+  typedef KLT_Annotation<Language> Annotation;
+
+  typename std::vector<typename Language::clause_t *>::const_iterator it_clause;
+  for (it_clause = clauses.begin(); it_clause != clauses.end(); it_clause++) {
+    typename Language::data_clause_t * data_clause = Language::isDataClause(*it_clause);
+    if (data_clause == NULL) continue;
+
+    const std::vector<DLX::Frontend::data_sections_t> & data_sections = Language::getDataSections(data_clause);
+    std::vector<DLX::Frontend::data_sections_t>::const_iterator it_data_sections;
+    for (it_data_sections = data_sections.begin(); it_data_sections != data_sections.end(); it_data_sections++)
+      loop_tree->addData(convertData<Language>(data_clause, *it_data_sections));
+  }
+}
+
+template <class Language>
 void extractLoopTrees(
-  const std::vector<DLX::Directives::directive_t<Language> *> & directives,
-  std::map<DLX::Directives::directive_t<Language> *, KLT::LoopTrees< ::DLX::KLT_Annotation<Language> > *> & loop_trees,
-  std::map<SgForStatement *, typename KLT::LoopTrees< ::DLX::KLT_Annotation<Language> >::loop_t *> & loop_map
+  const std::vector<typename Language::directive_t *> & directives,
+  std::map<typename Language::directive_t *, KLT::LoopTrees< KLT_Annotation<Language> > *> & loop_trees,
+  std::map<SgForStatement *, typename KLT::LoopTrees< KLT_Annotation<Language> >::loop_t *> & loop_map
 ) {
-  typedef ::DLX::KLT_Annotation<Language> Annotation;
+  typedef KLT_Annotation<Language> Annotation;
   typedef ::KLT::LoopTrees<Annotation> LoopTrees;
 
-  typename std::vector<DLX::Directives::directive_t<Language> *>::const_iterator it_directive;
+  typename std::vector<typename Language::directive_t *>::const_iterator it_directive;
   for (it_directive = directives.begin(); it_directive != directives.end(); it_directive++) {
+    typename Language::directive_t * directive = *it_directive;
 
-    DLX::Directives::directive_t<Language> * directive = *it_directive;
-
-    if (directive->construct->kind != Language::e_construct_kernel) continue;                                    // (language dependant)
-    DLX::Directives::construct_t<Language, Language::e_construct_kernel> * construct =                           // (language dependant)
-                 (DLX::Directives::construct_t<Language, Language::e_construct_kernel> *)(directive->construct); // (language dependant)
+    typename Language::kernel_construct_t * kernel_construct = Language::isKernelConstruct(directive->construct);
+    if (kernel_construct == NULL) continue;
 
     // Create empty loop-tree (generic)
     LoopTrees * loop_tree = new LoopTrees();
-    loop_trees.insert(std::pair<DLX::Directives::directive_t<Language> *, LoopTrees *>(directive, loop_tree));
+    loop_trees.insert(std::pair<typename Language::directive_t *, LoopTrees *>(directive, loop_tree));
 
-    // Add data to loop-tree (language dependant)
-    typename std::vector<DLX::Directives::generic_clause_t<Language> *>::const_iterator it_clause;
-    for (it_clause = directive->clause_list.begin(); it_clause != directive->clause_list.end(); it_clause++) {
-      DLX::Directives::generic_clause_t<Language> * clause = *it_clause;
-
-      if (clause->kind != Language::e_clause_data) continue;                               // (language dependant)
-      DLX::Directives::clause_t<Language, Language::e_clause_data> * data_clause =         // (language dependant)
-                   (DLX::Directives::clause_t<Language, Language::e_clause_data> *)clause; // (language dependant)
-
-      std::vector<DLX::Frontend::data_sections_t>::const_iterator it_data_sections;
-      for (it_data_sections = data_clause->parameters.data_sections.begin(); it_data_sections != data_clause->parameters.data_sections.end(); it_data_sections++) {
-        SgVariableSymbol * data_sym = it_data_sections->first;
-
-        SgType * base_type = data_sym->get_type();
-        std::vector<DLX::Frontend::section_t>::const_iterator it_section;
-        for (it_section = it_data_sections->second.begin(); it_section != it_data_sections->second.end(); it_section++) {
-          SgPointerType * ptr_type = isSgPointerType(base_type);
-          SgArrayType * arr_type = isSgArrayType(base_type);
-          if (ptr_type != NULL)
-            base_type = ptr_type->get_base_type();
-          else if (arr_type != NULL)
-            base_type = arr_type->get_base_type();
-          else assert(false);
-          assert(base_type != NULL);
-        }
-        KLT::Data<Annotation> * data = new KLT::Data<Annotation>(data_sym, base_type);
-        for (it_section = it_data_sections->second.begin(); it_section != it_data_sections->second.end(); it_section++) {
-          typename KLT::Data<Annotation>::section_t section;
-            section.lower_bound = it_section->lower_bound;
-            section.size = it_section->size;
-            section.stride = it_section->stride;
-          data->addSection(section);
-        }
-        loop_tree->addData(data);
-      }
-    }
+    // Extract Data
+    extractData(directive->clause_list, loop_tree);
 
     std::vector<SgVariableSymbol *> iterators, locals, others;
 
     // Parse the loop nest (generic)
-    SgStatement * region_base = construct->assoc_nodes.kernel_region;
+    SgStatement * region_base = Language::getKernelRegion(kernel_construct);
     SgBasicBlock * region_bb = isSgBasicBlock(region_base);
     size_t loop_cnt = 0;
     if (region_bb != NULL) {
@@ -98,13 +104,14 @@ void extractLoopTrees(
 
   // Apply loop construct to loops in loop-tree
   for (it_directive = directives.begin(); it_directive != directives.end(); it_directive++) {
-    DLX::Directives::directive_t<Language> * directive = *it_directive;
+    typename Language::directive_t * directive = *it_directive;
 
-    if (directive->construct->kind != Language::e_construct_loop) continue;                                                                     // (language dependant)
-    DLX::Directives::construct_t<Language, Language::e_construct_loop> * construct =                                                            // (language dependant)
-                 (DLX::Directives::construct_t<Language, Language::e_construct_loop> *)(directive->construct);                                  // (language dependant)
-    assert(construct->assoc_nodes.for_loop != NULL);                                                                                            // (language dependant)
-    typename std::map<SgForStatement *, typename LoopTrees::loop_t *>::const_iterator it_loop = loop_map.find(construct->assoc_nodes.for_loop); // (language dependant)
+    typename Language::loop_construct_t * loop_construct = Language::isLoopConstruct(directive->construct);
+    if (loop_construct == NULL) continue;
+    SgForStatement * for_loop = Language::getLoopStatement(loop_construct);
+    assert(for_loop != NULL);
+
+    typename std::map<SgForStatement *, typename LoopTrees::loop_t *>::const_iterator it_loop = loop_map.find(for_loop);
     assert(it_loop != loop_map.end());
 
     typename LoopTrees::loop_t * loop = it_loop->second;
