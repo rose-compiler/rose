bb0b99441e4dde712900b4328fae116d6b564d60
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2015-08-04 16:57:03 -0700

    [KLT] Testing subkernels in new KLT

--- tests/CompileTests/TileK/tests/rtl/test_1.c
+++ tests/CompileTests/TileK/tests/rtl/test_1.c
@@ -1,10 +1,14 @@
 
-#include "RTL/Host/klt-user.h"
-
-#include "KLT/RTL/kernel.h"
-#include "KLT/RTL/loop.h"
-#include "KLT/RTL/tile.h"
-#include "KLT/RTL/data.h"
+#ifndef ORIGINAL
+#define ORIGINAL 0
+#endif
+#if ORIGINAL == 0
+#  include "RTL/Host/klt-user.h"
+#  include "KLT/RTL/kernel.h"
+#  include "KLT/RTL/loop.h"
+#  include "KLT/RTL/tile.h"
+#  include "KLT/RTL/data.h"
+#endif
 
 #include <stdlib.h>
 
@@ -18,6 +22,12 @@ int main() {
   for (i = 0; i < n; i++)
     A[i] = i;
 
+#if ORIGINAL == 1
+  #pragma kernel data(A[0:n])
+  #pragma loop tile[0](dynamic)
+  for (i = 0; i < n; i++)
+    A[i] += b;
+#else
   struct klt_kernel_t * kernel = klt_build_kernel(0);
 
   kernel->param[0] = &n;
@@ -30,6 +40,7 @@ int main() {
   kernel->loops[0].stride = 1;
 
   klt_execute_kernel(kernel);
+#endif
 
   return 0;
 }
