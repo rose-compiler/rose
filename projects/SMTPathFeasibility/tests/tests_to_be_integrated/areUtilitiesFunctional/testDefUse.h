1d238987db6978c62f4beee9db061af5d6fa8bee
Michael Hoffman <hoffman34@llnl.gov>
2015-09-04 17:51:04 -0700

    Made changes to be sure that dev policy checks are satisfied

--- projects/SMTPathFeasibility/tests/tests_to_be_integrated/areUtilitiesFunctional/testDefUse.h
+++
@@ -1,7 +0,0 @@
-#ifndef HH_SMTTESTDEFUSE_HH
-#define HH_SMTTESTDEFUSE_HH
-
-void debugAnalyzeVariableReference(SgVarRefExp*);
-void initDefUseAnalysis(SgProject*);
-
-#endif
--- projects/SMTPathFeasibility/utils/expression_utils.cpp
+++
@@ -1,6 +0,0 @@
-#include "expression_utils.h" 
-
-void fixLongExp(SgExpression* se) {
-	SageInterface::splitExpressionIntoBasicBlock(se);
-	return;
-}
