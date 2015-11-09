2b6a35bd5975641aa04eef035c537e36fab67a96
Robb P. Matzke <matzke1@llnl.gov>
2015-11-09 14:25:06 -0500

    (Binary Analysis) Documentation and demos compairing attribute APIs

--- src/midend/binaryAnalyses/BinaryAttribute.h
+++ src/midend/binaryAnalyses/BinaryAttribute.h
@@ -12,7 +12,7 @@ namespace BinaryAnalysis {
 
 /** Attributes attached to other objects.
  *
- *  This namespace defines a mechanism for extending objects at runtime through the use of name/value pairs called
+ *  This namespace defines a mechanism for extending objects at run-time through the use of name/value pairs called
  *  "attributes".  Attributes are an extension mechanism orthogonal to class derivation and compile-time containment. Among
  *  other things, they are a convenient mechanism for users to cache data that needs to be associated with some ROSE
  *  object. See also, @ref AstAttributeMechanism for attributes attached to Sage IR nodes. There are two steps to using
@@ -80,9 +80,193 @@ namespace BinaryAnalysis {
  *  @snippet binaryAttribute.C providing
  *
  *  When such an object is assigned or copy-constructed the attribute values are copied using their assignment or copy
- *  constructors. When an object is deleted, its attribute values are also deleted. */
+ *  constructors. When an object is deleted, its attribute values are also deleted.
+ *
+ *
+ *
+ *  @section binary_attribute_comparison Comparison with other attribute APIs
+ *
+ *  ROSE has an older @ref AstAttributeMechanism that's derived from the AttributeMechanism class template. There are
+ *  differences between these APIs.
+ *
+ *  <table>
+ *    <tr>
+ *      <td><b>This API</b></td>
+ *      <td><b>AstAttributeMechanism</b></td>
+ *      <td><b>AttributeMechanism</b></td>
+ *    </tr>
+ *    <tr>
+ *      <td>Can store multiple attributes with many different value types.</td>
+ *      <td>Can store multiple attributes with many different value types as long as those types all derive from
+ *          AstAttribute.</td>
+ *      <td>Can store multiple attributes all having the same type (second template argument). To store multiple
+ *          types additional AttributeMechanism data members must be used.</td>
+ *    </tr>
+ *    <tr>
+ *      <td>Can directly store non-class values.</td>
+ *      <td>Requires non-class values to be wrapped in a class derived from @ref AstAttribute</td>
+ *      <td>Can directly store non-class values (one value type per AttributeMechanism)</td>
+ *    </tr>
+ *    <tr>
+ *      <td>%Attribute getter/setter methods are directly in containing class.</td>
+ *      <td>Must obtain the AstAttributeMechanism data member first (@ref SgNode::get_attributeMechanism), then the
+ *          getter/setter.</td>
+ *      <td>Must get the AttributeMechanism data member first unless only one value type is allowed (each AttributeMechanism
+ *          data member can store attribute values having a single type).</td>
+ *    </tr>
+ *    <tr>
+ *      <td>Can store values whose type is not user-modifiable, such as STL containers.</td>
+ *      <td>User must be able modify the value type so it inherits from AstAttribute, or he must wrap the type in his own
+ *          subclass of AstAttribute, adding an extra level of indirection to access the value.</td>
+ *      <td>Can store values whose type is not user-modifiable, such as STL containers.</td>
+ *    </tr>
+ *    <tr>
+ *      <td>Ensures that two users don't declare the same attribute name.</td>
+ *      <td>No assurance that names don't collide, but inserting an attribute that already exists is an error.</td>
+ *      <td>No assurance that names don't collide, but inserting an attribute that already exists is an error.</td>
+ *    </tr>
+ *    <tr>
+ *      <td>Uses normal C++ copy constructors and assignment operators for attribute values.</td>
+ *      <td>Requires implementation of virtual @c copy method (non-pure), which returns a null pointer if the user forgets.</td>
+ *      <td>Uses normal C++ copy constructors and assignment operators for attribute values.</td>
+ *    </tr>
+ *    <tr>
+ *      <td>Errors are reported by dedicated exception types allowing the user to gracefully recover.</td>
+ *      <td>Errors are reported by printing them to the standard error stream and then either doing nothing (production) or
+ *          aborting (debug).</td>
+ *      <td>Errors are reported by printing them to the standard error stream and then either doing nothing (production) or
+ *          aborting (debug).</td>
+ *    </tr>
+ *    <tr>
+ *      <td>%Attribute existence and retrieval can be performed on a reference to a const object.</td>
+ *      <td>The API does not support non-modifying operations on a reference to a const object.</td>
+ *      <td>The API does not support non-modifying operations on a reference to a const object.</td>
+ *    </tr>
+ *    <tr>
+ *      <td>Attempting to retrieve a non-existing attribute without providing a default value throws a
+ *          @ref DoesNotExist exception.</td>
+ *      <td>Attempting to retrieve a non-existing attribute emits an error message and then causes the object to exist
+ *          but have a null pointer (in Production mode only).</td>
+ *      <td>Attempting to retrieve a non-existing attribute emits an error message and then causes the attribute to
+ *          spring into existence with a default-constructed value (in Production mode only).</td>
+ *    </tr>
+ *    <tr>
+ *      <td>Inserting a new value for an existing attribute erases the old value like the C++ assignment operator.</td>
+ *      <td>Inserting a new value for an existing attribute is an error (see above); the user must check attribute
+ *          existence first.</td>
+ *      <td>Inserting a new value for an existing attribute is an error (see above); the user must check attribute
+ *          existence first.</td>
+ *    </tr>
+ *    <tr>
+ *      <td>Erasing an attribute that is not stored is a no-op similar to STL container erase methods.</td>
+ *      <td>Erasing an attribute that is not stored is an error, handled as described above; the user must check
+ *          attribute existence before erasing.</td>
+ *      <td>Erasing an attribute that is not stored is an error, handled as described above; the user must check
+ *          attribute existence before erasing.</td>
+ *    </tr>
+ *    <tr>
+ *      <td>%Attribute value types are runtime checked. A mismatch between writing and reading is reported by an
+ *          exception.</td>
+ *      <td>%Attribute values types are runtime checked. A mismatch is discovered by the user when they perform a
+ *          @c dynamic_cast from the AstAttribute base type to their subclass.</td>
+ *      <td>%Attribute values are compile-time checked since each AttributeMechanism can store only one value type.</td>
+ *    </tr>
+ *    <tr>
+ *      <td>All casting is hidden behind the API.</td>
+ *      <td>Requires user to use C++ @c dynamic_cast from the AstAttribute pointer to the user's subclass pointer.</td>
+ *      <td>No dynamic casting necessary since each AttributeMechanism stores only one value type.</td>
+ *    </tr>
+ *    <tr>
+ *      <td>%Attribute value destructors are called when the containing object is destroyed.</td>
+ *      <td>Attributes (all allocated on the heap) are never deleted automatically; the user must figure out when a
+ *          value is no longer in use and delete it explicitly.</td>
+ *      <td>%Attribute value destructors are called when the containing object is destroyed.</td>
+ *    </tr>
+ *  </table>
+ *
+ *  Some examples may help illuminate the differences. Let us assume that two types exist in some library header file somewhere
+ *  and the user wants to store these as attribute values in some object. The two value types are:
+ *
+ *  @snippet binaryAttribute.C comparison value types
+ *
+ *  Let us also assume that a ROSE developer has a class and wants the user to be able to store attributes in objects of that
+ *  class.  The first step is for the ROSE developer to prepare his class for storing attributes:
+ *
+ *  @snippet binaryAttribute.C comparison preparing storage
+ *
+ *  Notice that AttributeMechanism requires a data member for each type of value. AstAttributeMechanism, which derives from
+ *  AttributeMechanism, avoids this by using pointers to polymorhic value types. So the AttributeMechanism is already not
+ *  extensible unless the user can edit the type where the attributes are stored--but if he can edit the type, then why use
+ *  attributes at all?
+ *
+ *  Now we jump into the user code. The user wants to be able to store two attributes, one of each value type. As mentioned
+ *  above, the attribute value types are defined in some library header, and the class of objects in which to store them is
+ *  defined in a ROSE header file.  Methods 1 and 3 can store values of any type, but the user has more work to do before he
+ *  can use method 2:
+ *
+ *  @snippet binaryAttribute.C comparison attribute wrappers
+ *
+ *  Notice that AstAttributeMechanism needs a substantial amount of boilerplate to store even a simple enum value. Since @c
+ *  copy is not pure virtual, if the user forgets to implement it (or mistypes it without C++11 @c override, like accidentally
+ *  adding the @c const qualifier which is normally present for a copy constructor), then attribute values of that type will
+ *  not be copied when the containing object is copied, but the @c exists method will say they do exist.
+ *
+ *  The user will also want to use descriptive strings for the attribute so error messages are informative, but shorter names
+ *  in C++ code, so we declare the attribute names:
+ *
+ *  @snippet binaryAttribute.C comparison declare 1
+ *  @snippet binaryAttribute.C comparison declare 2
+ *  @snippet binaryAttribute.C comparison declare 3
+ *
+ *  The declarations in methods 2 and 3 are identical. Method 1 differs by using an integral type for attribute IDs, which has
+ *  two benefits: (1) it gives the API an opportunity to check whether two different users are trying to declare the same
+ *  attribute name for different purposes, and (2) it reduces the size and increases the speed of the underlying storage maps
+ *  by storing integer keys rather than strings.
+ *
+ *  Let us see how to insert two attributes into an object assuming that the object came from somewhere far away and we don't
+ *  know whether it already contains these attributes. If it does, we want to overwrite their old values with new
+ *  values. Overwriting values is likely to be a more common operation than inserting a value only if it doesn't already
+ *  exist. After all, languages generally don't have a dedicated assign-value-if-none-assigned operator (Perl and Bash being
+ *  exceptions).
+ *
+ *  @snippet binaryAttribute.C comparison insert 1
+ *  @snippet binaryAttribute.C comparison insert 2
+ *  @snippet binaryAttribute.C comparison insert 3
+ *
+ *  Methods 1 and 3 are able to insert a new value and clean up any old value if it existed by automatically calling the old
+ *  value's destructor.  Method 2 requires the user to check whether an old value existed and delete the value
+ *  explicitly. Fortunately deleting a null pointer is a no-op, otherwise we'd have to check that also.
+ *
+ *  Eventually the user will want to retrieve an attribute's value. Perhaps he wants the attribute value if it exists, or some
+ *  default if it doesn't:
+ *
+ *  @snippet binaryAttribute.C comparison retrieve 1
+ *  @snippet binaryAttribute.C comparison retrieve 2
+ *  @snippet binaryAttribute.C comparison retrieve 3
+ *
+ *  Notice that methods 2 and 3 require that the user first checks for attribute existence since querying a non-existing
+ *  attribute will emit an error message and either abort (when compiled in debug mode) or instantiate a new attribute (when
+ *  compiled in production mode).  In production mode, querying a non-existing attribute with method 2 will instantiate the
+ *  attribute with a null pointer which the @p exists method interprets as the attribute existing even though it doesn't, and
+ *  method 3 will instantiate a default-constructed attribute.  Method 2 also requires a cumbersome dynamic cast, an additional
+ *  pointer check, and then unrwapping the wrapped attribute.
+ *
+ *  Finally, when the object containing the attributes is destroyed the user needs to be able to clean up by destroying the
+ *  attributes that are attached. For instance, if a function has local variables that might be storing attributes and the
+ *  function calls something that might throw an exception we need to catch the exception, delete the attributes (checking if
+ *  they exist before we access them), and rethrow the same exception. This also makes debugging more difficult because GDB
+ *  will lose information about where the exception was originally thrown--it will give you only the location of the rethrow.
+ *
+ *  @snippet binaryAttribute.C comparison cleanup 1
+ *  @snippet binaryAttribute.C comparison cleanup 2
+ *  @snippet binaryAttribute.C comparison cleanup 3
+ *
+ *  Whether to use Method 1, 2, or 3 is up to the author of the class that wants to allow attributes to be stored. */
 namespace Attribute {
 
+/** Attribute identification.
+ *
+ *  Each attribute name has a unique identification number and vice versa. */
 typedef size_t Id;
 
 /** Invalid attribute ID. */
--- tutorial/binaryAttribute.C
+++ tutorial/binaryAttribute.C
@@ -32,8 +32,8 @@ struct MyClass: public Attribute::Storage {
 
 
 
-int
-main() {
+void
+example_usage() {
 
     //! [declaring]
     const Attribute::Id DUCKS    = Attribute::declare("number of ducks");
@@ -85,4 +85,281 @@ main() {
     //! [erasing]
     obj.eraseAttribute(DUCKS);
     //! [erasing]
+}
+
+static int
+something_that_might_throw() {
+    static int ncalls = 0;
+    if (ncalls++ != 0)
+        throw std::runtime_error("I did throw!");
+    return ncalls;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      Attribute value types we want to store
+//
+// Assume that the user does not have access to modify these types (they are in some library header file).
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//! [comparison value types]
+enum Approximation { UNDER_APPROXIMATED, OVER_APPROXIMATED, UNKNOWN_APPROXIMATION };
+
+struct AnalysisTime {
+    double cpuTime;
+    double elapsedTime;
+
+    AnalysisTime()
+        : cpuTime(0.0), elapsedTime(0.0) {}
+
+    AnalysisTime(double cpuTime, double elapsedTime)
+        : cpuTime(cpuTime), elapsedTime(elapsedTime) {}
+};
+//! [comparison value types]
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      Classes in which to store attributes.
+//
+// Assume that these types appear in the ROSE library header files.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//! [comparison preparing storage]
+// Method 1: BinaryAnalysis::Attribute uses inheritence
+class ObjectWithAttributes_1: public Attribute::Storage {
+    // other members here...
+};
+
+// Method 2: AstAttributeMechanism uses containment with a single data member.
+class ObjectWithAttributes_2 {
+public:
+    AstAttributeMechanism attributeMechanism;
+    // other members here...
+};
+
+// Method 3: AttributeMechanism<K,V> uses containment with one data member per value type.
+class ObjectWithAttributes_3 {
+public:
+    AttributeMechanism<std::string, Approximation> approximationAttributes;
+    AttributeMechanism<std::string, AnalysisTime> analysisTimeAttributes;
+    // Other members here...
+};
+//! [comparison preparing storage]
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      Wrappers around attribute values
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//! [comparison attribute wrappers]
+// Method 1: BinaryAnalysis::Attribute does not use wrappers.
+
+// Method 2: AstAttributeMechanism needs wrappers with "copy" methods.
+class ApproximationAttribute: public AstAttribute {
+public:
+    Approximation approximation;
+
+    virtual AstAttribute* copy() /*const*/ ROSE_OVERRIDE {
+        ApproximationAttribute *retval = new ApproximationAttribute;
+        retval->approximation = approximation;
+        return retval;
+    }
+};
+
+class AnalysisTimeAttribute: public AstAttribute {
+public:
+    AnalysisTime analysisTime;
+
+    virtual AstAttribute* copy() /*const*/ ROSE_OVERRIDE {
+        AnalysisTimeAttribute *retval = new AnalysisTimeAttribute;
+        retval->analysisTime = analysisTime;
+        return retval;
+    }
+};
+
+// Method 3: AttributeMechanism does not use wrappers.
+//! [comparison attribute wrappers]
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      Method 1: BinaryAnalysis::Attribute accessors
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int
+method_1() {
+    ObjectWithAttributes_1 obj_1;
+
+    //! [comparison declare 1]
+    // Method 1: BinaryAnalysis::Attribute
+    const Attribute::Id APPROXIMATION_ATTR = Attribute::declare("type of approximation performed");
+    const Attribute::Id ANALYSIS_TIME_ATTR = Attribute::declare("time taken for the analysis");
+    //! [comparison declare 1]
+
+    //! [comparison insert 1]
+    // Method 1: BinaryAnalysis::Attribute
+    obj_1.setAttribute(APPROXIMATION_ATTR, UNDER_APPROXIMATED);
+    obj_1.setAttribute(ANALYSIS_TIME_ATTR, AnalysisTime(1.1, 2.2));
+    //! [comparison insert 1]
+
+    //! [comparison retrieve 1]
+    // Method 1: BinaryAnalysis::Attribute
+    Approximation approx_1 = obj_1.attributeOrElse(APPROXIMATION_ATTR, UNKNOWN_APPROXIMATION);
+    double cpuTime_1 = obj_1.attributeOrDefault<AnalysisTime>(ANALYSIS_TIME_ATTR).cpuTime;
+    //! [comparison retrieve 1]
+
+    // Copy the containing object and its attributes.
+    ObjectWithAttributes_1 obj_2 = obj_1;
+
+    // Query the attributes from the second object without knowing if they exist
+    Approximation approx_2 = obj_2.attributeOrElse(APPROXIMATION_ATTR, UNKNOWN_APPROXIMATION);
+    double cpuTime_2 = obj_2.attributeOrDefault<AnalysisTime>(ANALYSIS_TIME_ATTR).cpuTime;
+    ASSERT_always_require(approx_1 == approx_2);
+    ASSERT_always_require(cpuTime_1 == cpuTime_2);
+
+    //! [comparison cleanup 1]
+    // Method 1: BinaryAnalysis::Attribute: value destructors called automatically
+    return something_that_might_throw();
+    //! [comparison cleanup 1]
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      Method 2: AstAttributeMechanism accessors
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int
+method_2() {
+    ObjectWithAttributes_2 obj_1;
+
+    //! [comparison declare 2]
+    // Method 2: AstAttributeMechanism
+    const std::string APPROXIMATION_ATTR = "type of approximation performed";
+    const std::string ANALYSIS_TIME_ATTR = "time taken for the analysis";
+    //! [comparison declare 2]
+
+    //! [comparison insert 2]
+    // Method 2: AstAttributeMechanism
+    if (obj_1.attributeMechanism.exists(APPROXIMATION_ATTR))
+        delete obj_1.attributeMechanism[APPROXIMATION_ATTR];
+    ApproximationAttribute *approximationAttribute = new ApproximationAttribute;
+    approximationAttribute->approximation = UNDER_APPROXIMATED;
+    obj_1.attributeMechanism.set(APPROXIMATION_ATTR, approximationAttribute);
+    if (obj_1.attributeMechanism.exists(ANALYSIS_TIME_ATTR))
+        delete obj_1.attributeMechanism[ANALYSIS_TIME_ATTR];
+    AnalysisTimeAttribute *analysisTimeAttribute = new AnalysisTimeAttribute;
+    analysisTimeAttribute->analysisTime = AnalysisTime(1.1, 2.2);
+    obj_1.attributeMechanism.set(ANALYSIS_TIME_ATTR, analysisTimeAttribute);
+    //! [comparison insert 2]
+    
+    //! [comparison retrieve 2]
+    // Method 2: AstAttributeMechanism
+    Approximation approx_1 = UNKNOWN_APPROXIMATION;
+    if (obj_1.attributeMechanism.exists(APPROXIMATION_ATTR)) {
+        ApproximationAttribute *tmp = dynamic_cast<ApproximationAttribute*>(obj_1.attributeMechanism[APPROXIMATION_ATTR]);
+        if (tmp != NULL)
+            approx_1 = tmp->approximation;
+    }
+    double cpuTime_1 = AnalysisTime().cpuTime;          // the default, assuming we don't want to hard-code it.
+    if (obj_1.attributeMechanism.exists(ANALYSIS_TIME_ATTR)) {
+        AnalysisTimeAttribute *tmp = dynamic_cast<AnalysisTimeAttribute*>(obj_1.attributeMechanism[ANALYSIS_TIME_ATTR]);
+        if (tmp != NULL)
+            cpuTime_1 = tmp->analysisTime.cpuTime;
+    }
+    //! [comparison retrieve 2]
+
+    // Copy the containing object and its attributes.
+    ObjectWithAttributes_2 obj_2 = obj_1;               // requires "copy" implementations in attribute value types
+
+    // Query the attributes from the second object not knowing if they exist.
+    Approximation approx_2 = UNKNOWN_APPROXIMATION;
+    if (obj_2.attributeMechanism.exists(APPROXIMATION_ATTR)) {
+        ApproximationAttribute *tmp = dynamic_cast<ApproximationAttribute*>(obj_2.attributeMechanism[APPROXIMATION_ATTR]);
+        if (tmp != NULL)
+            approx_2 = tmp->approximation;
+    }
+    double cpuTime_2 = AnalysisTime().cpuTime;          // the default, assuming we don't want to hard-code it.
+    if (obj_2.attributeMechanism.exists(ANALYSIS_TIME_ATTR)) {
+        AnalysisTimeAttribute *tmp = dynamic_cast<AnalysisTimeAttribute*>(obj_2.attributeMechanism[ANALYSIS_TIME_ATTR]);
+        if (tmp != NULL)
+            cpuTime_2 = tmp->analysisTime.cpuTime;
+    }
+    ASSERT_always_require(approx_1 == approx_2);
+    ASSERT_always_require(cpuTime_1 == cpuTime_2);
+
+    //! [comparison cleanup 2]
+    // Method 2: AstAttributeMechanism: manual cleanup required
+    int retval = 0;
+    try {
+        retval = something_that_might_throw();
+    } catch (...) {
+        if (obj_1.attributeMechanism.exists(APPROXIMATION_ATTR))
+            delete obj_1.attributeMechanism[APPROXIMATION_ATTR];
+        if (obj_1.attributeMechanism.exists(ANALYSIS_TIME_ATTR))
+            delete obj_1.attributeMechanism[APPROXIMATION_ATTR];
+        if (obj_2.attributeMechanism.exists(APPROXIMATION_ATTR))
+            delete obj_2.attributeMechanism[APPROXIMATION_ATTR];
+        if (obj_2.attributeMechanism.exists(ANALYSIS_TIME_ATTR))
+            delete obj_2.attributeMechanism[APPROXIMATION_ATTR];
+        throw;
+    }
+    return retval;
+    //! [comparison cleanup 2]
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                      Method 3: AttributeMechanism accessors
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int
+method_3() {
+    ObjectWithAttributes_3 obj_1;
+
+    //! [comparison declare 3]
+    // Method 3: AttributeMechanism
+    const std::string APPROXIMATION_ATTR = "type of approximation performed";
+    const std::string ANALYSIS_TIME_ATTR = "time taken for the analysis";
+    //! [comparison declare 3]
+
+    //! [comparison insert 3]
+    // Method 3: AttributeMechanism
+    obj_1.approximationAttributes.set(APPROXIMATION_ATTR, UNDER_APPROXIMATED);
+    obj_1.analysisTimeAttributes.set(ANALYSIS_TIME_ATTR, AnalysisTime(1.1, 2.2));
+    //! [comparison insert 3]
+    
+    //! [comparison retrieve 3]
+    // Method 3: AttributeMechanism
+    Approximation approx_1 = UNKNOWN_APPROXIMATION;
+    if (obj_1.approximationAttributes.exists(APPROXIMATION_ATTR))
+        approx_1 = obj_1.approximationAttributes[APPROXIMATION_ATTR];
+    double cpuTime_1 = AnalysisTime().cpuTime;          // the default, assuming we don't want to hard-code it.
+    if (obj_1.analysisTimeAttributes.exists(ANALYSIS_TIME_ATTR))
+        cpuTime_1 = obj_1.analysisTimeAttributes[ANALYSIS_TIME_ATTR].cpuTime;
+    //! [comparison retrieve 3]
+
+    // Copy the containing object and its attributes.
+    ObjectWithAttributes_3 obj_2 = obj_1;
+
+    // Query the attributes from the second object not knowing if they exist.
+    Approximation approx_2 = UNKNOWN_APPROXIMATION;
+    if (obj_2.approximationAttributes.exists(APPROXIMATION_ATTR))
+        approx_2 = obj_2.approximationAttributes[APPROXIMATION_ATTR];
+    double cpuTime_2 = AnalysisTime().cpuTime;          // the default, assuming we don't want to hard-code it.
+    if (obj_2.analysisTimeAttributes.exists(ANALYSIS_TIME_ATTR))
+        cpuTime_2 = obj_2.analysisTimeAttributes[ANALYSIS_TIME_ATTR].cpuTime;
+    ASSERT_always_require(approx_1 == approx_2);
+    ASSERT_always_require(cpuTime_1 == cpuTime_2);
+
+    //! [comparison cleanup 3]
+    // Method 3: AttributeMechanism: value destructors called automatically
+    return something_that_might_throw();
+    //! [comparison cleanup 3]
+}
+
+int
+main() {
+    example_usage();
+    method_1();
+    method_2();
+    method_3();
 }
