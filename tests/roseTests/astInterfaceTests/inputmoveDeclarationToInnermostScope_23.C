145407e3b692746b7d4f1f0a48c081b0d6a31098
Chunhua (Leo) Liao <liaoch@llnl.gov>
2015-11-10 13:13:50 -0800

    (move tool) improve handling of comments for assignments being merged * move the assignment comments to declaration * add a test case

---
+++ tests/roseTests/astInterfaceTests/inputmoveDeclarationToInnermostScope_23.C
@@ -0,0 +1,21 @@
+void foobar()
+   {
+     int i;
+     int ii,k;
+     int m;
+     int *array;
+
+     for (i=0; i < 42; i++)
+        {
+         /**********************************************/
+
+#define MACRO(n) (array[(n)])
+
+       /* comment comment comment */
+          k = ii;
+          if (k >= 42)
+             {
+               m = MACRO(k);
+             }
+        }
+   } 
