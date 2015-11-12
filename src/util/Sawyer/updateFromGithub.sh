3d29c197662650ce8c69251e3c82e651e701efe0
Robb P. Matzke <matzke1@llnl.gov>
2015-11-12 11:47:06 -0500

    (Sawyer) Upgrade to get ThreadWorkers
    * ThreadWorkers is a way to very easily work on items
      in parallel when those items have ordering dependencies.

--- src/util/Sawyer/updateFromGithub.sh
+++ src/util/Sawyer/updateFromGithub.sh
@@ -32,7 +32,7 @@ for f in \
     Access Assert AddressMap AddressSegment AllocatingBuffer BiMap BitVector BitVectorSupport Buffer CommandLine Cached \
     Callbacks DefaultAllocator DistinctList Graph GraphBoost GraphTraversal IndexedList Interval IntervalMap IntervalSet \
     IntervalSetMap Map MappedBuffer Markup MarkupPod Message NullBuffer Optional PoolAllocator ProgressBar Sawyer Set \
-    SharedPointer SmallObject Stack StaticBuffer Stopwatch Synchronization WarningsOff WarningsRestore
+    SharedPointer SmallObject Stack StaticBuffer Stopwatch Synchronization ThreadWorkers WarningsOff WarningsRestore
 do
     srcbase="$SAWYER_ROOT/Sawyer/$f";
     ( emit_cpp_warning; cat "$srcbase.h" ) > ./$f.h
