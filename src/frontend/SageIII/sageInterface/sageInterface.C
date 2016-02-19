20cbe40938c8d9b7e408984da5852cf2fcc31198
Sujan Khadka <sujank@uab.edu>
2016-02-19 16:52:45 -0600

    Updated sageinterface::replaceexpression to include SgMatlabForStatement

--- src/frontend/SageIII/sageInterface/sageInterface.C
+++ src/frontend/SageIII/sageInterface/sageInterface.C
@@ -8217,6 +8217,13 @@ void SageInterface::replaceExpression(SgExpression* oldExp, SgExpression* newExp
     isSgForStatement(parent)->set_increment(newExp);
     // TODO: any other cases here??
   }
+  else if(SgMatlabForStatement *matlabFor = isSgMatlabForStatement(parent))
+    {
+      if(matlabFor->get_index() == oldExp)
+        matlabFor->set_index(newExp);
+      else if(matlabFor->get_range() == oldExp)
+        matlabFor->set_range(newExp);
+    }
   else if (isSgReturnStmt(parent))
     isSgReturnStmt(parent)->set_expression(newExp);
   else  if (isSgBinaryOp(parent)!=NULL){
