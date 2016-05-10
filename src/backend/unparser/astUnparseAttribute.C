rose-compiler/rose

commit a5fc733eca946d41fff77c2a31ae197c7ea68e4f

(rose unparser) fixed warning when using AstUnparseAttribute in user code (added virtual function for ownership policy as required by new attribute mechanism).

src/backend/unparser/astUnparseAttribute.C
@@ -65,3 +65,7 @@ AstUnparseAttribute::replacementStringExists()
      return result;
    }
 
+AstUnparseAttribute::OwnershipPolicy
+AstUnparseAttribute::getOwnershipPolicy() const ROSE_OVERRIDE {
+  return CONTAINER_OWNERSHIP;
+}

src/backend/unparser/astUnparseAttribute.h
@@ -70,6 +70,10 @@ class AstUnparseAttribute : public AstAttribute
 
        //! This is selective in putting out only the strings that match a specific location.
           virtual std::string toString( RelativePositionType inputlocation );
+
+          //! required for ast attribute mechanism
+          virtual AstAttribute::OwnershipPolicy getOwnershipPolicy() const;
+
    };
 
 // This is the "endif" for ROSE_AST_UNPARSE_ATTRIBUTE_HEADER include guard

commit c8084d3987516cc566d110bc14ca4d351d158711

(Ast Unparser) Fixed C++ compile error introduced by a5fc733e

src/backend/unparser/astUnparseAttribute.C
@@ -66,6 +66,6 @@ AstUnparseAttribute::replacementStringExists()
    }
 
 AstUnparseAttribute::OwnershipPolicy
-AstUnparseAttribute::getOwnershipPolicy() const ROSE_OVERRIDE {
+AstUnparseAttribute::getOwnershipPolicy() const {
   return CONTAINER_OWNERSHIP;
 }

src/backend/unparser/astUnparseAttribute.h
@@ -72,7 +72,7 @@ class AstUnparseAttribute : public AstAttribute
           virtual std::string toString( RelativePositionType inputlocation );
 
           //! required for ast attribute mechanism
-          virtual AstAttribute::OwnershipPolicy getOwnershipPolicy() const;
+          virtual AstAttribute::OwnershipPolicy getOwnershipPolicy() const ROSE_OVERRIDE;
 
    };
 