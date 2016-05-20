f6b70cddc1f32db5a79a738a70c6a41c6a7bf609
Robb P. Matzke <matzke1@llnl.gov>
2016-05-20 13:14:39 -0400

    (Unit Testing) Also test translator "-o" switch

--- tests/CompilerOptionsTests/testFileNamesAndExtensions/fileExtensions/translator.C
+++ tests/CompilerOptionsTests/testFileNamesAndExtensions/fileExtensions/translator.C
@@ -10,6 +10,7 @@
 int
 main(int argc, char *argv[]) {
     using namespace Sawyer::CommandLine;
+    typedef std::vector<std::string> Strings;
 
     // Usage:
     //    translator --command=CMD NAMES [-- SWITCHES...]         -- runs: CMD SWITCHES NAMES
@@ -18,10 +19,9 @@ main(int argc, char *argv[]) {
     Parser p;
     std::string cmd;
     p.with(Switch("command").argument("cmd", anyParser(cmd)));
-    std::vector<std::string> args = p.parse(argc, argv).apply().unreachedArgs();
+    Strings args = p.parse(argc, argv).apply().unreachedArgs();
 
-    // Escape file names (everything up to the next "--" or the end of the arguments). Escape sequences have a syntax that
-    // makes them easy to pass through makefiles and shell scripts:
+    // Expand the "+SOMETHING+" escapes in all arguments.
     typedef Sawyer::Container::Map<std::string, std::string> Translations;
     Translations map;
     map.insert("+PLUS+",        "+");
@@ -31,52 +31,54 @@ main(int argc, char *argv[]) {
     map.insert("+HASH+",        "#");
     map.insert("+SQUOTE+",      "'");
     map.insert("+DQUOTE+",      "\"");
-
-    std::vector<std::string> translatedArgs;
-    for (std::vector<std::string>::iterator arg = args.begin(); arg != args.end(); ++arg) {
-        if (*arg == "--") {
-            // The rest of the args (other than the "--") are moved to the beginning of the translated args.
-            translatedArgs.insert(translatedArgs.begin(), ++arg, args.end());
-            break;
-        } else {
-            std::string translated;
-            for (size_t i=0; i<arg->size(); ++i) {
-                size_t endPlus;
-                if ((*arg)[i] == '+' && (endPlus=arg->find('+', i+1)) != std::string::npos) {
-                    std::string token = arg->substr(i, endPlus+1-i);
-                    translated += map.getOrElse(token, token);
-                    i = endPlus;
-                } else {
-                    translated += (*arg)[i];
-                }
+    for (Strings::iterator arg = args.begin(); arg != args.end(); ++arg) {
+        std::string translated;
+        for (size_t i=0; i<arg->size(); ++i) {
+            size_t endPlus;
+            if ((*arg)[i] == '+' && (endPlus=arg->find('+', i+1)) != std::string::npos) {
+                std::string token = arg->substr(i, endPlus+1-i);
+                translated += map.getOrElse(token, token);
+                i = endPlus;
+            } else {
+                translated += (*arg)[i];
             }
-            translatedArgs.push_back(translated);
-
-            // Create nearly empty source files
-            std::ofstream of(translated.c_str());
-            ASSERT_require(!of.bad());
-            of <<"int main() {}\n";
         }
+        *arg = translated;
+    }
+
+    // If there's a '--' argument, then create a stub C source file for each argument before the '--', move them all to the
+    // end, and remove the '--'. I.e., if the arguments are "a b -- c d" then create files "a" and "b" and then rearrange the
+    // arguments to read "c d a b".  If there's no "--", then create source files for all the arguments but don't rearrange
+    // anything.
+    Strings::iterator doubleHyphen = std::find(args.begin(), args.end(), "--");
+    for (Strings::iterator arg = args.begin(); arg != doubleHyphen; ++arg) {
+        std::ofstream of(arg->c_str());
+        ASSERT_require(!of.bad());
+        of <<"int main() {}\n";
+    }
+    if (doubleHyphen != args.end()) {
+        Strings tmp(args.begin(), doubleHyphen);
+        tmp.insert(tmp.begin(), ++doubleHyphen, args.end());
+        args = tmp;
     }
 
     // Either the ROSE translator or some other command
     if (cmd.empty()) {
         std::cout <<"translator args are:";
-        BOOST_FOREACH (const std::string &arg, translatedArgs)
+        BOOST_FOREACH (const std::string &arg, args)
             std::cout <<" \"" <<StringUtility::cEscape(arg) <<"\"";
         std::cout <<"\n";
         
-        translatedArgs.insert(translatedArgs.begin(), argv[0]);
-        SgProject *project = frontend(translatedArgs);
+        args.insert(args.begin(), argv[0]);
+        SgProject *project = frontend(args);
         ASSERT_not_null(project);
-        backend(project);
-    } else {
-        char const** newArgv = new char const*[translatedArgs.size()+2];
-        newArgv[0] = cmd.c_str();
-        for (size_t i=0; i<translatedArgs.size(); ++i)
-            newArgv[i+1] = translatedArgs[i].c_str();
-        newArgv[translatedArgs.size()+1] = NULL;
-        execvp(newArgv[0], (char*const*)newArgv);
-        ASSERT_not_reachable("exec failed");
+        exit(backend(project));
     }
+    char const** newArgv = new char const*[args.size()+2];
+    newArgv[0] = cmd.c_str();
+    for (size_t i=0; i<args.size(); ++i)
+        newArgv[i+1] = args[i].c_str();
+    newArgv[args.size()+1] = NULL;
+    execvp(newArgv[0], (char*const*)newArgv);
+    ASSERT_not_reachable("exec failed");
 }
