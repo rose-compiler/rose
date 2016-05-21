7c15bb8b3ca6e19d141c41b1806b6eec85248f9d
Markus Schordan <schordan1@llnl.gov>
2016-05-20 19:22:14 -0700

    (rosetta) integrated generation of placeholders for new constructors.

--- src/ROSETTA/src/buildConstructorsWithoutSourcePositionInformation.C
+++ src/ROSETTA/src/buildConstructorsWithoutSourcePositionInformation.C
@@ -204,6 +204,8 @@ Grammar::buildConstructorWithoutSourcePositionInformation ( AstNodeClass & node 
                constructorSource = StringUtility::copyEdit (constructorSource,"$CONSTRUCTOR_BODY",constructorFunctionBody);
              }
 
+          constructorSource = GrammarString::copyEdit (constructorSource,"$CONSTRUCTOR_ALL_DATA_MEMBERS","/* ALIEN3 */");
+
           returnString.insert(returnString.end(), constructorSource.begin(), constructorSource.end());
         }
 
