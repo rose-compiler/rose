4963201b35eecfc1d071932ac2438efbb980f424
Robb P. Matzke <matzke1@llnl.gov>
2016-06-03 10:14:02 -0400

    Removed ROSE_INITIALIZE from a non-ROSE program

--- tests/roseTests/utilTests/graphIO.C
+++ tests/roseTests/utilTests/graphIO.C
@@ -155,8 +155,6 @@ int main(int argc, char *argv[]) {
     typedef Graph::VertexIterator Vertex;
     typedef Graph::EdgeIterator Edge;
 
-    ROSE_INITIALIZE;
-
     size_t nverts = 10000;
     size_t nedges = 10000000;
     bool showGraph = false;
