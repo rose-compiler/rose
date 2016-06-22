ee5b55b9b6674a05980c098f0e9f4f69126d0692
Chunhua (Leo) Liao <liaoch@llnl.gov>
2016-06-22 15:25:09 -0700

    (autoPar) generate correct patch files for code using c99 init-stmt. The problem is that loop normalization will generate a new loop variable. The variable will be recognized as a private variable and show up in the patch. Applying the patch to the original code will generate wrong code. Solution: * exclude the new loop variable from OmpAttribute * keep track of normalizeForLoopInitDeclaration() * undo normalization for c99 init-stmt SageInterface::unnormalizeForLoopInitDeclaration()

--- projects/autoParallelization/tests/references/rose_accumulateForce.c
+++ projects/autoParallelization/tests/references/rose_accumulateForce.c
@@ -2,17 +2,15 @@
 
 void AccumulateForce(int *idxBound,int *idxList,int len,double *tmp,double *force)
 {
-  int jj;
-  int ii;
   
-#pragma omp parallel for private (ii,jj) firstprivate (len)
-  for (ii = 0; ii <= len - 1; ii += 1) {
+#pragma omp parallel for private (jj) firstprivate (len)
+  for (register int ii = 0; ii <= len - 1; ii += 1) {
     int count = idxBound[ii + 1] - idxBound[ii];
     int *list = &idxList[idxBound[ii]];
     double sum = 0.0;
     
-#pragma omp parallel for private (jj) reduction (+:sum) firstprivate (count)
-    for (jj = 0; jj <= count - 1; jj += 1) {
+#pragma omp parallel for reduction (+:sum) firstprivate (count)
+    for (register int jj = 0; jj <= count - 1; jj += 1) {
       int idx = list[jj];
       sum += tmp[list[jj]];
     }
