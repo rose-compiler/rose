95f3783cf5f50f7a77122b145070742a3371d4bf
Markus Schordan <schordan1@llnl.gov>
2016-07-12 20:11:23 -0700

    (rose) deactivated debug output of -include option.

--- src/frontend/SageIII/sage_support/cmdline.cpp
+++ src/frontend/SageIII/sage_support/cmdline.cpp
@@ -1215,7 +1215,7 @@ SgProject::processCommandLine(const vector<string>& input_argv)
              {
                iter++;
                tempHeaderFile = *iter;
-#if 1
+#if 0
                printf ("In processCommandLine(): Adding tempHeaderFile = %s to p_preincludeFileList \n",tempHeaderFile.c_str());
 #endif
                p_preincludeFileList.push_back(tempHeaderFile);
