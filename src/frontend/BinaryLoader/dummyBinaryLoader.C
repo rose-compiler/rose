6ac6393e54ddcaea1f281dd96c0822ee81933eff
Pei-Hung Lin <lin32@llnl.gov>
2016-08-02 12:09:45 -0700

    (CMake) add dummy function definition for Windows when Binary Analysis turning off
    (CMake) use rose_addr_t for address type in dummy function definitions
    
    (CMake) add dummyFunction.cpp in midend/programAnalysis/graphAnalysis

--- src/frontend/BinaryLoader/dummyBinaryLoader.C
+++ src/frontend/BinaryLoader/dummyBinaryLoader.C
@@ -2,3 +2,34 @@
 #include "BinaryLoader.h"
 
 void BinaryLoader::initDiagnostics() {}
+rose_addr_t
+SgAsmGenericSection::get_offset() const
+{
+    return p_offset;
+}
+
+size_t
+SgAsmGenericSection::read_content(const MemoryMap *map, rose_addr_t start_va, void *dst_buf, rose_addr_t size, bool strict)
+{
+  return 0;
+}
+
+rose_addr_t
+SgAsmGenericSection::get_mapped_preferred_rva() const
+{
+    ROSE_ASSERT(this != NULL);
+    return p_mapped_preferred_rva;
+}
+
+size_t
+SgAsmGenericFile::read_content(rose_addr_t offset, void *dst_buf, rose_addr_t size, bool strict)
+{
+    size_t retval;
+    return retval;
+}
+size_t
+SgAsmGenericFile::read_content(const MemoryMap *map, rose_addr_t start_va, void *dst_buf, rose_addr_t size, bool strict)
+{
+    size_t retval;
+    return retval;
+}
