77ff30c8e820c8471085b4ee81fca3a6ea74e0df
Markus Schordan <schordan1@llnl.gov>
2016-10-21 09:43:16 -0700

    (codethorn) added matrix multiply examples.

---
+++ projects/CodeThorn/src/tests/matrixmultiply.dr.cc
@@ -0,0 +1,17 @@
+
+void mxm44_0(float* A[],int N1, float* B[],int N2, float* C[], int N3)
+{
+  int k;
+  int i;
+  int j;
+  #pragma omp parallel for private(j,i,k)
+  for (j = 0; j <  N3; j = j + 1) {
+    for (i = 0; i <  N1; i = i + 1) {
+      C[j][i] = 0.0f;
+      for (k = 0; k <  N2; k = k + 1) {
+        C[j][i] = C[j][i] + A[k][i] * B[j][k];
+      }
+    }
+  }
+  return ;
+}
