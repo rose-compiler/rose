ce6ceea6d4fb75f97f9a859f7a1b302c98845919
Markus Schordan <schordan1@llnl.gov>
2017-02-15 16:44:05 -0800

    (codethorn) moved BoolLattice to a separate file.

--- projects/CodeThorn/src/AType.h
+++ projects/CodeThorn/src/AType.h
@@ -10,6 +10,7 @@
 #include <climits>
 #include <string>
 #include <sstream>
+#include "BoolLattice.h"
 
 using std::string;
 using std::istream;
@@ -17,70 +18,7 @@ using std::ostream;
 
 namespace CodeThorn {
 
-/*!
-  \brief Several abstract types implementing abstract semantics.
-  \details These types are used by the analyzer for computing abstract values according the semantics of the respective domain.
-  \date 2012
-  \author Markus Schordan
- */
-
 namespace AType {
-
-  /*!
-    \brief Serves as top value in analysis.
-    \date 2012
-    \author Markus Schordan
-  */
-  class Top {
-  };
-
-/*!
-  \brief Serves as bot value in analysis.
-  \date 2012
-  \author Markus Schordan
- */
-  class Bot {
-  };
-
-/*!
-  \brief Implements semantic functions of a boolean lattice.
-  \date 2012
-  \author Markus Schordan, Adrian Prantl
-  \details All lattice domain operators are implemented as overloaded functions.
- */
-class BoolLattice {
- public:
-  enum ValueType { FALSE, TRUE, BOT, TOP};
-  BoolLattice();
-  BoolLattice(bool val); // type conversion
-  BoolLattice(Top e); // type conversion
-  BoolLattice(Bot e); // type conversion
-  BoolLattice(int x); // type conversion
-  bool isTop() const;
-  bool isTrue() const;
-  bool isFalse() const;
-  bool isBot() const;
-  BoolLattice operator!();
-  BoolLattice operator||(BoolLattice other);
-  BoolLattice operator&&(BoolLattice other);
-  /// least upper bound
-  BoolLattice lub(BoolLattice other);
-  /// greatest lower bound
-  BoolLattice glb(BoolLattice other);
-  /// operator= : C++ default used
-  bool operator==(BoolLattice other) const;
-  bool operator!=(BoolLattice other) const;
-  /// for sorting only!
-  bool operator<(BoolLattice other) const;
-  string toString() const;
-  inline ValueType val() const { return value; }
-  friend ostream& operator<<(ostream& os, const BoolLattice& value);
- private:
-  ValueType value;
-};
-
- ostream& operator<<(ostream& os, const BoolLattice& value);
-
  class ConstIntLattice;
 
  bool strictWeakOrderingIsSmaller(const ConstIntLattice& c1, const ConstIntLattice& c2);
@@ -107,12 +45,12 @@ class ConstIntLattice {
   ConstIntLattice(signed char x);
   ConstIntLattice(unsigned char x);
   ConstIntLattice(short int x);
-  ConstIntLattice(int x);
-  ConstIntLattice(long int x);
-  ConstIntLattice(long long int x);
   ConstIntLattice(unsigned short int x);
+  ConstIntLattice(int x);
   ConstIntLattice(unsigned int x);
+  ConstIntLattice(long int x);
   ConstIntLattice(unsigned long int x);
+  ConstIntLattice(long long int x);
   ConstIntLattice(unsigned long long int x);
   bool isTop() const;
   bool isTrue() const;
