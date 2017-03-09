b9e943b0f79132b70da0e68ea27fd00dcabb0d2a
Charles A. Reynolds <reynolds12@llnl.gov>
2017-03-09 09:18:06 -0800

    Added tutorial test units

---
+++ test_units/test_unit.ads
@@ -0,0 +1,10 @@
+--  This is the test unit to try out the ASIS applications built
+--  in the framework of the ASIS tutorial.
+
+package Test_Unit is
+
+   generic
+      type T is private;
+   procedure Swap (A, B : in out T);
+
+end Test_Unit;
