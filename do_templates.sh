d291cdc35ed5ef20a190be1a6e822b2a48bcce75
Charles A. Reynolds <reynolds12@llnl.gov>
2017-03-13 09:51:07 -0700

    Improved error processing, directory handling, added multi file processing

--- do_templates.sh
+++ do_templates.sh
@@ -4,15 +4,18 @@
 # Builds ASIS templates executable, and runs it on some test code.
 
 # Prevents some informative error messages, so is usually commented out:
-# set -o errexit
+#set -o errexit
+#trap "__exit_status=$?; echo Error - exiting do_templates.sh with status ${__exit_status}; exit ${__exit_status}" ERR
 
 # Defines log, log_and_run, etc.:
 source `which utility_functions.sh`
 
 rel_script_dir=`dirname $0`
 script_dir=`(cd ${rel_script_dir}; pwd)`
+current_dir=`pwd`
 tool_name=run_asis_tool_2
 target_dir=./test_units
+target_units="test_unit"
 
 use_gnat () {
   # Make GNAT compiler and gprbuild available:
@@ -27,22 +30,30 @@ build_asis_tool () {
 }
 
 generate_adt_files () {
-  log "Generating adt files for units in ${target_dir}"
-  (cd ${target_dir}; gcc -c -gnatct test_unit.adb)
+  log "Generating .adt files for specified units in ${target_dir}"
+  cd ${target_dir}
+  for unit in ${units}
+  do
+    log "Generating ${target_unit}.adt" 
+    gcc -c -gnatct ${target_unit}.adb
+  done
+  cd ${current_dir}
 }
 
 run_asis_tool () {
-  log "Running ${tool_name} in ${target_dir}"
-  (cd ${target_dir}; ${script_dir}/obj/${tool_name})
+  log "Running ${tool_name} on all .adt files in ${target_dir}"
+  cd ${target_dir}
+  ${script_dir}/obj/${tool_name}
+  cd ${current_dir}
 }
 
 log_start
 log_invocation "$@"
 
 use_gnat
-log_and_run build_asis_tool
-log_and_run generate_adt_files
-log_and_run run_asis_tool
+log_and_run_or_exit build_asis_tool
+log_and_run_or_exit generate_adt_files
+log_and_run_or_exit run_asis_tool
 
 log_end
 
