6b81de77c7012a51cadd52b512b46b9f789a7aea
Daniel Quinlan <dquinlan@llnl.gov>
2017-03-19 14:42:01 -0700

    Fixed C11 testing to still work with non-Intel compilers.

--- tests/nonsmoke/functional/CompileTests/C11_tests/test2014_21.c
+++ tests/nonsmoke/functional/CompileTests/C11_tests/test2014_21.c
@@ -1,5 +1,5 @@
 /* This C11 feature is not defined for the Intel v14 compiler (only later versions). */
-#if defined(__INTEL_COMPILER) && (__INTEL_COMPILER > 1400)
+#if (defined(__INTEL_COMPILER) && (__INTEL_COMPILER > 1400)) || !defined(__INTEL_COMPILER)
 _Alignas(int) float x;
 #endif
 __attribute__((aligned(8))) float z;
