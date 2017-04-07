d76296e1708a222f16df4cb379a6a4b34e5087a9
Chunhua (Leo) Liao <liaoch@llnl.gov>
2017-04-07 09:40:50 -0700

    (raja checkers) capture lambda function body

--- tests/nonsmoke/functional/roseTests/astInterfaceTests/rajaChecker.C
+++ tests/nonsmoke/functional/roseTests/astInterfaceTests/rajaChecker.C
@@ -7,15 +7,43 @@
 using namespace std;
 using namespace SageInterface;
 
-class RoseVisitor : public ROSE_VisitTraversal
+class RoseVisitor : public AstSimpleProcessing
 {
-  public:
-    void visit ( SgNode* node);
+  protected:
+    void virtual visit ( SgNode* node);
 };
 
-void RoseVisitor::visit ( SgNode* node)
+void RoseVisitor::visit ( SgNode* n)
 {
+  //      if (isSgFunctionDeclaration(n)!=NULL)
+  //      This will match SgTemplateInstantiationFunctionDecl, which is not wanted.
+  //if (n->variantT() == V_SgFunctionDeclaration || n->variantT() == V_SgMemberFunctionDeclaration)
+  // Only catch lambda function, which is a SgMemberFunctionDeclaration
+  if (n->variantT() == V_SgMemberFunctionDeclaration)
+  {
+    ROSE_ASSERT (n->variantT() != V_SgTemplateInstantiationFunctionDecl);
+    SgFunctionDeclaration* func = isSgFunctionDeclaration(n);
+    ROSE_ASSERT(func != NULL);
+    if (func->get_definition() == NULL) return;
 
+    // TODO: skip things from headers. 
+    // skip compiler generated codes, mostly from template headers
+    if (func->get_file_info()->isCompilerGenerated())
+    {
+      return;
+    }
+
+    SgBasicBlock* body = func->get_definition()->get_body();
+    if (body == NULL) return;
+    // Prepare the function body: ensure body basic block for while, for, if, etc.
+    // SageInterface::changeAllBodiesToBlocks (body, false);
+
+    if ( SgLambdaExp* le = isSgLambdaExp(n->get_parent()))
+    {
+      cout<<"Found a lambda function body..."<<endl; 
+      body->get_file_info()->display();
+    }
+  }
 }
 
 int
@@ -26,7 +54,20 @@ main ( int argc, char* argv[])
 
   // ROSE memory traversal to catch all sorts of nodes, not just those on visible AST
   RoseVisitor visitor;
-  visitor.traverseMemoryPool();
+
+  SgFilePtrList file_ptr_list = project->get_fileList();
+  for (size_t i = 0; i<file_ptr_list.size(); i++)                                                                                          
+  {                                                                                                                                        
+    SgFile* cur_file = file_ptr_list[i];                                                                                                   
+    SgSourceFile* s_file = isSgSourceFile(cur_file);                                                                                       
+    if (s_file != NULL)                                                                                                                    
+    {                                                                                                                                      
+      visitor.traverseWithinFile(s_file, preorder);                                                                               
+    }                                                                                                                                      
+  }            
+
+
+
 
   return backend(project);
 }
