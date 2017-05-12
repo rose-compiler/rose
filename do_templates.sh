bdf5a67105bd4b233490b11a567e1e8f16733f27
Charles A. Reynolds <reynolds12@llnl.gov>
2017-05-12 09:53:32 -0700

    Now do_templates.sh processes unit_2.

--- do_templates.sh
+++ do_templates.sh
@@ -19,7 +19,7 @@ script_dir=`(cd ${rel_script_dir}; pwd)`
 current_dir=`pwd`
 tool_name=run_asis_tool_2
 target_dir=./test_units
-target_units="test_unit.adb"
+target_units="unit_2.adb"
 
 use_gnat () {
   # Make GNAT compiler and gprbuild available:
