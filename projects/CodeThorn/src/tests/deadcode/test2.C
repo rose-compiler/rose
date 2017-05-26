2b81cb736faf4e21542eaf81fcf3eede64c4ef2d
Markus Schordan <schordan1@llnl.gov>
2017-05-25 17:35:33 -0700

    (codethorn) added deadcode detection test files.

---
+++ projects/CodeThorn/src/tests/deadcode/test2.C
@@ -0,0 +1,19 @@
+int main() {
+  int x;
+  int y;
+  while(1) {
+    if(false) {
+      x=1;
+    } else{
+      x=2;
+      break;
+      x=3;
+    }
+    if(y==0) {
+      x=1;
+    } else{
+      x=200; x=201;
+    }
+    y=x;
+  }
+}
