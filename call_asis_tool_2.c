6a10bdd555f097be7a0369373bf5a62ea65f1796
Charles A. Reynolds <reynolds12@llnl.gov>
2017-06-28 08:27:23 -0700

    Leaving adafinal call aout of call_asis_tool_2.c for now because it hangs.

--- call_asis_tool_2.c
+++ call_asis_tool_2.c
@@ -12,6 +12,17 @@ int main (int argc, char *argv[])
   struct Element_Struct *last_element;
   printf ("c_main: Calling tool_2_wrapper with %s\n" ,target_file);
   last_element = tool_2_wrapper (target_file);
-  printf ("c_main: Returned %i elements.\n", last_element->next_count + 1);
-  adafinal();
+  printf ("c_main: Returned from tool_2_wrapper.\n");
+
+  if (last_element == 0) {
+    printf ("c_main: Returned NO elements.\n");
+  } else {
+    printf ("c_main: Returned %i elements.\n", last_element->next_count + 1);
+  }
+
+  // Skipping adafinal for now because it hangs in
+  // system__standard_library__adafinal:
+  // adafinal();
+  // printf ("c_main: adafinal done.\n");
+  printf ("c_main: Done.\n");
 }
