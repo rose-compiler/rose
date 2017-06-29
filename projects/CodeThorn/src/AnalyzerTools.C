7753c9b12f8bbd7b67a6b0c0c830d6254272383b
Josh Asplund <joshasplund@gmail.com>
2017-06-29 11:17:02 -0700

    (codethorn) Update analyzer tools

--- projects/CodeThorn/src/AnalyzerTools.C
+++ projects/CodeThorn/src/AnalyzerTools.C
@@ -5,6 +5,52 @@ using namespace CodeThorn;
 using namespace std;
 using namespace Sawyer::Message;
 
+
+
+string ExecutionTrace::toString() const {
+  string result = "[";
+  ExecutionTrace::const_iterator begin = this->begin();
+  ExecutionTrace::const_iterator end = this->end();
+  for (ExecutionTrace::const_iterator i = this->begin(); i != end; i++ ) {
+    if (i != begin) {
+      result += ";";
+    }
+    if ((*i)->io.isStdInIO()) {
+      result += "i";
+    } else if ((*i)->io.isStdOutIO()) {
+      result += "o";
+    }
+    result += (*i)->toString();
+  }
+  result += "]";
+  return result;
+}
+
+string ExecutionTrace::toString(SPRAY::VariableIdMapping* variableIdMapping) const {
+  string result = "[";
+  ExecutionTrace::const_iterator begin = this->begin();
+  ExecutionTrace::const_iterator end = this->end();
+  for (ExecutionTrace::const_iterator i = this->begin(); i != end; i++ ) {
+    if (i != begin) {
+      result += ";";
+    }
+    if ((*i)->io.isStdInIO()) {
+      result += "i";
+    } else if ((*i)->io.isStdOutIO()) {
+      result += "o";
+    }
+    result += (*i)->toString(variableIdMapping);
+  }
+  result += "]";
+  return result;
+}
+
+
+
+
+
+//=============================================================================
+
 Sawyer::Message::Facility CounterexampleGenerator::logger;
 
 void CounterexampleGenerator::initDiagnostics() {
@@ -17,14 +63,10 @@ void CounterexampleGenerator::initDiagnostics() {
 }
 
 
-CounterexampleGenerator::CounterexampleGenerator(Analyzer* analyzer):
-  analyzer(analyzer) { }
-
-EStateWorkList CounterexampleGenerator::reverseInOutSequenceBreadthFirst(const EState* errorState, const EState* target, bool counterexampleWithOutput, bool filterIO) {
-  TransitionGraph* transitionGraph = analyzer->getTransitionGraph();
-  // 1.) init: list wl , hashset predecessor, hashset visited
-  EStateWorkList worklist;
-  worklist.push_back(errorState);
+ExecutionTrace CounterexampleGenerator::traceBreadthFirst(TransitionGraph* transitionGraph, const EState* source, const EState* target) {
+  // 1.) init: list wl , hashmap predecessor, hashset visited
+  list<const EState*> worklist;
+  worklist.push_back(source);
   boost::unordered_map <const EState*, const EState*> predecessor;
   boost::unordered_set<const EState*> visited;
   // 2.) while (elem in worklist) {s <-- pop wl; if (s not yet visited) {update predecessor map;
@@ -48,146 +90,19 @@ EStateWorkList CounterexampleGenerator::reverseInOutSequenceBreadthFirst(const E
     }
   }
   if (!targetFound) {
-    logger[ERROR]<< "target state not connected to source while generating reversed trace source --> target." << endl;
+    logger[ERROR]<< "Target state not connected to source" << endl;
     ROSE_ASSERT(0);
   }
-  // 3.) reconstruct trace. filter list of only input ( & output) states and return it
-  EStateWorkList run;
-  run.push_front(target);
+
+  // 3.) reconstruct trace.
+  ExecutionTrace run;
+  run.push_back(target);
   boost::unordered_map <const EState*, const EState*>::iterator nextPred = predecessor.find(target);
 
   while (nextPred != predecessor.end()) {
-    run.push_front(nextPred->second);
+    run.push_back(nextPred->second);
     nextPred = predecessor.find(nextPred->second);
   }
 
-  EStateWorkList result;
-  if (filterIO) {
-    return filterStdInOutOnly(run, counterexampleWithOutput);
-  } else {
-    return run;
-  }
+  return run;
 }
-
-EStateWorkList CounterexampleGenerator::reverseInOutSequenceDijkstra(const EState* source, const EState* target, bool counterexampleWithOutput, bool filterIO) {
-  EStatePtrSet states = analyzer->getTransitionGraph()->estateSet();
-  boost::unordered_set<const EState*> worklist;
-  map <const EState*, int> distance;
-  map <const EState*, const EState*> predecessor;
-  //initialize distances and worklist
-  for(EStatePtrSet::iterator i=states.begin();i!=states.end();++i) {
-    worklist.insert(*i);
-    if ((*i) == source) {
-      distance.insert(pair<const EState*, int>((*i), 0));
-    } else {
-      distance.insert(pair<const EState*, int>((*i), (std::numeric_limits<int>::max() - 1)));
-    }
-  }
-  ROSE_ASSERT( distance.size() == worklist.size() );
-
-  //process worklist
-  while (worklist.size() > 0 ) {
-    //extract vertex with shortest distance to source
-    int minDist = std::numeric_limits<int>::max();
-    const EState* vertex = NULL;
-    for (map<const EState*, int>::iterator i=distance.begin(); i != distance.end(); ++i) {
-      if ( (worklist.find(i->first) != worklist.end()) ) {
-        if ( (i->second < minDist)) {
-          minDist = i->second;
-          vertex = i->first;
-        }
-      }
-    }
-    //check for all predecessors if a shorter path leading to them was found
-    EStatePtrSet predsOfVertex = analyzer->getTransitionGraph()->pred(vertex);
-    for(EStatePtrSet::iterator i=predsOfVertex.begin();i!=predsOfVertex.end();++i) {
-      int altDist;
-      if( (*i)->io.isStdInIO() ) {
-        altDist = distance[vertex] + 1;
-      } else {
-        altDist = distance[vertex]; //we only count the input sequence length
-      }
-      if (altDist < distance[*i]) {
-        distance[*i] = altDist;
-        //update predecessor
-        map <const EState*, const EState*>::iterator predListIndex = predecessor.find((*i));
-        if (predListIndex != predecessor.end()) {
-          predListIndex->second = vertex;
-        } else {
-          predecessor.insert(pair<const EState*, const EState*>((*i), vertex));
-        }
-        //optimization: stop if the target state was found
-        if ((*i) == target) {break;}
-      }
-    }
-    int worklistReducedBy = worklist.erase(vertex);
-    ROSE_ASSERT(worklistReducedBy == 1);
-  }
-
-  //extract and return input run from source to target (backwards in the STG)
-  EStateWorkList run;
-  run.push_front(target);
-  map <const EState*, const EState*>::iterator nextPred = predecessor.find(target);
-  while (nextPred != predecessor.end()) {
-    run.push_front(nextPred->second);
-    nextPred = predecessor.find(nextPred->second);
-  }
-  assert ((*run.begin()) == source);
-  if (filterIO) {
-    return filterStdInOutOnly(run, counterexampleWithOutput);
-  } else {
-    return run;
-  }
-}
-
-EStateWorkList CounterexampleGenerator::filterStdInOutOnly(EStateWorkList& states, bool counterexampleWithOutput) const {
-  EStateWorkList result;
-  for (EStateWorkList::iterator i = states.begin(); i != states.end(); i++ ) {
-    if( (*i)->io.isStdInIO() || (counterexampleWithOutput && (*i)->io.isStdOutIO())) {
-      result.push_back(*i);
-    }
-  }
-  return result;
-}
-
-string CounterexampleGenerator::reversedInOutRunToString(EStateWorkList& run) {
-  string result = "[";
-  for (EStateWorkList::reverse_iterator i = run.rbegin(); i != run.rend(); i++ ) {
-    if (i != run.rbegin()) {
-      result += ";";
-    }
-    //get input or output value
-    // PState* pstate = const_cast<PState*>( (*i)->pstate() );
-    // int inOutVal;
-    if ((*i)->io.isStdInIO()) {
-      result += "i";
-      result += (*i)->toString();
-    } else if ((*i)->io.isStdOutIO()) {
-      result += "o";
-      result += (*i)->toString();
-    } else {
-      ROSE_ASSERT(0);  //function is supposed to handle list of stdIn and stdOut states only
-    }
-  }
-  result += "]";
-  return result;
-}
-
-string CounterexampleGenerator::reversedRunToString(EStateWorkList& run) {
-  VariableIdMapping* variableIdMapping = analyzer->getVariableIdMapping();
-  string result = "[";
-  for (EStateWorkList::reverse_iterator i = run.rbegin(); i != run.rend(); i++ ) {
-    if (i != run.rbegin()) {
-      result += ";\n";
-    }
-    if ((*i)->io.isStdInIO()) {
-      result += "i";
-    } else if ((*i)->io.isStdOutIO()) {
-      result += "o";
-    }
-    result += (*i)->toString(variableIdMapping);
-  }
-  result += "]";
-  return result;
-}
-
--- projects/CodeThorn/src/AnalyzerTools.h
+++ projects/CodeThorn/src/AnalyzerTools.h
@@ -5,25 +5,50 @@
 
 namespace CodeThorn {
 
+/**
+ * @brief An ordered list of EStates.
+ * 
+ * \author Joshua Asplund
+ * \date June 2017
+ */
+  class ExecutionTrace:
+    public list<const EState*> {
+  private:
+
+  public:
+    ExecutionTrace() {};
+    ~ExecutionTrace() {};
+
+    std::string toString() const;
+    std::string toString(SPRAY::VariableIdMapping* variableIdMapping) const;
+
+  };
+
+/**
+ * \author Joshua Asplund
+ * \date June 2017
+ */
   class CounterexampleGenerator {
-    public:
-      CounterexampleGenerator(Analyzer* analyzer);
-      ~CounterexampleGenerator();
+  public:
+    CounterexampleGenerator();
+    ~CounterexampleGenerator();
+    static void initDiagnostics();
 
-      static void initDiagnostics();
-      EStateWorkList traceBreadthFirst(const EState* source, const EState* target, bool counterexampleWithOutput = false, bool filterIO = true);
-      EStateWorkList traceDijkstra(const EState* source, const EState* target, bool counterexampleWithOutput = false, bool filterIO = true);
-      EStateWorkList reverseInOutSequenceBreadthFirst(const EState* errorState, const EState* target, bool counterexampleWithOutput, bool filterIO);
-      EStateWorkList filterStdInOutOnly(EStateWorkList& states, bool counterexampleWithOutput = false) const;
-      EStateWorkList reverseInOutSequenceDijkstra(const EState* source, const EState* target, bool counterexampleWithOutput, bool filterIO);
-      std::string reversedInOutRunToString(EStateWorkList& run);
-      std::string reversedRunToString(EStateWorkList& run);
+/**
+ * @brief Extracts an execution trace using a breadth first search
+ * @details This will search backwards from source towards target and will return a forward
+ *    trace. That is, the order of the trace will follow execution order.
+ * 
+ * @param transitionGraph The transition graph describing program execution
+ * @param source The search will starting here, moving backwards along the transition graph
+ * @param target The search will end when encountering this state
+ * @return A trace starting at target and ending at source.
+ */
+    ExecutionTrace traceBreadthFirst(TransitionGraph* transitionGraph, const EState* source, const EState* target);
 
-    protected:
-      static Sawyer::Message::Facility logger;
+  protected:
+    static Sawyer::Message::Facility logger;
 
-    private:
-      Analyzer* analyzer;
   };
 
 }
