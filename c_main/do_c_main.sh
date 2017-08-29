947ce342b1181b000f0dac3913340db9eee4e52a
Charles A. Reynolds <reynolds12@llnl.gov>
2017-08-29 11:22:13 -0700

    Added c_main dir and do_c_main.sh which does not use gprbuild.  Made conventional subdirs in dot_asis_library

--- c_main/do_c_main.sh
+++ c_main/do_c_main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Builds a dot_asis tool, and runs it on some test code.
+# Builds a dot_asis tool with a C main program, and runs it on some test code.
 #
 # Parameters:
 # -d  Turns on debug
@@ -9,33 +9,41 @@
 #trap "__exit_status=$?; echo Error - exiting do_asis_tool_2.sh with status ${__exit_status}; exit ${__exit_status}" ERR
 
 # Defines log, log_and_run, etc.:
-source utility_functions.sh
+source ../utility_functions.sh
 
 rel_script_dir=`dirname $0`
 script_dir=`(cd ${rel_script_dir}; pwd)`
-current_dir=`pwd`
-tool_name=call_asis_tool_2
-target_dir=./test_units
-target_units="test_unit.adb"
+source_dir=`pwd`
 
-use_gnat () {
-  # Make GNAT compiler and gprbuild available:
-  # Needed for use:
-  . /usr/local/tools/dotkit/init.sh
-  use -q gnat
-}
+dot_asis_home=/usr/workspace/wsb/charles/code/ROSE/dot_asis/dot_asis_library
+gnat_home=/usr/workspace/wsb/charles/bin/adacore/gnat-gpl-2017-x86_64-linux
+target_dir=../test_units
+target_units="test_unit.adb"
+tool_name=call_asis_tool_2
 
 build_asis_tool () {
   log "Building ${tool_name}"
-  # -p       Create missing obj, lib and exec dirs
-  # -P proj  Use Project File proj
-  # -v       Verbose output
-  # -vl      Verbose output (low verbosity)
-  # -vm      Verbose output (medium verbosity)
-  # -vh      Verbose output (high verbosity)
-  # -vPx     Specify verbosity when parsing Project Files (x = 0/1/2)
-  # -Xnm=val Specify an external reference for Project Files
-  gprbuild -p -Pdot_asis.gpr -v -XLIBRARY_TYPE=static -XASIS_BUILD=default ${tool_name}
+  if [ ! -d obj ]; then
+    mkdir obj
+  fi
+  cd obj
+  
+  ${gnat_home}/bin/gcc -c -x c -MMD -MF ${tool_name}.d \
+  -I${dot_asis_home}/include \
+  ${source_dir}/${tool_name}.c
+
+  ${gnat_home}/bin/gcc \
+  ${tool_name}.o \
+  ${dot_asis_home}/lib/libdot_asis.a \
+  ${gnat_home}/lib/asis/asislib/libasis.a \
+  ${gnat_home}/lib/gcc/x86_64-pc-linux-gnu/6.3.1/adalib/libgnarl.a \
+  ${gnat_home}/lib/gcc/x86_64-pc-linux-gnu/6.3.1/adalib/libgnat.a \
+  -static-libgcc \
+  -lpthread \
+  -lrt \
+  -ldl \
+  -o ${tool_name}
+  cd ${source_dir}
 }
 
 # Keeps going.  Returns 1 if any failed, 0 if all succeeded:
@@ -54,7 +62,6 @@ process_units () {
 log_start
 log_invocation "$@"
 
-use_gnat
 log_and_run build_asis_tool    || exit $?
 log_and_run process_units "$@" || exit $?
 
