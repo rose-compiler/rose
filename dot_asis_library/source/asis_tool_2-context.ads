4d61e998ab68e1a7a946d57b66c09a00b7e9697a
Charles A. Reynolds <reynolds12@llnl.gov>
2017-09-15 16:24:42 -0700

    Now only processes tree file from unit passed in, not all tree files in directory (.dot file does not have extra info)

--- dot_asis_library/source/asis_tool_2-context.ads
+++ dot_asis_library/source/asis_tool_2-context.ads
@@ -8,9 +8,9 @@ package Asis_Tool_2.Context is
    type Class is tagged limited private;
 
    procedure Process
-     (This          : in out Class;
-      Tree_File_Dir : in     String;
-      Outputs       : in     Outputs_Record);
+     (This           : in out Class;
+      Tree_File_Name : in     String;
+      Outputs        : in     Outputs_Record);
 
 private
 
