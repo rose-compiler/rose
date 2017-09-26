0c208790af46cb8c69022d67f8480744fed03a10
Charles A. Reynolds <reynolds12@llnl.gov>
2017-09-26 15:08:24 -0700

    Added detection of adding duplicate nodes to the C node list

--- dot_asis_library/source/a_nodes.adb
+++ dot_asis_library/source/a_nodes.adb
@@ -1,18 +1,120 @@
+with Ada.Exceptions;
+with Ada.Text_IO;
 -- For int."+":
 with Interfaces.C;
 
+with a_nodes_h.Support;
+
 package body A_Nodes is
+
+   Package_Name : constant String := "A_Nodes";
+
+   package AEX renames Ada.Exceptions;
+   package ATI renames Ada.Text_IO;
+
+   -- Checks to be sure no node with this ID has already been pushed.  Raises
+   -- Usage_Error if so.
+   procedure Check_Context_Node
+     (This : access Class;
+      Node : in a_nodes_h.Context_Struct)
+   is
+      Name : constant String := Package_Name & ".Check_Context_Node";
+   begin
+      If This.Has_Context then
+         raise Usage_Error with Name &
+           ": Tried to push second Context";
+      else
+         This.Has_Context := True;
+      end if;
+   end Check_Context_Node;
+
+   -- Checks to be sure no node with this ID has already been pushed.  Raises
+   -- Usage_Error if so.
+   procedure Check_Unit_Node
+     (This : access Class;
+      Unit : in a_nodes_h.Unit_Struct)
+   is
+      Name : constant String := Package_Name & ".Check_Unit_Node";
+      ID   : constant a_nodes_h.Unit_ID := Unit.ID;
+   begin
+      if This.Unit_IDs.Contains (ID) then
+         raise Usage_Error with Name &
+           ": Tried to push second Unit with ID => " & ID'Image;
+      else
+         This.Unit_IDs.Insert (ID);
+      end if;
+   end Check_Unit_Node;
+
+   -- Checks to be sure no node with this ID has already been pushed.  Raises
+   -- Usage_Error if so.
+   procedure Check_Element_Node
+     (This : access Class;
+      Element : in a_nodes_h.Element_Struct)
+   is
+      Name : constant String := Package_Name & ".Check_Element_Node";
+      ID   : constant a_nodes_h.Element_ID := Element.ID;
+   begin
+      if This.Element_IDs.Contains (ID) then
+         raise Usage_Error with Name &
+           ": Tried to push second Element with ID => " & ID'Image;
+      else
+         This.Element_IDs.Insert (ID);
+      end if;
+   end Check_Element_Node;
+
+   -- Checks to be sure no node with this ID has already been pushed.  Raises
+   -- Usage_Error if so.
+   procedure Check_Node
+     (This : access Class;
+      Node : in a_nodes_h.Node_Struct)
+   is
+      Name : constant String := Package_Name & ".Check_Node";
+      use all type a_nodes_h.Node_Kinds;
+   begin
+      case Node.Node_Kind is
+         when Not_A_Node =>
+            raise Usage_Error with Name &
+              ": Tried to push Node with Node_Kind => " & Node.Node_Kind'Image;
+         when  A_Context_Node =>
+            Check_Context_Node (This, Node.The_Union.Context);
+         when A_Unit_Node =>
+            Check_Unit_Node (This, Node.The_Union.Unit);
+         when An_Element_Node =>
+            Check_Element_Node (This, Node.The_Union.Element);
+      end case;
+   end Check_Node;
+
+
+   procedure Print_Exception_Info
+     (Module_Name : in String;
+      X           : in AEX.Exception_Occurrence) is
+   begin
+      ATI.Put_Line ((1 .. 40 => '#'));
+      ATI.Put_Line (Module_Name & ": EXCEPTION: " & Aex.Exception_Name (X) &
+                  " (" & Aex.Exception_Information (X) & ")");
+      ATI.Put_Line ((1 .. 40 => '#'));
+   end Print_Exception_Info;
+
 
    ------------
    -- EXPORTED:
    ------------
    procedure Push
      (This : access Class;
-      Node : in a_nodes_h.Node_Struct)
+      Node : in     a_nodes_h.Node_Struct)
    is
-      List_Node : a_nodes_h.List_Node_Struct;
+      Module_Name : constant String := Package_Name & ".Push";
+      List_Node : a_nodes_h.List_Node_Struct :=
+        a_nodes_h.Support.Default_List_Node_Struct;
       use type Interfaces.C.int;
    begin
+      begin
+         Check_Node (This, Node);
+      exception
+         when X : Usage_Error =>
+            Print_Exception_Info (Module_Name, X);
+            ATI.Put_Line ("Continuing...");
+      end;
       List_Node.node := Node;
       if This.Is_Empty then
          List_Node.next := null;
--- dot_asis_library/source/a_nodes.ads
+++ dot_asis_library/source/a_nodes.ads
@@ -1,5 +1,8 @@
 with a_nodes_h;
 
+private with Ada.Containers.Hashed_Sets;
+private with Interfaces.C;
+
 -- This class hides and manages some of the details of the liniked
 -- list of Element_Struct C interface records.  Like Dot.ads, you only need to
 -- pass in Access_Class to the users - they don't need to pass it back.  The top
@@ -10,10 +13,11 @@ package A_Nodes is
    type Class is tagged private;
    type Access_Class is access Class;
 
-   -- Copies Node to a new node on the heap and links it to the top of the list:
+   -- Copies Node to a new node on the heap and links it to the top of the list.
+   -- Raises Usage_Error if a node with this ID has already been pushed:
    procedure Push
      (This : access Class;
-      Node : in a_nodes_h.Node_Struct);
+      Node : in     a_nodes_h.Node_Struct);
 
    -- Returns a pointer to the node at the top of the list:
    function Get_Head
@@ -22,12 +26,32 @@ package A_Nodes is
 
    function Is_Empty
      (This : access Class)
-     return Boolean;
+      return Boolean;
+
+   -- A client has used the package improperly:
+   Usage_Error : exception;
 
 private
 
-   type Class is tagged record
-      Head : a_nodes_h.Node_List_Ptr; -- Initialized
+   package AC renames Ada.Containers;
+   package IC renames Interfaces.C;
+
+   function Hash (Element : a_nodes_h.Node_ID) return AC.Hash_Type is
+      (AC.Hash_Type (Element));
+
+   package Node_ID_Sets is new AC.Hashed_Sets
+     (Element_Type        => a_nodes_h.Node_ID,
+      Hash                => Hash,
+      Equivalent_Elements => IC."=",
+      "="                 => IC."=");
+   -- Make primitive operations directly visible:
+   Type Node_ID_Set is new Node_ID_Sets.Set with null record;
+
+   type Class is tagged record -- Initialized
+      Head        : a_nodes_h.Node_List_Ptr; -- Initialized
+      Has_Context : Boolean := False;
+      Unit_IDs    : Node_ID_Set; -- Initialized
+      Element_IDs : Node_ID_Set; -- Initialized
    end record;
 
 end A_Nodes;
