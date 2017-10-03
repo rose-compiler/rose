4347a9f4c317d36086e96c4a35dcad54e23aa683
Daniel Quinlan <dquinlan@llnl.gov>
2017-10-03 16:07:24 -0700

    Intermediate commit to support working with Craig.

--- src/frontend/Experimental_Cobol_ROSE_Connection/cobol_support.C
+++ src/frontend/Experimental_Cobol_ROSE_Connection/cobol_support.C
@@ -6,10 +6,17 @@
 
 #include "cobol_support.h"
 
+#define ROSE_USING_GNUCOBOL 0
+
+#if ROSE_USING_GNUCOBOL
+// DQ (9/29/2017): We need a way to know when ROSE has been configured with the location of the GNUCobol compiler.
+// We need something like "ROSE_USING_GNUCOBOL" to be setup.
+
 // Rasmussen (9/28/2017): First two files from GNUCobol
 #include "cobc.h"
 #include "tree.h"
 #include "rose_cobol.h"
+#endif
 
 // using namespace std;
 
@@ -31,7 +38,10 @@ int cobol_main(int argc, char** argv)
      const struct cb_program* prog = NULL;
      struct rose_base_list* local_cache = NULL;
 
+#if ROSE_USING_GNUCOBOL
+  // DQ (9/29/2017): We need a way to know when ROSE has been configured with the location of the GNUCobol compiler. 
      status = rose_convert_cb_program(const struct cb_program* prog, struct rose_base_list* local_cache);
+#endif
 
      assert (status != 0);
 
