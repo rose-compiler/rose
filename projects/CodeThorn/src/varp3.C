a26def5618d4fc72928a92a41d0842eaf760f25f
Markus Schordan <schordan1@llnl.gov>
2018-02-06 13:06:17 -0800

    (varp3) added commandline options.

--- projects/CodeThorn/src/varp3.C
+++ projects/CodeThorn/src/varp3.C
@@ -1,5 +1,3 @@
-// Author: Markus Schordan, 2013.
-// Example AstMatcher : used for demonstrating and testing the matcher mechanism
 
 #include <iostream>
 #include <fstream>
@@ -10,6 +8,15 @@
 #include <list>
 #include <vector>
 #include "Timer.h"
+#include "CommandLineOptions.h"
+
+//preparation for using the Sawyer command line parser
+//#define USE_SAWYER_COMMANDLINE
+#ifdef USE_SAWYER_COMMANDLINE
+#include "Sawyer/CommandLineBoost.h"
+#else
+#include <boost/program_options.hpp>
+#endif
 
 using namespace std;
 
@@ -23,6 +30,17 @@ void write_file(std::string filename, std::string data) {
   myfile.open(filename.c_str(),std::ios::out);
   myfile << data;
   myfile.close();
+}
+
+void makeAllCastsExplicit(SgProject* root) {
+  RoseAst ast(root);
+  for(RoseAst::iterator i=ast.begin();i!=ast.end();++i) {
+    if(SgCastExp* castExp=isSgCastExp(*i)) {
+      if(castExp->isCompilerGenerated()) {
+	castExp->unsetCompilerGenerated();
+      }
+    }
+  }
 }
 
 void annotateImplicitCastsAsComments(SgProject* root) {
@@ -54,7 +72,7 @@ void annotateImplicitCastsAsComments(SgProject* root) {
       report.push_front(reportLine); 
       
       string newSourceCode;
-      newSourceCode="/* CAST ("+castTypeString+") */";
+      newSourceCode="/*CAST("+castTypeString+")*/";
       newSourceCode+=castExp->unparseToString();
       castExp->unsetCompilerGenerated(); // otherwise it is not replaced
       SgNodeHelper::replaceAstWithString(castExp,newSourceCode);
@@ -73,31 +91,93 @@ void changeVariableType(SgProject* root, string varNameToFind, SgType* type) {
   for(RoseAst::iterator i=ast.begin();i!=ast.end();++i) {
     if(SgVariableDeclaration* varDecl=isSgVariableDeclaration(*i)) {
       SgInitializedName* varInitName=SgNodeHelper::getInitializedNameOfVariableDeclaration(varDecl);
-      SgSymbol* varSym=SgNodeHelper::getSymbolOfInitializedName(varInitName);
-      string varName=SgNodeHelper::symbolToString(varSym);
-      if(varName==varNameToFind) {
-        cout<<"DEBUG: found declaration of var "<<varNameToFind<<". Changing type to "<<type->unparseToString()<<endl;
-        SgTypeFloat* ft=SageBuilder::buildFloatType();
-        varInitName->set_type(ft);
+      if(varInitName) {
+	SgSymbol* varSym=SgNodeHelper::getSymbolOfInitializedName(varInitName);
+	if(varSym) {
+	  string varName=SgNodeHelper::symbolToString(varSym);
+	  if(varName==varNameToFind) {
+	    cout<<"STATUS: found declaration of var "<<varNameToFind<<". Changed type to "<<type->unparseToString()<<"."<<endl;
+	    SgTypeFloat* ft=SageBuilder::buildFloatType();
+	    varInitName->set_type(ft);
+	  }
+	}
       }
     }
   }
 }
 
+string toolName="varp3";
+
 int main (int argc, char* argv[])
 {
+  ROSE_INITIALIZE;
   Rose::global_options.set_frontend_notes(false);
   Rose::global_options.set_frontend_warnings(false);
   Rose::global_options.set_backend_warnings(false);
 
+  // Command line option handling.
+#ifdef USE_SAWYER_COMMANDLINE
+    namespace po = Sawyer::CommandLine::Boost;
+#else
+    namespace po = boost::program_options;
+#endif
+
+  po::options_description desc
+    ("Supported options");
+
+  desc.add_options()
+    ("help,h", "produce this help message.")
+    ("version,v", "display the version.")
+    ("annotate", "annotate casts as comments.")
+    ("explicit", "make all imlicit casts explicit.")
+    ("float-var", po::value< string >()," change type of var to float.")
+    ("double-var", po::value< string >()," change type of var to double.")
+    ("long-double-var", po::value< string >()," change type of var to long double.")
+    ;
+
+  po::store(po::command_line_parser(argc, argv).
+            options(desc).allow_unregistered().run(), args);
+  po::notify(args);
+
+  if (args.count("help")) {
+    cout << toolName <<" <filename> [OPTIONS]"<<endl;
+    cout << desc << "\n";
+    exit(0);
+  }
+
+  if(args.isUserProvided("version")) {
+    cout<<toolName<<" version 0.1"<<endl;
+    return 0;
+  }
+
+  for (int i=1; i<argc; ++i) {
+    if (string(argv[i]) == "--float-var"
+        || string(argv[i]) == "--double-var"
+	|| string(argv[i]) == "--long-double-var"
+        ) {
+      argv[i] = strdup("");
+      assert(i+1<argc);
+      argv[i+1] = strdup("");
+    }
+  }
   vector<string> argvList(argv, argv+argc);
   argvList.push_back("-rose:skipfinalCompileStep");
   SgProject* sageProject=frontend (argvList); 
 
-  changeVariableType(sageProject, "testVar", SageBuilder::buildFloatType());
-
-  annotateImplicitCastsAsComments(sageProject);
+  if(args.isUserProvided("explicit")) {
+    makeAllCastsExplicit(sageProject);
+    cout<<"Converted all implicit casts to explicit casts."<<endl;
+  }
+  if(args.isUserProvided("annotate")) {
+    annotateImplicitCastsAsComments(sageProject);
+    cout<<"Annotated program with comments."<<endl;
+  } else if(args.isUserProvided("float-var")) {
+    cout<<"Changing variable type."<<endl;
+    string varName=args.getString("float-var");
+    changeVariableType(sageProject, varName, SageBuilder::buildFloatType());
+  }
 
   backend(sageProject);
+
   return 0;
 }
