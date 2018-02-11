52a1ce8ae65bb7051ebc7f601ac03877fa291e82
Daniel Quinlan <dquinlan@llnl.gov>
2018-02-11 15:33:47 -0800

    Fixed C99 bug reported by Liao specific to multi-dimensional VLA arrays in function parameters; also fixed warning output spew from VLA support.

--- tests/nonsmoke/functional/CompileTests/C_tests/test2018_16.c
+++ tests/nonsmoke/functional/CompileTests/C_tests/test2018_16.c
@@ -1,9 +1,8 @@
 /*
- *  error: array type has incomplete element type for double v[][][][5]
+ *  Bug: array type had incomplete element type for double v[][][][5]
  * */
-#if 1 // Must have this prototype to reproduce the bug
+// Must have this prototype to reproduce the bug
 void l2norm (int ldx, int ldy, double v[][ldy/2*2+1][ldx/2*2+1][5]);
-#endif
 
 void l2norm (int ldx, int ldy, double v[][ldy/2*2+1][ldx/2*2+1][5])
    {
