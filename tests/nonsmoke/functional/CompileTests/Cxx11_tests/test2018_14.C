fd9bf73db37df501e7bca24c802b4f0e8b70e7ee
Daniel Quinlan <dquinlan@llnl.gov>
2018-02-17 17:06:10 -0800

    Fixed template instantiation ambiguity by adding support for char16_t, char32_t, and there associated value expressions (fixes another Ares C++11 bug).

---
+++ tests/nonsmoke/functional/CompileTests/Cxx11_tests/test2018_14.C
@@ -0,0 +1,18 @@
+struct struct1 {};
+struct struct2 {};
+
+template < typename T >
+class template_class1 {};
+
+template < typename P, typename T >
+using template_class2 = template_class1< T >;
+
+
+// Should be unparsed as:
+// template_class2< struct1, double > instance_class1();
+   template_class2< struct1, double > instance_class1();
+
+// Should be unparsed as:
+// template_class2< struct2, double > instance_class2();
+   template_class2< struct2, double > instance_class2();
+
