83917d36a071f18c032af2fd45e8da86cf5e1743
Robb P. Matzke <matzke1@llnl.gov>
2018-02-21 08:46:13 -0500

    (Support) Simplified rose_isnan
    * Removed the rose_isnan macro and use proper C++ instead.
    
    * Remove all the special cases, which were incomplete anyway,
      and use the boost implementation instead which has undergone
      more portability testing.

--- src/util/rose_isnan.h
+++ src/util/rose_isnan.h
@@ -1,46 +1,14 @@
 #ifndef ROSE_isnan_H
 #define ROSE_isnan_H
 
-#if __GNUC__ < 6
-    #include <cmath>
+// In case anyone included a C header file, since isnan is a macro in C
+#undef isnan
 
-    // MS 2016: isnan is available in C++11 std namespace, but not in C++98
-    // std namespace. On *some* systems C++11 cmath puts it inside *and* outside the std
-    // namespace.
-    #if __cplusplus >= 201103L
-        #if __GNUC__ == 4
-            // The qualified "std::isnan" will not parse correctly with GCC-4.8.5 C++11 because isnan expands to
-            // "__builtin_isnan" which is not a member of "std" namespace.
-            //#define rose_isnan(x) std::isnan(x)
-            #define rose_isnan(x) isnan(x)
-        #else
-            #define rose_isnan(x) std::isnan(x)
-        #endif
-    #else
-        #ifdef _MSC_VER
-            #include <float.h> // for _isnan
-            #define rose_isnan(x) _isnan(x)
-        #else
-            #define rose_isnan(x) isnan(x)
-        #endif
-    #endif
+#include <boost/math/special_functions/fpclassify.hpp>
 
-#else
-
-    #include <math.h>
-
-    // DQ (11/16/2017): This is not defined properly for the Intel compiler when qualified
-    // because it will be expanded to an expression:
-    // std::(( sizeof( x ) > sizeof( double )) ? __isnanl( (long double)(x) ) : (( sizeof( x ) == sizeof( float )) ?
-    // __isnanf( (float)(x) ) : __isnan( (double)(x) ) ) ).
-    // and qualification of such an expression is not legal syntax.
-    // #define rose_isnan(x) std::isnan(x)
-    #ifdef __INTEL_COMPILER
-        #define rose_isnan(x) isnan(x)
-    #else
-        #define rose_isnan(x) std::isnan(x)
-    #endif
-
-#endif 
+template<typename T>
+bool rose_isnan(T x) {
+    return boost::math::isnan(x);
+}
 
 #endif
