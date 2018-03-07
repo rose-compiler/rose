49573c32cab29c7a754b0ad6d3f4b8416ca82f56
Markus Schordan <schordan1@llnl.gov>
2018-03-06 16:21:41 -0800

    (codethorn) added cast stats to varp3.

--- projects/CodeThorn/src/varp3.C
+++ projects/CodeThorn/src/varp3.C
@@ -10,7 +10,7 @@
 #include <vector>
 #include "Timer.h"
 #include "CommandLineOptions.h"
-
+#include <map>
 //preparation for using the Sawyer command line parser
 //#define USE_SAWYER_COMMANDLINE
 #ifdef USE_SAWYER_COMMANDLINE
@@ -18,6 +18,8 @@
 #else
 #include <boost/program_options.hpp>
 #endif
+
+#include "CastStats.h"
 
 using namespace std;
 
@@ -79,9 +81,9 @@ void addNode(SgExpression* node) {
 string typeColorName(SgType* type) {
   string color;
   switch(type->variantT()) {
-  case V_SgTypeFloat: color="blue";break;
-  case V_SgTypeDouble: color="green";break;
-  case V_SgTypeLongDouble: color="red";break;
+  case V_SgTypeFloat: color="red";break;
+  case V_SgTypeDouble: color="yellow";break;
+  case V_SgTypeLongDouble: color="green";break;
   default: color="white";
   }
   return color;
@@ -259,12 +261,13 @@ int main (int argc, char* argv[])
   desc.add_options()
     ("help,h", "produce this help message.")
     ("version,v", "display the version.")
-    ("annotate", "annotate casts as comments.")
+    ("annotate", "annotate implicit casts as comments.")
     ("explicit", "make all imlicit casts explicit.")
-    ("type-graph", "generated dot file with type graph.")
-    ("float-var", po::value< string >()," change type of var to float.")
-    ("double-var", po::value< string >()," change type of var to double.")
-    ("long-double-var", po::value< string >()," change type of var to long double.")
+    ("stats", "print statistics on casts of built-in floating point types.")
+    ("dot-type-graph", "generate typegraph in dot file 'typegraph.dot'.")
+    ("float-var", po::value< string >()," change type of var [arg] to float.")
+    ("double-var", po::value< string >()," change type of var [arg] to double.")
+    ("long-double-var", po::value< string >()," change type of var [arg] to long double.")
     ;
 
   po::store(po::command_line_parser(argc, argv).
@@ -278,7 +281,7 @@ int main (int argc, char* argv[])
   }
 
   if(args.isUserProvided("version")) {
-    cout<<toolName<<" version 0.1"<<endl;
+    cout<<toolName<<" version 0.1.0"<<endl;
     return 0;
   }
 
@@ -300,6 +303,11 @@ int main (int argc, char* argv[])
     makeAllCastsExplicit(sageProject);
     cout<<"Converted all implicit casts to explicit casts."<<endl;
   }
+  if(args.isUserProvided("stats")) {
+    CastStats castStats;
+    castStats.computeStats(sageProject);
+    cout<<castStats.toString();
+  }
   if(args.isUserProvided("annotate")) {
     annotateImplicitCastsAsComments(sageProject);
     cout<<"Annotated program with comments."<<endl;
@@ -308,7 +316,7 @@ int main (int argc, char* argv[])
     string varName=args.getString("float-var");
     changeVariableType(sageProject, varName, SageBuilder::buildFloatType());
   }
-  if(args.isUserProvided("type-graph")) {
+  if(args.isUserProvided("dot-type-graph")) {
     generateTypeGraph(sageProject);
     exit(0);
   }
