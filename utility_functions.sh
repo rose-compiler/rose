d8901e3cfd21ab1a242d2612bdfd8fd563f4b030
Charles A. Reynolds <reynolds12@llnl.gov>
2018-03-16 09:47:39 -0700

    Updated utility_functions.sh with log_then_run, strict_mode

--- utility_functions.sh
+++ utility_functions.sh
@@ -12,6 +12,40 @@
 # "$*"   - all args quoted in one string
 # "$@"   - all args quoted individually
 
+# BEGIN bash strict mode setup
+script_name=$0
+finish () {
+  __exit_status=$? 
+  if [[ ${__exit_status} -ne 0 ]]
+  then 
+    echo "ERROR: Exiting ${script_name} with status ${__exit_status}"
+  fi
+  exit ${__exit_status}
+}
+
+set_strict () {
+  # See http://redsymbol.net/articles/unofficial-bash-strict-mode/
+  # -e, -o errexit -
+  #      Exit if a command yields a nonzero exit status.  The ERR trap executes
+  #      before the shell exits.
+  # -u, -o nounset -
+  #      Treat unset variables as errors.
+  # -o pipefail -
+  #      Change exit status of a pipeline to be that of the rightmost commad that
+  #      failed, or zero if all exited successfully.
+  # Changes behavior of commands chained together with || or &&.
+  # Makes script exit if grep fails:
+  set -euo pipefail
+}
+
+unset_strict () {
+  set +euo pipefail
+}
+
+trap finish EXIT
+set_strict
+# END bash strict mode setup
+
 script_file=`basename $0`
 
 log () {
@@ -60,26 +94,35 @@ log_invocation () {
 }
 
 log_end () {
+  log_separator_0
   log "end time:     `do_date_log`"
   log_separator_2
 }
 
-log_and_run () {
+log_then_run () {
+  log_separator_0
   log_separator_1
   log "running:      \"$*\""
   log_blank_no_prefix
   "$@"
-  log_and_run_status=$?
+  log_then_run_status=$?
   log_blank_no_prefix
-  log "done running: \"$*\" (status ${log_and_run_status})"
+  log "done running: \"$*\" (status ${log_then_run_status})"
   log_separator_1
   # Exit with the right run status:
-  return ${log_and_run_status}
+  return ${log_then_run_status}
 }
 
+# Confusing name (too much like run_and_log), deprecated:
+log_and_run () {
+  log_separator_3
+  log "log_and_run is deprecated.  Use log_then_run.  Calling log_then_run \"$*\""
+  log_separator_3
+  log_then_run "$@"
+}
 # Exit the script with the command's status if the command fails:
-log_and_run_or_exit () {
-  log_and_run "$@" || exit $?
+log_then_run_or_exit () {
+  log_then_run "$@" || exit $?
 }
 
 do_date_log () {
