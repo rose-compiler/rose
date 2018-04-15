0f6f27514e8cec4f4e643c9115fa852c1f74de50
Daniel Quinlan <dquinlan@llnl.gov>
2018-04-15 11:58:50 -0700

    Fixed bug in Areas (identified by Charles), templated typedef instantation's template arguments were not being output with name qualification.

--- tests/nonsmoke/functional/CompileTests/Cxx11_tests/test2018_77.C
+++ tests/nonsmoke/functional/CompileTests/Cxx11_tests/test2018_77.C
@@ -1,83 +1,55 @@
-namespace namespace1 {
-template < int t_Parm1 >
-class t_Class1 {
-public:
-   int func3() const {
+namespace namespace1 
+   {
+     template < int t_Parm1 >
+     class t_Class1 
+        {
+          public:
+               int func3() const {}
+          private:
+               t_Class1();
+        };
+
+     template < int t_Parm1 >
+     t_Class1< t_Parm1 >::t_Class1() {}
    }
-private:
-  t_Class1();
-};
-
-template < int t_Parm1 >
-t_Class1< t_Parm1 >::t_Class1() {
-}
-}
-
- 
 
 template < typename policy, typename kernel_type >
-inline void t_func1( kernel_type kernel) {
-}
+inline void t_func1( kernel_type kernel) {}
 
-struct struct1 {
-};
+struct struct1 {};
 
-int func2() {
-  namespace1::t_Class1< 0 > *var2;
-  t_func1< struct1 >([=] {
-       var2->func3();
-  } );
-}
+int func2() 
+   {
+     namespace1::t_Class1< 0 > *var2;
+     t_func1< struct1 >([=] { var2->func3(); } );
+   }
 
  
 #if 0
-gets unparsed to this:
-
- 
+// gets unparsed to this:
 
 namespace namespace1
-
 {
-
 template < int t_Parm1 >
-
 class t_Class1 {
-
 public :
-
    int func3 ( ) const;
-
 private :
-
   t_Class1 ( );
-
 };
 
 template < int t_Parm1 >
-
 t_Class1 < t_Parm1 > :: t_Class1 ( ) {
-
 }
-
 }
 
 template < typename policy, typename kernel_type >
-
 inline void t_func1 ( kernel_type kernel ) {
-
 }
-
- 
 
 struct struct1
-
 {
-
-}
-
-;
-
- 
+};
 
 int func2()
 {
@@ -86,16 +58,12 @@ namespace1::t_Class1< 0 > *var2;
 {
 func3();
 });
-}
+} 
 
- 
-
-and results in this error:
+// and results in this error:
 
 rose_ROSE-27.cc(28): error: identifier "func3" is undefined
-
    func3();
-
    ^
 #endif
 
