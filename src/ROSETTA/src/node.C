7e787eaa803e018064a720649c20bbb6cb75cfeb
Craig Rasmussen <rasmussen17@llnl.gov>
2018-04-18 20:26:14 -0700

    (Experimental Languages) Added target_expression to SgUntypedGotoStatement.
      * Goto needs a place to go to

--- src/ROSETTA/src/node.C
+++ src/ROSETTA/src/node.C
@@ -692,6 +692,10 @@ Grammar::setUpNodes ()
      UntypedExpressionStatement.setDataPrototype     ( "SgUntypedExpression*", "statement_expression", "= NULL",
                   CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, DEF_TRAVERSAL, NO_DELETE);
 
+  // Rasmussen (4/18/2018): Added target_expression so that the goto statement has a place to go to.
+     UntypedGotoStatement.setDataPrototype           ( "SgUntypedExpression*", "target_expression", "= NULL",
+                  CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, DEF_TRAVERSAL, NO_DELETE);
+
      UntypedAssignmentStatement.setFunctionPrototype ( "HEADER_UNTYPED_ASSIGNMENT_STATEMENT", "../Grammar/LocatedNode.code");
      UntypedAssignmentStatement.setDataPrototype     ( "SgUntypedExpression*", "lhs_operand", "= NULL",
                   CONSTRUCTOR_PARAMETER, BUILD_ACCESS_FUNCTIONS, DEF_TRAVERSAL, NO_DELETE);
