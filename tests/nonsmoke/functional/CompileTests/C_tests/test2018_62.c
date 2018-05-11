f87010d0ba0ce4f484ce9621b91b3be582cce712
Daniel Quinlan <dquinlan@llnl.gov>
2018-05-10 18:18:31 -0700

    Added flag to SgFunctionDeclaration to allow it to be marked as an implicit function when used as such.

---
+++ tests/nonsmoke/functional/CompileTests/C_tests/test2018_62.c
@@ -0,0 +1,8 @@
+int main(int argc, char **argv) 
+   {
+     int i = 1, j = 2;
+
+     int array[2][3][4];
+
+     void *p = j[array[i]]+1;
+   }
---
+++ tests/nonsmoke/functional/CompileTests/C_tests/test2018_63.c
@@ -0,0 +1,4 @@
+void foobar() 
+   {
+     int i = foo(i);
+   }
