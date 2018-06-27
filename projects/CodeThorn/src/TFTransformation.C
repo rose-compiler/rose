839acd887e50d214cf46de576d197eb9729130d3
Markus Schordan <schordan1@llnl.gov>
2018-06-27 11:28:11 -0700

    (typeforge) made AD intstrumentation specific to floating-point types.

--- projects/CodeThorn/src/TFTransformation.C
+++ projects/CodeThorn/src/TFTransformation.C
@@ -264,26 +264,29 @@ void TFTransformation::instrumentADIntermediate(SgNode* root) {
   for(MatchResult::iterator j=r.begin();j!=r.end();++j) {
     SgExpression* assignOp=isSgExpression((*j)["$ASSIGNOP"]);
     SgVarRefExp* varRefExp=isSgVarRefExp((*j)["$VAR"]);
-    if(isWithinBlockStmt(assignOp)) {
-      SgVariableSymbol* varRefExpSymbol=varRefExp->get_symbol();
-      if(varRefExpSymbol) {
-        SgName varName=varRefExpSymbol->get_name();
-        string varNameString=varName;
-        string instrumentationString="AD_intermediate("+varNameString+",\""+varNameString+"\", FILE_INFO, FUNC_INFO);";
-        // locate root node of statement
-        SgNode* stmtSearch=assignOp;
-        while(!isSgStatement(stmtSearch)) {
-          stmtSearch=stmtSearch->get_parent();
-          if(!isSgStatement(stmtSearch)&&!isSgExpression(stmtSearch)) {
-            cerr<<"Error: Unsupported expression structure at "<<SgNodeHelper::sourceLineColumnToString(assignOp)<<endl;
-            exit(1);
+    SgType* varType=varRefExp->get_type();
+    if(SgNodeHelper::isFloatingPointType(varType)) {
+      if(isWithinBlockStmt(assignOp)) {
+        SgVariableSymbol* varRefExpSymbol=varRefExp->get_symbol();
+        if(varRefExpSymbol) {
+          SgName varName=varRefExpSymbol->get_name();
+          string varNameString=varName;
+          string instrumentationString="AD_intermediate("+varNameString+",\""+varNameString+"\", FILE_INFO, FUNC_INFO);";
+          // locate root node of statement
+          SgNode* stmtSearch=assignOp;
+          while(!isSgStatement(stmtSearch)) {
+            stmtSearch=stmtSearch->get_parent();
+            if(!isSgStatement(stmtSearch)&&!isSgExpression(stmtSearch)) {
+              cerr<<"Error: Unsupported expression structure at "<<SgNodeHelper::sourceLineColumnToString(assignOp)<<endl;
+              exit(1);
+            }
           }
+          // instrument now: insert empty statement and replace it with macro call
+          //cout<<"TRANSFORMATION: insert after "<< SgNodeHelper::sourceLineColumnToString(stmtSearch) <<" : "<<instrumentationString<<endl;
+          string newSource=stmtSearch->unparseToString()+"\n"+instrumentationString+"\n";
+          SgNodeHelper::replaceAstWithString(stmtSearch,newSource);
+          adIntermediateTransformations++;
         }
-        // instrument now: insert empty statement and replace it with macro call
-        //cout<<"TRANSFORMATION: insert after "<< SgNodeHelper::sourceLineColumnToString(stmtSearch) <<" : "<<instrumentationString<<endl;
-        string newSource=stmtSearch->unparseToString()+"\n"+instrumentationString+"\n";
-        SgNodeHelper::replaceAstWithString(stmtSearch,newSource);
-        adIntermediateTransformations++;
       }
     }
   }
