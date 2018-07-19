3ec749fa078c8e8c6db46a80c23ce3999b4d374c
Nathan Pinnow <pinnow2@llnl.gov>
2018-07-19 10:27:25 -0700

    (typeforge) added additional AD instrumentation tests

--- projects/CodeThorn/src/tests/casttest1.C
+++ projects/CodeThorn/src/tests/casttest1.C
@@ -1,17 +1,10 @@
 int main() {
   float f;
   double d=0.0;
-  double b[5];
-  double c[5][6][7];
   f=d;
   d=f;
   f=d+1.0;
   d=f+1.0f;
   d=f+1.0;
-  b[2]=3.0;
-  c[1][2][3]=4.0; 
-  d += 6;
-  b[2] += 5;
-  c[1][2][3] += 5;
   return 0;
 }
