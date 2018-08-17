6ae77edde002cd6e0bc9c16e164ca06ed2e1b71e
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2018-08-17 16:40:33 -0700

    (matrix-testing-rth) Added summary outputs of pass/fail

--- tests/nonsmoke/functional/CompileTests/status.sh
+++ tests/nonsmoke/functional/CompileTests/status.sh
@@ -3,6 +3,8 @@
 > status.csv
 
 files=
+anp=0
+anf=0
 
 for d in $@; do
 
@@ -13,16 +15,24 @@ for d in $@; do
       echo $d,$f,0 >> status.csv
     done
   fi
+  np=$(echo $passed | tr ' ' '\n' | wc -l)
+  anp=$((anp+np))
 
   failed=$(echo $d/*.failed | tr ' ' '\n' | grep -v \* || true)
   if [ ! -z "$failed" ]; then
     files="$files $(echo $failed | paste -sd\ )"
     for f in $(echo "$failed" | cut -d/ -f2- | rev | cut -d. -f2- | rev); do
-      msg=$(tail -n2 $d/$f.failed | head -n1)
-      echo "$d,$f,1,\"$msg\"" >> status.csv
+      echo "$d,$f,1" >> status.csv
     done
   fi
+  nf=$(echo $failed | tr ' ' '\n' | wc -l)
+  anf=$((anf+nf))
+
+  echo "[STATUS:pass/fail] CompileTests/$d $np $nf"
 
 done
 
-tar czf status.tgz $files
+echo "[STATUS:pass/fail] CompileTests $anp $anf"
+
+#tar czf status.tgz $files
+
