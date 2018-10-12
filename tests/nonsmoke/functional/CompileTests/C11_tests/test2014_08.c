2cafc6572322c13cd053c56d2c7adb7bc60f2e8b
lin32 <lin32@llnl.gov>
2018-10-11 17:12:48 -0700

    (ROSE-C11-test)
    uchar.h is supported from glibc 2.16.
    Fixed C11_tests/test2014_08.c to be tested if glibc is later than 2.16

--- tests/nonsmoke/functional/CompileTests/C11_tests/test2014_08.c
+++ tests/nonsmoke/functional/CompileTests/C11_tests/test2014_08.c
@@ -2,7 +2,10 @@
 // uchar.h defines the types char16_t, char32_t and declares the functions mbrtoc16, c16rtomb, mbrtoc32, c32rtomb. 
 // It is not possible to support this header file with GNU currently.
 
-#if __GNUC__ == 4 && __GNUC_MINOR__ > 8 || __GNUC__ > 4
+//#if __GNUC__ == 4 && __GNUC_MINOR__ > 8 || __GNUC__ > 4
+// Pei-Hung (10/11/18) uchar.h is supported from glibc 2.16
+// Reference at https://gcc.gnu.org/wiki/C11Status
+#if __GLIBC__ == 2 && __GLIBC_MINOR__ > 15 || __GLIBC__ > 2
   #include <uchar.h>
 #endif
 
