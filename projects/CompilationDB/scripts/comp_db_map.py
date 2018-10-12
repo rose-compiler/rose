d09e791d32571ef0d9def251b44b1aecb878dcc6
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2018-10-12 14:59:38 -0700

    (ROSE-1447) Added proper parsing of the command line argument to the script

--- projects/CompilationDB/scripts/comp_db_map.py
+++ projects/CompilationDB/scripts/comp_db_map.py
@@ -3,34 +3,52 @@ import os
 import sys
 import time
 import json
+import textwrap
 import argparse
+import traceback
 import subprocess
 import multiprocessing
 
-def apply_tool(comp_unit, tool, extra_args):
+def transform_original_args(args, filter_args, replace_args):
+	args = filter(lambda arg: not arg in filter_args, args)
+	args = map(lambda arg: replace_args[arg] if arg in replace_args else arg, args)
+	return args
+
+def substitute_args_placeholders(args, filepath, filename, workdir, origtool):
+	return map(lambda arg: arg.replace('%F',filepath).replace('%f',filename).replace('%d',workdir).replace('%t',origtool), args)
+
+def apply_tool(tool, comp_unit, args):
 	start_time = time.time()
 
 	if not 'arguments' in comp_unit:
 		assert 'command' in comp_unit
-		comp_unit.update({ 'arguments' : map(lambda s: s.strip() , filter(lambda s: len(s) > 0, comp_unit['command'].split(' '))) })
+		arguments = map(lambda s: s.strip() , filter(lambda s: len(s) > 0, comp_unit['command'].split(' ')))
+	else:
+		arguments = comp_unit['arguments']
+
+	comp_unit.update({ 'arguments' : { 'original' : arguments } })
+
+	origtool = arguments[0]
+	arguments = arguments[1:]
 
 	workdir  = comp_unit['directory']
 	filepath = comp_unit['file']
 	filename = filepath.split('/')[-1]
-	origtool = comp_unit['arguments'][0]
 
-	extra_args = map(lambda arg: arg.replace('%F',filepath).replace('%f',filename).replace('%d',workdir).replace('%t',origtool), extra_args)
+	arguments = transform_original_args(arguments, args['filter'], args['replace'])
 
-	cmdline = [ tool ] + comp_unit['arguments'][1:] + extra_args
+	arguments = args['tool'] + arguments
 
-	rose_proc = subprocess.Popen(cmdline, cwd=workdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	arguments = [ tool ] + substitute_args_placeholders(arguments, filepath, filename, workdir, origtool)
+
+	comp_unit['arguments'].update({ 'tool' : arguments })
+
+	rose_proc = subprocess.Popen(arguments, cwd=workdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 	out, err = rose_proc.communicate()
 
-	result = { 'returncode' : rose_proc.returncode, 'out' : out , 'err' : err, 'time' : time.time() - start_time }
+	result = { 'returncode' : rose_proc.returncode, 'out' : out , 'err' : err, 'elapsed' : time.time() - start_time }
 	result.update(comp_unit)
-	result.update({ 'tool' : tool })
-	result.update({ 'extra_args' : extra_args })
 
 	return result
 		
@@ -38,81 +56,215 @@ def apply_tool_helper(kwargs):
 	try:
 		return apply_tool(**kwargs)
 	except Exception as e:
-		return { 'exception' : e }
+		traceback.print_exc(file=sys.stdout)
+		cu = kwargs['comp_unit']
+		cu.update({ 'exception' : str(e) })
+		return cu
 
-def map_tool(comp_db, tool, directory, nprocs, extra_args):
+def map_tool(job):
 	start_time = time.time()
-	pool = multiprocessing.Pool(nprocs)
 
-	workload = map(lambda cu: { 'comp_unit' : cu, 'tool' : tool, 'extra_args' : extra_args }, comp_db)
-	future_result = pool.map_async(func=apply_tool_helper, iterable=workload, chunksize=1)
-	pool.close()
+	workload = map(lambda cu: { 'comp_unit' : cu, 'tool' : job['tool'], 'args' : job['arguments'] }, job['database'])
 
-	while (not future_result.ready()):
-		elapsed_time = time.time() - start_time
-		number_done = len(workload) - future_result._number_left
-		sys.stdout.write("\r                                       \r{}/{} in {:.1f} seconds".format(number_done, len(workload), elapsed_time ))
-		sys.stdout.flush()
-		time.sleep(.1)
-
-	sys.stdout.write("\r                                       \r{}/{} in {:.1f} seconds\n".format(number_done, len(workload), elapsed_time ))
+	sys.stdout.write("\r                                       \r{}/{} in {:.1f} seconds".format(0, len(workload), 0 ))
 	sys.stdout.flush()
 
-	return { 'tool' : tool , 'directory' : directory , 'nprocs' : nprocs , 'time' : time.time() - start_time , 'results' : future_result.get() }
+	if job['config']['nprocs'] == 0:
+		results = list()
+		for kwargs in workload:
+			results.append(apply_tool(**kwargs))
+			elapsed_time = time.time() - start_time
+			sys.stdout.write("\r                                       \r{}/{} in {:.1f} seconds".format(len(results), len(workload), elapsed_time ))
+			sys.stdout.flush()
+	else:
+		pool = multiprocessing.Pool(job['config']['nprocs'])
+		future_result = pool.map_async(func=apply_tool_helper, iterable=workload, chunksize=1)
+		pool.close()
 
-def generate_html_report(report):
-	with open('{}/report.html'.format(report['directory']), 'w') as F:
+		while (not future_result.ready()):
+			elapsed_time = time.time() - start_time
+			number_done = len(workload) - future_result._number_left
+			sys.stdout.write("\r                                       \r{}/{} in {:.1f} seconds".format(number_done, len(workload), elapsed_time ))
+			sys.stdout.flush()
+			time.sleep(.1)
+
+		results = future_result.get()
+		elapsed_time = time.time() - start_time
+
+	sys.stdout.write("\r                                       \r{}/{} in {:.1f} seconds\n".format(len(results), len(workload), elapsed_time ))
+	sys.stdout.flush()
+
+	job.update({ 'elapsed' : elapsed_time, 'results' : results })
+
+	return job
+
+def generate_report(job):
+	with open('{}/report.json'.format(job['directory']['build']), 'w') as F:
+		json.dump(job, F)
+
+	with open('{}/report.html'.format(job['directory']['build']), 'w') as F:
 		F.write('<table width="100%" border=1>\n')
-		F.write('  <tr><td width="1em">Directory</td><td>{}</td></tr>\n'.format(report['directory']))
-		F.write('  <tr><td width="1em">Tool</td><td>{}</td></tr>\n'.format(report['tool']))
-		F.write('  <tr><td width="1em">Number Processors</td><td>{}</td></tr>\n'.format(report['nprocs']))
-		F.write('  <tr><td width="1em">Time (second)</td><td>{}</td></tr>\n'.format(report['time']))
+		F.write('  <tr><td width="1em">Source Directory</td><td>{}</td></tr>\n'.format(job['directory']['source']))
+		F.write('  <tr><td width="1em">Build Directory</td><td>{}</td></tr>\n'.format(job['directory']['build']))
+		F.write('  <tr><td width="1em">Tool</td><td>{}</td></tr>\n'.format(job['tool']))
+		F.write('  <tr><td width="1em">Number Processors</td><td>{}</td></tr>\n'.format(job['config']['nprocs']))
+		F.write('  <tr><td width="1em">Time (second)</td><td>{}</td></tr>\n'.format(job['elapsed']))
 		F.write('</table>\n\n')
-		for result in report['results']:
+		for result in job['results']:
 			F.write('<br><hr><br>\n')
 			F.write('<table width="100%" border=1>\n')
 			F.write('  <td width="1em">Source File</td><td>{}</td></tr>\n'.format(result['file']))
 			F.write('  <td width="1em">Directory</td><td>{}</td></tr>\n'.format(result['directory']))
-			F.write('  <td width="1em">Original Command Line</td><td>{}</td></tr>\n'.format('<br/>'.join(result['arguments'])))
-			F.write('  <td width="1em">Tool Specific Arguments</td><td>{}</td></tr>\n'.format('<br/>'.join(result['extra_args'])))
-			F.write('  <td width="1em">Return Code</td><td>{}</td></tr>\n'.format(result['returncode']))
-			F.write('  <td width="1em">Elapsed Time</td><td>{}</td></tr>\n'.format(result['time']))
-			F.write('  <td width="1em">Standard Output</td><td><textarea rows="20" cols="240">{}</textarea></td></tr>\n'.format(result['out']))
-			F.write('  <td width="1em">Standard Error</td><td><textarea rows="20" cols="240">{}</textarea></td></tr>\n'.format(result['err']))
+			if 'exception' in result:
+				F.write('  <td width="1em">Exception</td><td><textarea rows="20" cols="240">{}</textarea></td></tr>\n'.format(result['exception']))
+			else:
+				F.write('  <td width="1em">Original Command Line</td><td>{}</td></tr>\n'.format('<br/>'.join(result['arguments']['original'])))
+				F.write('  <td width="1em">Tool Command Line</td><td>{}</td></tr>\n'.format('<br/>'.join(result['arguments']['tool'])))
+				F.write('  <td width="1em">Return Code</td><td>{}</td></tr>\n'.format(result['returncode']))
+				F.write('  <td width="1em">Elapsed Time</td><td>{}</td></tr>\n'.format(result['elapsed']))
+				F.write('  <td width="1em">Standard Output</td><td><textarea rows="20" cols="240">{}</textarea></td></tr>\n'.format(result['out']))
+				F.write('  <td width="1em">Standard Error</td><td><textarea rows="20" cols="240">{}</textarea></td></tr>\n'.format(result['err']))
 			F.write('</table>\n\n')
 
-def cli_parse_args():
-	parser = argparse.ArgumentParser(description='Apply a tool to each compilation unit in a CLANG-style compilation database.')
-	parser.add_argument('--srddir',              help='Source directory of the application being analyzed.')
-	parser.add_argument('--builddir',            help='Directory where a specific build of the application can be found.')
-	parser.add_argument('--database', nargs='?', help='Path for the compilation database (default is compile_commands.json in the build directory).')
-	parser.add_argument('--tool',                help='Tool to apply on each compilation unit (default is ROSE\'s identityTranslator).')
-	parser.add_argument('--filter',   nargs='+', help='Filter/replace command line arguments provided to the tool.')
-	parser.add_argument('--nprocs',   type=int,  help='Number of proccesses to use to run the tool.')
-	parser.add_argument('extra',      nargs='+', help='Command line arguments to be added for each compilation unit. Four place-holders are available: %F, %f, %d, %t corresponding, respectively, to source file path, source file name, working directory, and original tool/compiler.')
+def build_parser():
+	parser = argparse.ArgumentParser(
+				formatter_class=argparse.RawTextHelpFormatter, add_help=False,
+				usage=textwrap.dedent('''
+					(0) python comp_db_map.py [-h|--help]
+					(1) python comp_db_map.py srcdir builddir tool [--database DATABASE] [--filter FILTER] [--nprocs NPROCS] [ -- toolarg [toolarg ...] ]\
+                	'''),
+				description=textwrap.dedent('''\
+					This utility applies a given tool to each compilation unit in a CLANG-style compilation database.
+					It prevents ROSE's users from having to "hack" into a target applications build system.\
+					'''),
+				epilog=textwrap.dedent('''\
+					Following the -- argument, users can provide additional argument to be forwarded to the tool.
+					Four place-holders are available to specialize these arguments for each compilation unit:
+						- %F is replaced by the source file path
+						- %f is replaced by the source file name
+						- %d is replaced by the working directory
+						- %t is replaced by the original tool/compiler.\
+					''')
+			)
 
-	return parser.parse_args()
+	mandatory = parser.add_argument_group('Mandatory arguments')
+
+	mandatory.add_argument('srcdir',
+								 help=textwrap.dedent('''\
+									Source directory of the application being analyzed. Used to display shorter paths.\
+									'''))
+	mandatory.add_argument('builddir',
+								 help=textwrap.dedent('''\
+									Directory where a specific build of the application is located. Used to display shorter paths and find the database.\
+									'''))
+	mandatory.add_argument('tool',
+								 help=textwrap.dedent('''\
+									Tool to apply on each compilation unit, such as ROSE\'s identityTranslator.\
+									'''))
+
+	optional = parser.add_argument_group('Optional arguments')
+
+	optional.add_argument('--database',
+									help=textwrap.dedent('''\
+										Path to the compilation database relative to the utility's working directory (or absolute).
+										(default: $builddir/compile_commands.json).\
+										'''))
+
+	optional.add_argument('--filter', action='append', dest='filters',
+									help=textwrap.dedent('''\
+										Filter/replace command line argument provided to the tool.
+										Multiple filter/replace command can be provided.
+										  - "f:-x" means that "-x" argument will not be presented to the tool
+										  - "r:-x:-y" means the "-x" argument must be replaced by "-y"
+										The separator in these command do not have to be a semi-colon (:) but can be any character.
+										The separator do not have to be the same accross filter/replace commands.\
+										'''))
+
+	optional.add_argument('--nprocs', type=int, default=multiprocessing.cpu_count(),
+								 	help=textwrap.dedent('''\
+										Number of proccesses to use to run the tool.
+										(default: number of CPUs)\
+										'''))
+
+	optional.add_argument('-h', '--help', action='help', help='show this help message and exit')
+
+	return parser
+	
+
+def cli_parse_args(argv):
+	parser = build_parser()
+
+	if '--' in argv:
+		i = argv.index('--')
+		toolargs = argv[i+1:]
+		argv = argv[:i]
+	else:
+		toolargs = list()
+
+	args = parser.parse_args(argv)
+
+	if not os.path.isdir(args.srcdir):
+		print "Provided source directory is not a directory!"
+		exit(1)
+	args.srcdir = os.path.abspath(args.srcdir)
+
+	if not os.path.isdir(args.builddir):
+		print "Provided build directory is not a directory!"
+		exit(1)
+	args.builddir = os.path.abspath(args.builddir)
+
+	if args.database is None:
+		args.database = '{}/compile_commands.json'.format(args.builddir)
+		if not os.path.isfile(args.database):
+			print "Cannot find the compilation database in the build directory!"
+			exit(1)
+	else:
+		if not os.path.isfile(args.database):
+			print "Provided database path is not a file!"
+			exit(1)
+
+	try:
+		with open(args.database, 'r') as F:
+			args.database = json.load(F)
+	except:
+		print "The database file is not a valid JSON file!"
+		exit(1)
+
+	filter_args = list()
+	replace_args = dict()
+	if not args.filters is None:
+		for f in args.filters:
+			if f[0] == 'f':
+				filter_args.append(f[2:])
+			elif f[0] == 'r':
+				F = f[2:].split(f[1])
+				assert len(F) == 2, 'Invalid replace command: "{}"'.format(f)
+				replace_args.update({ F[0] : F[1] })
+			else:
+				print 'Invalid filter/replace command: first character must be one of "f" or "r". Probematic command is "{}"'.format(f)
+
+	return {
+		'directory' : {
+			'source' : args.srcdir,
+			'build' : args.builddir
+		},
+		'database' : args.database,
+		'tool' : args.tool,
+		'arguments' : {
+			'filter' : filter_args,
+			'replace' : replace_args,
+			'tool' : toolargs
+		},
+		'config' : {
+			'nprocs' : args.nprocs
+		}
+	}
 
 if __name__ == "__main__":
 
-	# args = cli_parse_args()
+	job = cli_parse_args(sys.argv[1:])
 
-	assert len(sys.argv) > 2
-	comp_db_path  = sys.argv[1]
-	tool = sys.argv[2]
-	nprocs = int(sys.argv[3])
-	extra_args = sys.argv[4:]
+	job = map_tool(job)
 
-	directory = '/'.join(comp_db_path.split('/')[:-1])
+	generate_report(job)
 
-	with open(comp_db_path, 'r') as F:
-		comp_db = json.load(F)
-
-	report = map_tool(comp_db=comp_db, tool=tool, directory=directory, nprocs=nprocs, extra_args=extra_args)
-
-	json_report_path = '{}/report.json'.format(directory)
-
-	with open(json_report_path, 'w') as F:
-		json.dump(report, F)
-
-	generate_html_report(report)
