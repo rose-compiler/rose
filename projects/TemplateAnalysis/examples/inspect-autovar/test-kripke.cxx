31b3deb0aaf1a95a07190098c322e73c69340d3c
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2019-01-29 11:21:03 -0800

    (Tpl Analysis) Improved test case demonstrating Kripke dispatch
    ROSE-1567

--- projects/TemplateAnalysis/examples/inspect-autovar/test-kripke.cxx
+++ projects/TemplateAnalysis/examples/inspect-autovar/test-kripke.cxx
@@ -13,9 +13,43 @@ namespace Kripke {
     class DataStore {};
   }
 
-  struct ArchT_Sequential {};
+  // Layout
 
-  struct LayoutT_DGZ {};
+  struct LayoutT_1 {};
+  struct LayoutT_2 {};
+
+  enum LayoutV {
+    LayoutV_1,
+    LayoutV_2
+  };
+
+  template<typename Function, typename ... Args>
+  void dispatchLayout(LayoutV layout_v, Function const &fcn, Args &&... args) {
+    switch(layout_v){
+      case LayoutV_1: fcn(LayoutT_1{}, std::forward<Args>(args)...); break;
+      case LayoutV_2: fcn(LayoutT_2{}, std::forward<Args>(args)...); break;
+    }
+  }
+
+  // Arch
+
+  struct ArchT_1 {};
+  struct ArchT_2 {};
+
+  enum ArchV {
+    ArchV_1,
+    ArchV_2
+  };
+
+  template<typename Function, typename ... Args>
+  void dispatchArch(ArchV arch_v, Function const &fcn, Args &&... args) {
+    switch(arch_v){
+      case ArchV_1: fcn(ArchT_1{}, std::forward<Args>(args)...); break;
+      case ArchV_2: fcn(ArchT_2{}, std::forward<Args>(args)...); break;
+    }
+  }
+
+  // Dispatch
 
   template<typename ARCH, typename LAYOUT>
   struct ArchLayoutT {
@@ -23,21 +57,18 @@ namespace Kripke {
     using layout_t = LAYOUT;
   };
 
+  struct ArchLayoutV {
+    ArchV arch_v;
+    LayoutV layout_v;
+  };
+
   class ArchLayout {
     public:
       ArchLayout() = default;
       virtual ~ArchLayout() = default;
+
+      ArchLayoutV al_v;
   };
-
-  template<typename Function, typename ... Args>
-  void dispatchLayout(Function const &fcn, Args &&... args) {
-    fcn(LayoutT_DGZ{}, std::forward<Args>(args)...);
-  }
-
-  template<typename Function, typename ... Args>
-  void dispatchArch(Function const &fcn, Args &&... args) {
-    fcn(ArchT_Sequential{}, std::forward<Args>(args)...);
-  }
 
   template<typename arch_t>
   struct DispatchHelper {
@@ -49,10 +80,10 @@ namespace Kripke {
   };
 
   template<typename Function, typename ... Args>
-  void dispatch(Function const &fcn, Args &&... args) {
-    dispatchArch([&](auto arch_t){
+  void dispatch(ArchLayoutV al_v, Function const &fcn, Args &&... args) {
+    dispatchArch(al_v.arch_v, [&](auto arch_t){
       DispatchHelper<decltype(arch_t)> helper;
-      dispatchLayout(helper, fcn, std::forward<Args>(args)...);
+      dispatchLayout(al_v.layout_v, helper, fcn, std::forward<Args>(args)...);
     });
   }
 }
@@ -68,6 +99,12 @@ struct SweepSdom {
 };
 
 void foo(Kripke::Core::DataStore & data_store, Kripke::SdomId sdom_id) {
-  Kripke::dispatch(SweepSdom{}, data_store, sdom_id);
+  ArchLayoutV al_1{ArchV_1,LayoutV_2};
+  Kripke::dispatch(al_1, SweepSdom{}, data_store, sdom_id);
+}
+
+void bar(Kripke::Core::DataStore & data_store, Kripke::SdomId sdom_id) {
+  ArchLayoutV al_2{ArchV_2,LayoutV_1};
+  Kripke::dispatch(al_2, SweepSdom{}, data_store, sdom_id);
 }
 
