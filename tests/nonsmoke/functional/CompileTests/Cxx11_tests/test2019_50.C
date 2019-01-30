7382538129240752c63ce46afe2cf8f939a0da20
Daniel Quinlan <dquinlan@llnl.gov>
2019-01-29 18:55:08 -0800

    (Bug Fix) Fixed use of constexpr variable declaration is switch condition.
    The use of a constexpr declaration with initialization was an error in the
    frontend.  It was a case that was not previously implemented and is not fixed.
    The declaration appears to be a const declaration instead of a constexpr expression
    used as a variable declaration in the conditional. I think this may be equivalent
    semantics.
    
    ROSE-1677

--- tests/nonsmoke/functional/CompileTests/Cxx11_tests/test2019_50.C
+++ tests/nonsmoke/functional/CompileTests/Cxx11_tests/test2019_50.C
@@ -1,5 +1,7 @@
 void foobar()
    {
+  // BUG: Should be unparsed as: switch (constexpr int i = 42); but we are loosing the constexpr and just getting const.
+  // However, I an unclear if this is not semantically equivalent.
      switch (constexpr int i = 42);
    }
 
