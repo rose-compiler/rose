986d11b7bdd611648d61ba88cf3d4a9f73426646
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2019-03-20 11:33:47 -0700

    (test cases)
    ROSE-1814
    ROSE-1834
    ROSE-1839
    ROSE-1846
    ROSE-1860

---
+++ tests/nonsmoke/functional/CompileTests/Cxx_tests/rose-1839.C
@@ -0,0 +1,11 @@
+template<typename t_parm1>
+struct struct_1 {
+};
+
+template<typename t_parm_2>
+class Class_1 {
+  class Class_2 {
+    template<typename t_parm_3>
+    friend struct struct_1;
+  };
+};
