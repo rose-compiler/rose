44e63beab3a4232ba87cd95336fe780a3a174e7c
Markus Schordan <schordan1@llnl.gov>
2019-03-22 18:20:14 -0700

    (codethorn) added bug checks.

--- projects/CodeThorn/src/tests/bugs/run.sh
+++ projects/CodeThorn/src/tests/bugs/run.sh
@@ -13,3 +13,14 @@ echo "========================="
 echo "BUG9: ROSE-1879 workaround"
 codethorn -I. bug9a.c bug9b.c
 echo "========================="
+
+echo "========================="
+echo "BUG10: CODETHORN-30 normalization bug"
+codethorn --normalize-all=yes bug10.C --out-of-bounds-analysis-file=bounds.csv
+NUM=`wc -l bounds.csv`
+if [ NUM != 0 ]; then
+    echo "Error: out of bounds detected (normalization wrong)."
+    exit 1
+fi
+echo "========================="
+echo "ALL TESTS PASSED."
