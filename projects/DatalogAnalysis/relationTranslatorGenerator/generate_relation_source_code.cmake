48bce8fb9665844c228a1b4a85f9a18c30180b24
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2019-05-03 11:09:58 -0700

    (projects) First round of deletion
    ROSE-1987

--- projects/DatalogAnalysis/relationTranslatorGenerator/generate_relation_source_code.cmake
+++
@@ -1,4 +0,0 @@
-set(ENV{PYTHONPATH} ${SOURCE_DIR})
-execute_process(
-  COMMAND ${PYTHON_EXECUTABLE} ${SOURCE_DIR}/main.py
-)
--- projects/programModeling/mapleRoseInput.C
+++
@@ -1,7 +0,0 @@
-
-int main()
-   {
-     int x;
-     x = 4;
-     return 0;
-   }
