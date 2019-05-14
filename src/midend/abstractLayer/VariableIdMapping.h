57298104b3cccee6b484552213d225c369bc8904
Markus Schordan <schordan1@llnl.gov>
2019-05-14 11:33:09 -0700

    (rose) (codethorn) merged namespace SPRAY into CodeThorn
    Renamed namespace SPRAY into namespace CodeThorn. This
    effectively merged SPRAY into CodeThorn. SPRAY::Exception
    was removed because CodeThorn::Exception is now used
    instead.
    
    ROSE-2002

--- src/midend/abstractLayer/VariableIdMapping.h
+++ src/midend/abstractLayer/VariableIdMapping.h
@@ -14,7 +14,7 @@
 #include "RoseAst.h"
 #include "SgNodeHelper.h"
 
-namespace SPRAY {
+namespace CodeThorn {
 
 class VariableId;
 typedef std::string VariableName;
@@ -102,8 +102,8 @@ class VariableIdMapping {
   size_t getElementSize(VariableId variableId);
 
   SgSymbol* createAndRegisterNewSymbol(std::string name);
-  SPRAY::VariableId createAndRegisterNewVariableId(std::string name);
-  SPRAY::VariableId createAndRegisterNewMemoryRegion(std::string name, int regionSize);
+  CodeThorn::VariableId createAndRegisterNewVariableId(std::string name);
+  CodeThorn::VariableId createAndRegisterNewMemoryRegion(std::string name, int regionSize);
   void registerNewSymbol(SgSymbol* sym);
   void registerNewArraySymbol(SgSymbol* sym, int arraySize);
   void toStream(std::ostream& os);
