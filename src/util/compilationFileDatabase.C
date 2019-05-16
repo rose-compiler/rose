bab99ff7fecc103169073b8f83ed2b01f22c9fbf
Peter Pirkelbauer <pirkelbauer2@llnl.gov>
2019-05-16 09:37:37 -0700

    (Solaris) Enable Solaris builds
    - set __int128_t for EDG 4.9
    - add file solaris-includes/err.h
    - reduce sage.h includes
    - eliminate tab use
    
    ROSE-2009
    ROSE-2010

--- src/util/compilationFileDatabase.C
+++ src/util/compilationFileDatabase.C
@@ -33,34 +33,34 @@ int flock(int fd, int op)
   int rc = 0;
 #if 0
 #if defined(F_SETLK) && defined(F_SETLKW)
-	struct flock fl = {0};
+  struct flock fl = {0};
 
-	switch (op & (LOCK_EX|LOCK_SH|LOCK_UN)) {
-	case LOCK_EX:
-		fl.l_type = F_WRLCK;
-		break;
+  switch (op & (LOCK_EX|LOCK_SH|LOCK_UN)) {
+  case LOCK_EX:
+    fl.l_type = F_WRLCK;
+    break;
 
-	case LOCK_SH:
-		fl.l_type = F_RDLCK;
-		break;
+  case LOCK_SH:
+    fl.l_type = F_RDLCK;
+    break;
 
-	case LOCK_UN:
-		fl.l_type = F_UNLCK;
-		break;
+  case LOCK_UN:
+    fl.l_type = F_UNLCK;
+    break;
 
-	default:
-		errno = EINVAL;
-		return -1;
-	}
+  default:
+    errno = EINVAL;
+    return -1;
+  }
 
-	fl.l_whence = SEEK_SET;
-	rc = fcntl(fd, op & LOCK_NB ? F_SETLK : F_SETLKW, &fl);
+  fl.l_whence = SEEK_SET;
+  rc = fcntl(fd, op & LOCK_NB ? F_SETLK : F_SETLKW, &fl);
 
-	if (rc && (errno == EAGAIN))
-		errno = EWOULDBLOCK;
+  if (rc && (errno == EAGAIN))
+    errno = EWOULDBLOCK;
 #endif
 #endif
-	return rc;
+  return rc;
 }
 #endif /* __sun */
 
