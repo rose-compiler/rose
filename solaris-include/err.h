4135aaac58b865ebe6afa16298237f1589709c48
Peter Pirkelbauer <pirkelbauer2@llnl.gov>
2019-06-11 07:07:14 -0700

    (Solaris) Add missing solaris file
    - add solaris-include/err.h - a fake replacement for Linux err.h
    
    ROSE-2009

---
+++ solaris-include/err.h
@@ -0,0 +1,36 @@
+#ifndef _FAKE_ERR_H
+#define _FAKE_ERR_H
+
+#include <stdlib.h>
+
+static inline
+void err(int eval, const char *fmt, ...) 
+{
+  exit(eval);
+}
+
+static inline
+void errx(int eval, const char *fmt, ...) 
+{
+  exit(eval);
+}
+
+static inline
+void warn(const char *fmt, ...) {}
+
+static inline
+void warnx(const char *fmt, ...) {}
+
+/*
+       #include <stdarg.h>
+
+       void verr(int eval, const char *fmt, va_list args);
+
+       void verrx(int eval, const char *fmt, va_list args);
+
+       void vwarn(const char *fmt, va_list args);
+
+       void vwarnx(const char *fmt, va_list args);
+*/
+
+#endif /* _FAKE_ERR_H */
