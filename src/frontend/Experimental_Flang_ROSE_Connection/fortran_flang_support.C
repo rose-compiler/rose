0384c67df6cdcc015afe17c995affcab495a6046
Craig Rasmussen <rasmussen17@llnl.gov>
2019-09-03 10:53:01 -0700

    (Fortran) Added support for using F18/Flang's parser.
      * Configure with -rose:experimental_flang_frontend option
      * Files that include f18 headers must be compiled with std=c++17 which
        doesn't supported by ROSE.  Thus these files must be manually built by
        modifying Makefiles appropriately.
    
    ROSE-2207

---
+++ src/frontend/Experimental_Flang_ROSE_Connection/fortran_flang_support.C
@@ -0,0 +1,42 @@
+#include "sage3basic.h"
+#include "rose_config.h"
+
+#include "fortran_flang_support.h"
+#include "flang-external-builder-main.h"
+
+using namespace Rose;
+
+#include <iostream>
+using std::string;
+using std::cout;
+using std::endl;
+
+#define DEBUG_EXPERIMENTAL_FORTRAN 0
+#define DOT_FILE_GENERATION 0
+
+#if DOT_FILE_GENERATION
+#   include "wholeAST_API.h"
+#endif
+
+int
+experimental_fortran_main(int argc, char* const argv[], SgSourceFile* sg_source_file)
+   {
+      // Perhaps do the following:
+      //   1. Run f18 from the command line
+      //   2. This function is a call back
+
+      // Or:
+      //   1. Call a function in the f18 frontend passing (argc, argv)
+      //   2. The f18 function then calls this function as a call back
+
+      cout << "\n";
+      cout << "-->  experimental_fortran_flang_main: will call flang parser \n";
+
+      int status = 0;
+
+      status = flang_external_builder_main(argc, argv);
+
+      cout << "\n";
+
+      return status;
+  }
