2bf1c054490f6282a555cce63617e6ad1702b950
Markus Schordan <schordan1@llnl.gov>
2019-11-07 09:18:40 -0800

    (codethorn) v1.11.0
    * np+buffer analysis reporting correctly
    * runtime reported correctly
    * --start-fun option working.
    
    CODETHORN-47

--- projects/CodeThorn/src/codethorn.C
+++ projects/CodeThorn/src/codethorn.C
@@ -588,7 +588,7 @@ CommandLineOptions& parseCommandLine(int argc, char* argv[], Sawyer::Message::Fa
     cout << infoOptions << "\n";
     exit(0);
   } else if (args.count("version")) {
-    cout << "CodeThorn version 1.10.15\n";
+    cout << "CodeThorn version 1.11.0\n";
     cout << "Written by Markus Schordan, Marc Jasper, Simon Schroder, Maximilan Fecke, Joshua Asplund, Adrian Prantl\n";
     exit(0);
   }
