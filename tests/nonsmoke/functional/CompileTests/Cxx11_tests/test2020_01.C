61f4b15c639ad6648f6d0be1a00d2e5bc7cc13b9
Daniel Quinlan <dquinlan@llnl.gov>
2020-01-01 12:37:10 -0800

    (Bug) Fixed support for multiple files using same defining class declaration.
    Increased timeout limit for RoseExample_tests/Makefile.am from 45 minutes
    to 60 minutes (testRoseHeaders_04.C timed out on hudson-rose-27).
    
    Added more test codes that demonstrated sharing acorss multiple files,
    these are now fixed. There remains the possible test case of field veriables
    having defining declarations, but this should be handled as regular variable
    declarations, but we still might need some test ccodes to make sure this is
    supported correctly.
    
    Fixed case of defining declaration from multiple files used as the defining
    declaration in a variable declaration (in multiple files). This is the
    last Plumhall bug in the C++11 support specific to multiple file handiling
    (and unparsing).
    
    Multiple files containing the same defining class declaration is
    a problem for the unparsing since the defining class declaration is shared.
    The merge mechanism is supposed to be the only mechanism to support
    sharing or IR nodes across multiple translation units.  This bug fix
    sets up merge to do exactly that and removes the sharing of the
    defining class declarations across translation units which was a problem
    for the unparsing.  This is representative of about 17 plumhall bugs, and
    it fixes all of those 17 cases (remaining plumhall C++11 compile bugs: 141).
    
    ROSE-2426

---
+++ tests/nonsmoke/functional/CompileTests/Cxx11_tests/test2020_01.C
@@ -0,0 +1,36 @@
+// Here is your next ARDRA bug: ROSE-2428
+
+// REQUIRED: typedef:
+// typedef __SIZE_TYPE__ size_t;
+typedef unsigned long size_t;
+
+//REQUIRED: template:
+template <typename t_t_parm_5>
+struct Struct_3 {
+  template <int t_parm_2>
+  struct Struct_2 {
+  };
+};
+
+template <typename T>
+class Class_1 {
+public:
+  static void func_2() {};
+};
+
+// REQUIRED: size_t, not int:
+template <size_t t_parm_1>
+struct Struct_1 {
+  template <typename t_t_parm_4>
+  static void func_1() {
+ // Class_1<typename Struct_3<t_t_parm_4>::template Struct_2<t_parm_1 - 1>::type>::func_2();
+    Class_1<typename Struct_3<t_t_parm_4>::template Struct_2<t_parm_1 - 1>::type>::func_2();
+  }
+};
+
+// produces this error:
+// identityTranslator[9216] 1.23522s Rose[FATAL]: assertion failed:
+// identityTranslator[9216] 1.23535s Rose[FATAL]:   /g/g17/charles/code/ROSE/rose-master-0.9.13.8/src/frontend/CxxFrontend/EDG/edgRose/edgRose.C:18987
+// identityTranslator[9216] 1.23541s Rose[FATAL]:   SgExpression *EDG_ROSE_Translation::convert_constant_template_parameter(a_constant *)
+// identityTranslator[9216] 1.23546s Rose[FATAL]:   required: edg_scope != __null
+
