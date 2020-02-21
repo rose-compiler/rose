ac3fe8c93f1b9d96c5a942132d94d518e14ffe77
Markus Schordan <schordan1@llnl.gov>
2020-02-20 18:17:08 -0800

    (codethorn) version 1.11.5
    Fixed bug for variables of type char.
    Improved reporting of type errors.
    Added test DOM024.
    
    CODETHORN-70

--- projects/CodeThorn/src/codethorn.C
+++ projects/CodeThorn/src/codethorn.C
@@ -596,7 +596,7 @@ CommandLineOptions& parseCommandLine(int argc, char* argv[], Sawyer::Message::Fa
     cout << infoOptions << "\n";
     exit(0);
   } else if (args.count("version")) {
-    cout << "CodeThorn version 1.11.4\n";
+    cout << "CodeThorn version 1.11.5\n";
     cout << "Written by Markus Schordan, Marc Jasper, Simon Schroder, Maximilan Fecke, Joshua Asplund, Adrian Prantl\n";
     exit(0);
   }
