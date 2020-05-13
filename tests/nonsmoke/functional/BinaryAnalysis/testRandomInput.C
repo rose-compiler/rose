41f01da19057b0bd198fd06cbe804460bdc9ce71
Robb Matzke <matzke1@llnl.gov>
2020-05-13 17:21:49 -0400

    (Binary Analysis) A64 tests should not fail if capstone is not present
    ROSE-2644

--- tests/nonsmoke/functional/BinaryAnalysis/testRandomInput.C
+++ tests/nonsmoke/functional/BinaryAnalysis/testRandomInput.C
@@ -91,7 +91,10 @@ main(int argc, char *argv[]) {
     MemoryMap::Ptr map = createInput(settings);
 
     // Obtain a disassembler
-    Disassembler *disassembler = Disassembler::lookup(settings.isa);
+    Disassembler *disassembler = NULL;
+    try {
+        disassembler = Disassembler::lookup(settings.isa);
+    } catch (...) {};
     if (!disassembler) {
         if (settings.isa == "list")
             exit(0);
