7b7382e8e0cf8b33278c90882b52c2983a867dc4
Markus Schordan <schordan1@llnl.gov>
2020-05-13 17:24:05 -0700

    (codethorn) version 1.12.4
    * added support for option -rose:ast:read
    * fixed g++8 compilation issue (PState overloaded operators)
    * bugfix: semantics of short circuit ||-operator of first operand if a
      number different to 0 and 1.
    * added unit test for exprAnalyzer
    
    CODETHORN-70

--- projects/CodeThorn/src/codethorn.C
+++ projects/CodeThorn/src/codethorn.C
@@ -88,7 +88,7 @@ using namespace Sawyer::Message;
 #include <stdlib.h>
 #include <unistd.h>
 
-const std::string versionString="1.12.3";
+const std::string versionString="1.12.4";
 
 // handler for generating backtrace
 void handler(int sig) {
