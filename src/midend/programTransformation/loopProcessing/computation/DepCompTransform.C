f5da76f17297d72b24af7ca9025b33221187ce14
Jim Reus <reus@simba.reusfamily.org>
2020-07-24 12:48:08 -0700

    (klocwork) Klocwork Midend 1491
    KLOC-112

--- src/midend/programTransformation/loopProcessing/computation/DepCompTransform.C
+++ src/midend/programTransformation/loopProcessing/computation/DepCompTransform.C
@@ -131,6 +131,7 @@ tc.DumpDep();
     for (GroupGraphNode::const_iterator iter=scc->begin(); !iter.ReachEnd(); iter++) {
        LoopTreeDepGraphNode *n =
           static_cast<LoopTreeDepGraphNode*>(iter.Current());
+       assert(tc.GetTreeNode(n) != NULL);
        treeSet.insert( tc.GetTreeNode(n) );
     }
     if (treeSet.NumberOfEntries()) {
