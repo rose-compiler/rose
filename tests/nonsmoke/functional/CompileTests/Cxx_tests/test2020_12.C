bd44d16aae82261249abd2593f107592f3bb2ef3
Daniel Quinlan <dquinlan@llnl.gov>
2020-07-23 19:07:52 -0700

    (Bug) Fixed C_tests and Cxx_tests for EDG 6.0 and GNU 10.1.
    Using the combination of EDG 6.0 and GNU 10.1, and number of directories
    demonstrated some failing tests (these test are not fixed for the C_tests
    and Cxx_tests directories).  Some of the Cxx11_tests are also fixed,
    but 13 test codes remain to be fixed and are separated out for exclusion
    in the Makefile.am when GNU 10.1 or later compilers are used.  In several
    cases the GNU 10.1 STL requires explicit inclusion of the string header file
    when used with the STL map header file. Additional EDG 6.0 specific
    additions were also made to handle new cases for EDG 6.0.
    
    ROSE-2830

--- tests/nonsmoke/functional/CompileTests/Cxx_tests/test2020_12.C
+++ tests/nonsmoke/functional/CompileTests/Cxx_tests/test2020_12.C
@@ -1,4 +1,10 @@
+#if _GNUC_ >= 10
+
+// DQ (7/23/2020): This appears to work for EDG 5.0 with GNU 6.1, but 
+// fails with EDG 6.0 using GNU 6.1, and it works for EDG 6.0 and GNU 10.1, 
+// so I'm not clear on where the boundaries are for when this works.
 #if __has_include(<test2020_12.h>)
 
 #endif
 
+#endif
