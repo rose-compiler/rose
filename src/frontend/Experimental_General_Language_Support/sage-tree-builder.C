0ec0dd01b02d5da0b461abce546ccb9967d0ba2e
Craig Rasmussen <rasmussen17@llnl.gov>
2020-07-30 13:30:08 -0700

    (Jovial) Fixed building of SgEnumDeclaration and SgEnumVal
      * Required fixing duplicate SgEnumTypes and searching symbol table for an
        SgEnumFieldSymbol rather than the declaration name.
      * regression test is rose-issue-rc-7.cpl
    
    RC-7

--- src/frontend/Experimental_General_Language_Support/sage-tree-builder.C
+++ src/frontend/Experimental_General_Language_Support/sage-tree-builder.C
@@ -491,6 +491,10 @@ Enter(SgExprStatement* &assign_stmt, SgExpression* &rhs, const std::vector<SgExp
    SgEnumVal* old_val = isSgEnumVal(rhs);
 
    if (old_val && old_val->get_value() == -1) {
+      // I don't think this is true anymore as the SgEnumVal is found by symbol lookup
+      // Need to test for assignment to a status
+      ROSE_ASSERT(false);
+
       SgEnumType* enum_type = isSgEnumType(vars[0]->get_type());
       ROSE_ASSERT(enum_type);
       SgEnumVal* enum_val = ReplaceEnumVal(enum_type, old_val->get_name());
@@ -990,36 +994,11 @@ Leave(SgVariableDeclaration* var_decl)
 }
 
 void SageTreeBuilder::
-Enter(SgEnumDeclaration* &enum_decl, const std::string &name, std::list<SgInitializedName*> &status_list)
+Enter(SgEnumDeclaration* &enum_decl, const std::string &name)
 {
    mlog[TRACE] << "SageTreeBuilder::Enter(SgEnumDeclaration* &, ...) \n";
 
    enum_decl = SageBuilder::buildEnumDeclaration_nfi(name, SageBuilder::topScopeStack());
-   ROSE_ASSERT(enum_decl);
-
-   BOOST_FOREACH(SgInitializedName* status_constant, status_list) {
-      enum_decl->append_enumerator(status_constant);
-      status_constant->set_scope(enum_decl->get_scope());
-
-      SgEnumType* enum_type = isSgEnumType(status_constant->get_typeptr());
-      ROSE_ASSERT(enum_type);
-      enum_type->set_declaration(enum_decl);
-
-      SgEnumFieldSymbol* enum_field_symbol = new SgEnumFieldSymbol(status_constant);
-      ROSE_ASSERT(enum_field_symbol);
-      enum_decl->get_scope()->insert_symbol(status_constant->get_name(), enum_field_symbol);
-
-      SgAssignInitializer* assign_init = isSgAssignInitializer(status_constant->get_initializer());
-      ROSE_ASSERT(assign_init);
-      ROSE_ASSERT(assign_init->get_operand_i());
-      SgIntVal* intval = isSgIntVal(assign_init->get_operand_i());
-      ROSE_ASSERT(intval);
-
-      SgEnumVal* new_operand = SageBuilder::buildEnumVal(intval->get_value(), enum_decl,
-                                                         status_constant->get_name());
-      assign_init->set_operand_i(new_operand);
-      delete intval;
-   }
 }
 
 void SageTreeBuilder::
@@ -1028,6 +1007,29 @@ Leave(SgEnumDeclaration* enum_decl)
    mlog[TRACE] << "SageTreeBuilder::Leave(SgEnumDeclaration*) \n";
 
    SageInterface::appendStatement(enum_decl, SageBuilder::topScopeStack());
+}
+
+void SageTreeBuilder::
+Enter(SgEnumVal* &enum_val, const std::string &name, SgEnumDeclaration* enum_decl, int value)
+{
+   mlog[TRACE] << "SageTreeBuilder::Enter(SgEnumVal*) \n";
+
+   ROSE_ASSERT(enum_decl);
+   SgEnumType* enum_type = enum_decl->get_type();
+
+   enum_val = SageBuilder::buildEnumVal(value, enum_decl, name);
+   SageInterface::setSourcePosition(enum_val);
+
+   SgAssignInitializer* initializer = SageBuilder::buildAssignInitializer_nfi(enum_val, enum_type);
+   SgInitializedName* init_name = SageBuilder::buildInitializedName_nfi(name, enum_type, initializer);
+   SageInterface::setSourcePosition(init_name);
+
+   enum_decl->append_enumerator(init_name);
+   init_name->set_scope(enum_decl->get_scope());
+
+   SgEnumFieldSymbol* enum_field_symbol = new SgEnumFieldSymbol(init_name);
+   ROSE_ASSERT(enum_field_symbol);
+   enum_decl->get_scope()->insert_symbol(name, enum_field_symbol);
 }
 
 void SageTreeBuilder::
