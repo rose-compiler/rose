3a549f322a47cab464fc4d05d1a8d72f43b04f24
Robb Matzke <matzke1@llnl.gov>
2020-08-10 16:52:21 -0400

    (Binary Analysis) InstructionProvider assertions were in wrong scope
    WRC-4

--- src/frontend/Partitioner2/InstructionProvider.C
+++ src/frontend/Partitioner2/InstructionProvider.C
@@ -24,10 +24,10 @@ InstructionProvider::operator[](rose_addr_t va) const {
                     ASSERT_require(insn->get_size()==1);
                 }
             }
+            ASSERT_not_null(insn);
+            ASSERT_require(insn->get_address() == va);
+            insnMap_.insert(va, insn);
         }
-        ASSERT_not_null(insn);
-        ASSERT_require(insn->get_address() == va);
-        insnMap_.insert(va, insn);
     }
     return insn;
 }
