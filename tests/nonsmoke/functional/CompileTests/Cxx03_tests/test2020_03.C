12fc4422e2f10c4eaf3493b3483be40b2f2d43e6
Daniel Quinlan <dquinlan@llnl.gov>
2020-08-18 10:41:46 -0700

    (Bug) Fixed class member access modifiers (fixed C++03 gnu test).
    The default value for the enum used for the access modifier is set to public,
    this is now fix and access modifiers can now have a valid default value (e.g
    namespaces are given a default value). Also the definaing and nondefining
    declaration no long need have the same value. The problem with the previous
    design was demonstrated by Cxx_tests/test2020_28.C and Cxx_tests/test2020_29.C.
    
    ROSE-2841

--- tests/nonsmoke/functional/CompileTests/Cxx03_tests/test2020_03.C
+++ tests/nonsmoke/functional/CompileTests/Cxx03_tests/test2020_03.C
@@ -6,6 +6,9 @@ class hash_map
   // fail.  with g++ 6.1 and -std=c++ it will pass fine, because g++ defaults to 
   // C++11 or C++14.  Using -std=c++ causes no use of the -std option when calling
   // the backend compiler.
-     [[no_unique_address]] int x;
+
+  // DQ (8/18/2020): I think this is C++11, or C++14 code that does not wotk with GNU 4.9 using C++11 support.
+  // [[no_unique_address]] int x;
+     int x;
    };
 
