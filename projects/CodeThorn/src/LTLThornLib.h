9fa830fd466c6ed2da17175d8c98bc437447471e
Markus Schordan <schordan1@llnl.gov>
2020-08-19 03:32:51 -0700

    (codethorn) moved ltl analysis init into LTL library
    * moved all initialization code for LTL verification into the LTL library.
    
    CODETHORN-70

--- projects/CodeThorn/src/LTLThornLib.h
+++ projects/CodeThorn/src/LTLThornLib.h
@@ -5,4 +5,7 @@ namespace CodeThorn {
   void initDiagnosticsLTL();
 }
 
+void runLTLAnalysis(CodeThornOptions& ctOpt, LTLOptions& ltlOpt,IOAnalyzer* analyzer, TimingCollector& tc);
+void optionallyInitializePatternSearchSolver(CodeThornOptions& ctOpt,IOAnalyzer* analyzer,TimingCollector& timingCollector);
+
 #endif
