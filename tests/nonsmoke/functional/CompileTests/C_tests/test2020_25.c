2867e18e6c03e48272d656abfc9deaa33b62606f
Daniel Quinlan <dquinlan@llnl.gov>
2020-08-27 13:00:23 -0700

    (Bug) Fixed RC-74 (fixed missing files in CMake).
    Implemented post processing fixup of the AST to support the removal of
    #include files that are used to initalized variable declaration (typically arrays).
    This bug is demonstrated by Cxx_tests/test2020_46.C, test2020_52.C, test2020_53.C,
    test2020_54.C.  Previously the AST would include the initializers, but then the
    CPP include directive would be redundant and cause an error at backend compile-time
    for the generated code.  Now the #include directive is removed when it is detected
    that it is redundant (the logic is a bit complex, and detailed in new files
    src/frontend/SageIII/astPostProcessing/fixupInitializers.C and
    src/frontend/SageIII/astPostProcessing/fixupInitializers.h).
    
    RC-74

--- tests/nonsmoke/functional/CompileTests/C_tests/test2020_25.c
+++ tests/nonsmoke/functional/CompileTests/C_tests/test2020_25.c
@@ -1,44 +1,25 @@
 // RC-67:
 
-    #ifndef BYTE
+#ifndef BYTE
+# define BYTE
+# define INSIDE_RECURSION
+# include "rc-67-0.c"
+# undef INSIDE_RECURSION
+#endif
 
-    # define BYTE
+#ifndef INSIDE_RECURSION
+static const char * re_error_msgid[] = { "Success" };
+#elif !defined(DEFINED_ONCE)
+int re_max_failures = 2000;
+#endif
 
-    # define INSIDE_RECURSION
+#ifndef INSIDE_RECURSION
+const char * foo() 
+   {
+     return (re_error_msgid[0]);
+   }
+#endif
 
-    # include "rc-67-0.c"
-
-    # undef INSIDE_RECURSION
-
-    #endif
-
-     
-
-    #ifndef INSIDE_RECURSION
-
-    static const char * re_error_msgid[] = { "Success" };
-
-    #elif !defined(DEFINED_ONCE)
-
-    int re_max_failures = 2000;
-
-    #endif
-
-     
-
-    #ifndef INSIDE_RECURSION
-
-    const char * foo() {
-
-      return (re_error_msgid[0]);
-
-    }
-
-    #endif
-
-     
-
-    # undef BYTE
-
-    # define DEFINED_ONCE
+# undef BYTE
+# define DEFINED_ONCE
 
