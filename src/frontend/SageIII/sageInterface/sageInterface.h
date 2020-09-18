dbc6e2542954227c22bfea2246fd6fbe3756cdfa
Chunhua (Leo) Liao <liao6@llnl.gov>
2020-09-18 12:30:53 -0700

    (SageInterface) a translator to dump AST into text-form tree
    * textASTGenerator
    
    ROSE-018

--- src/frontend/SageIII/sageInterface/sageInterface.h
+++ src/frontend/SageIII/sageInterface/sageInterface.h
@@ -414,7 +414,7 @@ int64_t getAsmSignedConstant(SgAsmValueExpression *e);
 
    //! Pretty print AST horizontally, output to a specified text file.
    void printAST2TextFile (SgNode* node, const char* filename); 
-   void printAST2TextFile (SgNode* node, const std::string& filename); 
+   void printAST2TextFile (SgNode* node, std::string filename); 
 
  // DQ (2/12/2012): Added some diagnostic support.
 //! Diagnostic function for tracing back through the parent list to understand at runtime where in the AST a failure happened.
