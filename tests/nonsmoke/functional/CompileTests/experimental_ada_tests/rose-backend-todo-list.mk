76329fc97f4826918f6926aec20c5e97e52ea05a
Peter Pirkelbauer <pirkelbauer2@llnl.gov>
2020-10-12 16:01:22 -0700

    (Ada) add new tests and Makefile lists
    - added enum test
    - added Makefile sublists
    
    ROSE-2325

---
+++ tests/nonsmoke/functional/CompileTests/experimental_ada_tests/rose-backend-todo-list.mk
@@ -0,0 +1,6 @@
+# this tests fail in the backend
+ROSE_BACKEND_TODO :=                   \
+  constants.ads                        \
+  loop_exit.adb                        \
+  operator.adb                         \
+  EndOfList
---
+++ tests/nonsmoke/functional/CompileTests/experimental_ada_tests/rose-frontend-todo-list.mk
@@ -0,0 +1,5 @@
+# these tests fail in frontend (Asis to ROSE conversion)
+ROSE_FRONTEND_TODO :=                  \
+  hello_world.adb                      \
+  choice_parameter_specification.adb   \
+  EndOfList
