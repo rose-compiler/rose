746b73b32f0f76c80cca45e531ca947c5cf814f7
Peter Pirkelbauer <pirkelbauer2@llnl.gov>
2020-10-23 09:52:16 -0700

    (Ada) use offset of initialzed name for renaming
    - added support to record an offset with in a multi-declaration
      (e.g., SgVariabelDeclaration) so that exceptions and variables
      can be renamed.
    
    ROSE-2325

--- src/frontend/Experimental_Ada_ROSE_Connection/AdaMaker.h
+++ src/frontend/Experimental_Ada_ROSE_Connection/AdaMaker.h
@@ -216,18 +216,17 @@ namespace Ada_ROSE_Translation
   /// creates an Ada renaming declaration
   /// \param name    the new name
   /// \param aliased the aliased declaration
-  /// \param idx     the index of the renamed element within aliased (e.g., a renamed variable)
-  /// \param scope   the scope of the renaming decl
-  SgAdaRenamingDecl&
-  mkAdaRenamingDecl(const std::string& name, SgDeclarationStatement& aliased, size_t idx, SgScopeStatement& scope);
-
-  /// creates an Ada renaming declaration
-  /// \param name    the new name
-  /// \param aliased the aliased declaration
   /// \param scope   the scope of the renaming decl
   /// \note the idx is assumed to be 0.
   SgAdaRenamingDecl&
   mkAdaRenamingDecl(const std::string& name, SgDeclarationStatement& aliased, SgScopeStatement& scope);
+
+  /// creates an Ada renaming declaration
+  /// \param name    the new name
+  /// \param aliased the aliased initialized name
+  /// \param scope   the scope of the renaming decl
+  SgAdaRenamingDecl&
+  mkAdaRenamingDecl(const std::string& name, SgInitializedName& ini, SgScopeStatement& scope);
 
   /// creates an Ada package body declaration
   SgAdaPackageBodyDecl&
