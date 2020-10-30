375cc81c574a3ca01f4788bbce5bc16b1851aeab
Pei-Hung Lin <lin32@llnl.gov>
2020-10-30 16:54:19 -0700

    (Ada) added Ada clauses support
    RC-298

--- src/frontend/Experimental_Ada_ROSE_Connection/dot_asis/dot_asis_library/source/asis_tool_2-element-clauses.adb
+++ src/frontend/Experimental_Ada_ROSE_Connection/dot_asis/dot_asis_library/source/asis_tool_2-element-clauses.adb
@@ -35,6 +35,56 @@ package body Asis_Tool_2.Element.Clauses is
          Result.Has_Limited := a_nodes_h.Support.To_bool (Value);
       end;
 
+      procedure Add_Representation_Clause_Name is
+         ID : constant a_nodes_h.Name_ID :=
+           Get_Element_ID (Asis.Clauses.Representation_Clause_Name (Element));
+      begin
+         State.Add_To_Dot_Label ("Representation_Clause_Name", To_String(ID));
+         Result.Representation_Clause_Name := ID;
+      end;
+
+      procedure Add_Representation_Clause_Expression is
+         ID : constant a_nodes_h.Expression_ID :=
+           Get_Element_ID (Asis.Clauses.Representation_Clause_Expression (Element));
+      begin
+         State.Add_To_Dot_Label ("Representation_Clause_Expression", To_String(ID));
+         Result.Representation_Clause_Expression := ID;
+      end;
+
+      procedure Add_Mod_Clause_Expression is
+         ID : constant a_nodes_h.Expression_ID :=
+           Get_Element_ID (Asis.Clauses.Mod_Clause_Expression (Element));
+      begin
+         State.Add_To_Dot_Label ("Mod_Clause_Expression", To_String(ID));
+         Result.Mod_Clause_Expression := ID;
+      end;
+
+      procedure Add_Component_Clauses is
+      begin
+         Add_Element_List
+           (This           => State,
+            Elements_In    => Asis.Clauses.Component_Clauses (Element),
+            Dot_Label_Name => "Component_Clauses",
+            List_Out       => Result.Component_Clauses,
+            Add_Edges      => True);
+      end;
+
+      procedure Add_Component_Clause_Position is
+         ID : constant a_nodes_h.Expression_ID :=
+           Get_Element_ID (Asis.Clauses.Component_Clause_Position (Element));
+      begin
+         State.Add_To_Dot_Label ("Component_Clause_Position", To_String(ID));
+         Result.Component_Clause_Position := ID;
+      end;
+
+      procedure Add_Component_Clause_Range is
+         ID : constant a_nodes_h.Element_ID :=
+           Get_Element_ID (Asis.Clauses.Component_Clause_Range (Element));
+      begin
+         State.Add_To_Dot_Label ("Component_Clause_Range", To_String(ID));
+         Result.Component_Clause_Range := ID;
+      end;
+
       procedure Add_Common_Items is
       begin
          State.Add_To_Dot_Label ("Clause_Kind", Clause_Kind'Image);
@@ -65,7 +115,12 @@ package body Asis_Tool_2.Element.Clauses is
             --                                         -> Representation_Clause_Kinds
             State.Add_Not_Implemented;
          when A_Component_Clause =>
-            State.Add_Not_Implemented;
+            Add_Representation_Clause_Name;
+            Add_Representation_Clause_Expression;
+            Add_Mod_Clause_Expression;
+            Add_Component_Clauses;
+            Add_Component_Clause_Position;
+            Add_Component_Clause_Range;
       end case;
 
       State.A_Element.Element_Kind := a_nodes_h.A_Clause;
