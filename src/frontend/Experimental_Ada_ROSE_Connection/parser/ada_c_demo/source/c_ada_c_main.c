e6cfa64d4fd6a8e7f0e7eaf11bda842a31785414
Charles A. Reynolds <reynolds12@llnl.gov>
2020-12-14 07:10:12 -0800

    (Ada) Retiring dot_asis
    Moving Experimental_Ada_ROSE_Connection/dot_asis to
    Experimental_Ada_ROSE_Connection/parser/asis.
    
    Step 4: Change the names inside the files so that eveything builds
    
    RC-443

--- src/frontend/Experimental_Ada_ROSE_Connection/parser/ada_c_demo/source/c_ada_c_main.c
+++ src/frontend/Experimental_Ada_ROSE_Connection/parser/ada_c_demo/source/c_ada_c_main.c
@@ -5,8 +5,8 @@
 int main (int argc, char *argv[])
 {
   adainit ();
-  printf ("c_main: Calling Ada_C_Caller\n");
+  printf ("c_ada_c_main: Calling Ada_C_Caller\n");
   Ada_C_Caller ();
-  printf ("c_main: Returned from Ada_C_Caller\n");
+  printf ("c_ada_c_main: Returned from Ada_C_Caller\n");
   adafinal ();
 }
