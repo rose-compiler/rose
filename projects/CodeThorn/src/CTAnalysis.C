87b2d9d269f38c1408074bb11f293e24a6765d22
Markus Schordan <schordan1@llnl.gov>
2021-01-12 11:06:29 -0800

    (codethorn) added omp guards for summary state access
    * added omp guards for set/get functions of summary states in
      estate-transfer fucntions
    
    CODETHORN-70

--- projects/CodeThorn/src/CTAnalysis.C
+++ projects/CodeThorn/src/CTAnalysis.C
@@ -403,18 +403,23 @@ bool CodeThorn::CTAnalysis::isUnreachableLabel(Label lab) {
 }
 
 const CodeThorn::EState* CodeThorn::CTAnalysis::getSummaryState(CodeThorn::Label lab, CodeThorn::CallString cs) {
-  auto iter1=_summaryCSStateMapMap.find(lab.getId());
-  if(iter1==_summaryCSStateMapMap.end()) {
-    return getBottomSummaryState(lab,cs);
-  } else {
-    SummaryCSStateMap& summaryCSStateMap=(*iter1).second;
-    auto iter2=summaryCSStateMap.find(cs);
-    if(iter2==summaryCSStateMap.end()) {
-      return getBottomSummaryState(lab,cs);
+  const CodeThorn::EState* res;
+#pragma omp critical(SUMMARY_STATES)
+  {
+    auto iter1=_summaryCSStateMapMap.find(lab.getId());
+    if(iter1==_summaryCSStateMapMap.end()) {
+      res=getBottomSummaryState(lab,cs);
     } else {
-      return (*iter2).second;
+      SummaryCSStateMap& summaryCSStateMap=(*iter1).second;
+      auto iter2=summaryCSStateMap.find(cs);
+      if(iter2==summaryCSStateMap.end()) {
+        res=getBottomSummaryState(lab,cs);
+      } else {
+        res=(*iter2).second;
+      }
     }
   }
+  return res;
 }
 
 void CodeThorn::CTAnalysis::setSummaryState(CodeThorn::Label lab, CodeThorn::CallString cs, CodeThorn::EState const* estate) {
@@ -424,16 +429,18 @@ void CodeThorn::CTAnalysis::setSummaryState(CodeThorn::Label lab, CodeThorn::Cal
 
   //pair<int,CallString> p(lab.getId(),cs);
   //_summaryCSStateMap[p]=estate;
-
-  auto iter1=_summaryCSStateMapMap.find(lab.getId());
-  if(iter1==_summaryCSStateMapMap.end()) {
-    // create new
-    SummaryCSStateMap newSummaryCSStateMap;
-    newSummaryCSStateMap[cs]=estate;
-    _summaryCSStateMapMap[lab.getId()]=newSummaryCSStateMap;
-  } else {
-    SummaryCSStateMap& summaryCSStateMap=(*iter1).second;
-    summaryCSStateMap[cs]=estate;
+#pragma omp critical(SUMMARY_STATES)
+  {
+    auto iter1=_summaryCSStateMapMap.find(lab.getId());
+    if(iter1==_summaryCSStateMapMap.end()) {
+      // create new
+      SummaryCSStateMap newSummaryCSStateMap;
+      newSummaryCSStateMap[cs]=estate;
+      _summaryCSStateMapMap[lab.getId()]=newSummaryCSStateMap;
+    } else {
+      SummaryCSStateMap& summaryCSStateMap=(*iter1).second;
+      summaryCSStateMap[cs]=estate;
+    }
   }
 }
 
