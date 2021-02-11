2cb2661f55a9edae913cba6d38d0c3b231e3dab7
Robb Matzke <matzke1@llnl.gov>
2021-02-10 19:55:49 -0500

    (AArch32) These features temporarily require C++17
    CYT-1

--- src/featureTests.h
+++ src/featureTests.h
@@ -32,7 +32,8 @@
 #endif
 
 // ARM AArch32 instructions (Sage nodes, disassembly, unparsing, semantics, etc.)
-#if !defined(ROSE_ENABLE_ASM_AARCH32) && __cplusplus >= 201103L && defined(ROSE_HAVE_CAPSTONE)
+// Instruction semantics for AArch32 requires C++17 or later.
+#if !defined(ROSE_ENABLE_ASM_AARCH32) && __cplusplus >= 201703L && defined(ROSE_HAVE_CAPSTONE)
     #define ROSE_ENABLE_ASM_AARCH32
 #endif
 
