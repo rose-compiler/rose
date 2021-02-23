d5135413b45392e3e7250d75cbc9bfb2d28b822f
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2021-02-23 12:36:57 -0800

    (ASTIO) Introducing memory-pool visualization
    Moved memory-pools to be static member of the respective SgNode
    Added prototype function to snapshot all memory pools
    
    RC-369

---
+++ src/frontend/SageIII/astFileIO/memory-pool-snapshot.C
@@ -0,0 +1,13 @@
+
+
+#include "memory-pool-snapshot.h"
+#include "sage3basic.h"
+
+namespace Rose {
+namespace MemPool {
+
+void snapshot(std::ostream &) {}
+void snapshot(std::string const &) {}
+
+}
+}
