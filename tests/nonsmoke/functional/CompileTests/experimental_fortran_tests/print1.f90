505353592a4702f5bf4c88606e57c5535f1ed8b3
Katherine Rasmussen <ktrasmussn@gmail.com>
2021-03-15 11:43:12 -0700

    Add tests for simple common blocks, print statements, and more type declarations

---
+++ tests/nonsmoke/functional/CompileTests/experimental_fortran_tests/print1.f90
@@ -0,0 +1,8 @@
+program main
+  integer :: i = 1
+  integer :: j = 1
+
+  print *, i
+  print *, "the value of i is ", i, "and the value of j is ", j
+
+end program main
---
+++ tests/nonsmoke/functional/CompileTests/experimental_fortran_tests/type_decls_2.f90
@@ -0,0 +1,10 @@
+program main
+  integer :: myInt
+  integer(kind = 4) :: myInt4
+  integer(kind = 8) :: myInt8
+  real(kind = 4) :: myReal4
+  real(kind = 8) :: myReal8
+
+  myInt = 3
+
+end program
