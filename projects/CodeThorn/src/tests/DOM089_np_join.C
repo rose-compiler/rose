75ceabad631eef2cbf7f639260d50de02b3ecedc
Markus Schordan <schordan1@llnl.gov>
2021-04-20 12:12:08 -0700

    (codethorn) refined DOM089 test
    * refined DOM089 test to show variants of imprecision
    
    CODETHORN-70

--- projects/CodeThorn/src/tests/DOM089_np_join.C
+++ projects/CodeThorn/src/tests/DOM089_np_join.C
@@ -5,20 +5,27 @@ int main() {
   int y;
   x=1;
   y=2;
-  int* xp;
-  xp=0;
+  int* p;
+  p=0;
   int c=rand();
   if(c) {
-    xp=&x;
+    p=&x;
     ;
   } else {
-    xp=&y;
+    p=&y;
     ;
   }
-  *xp=100;
-  int** xxp;
-  xxp=&xp;
-  printf("%d",*xp);
+  *p=100;
+  int** pp;
+  pp=&p;
+  if(*p==50) {
+    // this is dead code
+    printf("dead:%d",*p);
+  }
+  if(x+y==101 || x+y==102) {
+    printf("%d",*p);
+  }
+  printf("%d",*p);
   return 0;
 }
 
