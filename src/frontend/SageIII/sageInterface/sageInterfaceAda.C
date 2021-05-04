14eae58462b442242782b8ae3e28ea93c53fd834
Peter Pirkelbauer <pirkelbauer2@llnl.gov>
2021-05-04 14:59:38 -0700

    (Ada) Add tests for Ada literal conversion
    - added stand-alone test to test Ada int and real literal conversion to
      numeric values.
    
    RC-949

--- src/frontend/SageIII/sageInterface/sageInterfaceAda.C
+++ src/frontend/SageIII/sageInterface/sageInterfaceAda.C
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <exception>
 
+#include <boost/lexical_cast.hpp>
+
 namespace si = SageInterface;
 
 namespace
@@ -594,6 +596,243 @@ namespace ada
   {
     conversionTraversal(convertSymbolTablesToCaseSensitive_internal, root);
   }
+
+  /*
+  template<class T>
+  T powInt(T num, size_t exp, size_t res = 1)
+  {
+    if (exp == 0)
+      return res;
+
+    if ((exp % 2) == 0)
+      return powInt(num*num, exp/2, res);
+
+    return powInt(num, exp-1, num*res);
+  }
+*/
+
+namespace
+{
+  std::pair<size_t, bool>
+  check(size_t s, size_t m)
+  {
+    return std::make_pair(s, s < m);
+  }
+
+  std::pair<size_t, bool>
+  char2Val(char c, size_t max)
+  {
+    using ResultType = std::pair<size_t, bool>;
+
+    if ((c >= '0') && (c <= '9'))
+      return check(c - '0', max);
+
+    if ((c >= 'A') && (c <= 'F'))
+      return check(c - 'A' + 10, max);
+
+    if ((c >= 'a') && (c <= 'f'))
+      return check(c - 'a' + 10, max);
+
+    return ResultType{0, false};
+  }
+
+  template <class T>
+  std::pair<T, const char*>
+  parseDec(const char* buf, size_t base = 10)
+  {
+    ROSE_ASSERT((*buf != 0) && char2Val(*buf, base).second);
+
+    T res = 0;
+
+    while (*buf != 0)
+    {
+      const auto v = char2Val(*buf, base);
+
+      if (!v.second)
+        return std::make_pair(res, buf);
+
+      res = res*base + v.first;
+
+      ++buf;
+
+      // skip underscores
+      // \note (this is imprecise, since an underscore must be followed
+      //       by an integer.
+      while (*buf == '_') ++buf;
+    }
+
+    return std::make_pair(res, buf);
+  }
+
+  template <class T>
+  std::pair<T, const char*>
+  parseFrac(const char* buf, size_t base = 10)
+  {
+    ROSE_ASSERT((*buf != 0) && char2Val(*buf, base).second);
+
+    T      res = 0;
+    size_t divisor = 1*base;
+
+    while ((*buf != 0) && (*buf != '#'))
+    {
+      const auto v = char2Val(*buf, base);
+
+      ROSE_ASSERT(v.second);
+
+      T val = v.first;
+
+      res += val/divisor;
+      divisor = divisor*base;
+
+      ++buf;
+
+      // skip underscores
+      // \note (this is imprecise, since an underscore must be followed
+      //       by an integer.
+      while (*buf == '_') ++buf;
+    }
+
+    return std::make_pair(res, buf);
+  }
+
+
+  std::pair<int, const char*>
+  parseExp(const char* buf)
+  {
+    if (*buf == 0)
+      return std::make_pair(0, buf);
+
+    long int exp = 0;
+
+    if ((*buf == 'e') || (*buf == 'E'))
+    {
+      ++buf;
+      const bool positiveE = (*buf != '-');
+
+      // skip sign
+      if (!positiveE || (*buf == '+')) ++buf;
+
+      std::tie(exp, buf) = parseDec<long int>(buf, 10);
+
+      if (!positiveE) exp = -exp;
+    }
+
+    return std::make_pair(exp, buf);
+  }
+
+  template <class T>
+  T computeLiteral(T val, int base, int exp)
+  {
+    return val * std::pow(base, exp);
+  }
+
+
+  long int
+  basedLiteral(long int res, const char* cur, int base)
+  {
+    int exp = 0;
+
+    ROSE_ASSERT(*cur == '#');
+
+    ++cur;
+    base = res;
+
+    std::tie(res, cur) = parseDec<long int>(cur, base);
+
+    if (*cur == '#')
+    {
+      ++cur;
+
+      std::tie(exp, cur) = parseExp(cur);
+    }
+
+    return computeLiteral(res, base, exp);
+  }
+} // anonymous
+
+
+int convertIntLiteral(const char* img)
+{
+  long int    res  = 0;
+  int         base = 10;
+  int         exp  = 0;
+  const char* cur  = img;
+
+  std::tie(res, cur) = parseDec<long int>(cur);
+
+  if (*cur == '#')
+  {
+    return basedLiteral(res, cur, base);
+  }
+
+  if (*cur == '.')
+  {
+    throw std::logic_error("SageInterfaceAda.C: Ada decimal literals not yet handled");
+
+    //~ long int decimal = 0;
+
+    //~ ++cur;
+    //~ std::tie(decimal, cur) = parseDec<long int>(cur);
+  }
+
+  std::tie(exp, cur) = parseExp(cur);
+
+  //~ logWarn() << "r: "
+            //~ << res << ' ' << base << '^' << exp << '\n'
+            //~ << std::endl;
+
+  return computeLiteral(res, base, exp);
+}
+
+
+long double convertRealLiteral(const char* img)
+{
+  std::string litText{img};
+
+  boost::replace_all(litText, "_", "");
+
+  // handle 'normal' real literals
+  if (litText.find('#') == std::string::npos)
+  {
+    // logWarn() << "R: " << conv<long double>(litText) << std::endl;
+    return boost::lexical_cast<long double>(litText);
+  }
+
+  // handle based real literals
+  long double dec  = 0;
+  long double frac = 0;
+  int         base = 10;
+  int         exp  = 0;
+  const char* cur  = img;
+
+  std::tie(base, cur) = parseDec<long int>(cur);
+  ROSE_ASSERT(*cur == '#');
+
+  ++cur;
+  std::tie(dec, cur) = parseDec<long double>(cur, base);
+
+  if (*cur == '.')
+  {
+    ++cur;
+    std::tie(frac, cur) = parseFrac<long double>(cur, base);
+  }
+
+  const long double res = dec + frac;
+
+  ROSE_ASSERT(*cur == '#');
+  ++cur;
+
+  std::tie(exp, cur) = parseExp(cur);
+
+/*
+    logWarn() << "r: "
+            << res << ' ' << dec << '+' << frac << ' ' << base << ' ' << exp << '\n'
+            << res * base
+            << std::endl;
+*/
+  return computeLiteral(res, base, exp);
+}
+
 } // Ada
 } // SageInterface
 
