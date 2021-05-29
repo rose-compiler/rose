8897c0e8c1f46e172411b51af44ddabc30980ecb
Markus Schordan <schordan1@llnl.gov>
2021-05-29 02:56:38 -0700

    (codethorn) added IOC tests
    * IOCxxx tests are added for checking whether all variants of
      scanf/printf functions are handled properly.
    * add test script runIOCTests (included in default test now)
    
    RC-477

--- projects/CodeThorn/src/tests/IOC001.c
+++ projects/CodeThorn/src/tests/IOC001.c
@@ -1,25 +1,13 @@
-#include <cstdio>
+#include <stdio.h>
 
 int main() {
-  char s[80];
+  char str[80];
   int x;
-  scanf("%s",s);
-  scanf ("%79s",s);
+  scanf("%s",str);
+  scanf ("%79s",str);
   scanf ("%d",&x);
   
-  printf("output: %d %s",x,s);
-  
-  FILE *fptr = fopen("sample.txt", "w");
-  if (fptr == NULL) {
-    printf("Could not open file");
-    return 0;
-  }
-   
-  for (i = 0; i < n; i++) {
-    puts("Enter a name");
-    scanf("%[^\n]%*c", str);
-    fprintf(fptr,"%d.%s\n", i, str);
-  }
-  fclose(fptr);
+  printf("output: %d %s",x,str);
+
   return 0;
 }
--- projects/CodeThorn/src/tests/IOC002.c
+++ projects/CodeThorn/src/tests/IOC002.c
@@ -1,4 +1,4 @@
-#include <cstdio.h>
+#include <stdio.h>
 
 int main() {
   char str[80];
