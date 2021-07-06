24d8d799823e059c67c2a384932ddabe38b8cb43
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2021-07-06 08:13:40 -0700

    (MFB) Moved API interface to MFB
    RC-1026

--- src/midend/MFB/include/MFB/Sage/api.hpp
+++ src/midend/MFB/include/MFB/Sage/api.hpp
@@ -8,8 +8,12 @@
 #ifndef _MFB_API_HPP_
 #define _MFB_API_HPP_
 
+#include "MFB/Sage/driver.hpp"
+
 #include <set>
 #include <map>
+#include <vector>
+#include <string>
 #include <boost/filesystem.hpp>
 
 class SgNamespaceSymbol;
@@ -20,6 +24,8 @@ class SgVariableSymbol;
 class SgMemberFunctionSymbol;
 class SgTypedefSymbol;
 class SgNonrealSymbol;
+
+class SgType;
 
 namespace MFB {
 
@@ -56,9 +62,42 @@ void dump_api(const api_t * api);
 
 void merge_api(api_t * base_api, const api_t * api);
 
+template <typename CRT>
+class API {
+  public:
+    using a_namespace = SgNamespaceDeclarationStatement *;
+    using a_type      = SgType *;
+    using a_variable  = SgVariableSymbol *;
+    using a_function  = SgFunctionSymbol *;
+
+    void load(Driver<Sage> & driver);
+    void display(std::ostream & out) const;
+
+  private:
+    static std::string const name;
+
+    // Static fields used to locate and open the header files
+    static std::string const cache;
+    static std::vector<std::string> const paths;
+    static std::vector<std::string> const files;
+    static std::vector<std::string> const flags;
+
+    // Object maps used to load API elements into the CRT "sub-class" (introspection of sort)
+    static std::map<std::string, a_namespace CRT::* > const namespaces;
+    static std::map<std::string, a_type      CRT::* > const types;
+    static std::map<std::string, a_variable  CRT::* > const variables;
+    static std::map<std::string, a_function  CRT::* > const functions;
+
+    void build_command_line(std::vector<std::string> & cmdline) const;
+    void load_headers(Driver<Sage> & driver);
+    void load_api(Driver<Sage> & driver);
+};
+
 /** @} */
 
 }
+
+#include "MFB/Sage/api.tpp"
 
 #endif /* _MFB_API_HPP_ */
 
