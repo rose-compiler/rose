40bb74a017bbec82dd4e3d42fe825135ae566919
Daniel Quinlan <dquinlan@llnl.gov>
2021-08-12 12:44:53 -0700

    (Bug) Evaluating fialing tests in ROSE due to the newest token-based work.
      This commeit is part of testing to evaluating if I have captured all of the
    failing test files from three directories:
    
    1) git_rose_development/tests/nonsmoke/functional/roseTests/astInliningTests
    
    2) git_rose_development/tests/nonsmoke/functional/moveDeclarationTool
    
    3) git_rose_development/tests/nonsmoke/functional/roseTests/astOutliningTests
    
    All failing tests are marked in the Makefile.am (DQ(8/9/2021).
    
    J-17

---
+++ tests/nonsmoke/functional/CompileTests/UnparseHeadersUsingTokenStream_tests/test22/Simple22.C
@@ -0,0 +1,29 @@
+// #include "Simple.h"
+
+int v1;
+
+
+class   Simple{
+
+// comment in Simple.h header file.
+  public:
+    int v1;
+
+ // comment 2
+    Simple  ();
+};
+
+
+
+Simple::Simple()
+   {
+     v1 = 15;
+   }
+
+
+
+
+   // comment 3
+int main(int argc, char* argv[]) {
+   }
+int xyz;
---
+++ tests/nonsmoke/functional/CompileTests/UnparseHeadersUsingTokenStream_tests/test23/Simple23.C
@@ -0,0 +1,8 @@
+#include "Simple.h"
+
+Simple::Simple(){
+  v1 = 15;
+  v2_rename_me = 155;
+}
+
+int main() { }
