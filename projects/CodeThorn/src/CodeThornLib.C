f3dd63c749f3cdfe828b7a11e542370621695d29
Markus Schordan <schordan1@llnl.gov>
2021-10-20 01:17:00 -0700

    (codethorn) v1.13.30
    * Fixed failing case of multi-dim arrays
    
    CODETHORN-70

--- projects/CodeThorn/src/CodeThornLib.C
+++ projects/CodeThorn/src/CodeThornLib.C
@@ -93,7 +93,7 @@ using namespace Sawyer::Message;
 
 using namespace Sawyer::Message;
 
-static std::string CodeThornLibraryVersion="1.13.29";
+static std::string CodeThornLibraryVersion="1.13.30";
 
 // handler for generating backtrace
 void codethornBackTraceHandler(int sig) {
