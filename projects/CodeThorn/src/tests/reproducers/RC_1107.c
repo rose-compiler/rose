1dbac412030f790558c5af98e3342b76d4a167a6
Markus Schordan <schordan1@llnl.gov>
2021-10-28 16:25:36 -0700

    (codethorn) added RC_1107 test
    RC-1107

---
+++ projects/CodeThorn/src/tests/reproducers/RC_1107.c
@@ -0,0 +1,9 @@
+//const char BLANK[2] = { ' ', ' '};
+const char BLANK[2] = "  ";
+void FOO() {
+  &BLANK[0];
+}
+
+int BAR() {
+  return 4;
+}
---
+++ projects/CodeThorn/src/tests/reproducers/RC_1107.sh
@@ -0,0 +1,1 @@
+codethorn RC_1107.c --generate-reports --start-function=FOO --precision=1 --exploration-mode=topologic-sort --normalize-level=2
