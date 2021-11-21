514ed7a1468bdcd1d61d9da711f6033ac8b18859
Markus Schordan <schordan1@llnl.gov>
2021-11-20 23:45:53 -0800

    (codethorn) v1.13.38
    * added AstMatcher documentation (doxygen)
    * adapted PState/EState to be modifyable (non-const)
    * fixed intialization of states in abstraction mode with array
      abstraction
    
    CODETHORN-70

--- projects/CodeThorn/src/CodeThornLib.C
+++ projects/CodeThorn/src/CodeThornLib.C
@@ -93,7 +93,7 @@ using namespace Sawyer::Message;
 
 using namespace Sawyer::Message;
 
-static std::string CodeThornLibraryVersion="1.13.37";
+static std::string CodeThornLibraryVersion="1.13.38";
 
 // handler for generating backtrace
 void codethornBackTraceHandler(int sig) {
