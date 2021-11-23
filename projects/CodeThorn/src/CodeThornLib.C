043cf336865906b894fcb9fd86a9fb27ba0bf24d
Markus Schordan <schordan1@llnl.gov>
2021-11-23 00:27:03 -0800

    (codethorn) v1.13.39
    * adapted estate lists in Solvers and API of EStateTransferFunctions
    * adapted HSetMaintainer to accept pointers to objects
    * optimized combine function
    
    CODETHORN-70

--- projects/CodeThorn/src/CodeThornLib.C
+++ projects/CodeThorn/src/CodeThornLib.C
@@ -93,7 +93,7 @@ using namespace Sawyer::Message;
 
 using namespace Sawyer::Message;
 
-static std::string CodeThornLibraryVersion="1.13.38"; //b
+static std::string CodeThornLibraryVersion="1.13.39";
 
 // handler for generating backtrace
 void codethornBackTraceHandler(int sig) {
