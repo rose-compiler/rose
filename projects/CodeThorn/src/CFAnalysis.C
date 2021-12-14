ec43536947ac2e2b858c12713ab898fd19268bf3
Markus Schordan <schordan1@llnl.gov>
2021-12-13 23:23:19 -0800

    (codethorn) removed status output of reduce begin/end nodes
    CODETHORN-70

--- projects/CodeThorn/src/CFAnalysis.C
+++ projects/CodeThorn/src/CFAnalysis.C
@@ -959,7 +959,7 @@ int CFAnalysis::reduceBlockBeginNodes(Flow& flow) {
   int cnt=0;
   for(LabelSet::iterator i=labs.begin();i!=labs.end();++i) {
     if(labeler->isBlockBeginLabel(*i)) {
-      cout<<"Reducing block begin node:"<<(*i).toString()<<endl;
+      //cout<<"Reducing block begin node:"<<(*i).toString()<<endl;
       cnt+=reduceNode(flow,*i);
     }
   }
@@ -972,7 +972,7 @@ int CFAnalysis::reduceBlockEndNodes(Flow& flow) {
   for(LabelSet::iterator i=labs.begin();i!=labs.end();++i) {
     if(labeler->isBlockEndLabel(*i)) {
       cnt+=reduceNode(flow,*i);
-      cout<<"Reducing block end node:"<<(*i).toString()<<endl;
+      //cout<<"Reducing block end node:"<<(*i).toString()<<endl;
     }
   }
   return cnt;
