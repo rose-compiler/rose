1aa9ef1348480b338f3f39a3258fddebaca5e8f0
Robb Matzke <matzke1@llnl.gov>
2022-01-13 17:00:07 -0500

    (Documentation) Fixed documentation issues for library identification framework
    * Removed duplicate documentation. Much of the documentation from the
      header files was duplicated in the implementation files.
    
    * Fixed wrong @breif lines that were used for nearly everything.
    
    * Changed doxygen documentation to normal C++ comments for private
      members.
    
    * Removed commented out code.
    
    * Fixed some misleading indentation.
    
    RPM-43

--- src/midend/BinaryAnalysis/libraryIdentification/FunctionInfo.h
+++ src/midend/BinaryAnalysis/libraryIdentification/FunctionInfo.h
@@ -7,138 +7,107 @@
 #include "Combinatorics.h"
 #include <Rose/BinaryAnalysis/Partitioner2/Partitioner.h>
 #include <Rose/BinaryAnalysis/Partitioner2/Function.h>
-/** LibraryIdentification.
- *
- *  This namespace encapsulates function for FLIRT ( Fast Library
- *  Identification and Recognition Technology) like functionality for
- *  ROSE binary analysis.
- *  Currently only uses the Fowler-Noll-Vo HasherFnv class in
- *  Combinatorics.h.  We should add options for others.
- **/
+
+// documented elsewhere
 namespace LibraryIdentification
 {
-    /** class FunctionInfo
-     *  Combines all the information to uniquely identify a single
-     *  function in one object.
+    /** Information about a function.
+     *
+     *  Combines all the information to uniquely identify a single function in one object.
      **/
     class FunctionInfo
     {
     public:
-        /**
-         *  FunctionInfo
+        /** Constructor.
          *
-         *  Constructor.  Combines all the information required to
-         *  identify a function. 
-         *  This constructor allows the user to define everything.
+         *  Combines all the information required to identify a function.  This constructor allows the user to define
+         *  everything.
          *
          * @param[in] funcName  Name of the function to add
-         * @param[in] funcHash  Unique Hash of the function to add
-         * (Please use Fowler-Noll-Vo HasherFnv class in Combinatorics.h)
-         * @param[in] libHash  Unique Hash of the library this
-         * function belongs to. The Library should already be in the database.
+         * @param[in] funcHash  Unique Hash of the function to add Please use Fowler-Noll-Vo HasherFnv class in Combinatorics.h.
+         * @param[in] libHash  Unique Hash of the library this function belongs to. The Library should already be in the database.
          **/
-    FunctionInfo(const std::string& ifuncName, const std::string& ifuncHash, const std::string& ilibHash) :
-        funcName(ifuncName), funcHash(ifuncHash), libHash(ilibHash), binaryFunction() {};
+        FunctionInfo(const std::string& funcName, const std::string& funcHash, const std::string& libHash) :
+        funcName(funcName), funcHash(funcHash), libHash(libHash), binaryFunction() {};
         
-        /**
-         *  FunctionInfo
+        /** Constructor.
          *
-         *  Constructor.  Combines all the information required to
-         *  identify a function. 
-         *  This constructor constructs the hash from the
-         *  SgAsmFunction node.
-         *  Note that currently on FNV hasher is used.  This should be
-         *  an option.
+         *  Combines all the information required to identify a function.  This constructor constructs the hash from the
+         *  SgAsmFunction node.  Note that currently on FNV hasher is used.  This should be an option.
          *
-         * @param[in] partitioner Required to get the basic blocks of
-         * the function
+         * @param[in] partitioner Required to get the basic blocks of the function
          * @param[in] function Binary AST Function Node
-         * @param[in] libHash  Unique Hash of the library this
-         * function belongs to. The Library should already be in the database.
+         * @param[in] libHash  Unique Hash of the library this function belongs to. The Library should already be in the database.
          **/
-    FunctionInfo(const Rose::BinaryAnalysis::Partitioner2::Partitioner& partitioner, Rose::BinaryAnalysis::Partitioner2::Function::Ptr function, const std::string& ilibHash) :
-        funcName(function->name()),  libHash(ilibHash), binaryFunction(function)
+        FunctionInfo(const Rose::BinaryAnalysis::Partitioner2::Partitioner& partitioner,
+                     Rose::BinaryAnalysis::Partitioner2::Function::Ptr function, const std::string& libHash) :
+        funcName(function->name()), libHash(libHash), binaryFunction(function)
         {
              initializeHash(partitioner, function);
         }
 
-
-        /**
-         *  FunctionInfo
+        /** Constructor.
          *
-         *  Constructor.  Combines all the information required to
-         *  identify a function. 
-         *  This constructor constructs the hash from the
-         *  SgAsmFunction node.
-         *  Note that currently on FNV hasher is used.  This should be
-         *  an option.
+         *  Combines all the information required to identify a function.  This constructor constructs the hash from the
+         *  SgAsmFunction node.  Note that currently on FNV hasher is used.  This should be an option.
          *
-         * @param[in] partitioner Required to get the basic blocks of
-         * the function
+         * @param[in] partitioner Required to get the basic blocks of the function
          * @param[in] function Binary AST Function Node
          **/
-    FunctionInfo(const Rose::BinaryAnalysis::Partitioner2::Partitioner& partitioner, Rose::BinaryAnalysis::Partitioner2::Function::Ptr function) :
+        FunctionInfo(const Rose::BinaryAnalysis::Partitioner2::Partitioner& partitioner,
+                     Rose::BinaryAnalysis::Partitioner2::Function::Ptr function) :
         funcName(function->name()),  libHash(), binaryFunction(function)
         {
              initializeHash(partitioner, function);
         }
         
-        /**
-         *  FunctionInfo
+        /** Constructor.
          *
-         *  Constructor.  Combines all the information required to
-         *  identify a function. 
-         *  This constructor constructs the hash from the
-         *  SgAsmFunction node.
-         *  Note that currently on FNV hasher is used.  This should be
-         *  an option.
+         *  Combines all the information required to identify a function.  This constructor constructs the hash from the
+         *  SgAsmFunction node.  Note that currently on FNV hasher is used.  This should be an option.
          *
-         * @param[in] partitioner Required to get the basic blocks of
-         * the function
+         * @param[in] partitioner Required to get the basic blocks of the function
          * @param[in] function Binary AST Function Node
          * @param[in] libInfo  LibraryInfo that should be in the database
          **/
-    FunctionInfo(const Rose::BinaryAnalysis::Partitioner2::Partitioner& partitioner, Rose::BinaryAnalysis::Partitioner2::Function::Ptr function, LibraryInfo& libInfo) :
+        FunctionInfo(const Rose::BinaryAnalysis::Partitioner2::Partitioner& partitioner, Rose::BinaryAnalysis::Partitioner2::Function::Ptr function, LibraryInfo& libInfo) :
         funcName(function->name()),  libHash(libInfo.libHash), binaryFunction(function)
         {
              initializeHash(partitioner, function);
         }
 
-        /**
-         *  FunctionInfo
+        /** Constructor.
          *
-         *  Constructor.  Only takes the hash. (Never used?  Nomrmally
-         *  there is a binaryFunction availible as well.) 
-         *  Rest to be filled in from matching in the database. 
+         *  Only takes the hash. (Never used?  Nomrmally there is a binaryFunction availible as well.)  Rest to be filled in
+         *  from matching in the database.
          *
          * @param[in] funcHash  Unique Hash of the function
          **/
-    FunctionInfo(const std::string& ifuncHash) :
+        FunctionInfo(const std::string& ifuncHash) :
         funcName(""), funcHash(ifuncHash), libHash(""), binaryFunction() {};
 
-        /**
-         *  FunctionInfo
+        /** Constructor.
          *
-         *  Constructor.  Only takes the hash and the binaryFunction. 
-         *  Rest to be filled in from matching in the database. 
+         *  Only takes the hash and the binaryFunction.  Rest to be filled in from matching in the database.
          *
-         * @param[in] funcHash  Unique Hash of the function to add
-         * (Please use Fowler-Noll-Vo HasherFnv class in Combinatorics.h)
+         * @param[in] funcHash  Unique Hash of the function to add. Please use Fowler-Noll-Vo HasherFnv class in Combinatorics.h
          * @param[in] binaryFunction  from the partitioner
          **/
-    FunctionInfo(const std::string& ifuncHash, Rose::BinaryAnalysis::Partitioner2::Function::Ptr function) :
+        FunctionInfo(const std::string& ifuncHash, Rose::BinaryAnalysis::Partitioner2::Function::Ptr function) :
         funcHash(ifuncHash), libHash(""), binaryFunction(function) {
             if(function != 0) {
                 funcName = function->name();    
             }
         }; 
 
-        friend bool operator<(const FunctionInfo& lhs, const FunctionInfo& rhs) 
+        /** True if the first hash is less than the second hash. */
+        friend bool operator<(const FunctionInfo& lhs, const FunctionInfo& rhs)
         {
             return lhs.funcHash < rhs.funcHash;
         }
 
-        bool operator==(const FunctionInfo& rhs) 
+        /** True if the name, function hash, and library hash are equal. */
+        bool operator==(const FunctionInfo& rhs)
         {
             if(funcName == rhs.funcName &&
                funcHash == rhs.funcHash &&
@@ -149,26 +118,27 @@ namespace LibraryIdentification
             return false;
             
         }
-        
 
-
-        //@brief The name of the function
+        /** The name of the function. */
         std::string funcName;
 
-        //@brief A hash that should uniquely identify the function
+        /** A hash that should uniquely identify the function. */
         std::string funcHash;
 
-        //@brief A hash that should uniquely identify the library the
-        //function belongs to.  It should already be in the database.
+        /** Hash uniquely identifying the library to which the function belongs. */
         std::string libHash;
 
-        //@brief A pointer to the binary version of this function IF
-        //IT'S AVAILIBLE.  THIS IS LIKELY TO BE NULL
+        /** Optional pointer to the function definition.
+         *
+         *  Null if the function definition is not available. */
         Rose::BinaryAnalysis::Partitioner2::Function::Ptr binaryFunction;
-        
-        static std::string getHash(const Rose::BinaryAnalysis::Partitioner2::Partitioner& partitioner, Rose::BinaryAnalysis::Partitioner2::Function::Ptr function) 
+
+        /** Compute the hash for the specified function. */
+        static std::string getHash(const Rose::BinaryAnalysis::Partitioner2::Partitioner& partitioner,
+                                   Rose::BinaryAnalysis::Partitioner2::Function::Ptr function)
         {
-            boost::shared_ptr<Rose::Combinatorics::Hasher> hasher = Rose::Combinatorics::Hasher::HasherFactory::Instance().createHasher("SHA256");
+            boost::shared_ptr<Rose::Combinatorics::Hasher> hasher =
+                Rose::Combinatorics::Hasher::HasherFactory::Instance().createHasher("SHA256");
             Rose::BinaryAnalysis::AstHash astHash(hasher);            
 
             const std::set<rose_addr_t>& basicBlocks = function->basicBlockAddresses();
@@ -183,7 +153,6 @@ namespace LibraryIdentification
 
             return hasher->toString();
         }
-        
 
     private:
         void initializeHash(const Rose::BinaryAnalysis::Partitioner2::Partitioner& partitioner, Rose::BinaryAnalysis::Partitioner2::Function::Ptr function) 
@@ -191,9 +160,7 @@ namespace LibraryIdentification
             funcHash = getHash(partitioner, function);
             
         }
-
     };
-    
 }
 
 #endif
