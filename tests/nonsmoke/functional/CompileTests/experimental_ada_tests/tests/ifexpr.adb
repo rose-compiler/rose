a72c27a83eb6ab9f18741bc209e2398696a2fdb5
Peter Pirkelbauer <pirkelbauer2@llnl.gov>
2022-02-08 09:23:16 -0800

    (Ada) support if expressions
    if expressions are translated to conditional expressions
    (SgConditionalExp).
    
    To represent (if elsif else) expressions, nested conditional expressions
    are used on the false branch. By convention, if the nested conditional
    expressions needs a parenthesis then it is unparsed as separate
    conditional (if else (if else)), if the nested conditional does not
    require parenthesis, the unparser uses the elsif (if elsif /* nested
    conditional*/ else).
    
    This logic is implemented in SageInterface::ada::flattenIfExpressions.
    
    + translate unknown "standard" names to the standard package.
    
    RC-1171

--- tests/nonsmoke/functional/CompileTests/experimental_ada_tests/tests/ifexpr.adb
+++ tests/nonsmoke/functional/CompileTests/experimental_ada_tests/tests/ifexpr.adb
@@ -1,6 +1,6 @@
 procedure ifexpr is
   x : integer := 0;
---  y : integer := (if x > 1 then 2 else (if x = -1 then -2 else -3));
+  y : integer := (if x > 1 then 2 else (if x = -1 then -2 else -3));
 
   procedure consumer(a, b : in integer := 3) is
   begin
@@ -8,6 +8,6 @@ procedure ifexpr is
   end consumer;
 begin
   consumer(if x > 4 then 5 else 6);
---  consumer(x, (if x > 7 then 8 else 9));
---  consumer(b => (if x > -4 then -5 elsif x > -6 then -7 else -8));
+  consumer(x, (if x > 7 then 8 else 9));
+  consumer(b => (if x > -4 then -5 elsif x > -6 then -7 else -8));
 end ifexpr;
