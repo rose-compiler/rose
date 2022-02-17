586800421f730bea735c8685cfa34f6cc0d55a39
Robb Matzke <matzke1@llnl.gov>
2022-02-17 13:56:34 -0500

    (Binary Analysis) SgAsmInterpretation has RegisterDictionary
    * This is a bug fix. The SgAsmInterpretation's "registers" property
      was sometimes null.
    
    RPM-77

---
+++ tests/nonsmoke/functional/BinaryAnalysis/testInterpRegs.C
@@ -0,0 +1,20 @@
+static const char *purpose = "check that interpretation's register dictionary is set";
+static const char *description =
+    "Test that calls Rose::BinaryAnalysis::Partitioner2::Engine::frontend and then checks that the "
+    "return value has a SgAsmInterpretation parent whose registers property is non-null.";
+
+#include <rose.h>                                       // must be first
+#include <Rose/BinaryAnalysis/Partitioner2/Engine.h>
+
+int main(int argc, char *argv[]) {
+    ROSE_INITIALIZE;
+
+    SgAsmBlock *gblock = Rose::BinaryAnalysis::Partitioner2::Engine().frontend(argc, argv, purpose, description);
+    ASSERT_always_not_null2(gblock, "Engine::frontend didn't return a SgAsmBlock pointer");
+
+    SgAsmInterpretation *interp = SageInterface::getEnclosingNode<SgAsmInterpretation>(gblock);
+    ASSERT_always_not_null2(interp, "The gblock doesn't have SgAsmInterpretation as an ancestor");
+
+    const Rose::BinaryAnalysis::RegisterDictionary *registerDictionary = interp->get_registers();
+    ASSERT_always_not_null2(registerDictionary, "The SgAsmInterpretation::registers property should not be null");
+}
