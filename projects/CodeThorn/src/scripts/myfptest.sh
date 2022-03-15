67b5986169cdf46cf7cc2317f89b6aea7535bbff
Markus Schordan <schordan1@llnl.gov>
2022-03-14 23:28:38 -0700

    (codethorn) added fp test
    CODETHORN-70

---
+++ projects/CodeThorn/src/scripts/myfptest.sh
@@ -0,0 +1,1 @@
+codethorn tests/DOM058_functionpointers.C --interpreter-mode=1 --abstraction-mode=1 --precision=2 --solver=18 --exploration-mode=topologic-sort --context-sensitive --call-string-length=10 --icfg=icfg.dot --icfg-pass-through-labels=1 --pt-opt=1 
