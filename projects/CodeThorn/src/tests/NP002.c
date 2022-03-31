b088c5b3fc62aab858465d3f11e06e75935e5f07
Markus Schordan <schordan1@llnl.gov>
2022-03-30 18:24:46 -0700

    (codethorn) added NP001, NP002 tests
    CODETHORN-70

---
+++ projects/CodeThorn/src/tests/NP002.c
@@ -0,0 +1,15 @@
+int BLK[16];
+void FOO(int SUB, int CNT, int *DST_PTR) {
+  int ii;
+  for(ii=0;ii<CNT;++ii) {
+    DST_PTR[ii]=0;
+  }
+  BLK[SUB] = 1;
+  return;
+}
+
+int main() {
+  int DST[16];
+  FOO(0,2,DST);
+  return 0;
+}
