08c332b272a7d694b8434d64076d8f46ced51918
Markus Schordan <schordan1@llnl.gov>
2022-04-29 19:26:40 -0700

    (codethorn) v1.13.65
    * fixed ExprStatement bug for special case of pointer-deref
      in unused expression
    
    CODETHORN-70

--- projects/CodeThorn/src/CodeThornLib.C
+++ projects/CodeThorn/src/CodeThornLib.C
@@ -94,7 +94,7 @@ using namespace Sawyer::Message;
 
 using namespace Sawyer::Message;
 
-static std::string CodeThornLibraryVersion="1.13.64";
+static std::string CodeThornLibraryVersion="1.13.65";
 
 // handler for generating backtrace
 void codethornBackTraceHandler(int sig) {
