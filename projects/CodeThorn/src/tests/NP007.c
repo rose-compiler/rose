742f74f3a184435f4d86bdb03ea4692d697ba530
Markus Schordan <schordan1@llnl.gov>
2022-05-02 12:29:07 -0700

    (codethorn) added NP007 tests
    * added NONP007, NP007 and included it in make check-np check-no-np
    
    RC-1314

---
+++ projects/CodeThorn/src/tests/NP007.c
@@ -0,0 +1,20 @@
+struct S3 {
+  int c;
+  int *p;
+};
+
+struct S2 {
+  int b;
+  struct S3 s3;
+};
+
+struct S1 {
+  int a;
+  struct S2 s2;
+};
+
+int main() {
+  struct S1 s1;
+  s1.s2.s3.p=0;
+  *s1.s2.s3.p;
+}
