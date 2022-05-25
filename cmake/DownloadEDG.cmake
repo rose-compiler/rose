c2b2d56d5f50f0ed739ff1306dfce556dd58cff2
Milan Hauth <milahu@gmail.com>
2022-05-25 12:29:57 +0200

    fix cmake: dont check gcc version

--- cmake/DownloadEDG.cmake
+++ cmake/DownloadEDG.cmake
@@ -19,43 +19,12 @@ if(platform STREQUAL "")
 endif()
 
 # Detect compiler by asking GCC what version it is
-set(compiler "")
-set(min_supported "4.4")
-set(max_supported "5.2")
 execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion
                 OUTPUT_VARIABLE GCC_version)
 # strip patch version; we only care about major & minor
 string(REGEX MATCH "^([0-9]+\\.[0-9]+)" GCC_version ${GCC_version})
+set(compiler "gnu-${GCC_version}")
 
-if(GCC_version VERSION_GREATER max_supported OR
-   GCC_version VERSION_LESS min_supported)
-  message(FATAL_ERROR
-    "ROSE only supports GCC versions ${min_supported} to ${max_supported}.\n"
-    "<gcc -dumpversion> reported ${GCC_version}")
-endif()
-if(GCC_version VERSION_EQUAL 5.2)
-  set(compiler "gnu-5.2")
-elseif(GCC_version VERSION_EQUAL 5.1)
-  set(compiler "gnu-5.1")
-elseif(GCC_version VERSION_EQUAL 5.0)
-  set(compiler "gnu-5.0")
-elseif(GCC_version VERSION_EQUAL 4.9)
-  set(compiler "gnu-4.9")
-elseif(GCC_version VERSION_EQUAL 4.8)
-  set(compiler "gnu-4.8")
-elseif(GCC_version VERSION_EQUAL 4.7)
-  set(compiler "gnu-4.7")
-elseif(GCC_version VERSION_EQUAL 4.6)
-  set(compiler "gnu-4.6")
-elseif(GCC_version VERSION_EQUAL 4.5)
-  set(compiler "gnu-4.5")
-elseif(GCC_version VERSION_EQUAL 4.4)
-  set(compiler "gnu-4.4")
-endif()
-if(compiler STREQUAL "")
-  message(FATAL_ERROR
-    "Unable to detect a supported compiler when attempting to download EDG binary tarball")
-endif()
 
 # Get binary compatibility signature
 set(EDG_SIG_OUTPUT
