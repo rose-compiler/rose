22c11647ef2e4a5d232540a9d921863c4bfec082
Lin, Pei-Hung <lin32@llnl.gov>
2022-06-01 10:46:53 -0700

    (ClangFE) Added and organized supporting functions
    RC-1333

--- src/frontend/CxxFrontend/Clang/clang-to-rose-support.hpp
+++ src/frontend/CxxFrontend/Clang/clang-to-rose-support.hpp
@@ -1,8 +1,6 @@
 #ifndef _CLANG_TO_ROSE_SUPPORT_HPP_
 # define _CLANG_TO_ROSE_SUPPORT_HPP_
 
-SgAsmOp::asm_operand_modifier_enum get_sgAsmOperandModifier(std::string modifier);
-SgAsmOp::asm_operand_constraint_enum get_sgAsmOperandConstraint(std::string constraint);
-SgInitializedName::asm_register_name_enum get_sgAsmRegister(std::string reg);
+#include "clang-frontend-private.hpp"
 
 #endif /* _CLANG_TO_ROSE_SUPPORT_HPP_ */
