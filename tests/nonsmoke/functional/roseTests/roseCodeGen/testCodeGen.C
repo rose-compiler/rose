e354de1bcf879fc397096099f0b9fc0ee5a670d7
Tristan Vanderbruggen <vanderbrugge1@llnl.gov>
2022-07-13 12:45:24 -0700

    (Rose::CodeGen) added support for file building
    RC-1362

--- tests/nonsmoke/functional/roseTests/roseCodeGen/testCodeGen.C
+++ tests/nonsmoke/functional/roseTests/roseCodeGen/testCodeGen.C
@@ -21,6 +21,9 @@ template <>
 std::string const API<my_api_t>::name{"MyAPI"};
 
 template <>
+std::string const API<my_api_t>::cache{""};
+
+template <>
 std::vector<std::string> const API<my_api_t>::paths{MY_HEADER_PATH};
 
 template <>
@@ -60,18 +63,63 @@ struct my_factory_t : Rose::CodeGen::Factory<my_factory_t, my_api_t> {
   using Rose::CodeGen::Factory<my_factory_t, my_api_t>::Factory;
 };
 
+void build_test_files(
+  Rose::CodeGen::Driver & driver,
+  SgGlobal * & header_global,
+  SgGlobal * & source_global,
+  SgGlobal * & main_global,
+  std::string lib_header
+) {
+  auto header_id = driver.create("test.hxx");
+  driver.setUnparsedFile(header_id);
+  header_global = driver.getGlobalScope(header_id);
+  driver.addExternalHeader(header_id, lib_header, false);
+
+  auto source_id = driver.create("test.cxx");
+  driver.setUnparsedFile(source_id);
+  driver.setCompiledFile(source_id);
+  source_global = driver.getGlobalScope(source_id);
+  driver.addExternalHeader(source_id, "test.hxx", false);
+
+  auto main_id = driver.create("main.cxx");
+  driver.setUnparsedFile(main_id);
+  driver.setCompiledFile(main_id);
+  main_global = driver.getGlobalScope(main_id);
+  driver.addExternalHeader(main_id, "test.hxx", false);
+}
+
 int main( int argc, char * argv[] ) {
   ROSE_INITIALIZE;
   std::vector<std::string> args(argv, argv+argc);
   Rose::CodeGen::Driver driver(args);
 
+  // Constructing the factory loads the API
   my_factory_t my_factory(driver);
-
   my_factory.api.display(std::cout);
+
+  // Prepare some files where to test the code generation
+  SgGlobal * header_global;
+  SgGlobal * source_global;
+  SgGlobal * main_global;
+  build_test_files(driver, header_global, source_global, main_global, "my_lib.h");
+
+  // Adding a main function to run-test the generated code
+  // TODO argc/argv
+  SgFunctionDeclaration * main_decl = SageBuilder::buildDefiningFunctionDeclaration(
+      "main", SageBuilder::buildIntType(), SageBuilder::buildFunctionParameterList(), source_global
+  );
+  SgFunctionDefinition * main_defn = main_decl->get_definition();
+  ROSE_ASSERT(main_defn);
+
+  // Test 0: added a reference in `main`
 
   auto res = my_factory.reference<Rose::CodeGen::Object::a_variable>(&my_api_t::my_variable);
   std::cout << "res = " << std::hex << res << " : " << (res ? res->class_name() : "") << std::endl;
+  main_defn->append_statement(SageBuilder::buildExprStatement(res));
 
+  // Done: add `main` to its file at last
+
+  main_global->append_statement(main_decl);
   return backend(driver.project);
 }
 
