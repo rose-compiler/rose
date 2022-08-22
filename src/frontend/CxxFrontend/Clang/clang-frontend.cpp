82661a5cb679922a345fbcd20916e8773839c693
Tristan Vanderbruggen <vanderbrugge1@llnl.gov>
2022-08-22 16:28:28 -0700

    (clang) remove the preinclude builtins file
    RC-1380

--- src/frontend/CxxFrontend/Clang/clang-frontend.cpp
+++ src/frontend/CxxFrontend/Clang/clang-frontend.cpp
@@ -157,20 +157,20 @@ int clang_main(int argc, char ** argv, SgSourceFile& sageFile) {
     switch (language) {
         case ClangToSageTranslator::C:
             inc_dirs_list.insert(inc_dirs_list.begin(), c_config_include_dirs.begin(), c_config_include_dirs.end());
-            inc_list.push_back("clang-builtin-c.h");
+//            inc_list.push_back("clang-builtin-c.h");
             break;
         case ClangToSageTranslator::CPLUSPLUS:
             inc_dirs_list.insert(inc_dirs_list.begin(), cxx_config_include_dirs.begin(), cxx_config_include_dirs.end());
-            inc_list.push_back("clang-builtin-cpp.hpp");
+//            inc_list.push_back("clang-builtin-cpp.hpp");
             break;
         case ClangToSageTranslator::CUDA:
             inc_dirs_list.insert(inc_dirs_list.begin(), cxx_config_include_dirs.begin(), cxx_config_include_dirs.end());
-            inc_list.push_back("clang-builtin-cuda.hpp");
+//            inc_list.push_back("clang-builtin-cuda.hpp");
             break;
         case ClangToSageTranslator::OPENCL:
          // inc_dirs_list.insert(inc_dirs_list.begin(), c_config_include_dirs.begin(), c_config_include_dirs.end());
          // FIXME get the path right
-            inc_list.push_back("clang-builtin-opencl.h");
+//            inc_list.push_back("clang-builtin-opencl.h");
             break;
         case ClangToSageTranslator::OBJC:
           {
@@ -473,7 +473,6 @@ void ClangToSageTranslator::applySourceRange(SgNode * node, clang::SourceRange s
                     std::string filenameWithoutPath = Rose::StringUtility::stripPathFromFileName(rawFileName);
                     printf ("filenameWithoutPath = %s file = %s \n",filenameWithoutPath.c_str(),file.c_str());
 #endif
-                 // if (file == "clang-builtin-c.h")
                     if (file.find("clang-builtin-c.h") != std::string::npos) 
                        {
 #if 0
