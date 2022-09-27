fdb33247dd5f7da43ac99854ccc4ecab1e371ba0
Liao, Chunhua <liao6@llnl.gov>
2022-09-27 15:04:25 -0700

    (Clang Static Analyzer) fixed bugs in AstFromString.cpp and OmpAttribute.C
    * Dead initializtion
    * Dead assignment
    
    ROSE-ISCP-2022

--- src/frontend/SageIII/OmpAttribute.C
+++ src/frontend/SageIII/OmpAttribute.C
@@ -1520,27 +1520,27 @@ namespace OmpSupport
   std::vector<std::pair<std::string,SgNode* > > 
     OmpAttribute::getVariableList(omp_construct_enum targetConstruct)
     {
-      std::vector<std::pair<std::string,SgNode* > > * result = new std::vector<std::pair<std::string,SgNode* > >; 
       // e_reduction is a collective concept, 
       // There may have multiple reduction clauses for different operations.
       // return all of them. Return special one if e_reduction_operatorX is used
       if (targetConstruct==e_reduction)
       {
-        std::vector<omp_construct_enum> ops = getReductionOperators();
-        std::vector<omp_construct_enum>::iterator iter = ops.begin();
-        for (;iter!=ops.end(); iter++) // for each reduction operator
-        { 
-          omp_construct_enum operation = *iter;
-          assert(isReductionOperator(operation));
-          std::vector<std::pair<std::string,SgNode* > > temp = variable_lists[operation];
-          std::vector<std::pair<std::string,SgNode* > > ::iterator iter2 = temp.begin();
-          for (;iter2!=temp.end(); iter2++)
-            result->push_back(*iter2);
-        }  
-        return *result;
+	std::vector<std::pair<std::string,SgNode* > > * result = new std::vector<std::pair<std::string,SgNode* > >; 
+	std::vector<omp_construct_enum> ops = getReductionOperators();
+	std::vector<omp_construct_enum>::iterator iter = ops.begin();
+	for (;iter!=ops.end(); iter++) // for each reduction operator
+	{ 
+	  omp_construct_enum operation = *iter;
+	  assert(isReductionOperator(operation));
+	  std::vector<std::pair<std::string,SgNode* > > temp = variable_lists[operation];
+	  std::vector<std::pair<std::string,SgNode* > > ::iterator iter2 = temp.begin();
+	  for (;iter2!=temp.end(); iter2++)
+	    result->push_back(*iter2);
+	}  
+	return *result;
       } 
       else
-        return variable_lists[targetConstruct];
+	return variable_lists[targetConstruct];
     }
   //! Check if a variable list is associated with a construct
   bool OmpAttribute::hasVariableList(omp_construct_enum omp_type)
