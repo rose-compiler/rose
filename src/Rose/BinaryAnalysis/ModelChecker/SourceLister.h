e757dc871e91bf01d11f785c0669dba8a10924e6
Robb Matzke <matzke1@llnl.gov>
2022-10-13 15:07:29 -0400

    (Binary Analysis) The model checker is conditionally compiled
    * The Rose::BinaryAnalysis::ModelChecker is only available when ROSE
      is configured with the necessary prerequisites. The CPP macro
      ROSE_ENABLE_MODEL_CHECKER controls this.
    
    RPM-272

--- src/Rose/BinaryAnalysis/ModelChecker/SourceLister.h
+++ src/Rose/BinaryAnalysis/ModelChecker/SourceLister.h
@@ -1,7 +1,7 @@
 #ifndef ROSE_BinaryAnalysis_ModelChecker_SourceLister_H
 #define ROSE_BinaryAnalysis_ModelChecker_SourceLister_H
 #include <featureTests.h>
-#ifdef ROSE_ENABLE_BINARY_ANALYSIS
+#ifdef ROSE_ENABLE_MODEL_CHECKER
 
 #include <Rose/BinaryAnalysis/ModelChecker/Types.h>
 
