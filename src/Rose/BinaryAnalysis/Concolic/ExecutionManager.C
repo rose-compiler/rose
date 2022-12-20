2590a0589d567e86bb9164a9dcfcd9a3a09c3cdc
Robb Matzke <matzke1@llnl.gov>
2022-12-20 12:11:56 -0500

    (Concolic) Generalized Concolic::ExecutionManager and removed subclasses
    RPM-305

--- src/Rose/BinaryAnalysis/Concolic/ExecutionManager.C
+++ src/Rose/BinaryAnalysis/Concolic/ExecutionManager.C
@@ -3,39 +3,112 @@
 #include <sage3basic.h>
 #include <Rose/BinaryAnalysis/Concolic/ExecutionManager.h>
 
+#include <Rose/BinaryAnalysis/Concolic/ConcolicExecutor.h>
 #include <Rose/BinaryAnalysis/Concolic/ConcreteExecutor.h>
 #include <Rose/BinaryAnalysis/Concolic/ConcreteResult.h>
 #include <Rose/BinaryAnalysis/Concolic/Database.h>
+#include <Rose/BinaryAnalysis/Concolic/Specimen.h>
 #include <Rose/BinaryAnalysis/Concolic/TestCase.h>
+#include <Rose/BinaryAnalysis/Concolic/TestSuite.h>
+
+using namespace Sawyer::Message::Common;
 
 namespace Rose {
 namespace BinaryAnalysis {
 namespace Concolic {
+
+ExecutionManager::~ExecutionManager() {}
 
 ExecutionManager::ExecutionManager(const Database::Ptr &db)
     : database_(db) {
     ASSERT_not_null(db);
 }
 
-ExecutionManager::~ExecutionManager() {}
+ExecutionManager::ExecutionManager(const Database::Ptr &db, const Yaml::Node &config)
+    : database_(db), config_(config) {}
+
+ExecutionManager::Ptr
+ExecutionManager::instance(const Yaml::Node &config) {
+    ASSERT_require(config);
+    ASSERT_require(config.isMap());
+    if (!config["database"].isScalar())
+        throw Exception("no database specified in YAML file");
+    if (!config["architecture"].isScalar())
+        throw Exception("no architecture specified in YAML file");
+    if (!config["concrete"].isScalar())
+        throw Exception("no concrete executor specified in YAML file");
+    if (config["test-suite"] && !config["test-suite"].isScalar())
+        throw Exception("test-suite must be a name when provided");
+
+    const std::string databaseUrl = config["database"].as<std::string>();
+    const auto db = Database::instance(databaseUrl);
+
+    const std::string testSuiteName = config["test-suite"].as<std::string>();
+    if (!testSuiteName.empty()) {
+        TestSuite::Ptr ts = db->findTestSuite(testSuiteName);
+        if (!ts)
+            throw Exception("cannot find test suite \"" + StringUtility::cEscape(testSuiteName) + "\"");
+        db->testSuite(ts);
+    }
+
+    return Ptr(new ExecutionManager(db, config));
+}
+
+ExecutionManager::Ptr
+ExecutionManager::open(const std::string &databaseUrl) {
+    auto db = Database::instance(databaseUrl);
+    return Ptr(new ExecutionManager(db));
+}
+
+ExecutionManager::Ptr
+ExecutionManager::create(const std::string &databaseUrl, const boost::filesystem::path &executableName,
+                         const std::vector<std::string> &arguments, const std::string &testCaseName) {
+    const std::string executableBaseName = executableName.filename().string();
+    auto specimen = Specimen::instance(executableName);
+
+    auto testCase0 = TestCase::instance(specimen);
+    testCase0->name(executableBaseName + " #0");
+
+    auto db = Database::create(databaseUrl, executableBaseName);
+    db->save(testCase0);
+
+    return Ptr(new ExecutionManager(db));
+}
 
 Database::Ptr
 ExecutionManager::database() const {
     return database_;
 }
 
+std::vector<TestSuiteId>
+ExecutionManager::testSuites() const {
+    ASSERT_not_null(database_);
+    return database_->testSuites();
+}
+
+TestSuite::Ptr
+ExecutionManager::testSuite() const {
+    ASSERT_not_null(database_);
+    return database_->testSuite();
+}
+
+void
+ExecutionManager::testSuite(const TestSuite::Ptr &ts) {
+    ASSERT_not_null(database_);
+    database_->testSuite(ts);
+}
+
 std::vector<TestCaseId>
 ExecutionManager::pendingConcreteResults(size_t n) {
-  return database_->needConcreteTesting(n);
+    ASSERT_not_null(database_);
+    return database_->needConcreteTesting(n);
 }
 
 TestCaseId
 ExecutionManager::pendingConcreteResult() {
-  std::vector<TestCaseId> res = database_->needConcreteTesting(1);
-  
-  if (0 == res.size()) return TestCaseId();
-  
-  return res.front();
+    for (auto tcid: pendingConcreteResults(1))
+        return tcid;
+    return {};
 }
 
 void
@@ -46,29 +119,67 @@ ExecutionManager::insertConcreteResults(const TestCase::Ptr &testCase, const Con
 
 std::vector<TestCaseId>
 ExecutionManager::pendingConcolicResults(size_t n) {
-  return database_->needConcolicTesting(n);
+    ASSERT_not_null(database_);
+    return database_->needConcolicTesting(n);
 }
 
 TestCaseId
 ExecutionManager::pendingConcolicResult() {
-  std::vector<TestCaseId> res = database_->needConcolicTesting(1);
-  
-  if (0 == res.size()) return TestCaseId();
-  
-  return res.front();
+    for (auto tcid: pendingConcolicResults(1))
+        return tcid;
+    return {};
 }
 
 void
 ExecutionManager::insertConcolicResults(const TestCase::Ptr& original, const std::vector<TestCase::Ptr> &newCases) {
-  original->concolicResult(1);
-  database_->id(original, Update::YES);
-  for (const TestCase::Ptr &tc: newCases)
-      database_->save(tc);
+    ASSERT_not_null(database_);
+    original->concolicResult(1);
+    database_->id(original, Update::YES);
+    for (const TestCase::Ptr &tc: newCases)
+        database_->save(tc);
 }
 
 bool
 ExecutionManager::isFinished() const {
-    return database_->hasUntested();
+    ASSERT_not_null(database_);
+    return !database_->hasUntested();
+}
+
+void
+ExecutionManager::run() {
+    ASSERT_not_null(database_);
+    ASSERT_not_null(config_);
+    Sawyer::Message::Stream debug(mlog[DEBUG]);
+
+    const std::string architectureName = config_["architecture"].as<std::string>();
+    const std::string concreteName = architectureName + "::" + config_["concrete"].as<std::string>();
+    SAWYER_MESG(debug) <<"execution manager running\n"
+                       <<"  architecture = \"" <<StringUtility::cEscape(architectureName) <<"\"\n"
+                       <<"  concrete = \"" <<StringUtility::cEscape(concreteName) <<"\"\n";
+
+    while (!isFinished()) {
+        // Run as many test cases concretely as possible.
+        while (TestCaseId testCaseId = pendingConcreteResult()) {
+            TestCase::Ptr testCase = database()->object(testCaseId);
+            auto concreteExecutor = ConcreteExecutor::forge(database_, concreteName);
+            if (!concreteExecutor)
+                throw Exception("cannot instantiate concrete executor \"" + StringUtility::cEscape(concreteName) + "\"");
+            auto concreteResult = concreteExecutor->execute(testCase);
+            ASSERT_not_null(concreteResult);
+            insertConcreteResults(testCase, concreteResult);
+        }
+
+        // Now that all the test cases have run concretely, run a few of the "best" ones concolically.  The "best" is defined
+        // either by the ranks returned from the concrete executor, or by this class overriding pendingConcolicResult (which we
+        // haven't done).
+        for (TestCaseId testCaseId: pendingConcolicResults(10 /*arbitrary*/)) {
+            TestCase::Ptr testCase = database()->object(testCaseId);
+            auto concolicExecutor = ConcolicExecutor::instance();
+            concolicExecutor->configureExecution(database_, testCase, architectureName);
+            std::vector<TestCase::Ptr> newTestCases = concolicExecutor->execute();
+            insertConcolicResults(testCase, newTestCases);
+        }
+    }
 }
 
 } // namespace
--- src/Rose/BinaryAnalysis/Concolic/ExecutionManager.h
+++ src/Rose/BinaryAnalysis/Concolic/ExecutionManager.h
@@ -4,6 +4,8 @@
 #ifdef ROSE_ENABLE_CONCOLIC_TESTING
 #include <Rose/BinaryAnalysis/Concolic/BasicTypes.h>
 
+#include <Rose/Yaml.h>
+
 #include <boost/noncopyable.hpp>
 #include <Sawyer/SharedObject.h>
 #include <vector>
@@ -17,25 +19,109 @@ namespace Concolic {
  *  An @ref ExecutionManager is responsible for running an entire test suite either starting from scratch or restarting from a
  *  previous state that was saved in a database. */
 class ExecutionManager: boost::noncopyable, public Sawyer::SharedObject {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Types
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 public:
     /** Reference counting pointer to an @ref ExecutionManager. */
-    typedef Sawyer::SharedPointer<ExecutionManager> Ptr;
+    using Ptr = Sawyer::SharedPointer<ExecutionManager>;
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Data members
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 private:
     DatabasePtr database_;
+    const Yaml::Node config_;
 
-protected:
-    // Subclasses should implement allocating constructors
-    explicit ExecutionManager(const DatabasePtr &db);
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Construction and destruction
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 public:
     virtual ~ExecutionManager();
+protected:
+    explicit ExecutionManager(const DatabasePtr&);
+    ExecutionManager(const DatabasePtr&, const Yaml::Node&);
 
+public:
+    /** Configure a manager from a YAML file.
+     *
+     *  This reads and parses the specified YAML file and creates a new manager based on the configuration in that file.  The
+     *  file should have a top-level map with the following keys:
+     *
+     *  @li "database" is the URL for the database that will be opened. The database must exist and be initialized already.
+     *
+     *  @li "test-suite" is the optional name of the test suite for the test campaign being run. If it is missing then the most
+     *  recently created test suite is used.
+     *
+     *  @li "architecture" describes what architecture is being tested, and is one of the names of the factories returned by
+     *  the @ref Concolic::Architecture::registeredFactories method.
+     *
+     *  @li "concrete" is the base name of the concrete executor. The full name is formed by concatenating the architecture
+     *  name, two colons ("::") and the concrete name and must result in one of the names of the concrete executors returned by
+     *  the @ref Concolic::ConcreteExecutor::registeredFactories method.
+     *
+     *  @li "shared-memory" is a list of YAML maps describing each shared memory region and its behavior. At a minimum, each
+     *  shared memory YAML map must contain a "driver" that names the class of object handling the shared memory, an "address"
+     *  to desribe the lowest address in this shared memory region, and the "size" of the region measured in bytes. The YAML
+     *  map is passed to the initializer for the object that handles the memory.
+     *
+     *  A reference to the YAML configuration tree is stored in the returned object. The existing database is opened and will
+     *  be closed by the destructor. A particular test suite within the database is active. */
+    static Ptr instance(const Yaml::Node&);
+
+    /** Allocating constructor to open an existing database.
+     *
+     *  The specified database, which must exist and be initialized, is opened. The database will be closed by the destructor.
+     *
+     *  If the database contains more than one test suite, then the latest created test suite is opened. See also, @ref
+     *  testSuites, @ref testSuite.
+     *
+     *  Throws an @ref Exception if the database cannot be opened. */
+    static Ptr open(const std::string &databaseUrl);
+
+    /** Allocating constructor to create a new database.
+     *
+     *  The specified database is initialized with a specimen and an initial test case. For SQLite databases, the database file
+     *  is created, removing any previous file with the same name. For PostgreSQL databases, the database is assumed to exit in
+     *  an empty state and all necessary tables are created. The database will be closed by the destructor.
+     *
+     *  If a non-empty test suite name is specified then it will be used as the name for the test suite. Otherwise a test suite
+     *  will be created whose name is based on the executable name.
+     *
+     *  An @ref Exception is thrown for any errors. */
+    static Ptr create(const std::string &databaseUrl, const boost::filesystem::path &executableName,
+                      const std::vector<std::string> &arguments, const std::string &testSuiteName = "");
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Properties
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public:
     /** Property: Database.
      *
      *  The database used by this manager.  The database is set in the constructor and cannot be changed later. */
     DatabasePtr database() const;
 
+    /** Property: List of available test suites.
+     *
+     *  Every database has one or more test suites. A test suite controls which specimens and test cases compose a testing
+     *  campaign. Users can organize databases so each database contains a single test suite, or a single database contains
+     *  many test suites. The former method is often used with SQLite databases (which are just files) while the latter is
+     *  sometimes used with PostgreSQL databases. */
+    std::vector<TestSuiteId> testSuites() const;
+
+    /** Property: Current test suite.
+     *
+     *  The test suite being used for the current test campaign.
+     *
+     * @{ */
+    TestSuitePtr testSuite() const;
+    void testSuite(const TestSuitePtr&);
+    /** @} */
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Actions
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+public:
     /** Next test case for concrete execution.
      *
      *  Returns up to @p n (default unlimited) test cases that need to be run concretely. A test case needs to be run
@@ -79,7 +165,7 @@ public:
      *
      *  Runs concrete and concolic executors until the application is interrupted or there's nothing left to do. Subclasses
      *  will likely reimplement this method in order to do parallel processing, limit execution time, etc. */
-    virtual void run() = 0;
+    virtual void run();
 };
 
 } // namespace
