7e2e85a43c74477f93455ad8606e1c09aa5081d8
Robb Matzke <matzke1@llnl.gov>
2023-02-27 14:36:56 -0500

    (Binary Analysis) Model checker handles global variables
    RPM-328

--- src/Rose/BinaryAnalysis/ModelChecker/OutOfBoundsTag.C
+++ src/Rose/BinaryAnalysis/ModelChecker/OutOfBoundsTag.C
@@ -13,23 +13,18 @@ namespace BinaryAnalysis {
 namespace ModelChecker {
 
 OutOfBoundsTag::OutOfBoundsTag(size_t nodeStep, TestMode tm, IoMode io, SgAsmInstruction *insn, const BS::SValue::Ptr &addr,
-                               const Variables::StackVariable &intendedVariable, const AddressInterval &intendedVariableLocation,
-                               const Variables::StackVariable &accessedVariable, const AddressInterval &accessedVariableLocation)
+                               const FoundVariable &intendedVariable, const FoundVariable &accessedVariable)
     : Tag(nodeStep), testMode_(tm), ioMode_(io), insn_(insn), addr_(addr), intendedVariable_(intendedVariable),
-      intendedVariableLocation_(intendedVariableLocation), accessedVariable_(accessedVariable),
-      accessedVariableLocation_(accessedVariableLocation) {}
+      accessedVariable_(accessedVariable) {}
 
 OutOfBoundsTag::~OutOfBoundsTag() {}
 
 OutOfBoundsTag::Ptr
 OutOfBoundsTag::instance(size_t nodeStep, TestMode tm, IoMode io, SgAsmInstruction *insn, const BS::SValue::Ptr &addr,
-                         const Variables::StackVariable &intendedVariable, const AddressInterval &intendedVariableLocation,
-                         const Variables::StackVariable &accessedVariable, const AddressInterval &accessedVariableLocation) {
+                         const FoundVariable &intendedVariable, const FoundVariable &accessedVariable) {
     ASSERT_forbid(TestMode::OFF == tm);
     ASSERT_not_null(addr);
-    return Ptr(new OutOfBoundsTag(nodeStep, tm, io, insn, addr,
-                                  intendedVariable, intendedVariableLocation,
-                                  accessedVariable, accessedVariableLocation));
+    return Ptr(new OutOfBoundsTag(nodeStep, tm, io, insn, addr, intendedVariable, accessedVariable));
 }
 
 std::string
@@ -89,17 +84,15 @@ OutOfBoundsTag::print(std::ostream &out, const std::string &prefix) const {
 
     out <<prefix <<"  attempted " <<toFrom <<" address " <<*addr_ <<"\n";
 
-    if (intendedVariableLocation_) {
-        out <<prefix <<"  intended to access " <<intendedVariable_
-            <<" at " <<StringUtility::addrToString(intendedVariableLocation_);
+    if (intendedVariable_) {
+        out <<prefix <<"  intended to access " <<intendedVariable_;
         if (auto function = intendedVariable_.function())
             out <<" in " <<function->printableName();
         out <<"\n";
     }
 
-    if (accessedVariableLocation_) {
-        out <<prefix <<"  actually accessed " <<accessedVariable_
-            <<" at " <<StringUtility::addrToString(accessedVariableLocation_);
+    if (accessedVariable_) {
+        out <<prefix <<"  actually accessed " <<accessedVariable_;
         if (auto function = accessedVariable_.function())
             out <<" in " <<function->printableName();
         out <<"\n";
@@ -137,18 +130,18 @@ OutOfBoundsTag::toYaml(std::ostream &out, const std::string &prefix1) const {
 
     out <<prefix <<"memory-address: " <<StringUtility::yamlEscape(boost::lexical_cast<std::string>(*addr_)) <<"\n";
 
-    if (intendedVariableLocation_) {
+    if (intendedVariable_) {
         out <<prefix <<"intended:\n";
         out <<prefix <<"  variable: " <<StringUtility::yamlEscape(intendedVariable_.toString()) <<"\n";
-        out <<prefix <<"  location: " <<StringUtility::yamlEscape(StringUtility::addrToString(intendedVariableLocation_)) <<"\n";
+        out <<prefix <<"  location: " <<StringUtility::yamlEscape(StringUtility::addrToString(intendedVariable_.where())) <<"\n";
         if (auto function = intendedVariable_.function())
             out <<prefix <<"  function: " <<StringUtility::yamlEscape(function->printableName()) <<"\n";
     }
 
-    if (accessedVariableLocation_) {
+    if (accessedVariable_) {
         out <<prefix <<"actual:\n";
         out <<prefix <<"  variable: " <<StringUtility::yamlEscape(accessedVariable_.toString()) <<"\n";
-        out <<prefix <<"  location: " <<StringUtility::yamlEscape(StringUtility::addrToString(accessedVariableLocation_)) <<"\n";
+        out <<prefix <<"  location: " <<StringUtility::yamlEscape(StringUtility::addrToString(accessedVariable_.where())) <<"\n";
         if (auto function = accessedVariable_.function())
             out <<prefix <<"  function: " <<StringUtility::yamlEscape(function->printableName()) <<"\n";
     }
