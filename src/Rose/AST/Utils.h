c76bf688ca0bbe99d63371830ed03a441ba3a898
Robb Matzke <matzke1@llnl.gov>
2023-04-13 12:17:56 -0400

    (AST) Added functions to check and repair parent pointers
    * Rose::AST::checkParentPointers (sic) returns true if a subtree's
      parents are all correct, false otherwise. An optional side effect
      is to emit detailed diagnostics when a problem is found.
    
    * Rose::AST::repairParentPointers (sic) adjusts all parent pointers
      of a subtree so they point to the parent node in the tree.
    
    * These functions are used during AST serialization. Non-tree pointers
      from one subtree to another are still not serialized correctly because
      if the target is a tree with parents the parent pointers will be null
      after deserializing. The repairParentPointers won't be able to
      repair them because cross-tree edges aren't traversed.
    
    * The incorrect spelling of `AST` and `Utils` as C++ symbols is not
      corrected by this commit. They should be `Ast` and `Utility` per
      ROSE policy and will be changed in a future commit.
    
    RPM-383
    RPM-382

--- src/Rose/AST/Utils.h
+++ src/Rose/AST/Utils.h
@@ -1,16 +1,16 @@
 #ifndef ROSE_AST_Utils_H
 #define ROSE_AST_Utils_H
 
+#include <iostream>
 #include <map>
+#include <ostream>
 
 class SgNode;
 
 namespace Rose {
 namespace AST {
 
-/**
- * \brief Collection of utility functions to operate on the AST.
- */
+/** Collection of utility functions to operate on the AST. */
 namespace Utils {
 
 using replacement_map_t = std::map<SgNode *, SgNode *>;
@@ -20,6 +20,33 @@ void edgePointerReplacement(replacement_map_t const &);
 
 //! Traverse the AST `root` looking for the edges in the replacement map. If a match is found the edge is updated.
 void edgePointerReplacement(SgNode * root, replacement_map_t const &);
+
+
+/** Check that all parent pointers in the specified subtree are correct.
+ *
+ *  This function does a traversal of the tree starting at the specified root and checks that each child points to the
+ *  correct parent. If not, then an error message is printed and the function returns false. If you do not want error
+ *  messages, then pass a closed output stream.
+ *
+ *  Safety: This function is not thread safe. */
+bool checkParentPointers(SgNode *root, std::ostream &out = std::cerr);
+
+/** Set parent pointers based on child pointers in a subtree.
+ *
+ *  Traverses a subtree, and makes sure that each child points to its parent. No diagnostics are emitted; if you want diagnostics
+ *  then you should call @ref checkParentPointers instead. I.e., checking and repairing are two separate operations.
+ *
+ *  Caveats:
+ *
+ *  @li If @p root is null, then this function is a no-op.
+ *
+ *  @li The parent pointer for the node pointed to by @p root is not adjusted.
+ *
+ *  @li Parent pointers in other subtrees that might be reachable from non-tree pointers that cross from this subtree to another
+ *  are not adjusted.
+ *
+ *  Safety: This function is not thread safe. */
+void repairParentPointers(SgNode *root);
 
 } } }
 
