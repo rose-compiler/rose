f276fd93e96747d6a2296f01321cf939482ebb26
Robb Matzke <matzke1@llnl.gov>
2023-07-24 13:51:49 -0400

    (JSON) The nholmann "json.hpp" file should be named "json.h"
    * ROSE uses ".h" for header files. The nholmann/README license
      requires the file to be named "json.hpp". Therefore, we add
      "json.h" that includes "json.hpp".
    
    RPM-485

---
+++ src/3rdPartyLibraries/json/nlohmann/json.h
@@ -0,0 +1,3 @@
+// ROSE headers are required to end with *.h but the nlohmann license requires the file to be named "nlohmann/json.hpp". Therefore,
+// we have this trampoline.
+#include <nlohmann/json.hpp>
