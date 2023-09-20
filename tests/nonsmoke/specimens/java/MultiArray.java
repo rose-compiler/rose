cf8000d5b6084fefc0fccefd9dbd2465824c0e78
Craig Rasmussen <rasmussen17@llnl.gov>
2023-09-20 12:08:08 -0700

    (ByteCode JVM) Implement JVM multianewarray instruction
    - Implemented instruction and added test
    - Also added tests for instructions: wide, ifnull, ifnonnull
    
    TT-5

---
+++ tests/nonsmoke/specimens/java/MultiArray.java
@@ -0,0 +1,20 @@
+public class MultiArray {
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][4]; // Creates a 3x4 integer matrix
+
+        // Initializing the elements of the 2D array
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = i * 10 + j;
+            }
+        }
+
+        // Accessing and printing the elements of the 2D array
+        System.out.println("Elements of the matrix:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.println("matrix[" + i + "][" + j + "] = " + matrix[i][j]);
+            }
+        }
+    }
+}
---
+++ tests/nonsmoke/specimens/java/TestNull.java
@@ -0,0 +1,23 @@
+public class TestNull {
+    public static void main(String[] args) {
+        // Declare a reference variable without initializing it
+        String myString = null;
+
+        // Test for null
+        if (myString == null) {
+            System.out.println("myString is null.");
+        } else {
+            System.out.println("myString is not null.");
+        }
+
+        // Initialize the variable with a value
+        myString = "Hello, World!";
+
+        // Test for not null
+        if (myString != null) {
+            System.out.println("myString is not null.");
+        } else {
+            System.out.println("myString is null.");
+        }
+    }
+}
