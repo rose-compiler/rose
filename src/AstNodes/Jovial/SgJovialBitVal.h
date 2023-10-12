1e8022055a671f05d967b8057e291e20d1e115b2
Robb Matzke <matzke1@llnl.gov>
2023-10-12 13:02:29 -0400

    (ROSETTA) Removed empty expression code fragements
    * In preparation for moving SgExpression definitions from ROSETTA to
      Rosebud, all calls to setFunctionPrototype that specified a code
      fragement that didn't generate any code were removed, along with the
      empty code fragements.
    
    * This is part of work to simplify the ROSETTA node definitions as
      much as possible before rewriting them as C++ class definitions.
    
    RPM-427

--- src/AstNodes/Jovial/SgJovialBitVal.h
+++ src/AstNodes/Jovial/SgJovialBitVal.h
@@ -7,7 +7,6 @@ class SgJovialBitVal: public SgValueExp {
     // experience most of this stuff doesn't need to be generated--it can be written directly in C++ using virtual functions,
     // template meta programming, and introspection rather than text substitution.
 #pragma rosetta setFunctionSource("SOURCE_EMPTY_POST_CONSTRUCTION_INITIALIZATION", "../Grammar/Expression.code")
-#pragma rosetta setFunctionPrototype("HEADER_JOVIAL_BIT_VALUE_EXPRESSION", "../Grammar/Expression.code")
 #pragma rosetta setFunctionSource("SOURCE_GET_TYPE_GENERIC","../Grammar/Expression.code");
 #pragma rosetta editSubstitute("GENERIC_TYPE", "SgJovialBitType");
 };
