025aeae9652adc2698eefb0332dab703ef7d96c1
Robb Matzke <matzke1@llnl.gov>
2024-01-16 15:31:31 -0500

    (Documentation) Added Rose::BinaryAnalysis::Disassembler::Mips
    RPM-622

--- src/Rose/BinaryAnalysis/Disassembler/Mips.h
+++ src/Rose/BinaryAnalysis/Disassembler/Mips.h
@@ -15,6 +15,25 @@ namespace Rose {
 namespace BinaryAnalysis {
 namespace Disassembler {
 
+/** MIPS32 instruction decoder.
+ *
+ *  MIPS (Microprocessor without Interlocked Pipelined Stages) is a family of reduced instruction set computer (RISC) instruction
+ *  set architectures (ISA) developed by MIPS Computer Systems, now MIPS Technologies, based in the United States.
+ *
+ *  There are multiple versions of MIPS: including MIPS I, II, III, IV, and V; as well as five releases of MIPS32/64 (for 32- and
+ *  64-bit implementations, respectively). The early MIPS architectures were 32-bit; 64-bit versions were developed later. As of
+ *  April 2017, the current version of MIPS is MIPS32/64 Release 6. MIPS32/64 primarily differs from MIPS I–V by defining the
+ *  privileged kernel mode System Control Coprocessor in addition to the user mode architecture.
+ *
+ *  The MIPS architecture has several optional extensions. MIPS-3D which is a simple set of floating-point SIMD instructions
+ *  dedicated to common 3D tasks, MDMX (MaDMaX) which is a more extensive integer SIMD instruction set using the 64-bit
+ *  floating-point registers, MIPS16e which adds compression to the instruction stream to make programs take up less room, and MIPS
+ *  MT, which adds multithreading capability.
+ *
+ *  The first release of MIPS32, based on MIPS II, added conditional moves, prefetch instructions, and other features from the R4000
+ *  and R5000 families of 64-bit processors. The MUL and MADD (multiply-add) instructions, previously available in some
+ *  implementations, were added to the MIPS32 specifications, as were cache control instructions. For the purpose of cache control,
+ *  both SYNC and SYNCI instructions were prepared. */
 class Mips: public Base {
 public:
     /** Reference counting pointer. */
