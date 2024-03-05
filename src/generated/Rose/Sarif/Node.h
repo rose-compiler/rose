cd19dcf693ace03fe3164a85d3145a4205805f6f
Robb Matzke <matzke1@llnl.gov>
2024-03-04 21:31:37 -0500

    (Sarif) Regenerated Rose::Sarif AST nodes for boost serialization
    RC2-27

--- src/generated/Rose/Sarif/Node.h
+++ src/generated/Rose/Sarif/Node.h
@@ -22,6 +22,7 @@
 #include <boost/serialization/base_object.hpp>
 #include <boost/serialization/map.hpp>
 #include <boost/serialization/set.hpp>
+#include <boost/serialization/shared_ptr.hpp>
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/version.hpp>
 #endif
