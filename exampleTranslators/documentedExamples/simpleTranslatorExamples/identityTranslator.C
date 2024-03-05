ad349e5225c9bf8051ba64bb95b53e0da0c1fc4c
Leek, Jim <leek2@llnl.gov>
2024-03-05 18:43:18 +0000

    (Tutorial Fix) Removed AST Consistency checks from identityTranslator

--- exampleTranslators/documentedExamples/simpleTranslatorExamples/identityTranslator.C
+++ exampleTranslators/documentedExamples/simpleTranslatorExamples/identityTranslator.C
@@ -11,9 +11,6 @@ int main( int argc, char * argv[] )
   // Build the AST used by ROSE
      SgProject* project = frontend(argc,argv);
 
-  // Run internal consistency tests on AST
-     AstTests::runAllTests(project);
-
   // Insert your own manipulation of the AST here...
 
   // Generate source code from AST and call the vendor's compiler
--- tutorial/spawn-frontend/identityTranslator.C
+++ tutorial/spawn-frontend/identityTranslator.C
@@ -11,9 +11,6 @@ int main( int argc, char * argv[] )
   // Build the AST used by ROSE
      SgProject* sageProject = spawnFrontend(argc,argv);
 
-  // Run internal consistancy tests on AST
-     AstTests::runAllTests(sageProject);
-
   // Insert your own manipulation of the AST here...
 
   // Generate source code from AST and call the vendor's compiler
