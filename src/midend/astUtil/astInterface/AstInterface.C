20d7da88c66f21729912c5c4134cf8e4cf521a00
Qing  Yi7 <yi7@tux440.llnl.gov>
2024-07-19 09:57:10 -0700

    (AstInterface) Do not treat ClassDeclaration as block.
    Because the declaration may not have a definition body.
    
    RC-1158

--- src/midend/astUtil/astInterface/AstInterface.C
+++ src/midend/astUtil/astInterface/AstInterface.C
@@ -2529,14 +2529,6 @@ bool AstInterface::IsBlock( const AstNodePtr& _n, std::string* blockname, AstNod
         l2 = isSgNamespaceDeclarationStatement(n.get_ptr())->get_definition()->getDeclarationList();
     }
     break;
-  case V_SgClassDeclaration:
-     if (blockname != 0) {
-         *blockname = isSgClassDeclaration(n.get_ptr())->get_name().str();
-     }
-    if (_stmts != 0) {
-        l2 = isSgClassDeclaration(n.get_ptr())->get_definition()->getDeclarationList();
-    }
-    break;
   case V_SgClassDefinition: 
     if (blockname != 0) {
          *blockname = isSgClassDefinition(n.get_ptr())->get_declaration()->get_name().str();
