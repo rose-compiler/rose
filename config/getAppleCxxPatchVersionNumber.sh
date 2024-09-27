7f59b9f84b89d56308d0690e5b9db0a436a2ffe8
Lin, Pei-Hung <lin32@llnl.gov>
2024-09-27 13:26:03 -0700

    (Mac) moved up Apple Clang support to v.16
    rose#312

--- config/getAppleCxxPatchVersionNumber.sh
+++ config/getAppleCxxPatchVersionNumber.sh
@@ -2,4 +2,4 @@
 
 # This shell script logic is isolated into a separate shell script because it is a problem to call it from m4 files in autoconf.
 
-echo `c++ --version | grep -o 'version[\ ]\([1][0-5]\|[7-9]\)\.[0-9]*\.[0-9]*' | grep -o '\([1][0-5]\|[7-9]\)\.[0-9]*\.[0-9]*' | cut -d\. -f3`
+echo `c++ --version | grep -o 'version[\ ]\([1][0-6]\|[7-9]\)\.[0-9]*\.[0-9]*' | grep -o '\([1][0-6]\|[7-9]\)\.[0-9]*\.[0-9]*' | cut -d\. -f3`
