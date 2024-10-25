b15cacb34f0fc06b63a67ef593c3298b5ea8c3d9
Robb Matzke <matzke1@llnl.gov>
2024-10-25 07:52:03 -0400

    (Binary Analysis) Reduce header dependencies
    Issue #355

--- src/Rose/BinaryAnalysis/ControlFlow.C
+++ src/Rose/BinaryAnalysis/ControlFlow.C
@@ -1,29 +1,21 @@
 #include <featureTests.h>
 #ifdef ROSE_ENABLE_BINARY_ANALYSIS
-#include <sage3basic.h>
+#include <sage3basic.h>                                 // needed by the ControlFlow.h header
 #include <Rose/BinaryAnalysis/ControlFlow.h>
 
-#include <boost/graph/depth_first_search.hpp>
+#include <Rose/AST/Traversal.h>
 
 namespace Rose {
 namespace BinaryAnalysis {
 
-/* See header file for documentation. */
 void
-ControlFlow::clear_ast(SgNode *root)
-{
-    struct T1: public AstSimpleProcessing {
-        ControlFlow *analyzer;
-        T1(ControlFlow *analyzer): analyzer(analyzer) {}
-        void visit(SgNode *node) {
-            SgAsmBlock *block = isSgAsmBlock(node);
-            if (block && !analyzer->is_vertex_filtered(block)) {
-                block->get_successors().clear();
-                block->set_successorsComplete(false);
-            }
+ControlFlow::clear_ast(SgNode *root) {
+    AST::Traversal::forwardPre<SgAsmBlock>(root, [this](SgAsmBlock *block) {
+        if (block && !is_vertex_filtered(block)) {
+            block->get_successors().clear();
+            block->set_successorsComplete(false);
         }
-    };
-    T1(this).traverse(root, preorder);
+    });
 }
 
 } // namespace
