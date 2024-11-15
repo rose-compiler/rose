27e2f12209419289a0a08d877c91dfca17f7cd00
Robb Matzke <matzke1@llnl.gov>
2024-11-15 11:14:14 -0500

    (Support) ASSERT_* macros emit a stack trace
    Issue #405

--- src/Sawyer/Assert.C
+++ src/Sawyer/Assert.C
@@ -9,6 +9,11 @@
 #include <Sawyer/Message.h>
 #include <Sawyer/Sawyer.h>
 
+#if BOOST_VERSION >= 106500
+    #define BOOST_STACKTRACE_USE_ADDR2LINE
+    #include <boost/stacktrace.hpp>
+#endif
+
 #include <cstdlib>
 
 namespace Sawyer {
@@ -33,6 +38,43 @@ fail(const char *mesg, const char *expr, const std::string &note, const char *fi
         *Message::assertionStream <<"  " <<expr <<"\n";
     if (!note.empty())
         *Message::assertionStream <<"  " <<note <<"\n";
+
+    // Print a stack trace
+#if BOOST_VERSION >= 106500
+    auto stack = boost::stacktrace::stacktrace();
+    static const size_t nSkip = 1;                      // number of frames to skip
+    static const size_t maxShow = 15;                   // maximum number of frames to show
+    if (stack.size() > nSkip) {
+        size_t frameIdx = 0;
+        *Message::assertionStream <<"  stack:\n";
+        for (boost::stacktrace::frame frame: stack) {
+            if (++frameIdx <= nSkip) {
+                // skip the first frames because they're not interesting. E.g., the first frame is this very function, which is part
+                // of the mechanism handling the failure and thus not of interest to a normal user.
+            } else if (frameIdx - nSkip > maxShow) {
+                *Message::assertionStream <<"    additional stack frames suppressed\n";
+                break;
+            } else {
+                *Message::assertionStream <<"    #" <<(frameIdx - nSkip) <<" at " <<frame.address() <<"\n";
+
+                const std::string name = frame.name();
+                if (!name.empty())
+                    *Message::assertionStream <<"      " <<name <<"\n";
+
+                const std::string file = frame.source_file();
+                if (!file.empty())
+                    *Message::assertionStream <<"      " <<frame.source_file() <<":" <<frame.source_line() <<"\n";
+
+                if ("main" == name)
+                    break;                              // normal users think "main" is the start of their program
+            }
+        }
+    }
+#else
+    *Message::assertionStream <<"  stack: your compiler and Boost versions are too old to show a stack\n";
+#endif
+
+
 
     if (assertFailureHandler)
         assertFailureHandler(mesg, expr, note, filename, linenum, funcname);
