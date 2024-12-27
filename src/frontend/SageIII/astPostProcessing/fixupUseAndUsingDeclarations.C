0a494b7bfc8cdaf3d66e13e491fd74b931cee7e1
Craig Rasmussen <rasmussen17@llnl.gov>
2024-12-26 18:00:41 -0800

    (Engineering) Fix compiler warnings issued by (Apple) clang
    Fixed warnings associated with unused variable(s): node
    
    Issue #444

--- src/frontend/SageIII/astPostProcessing/fixupUseAndUsingDeclarations.C
+++ src/frontend/SageIII/astPostProcessing/fixupUseAndUsingDeclarations.C
@@ -1,30 +1,15 @@
-// tps (01/14/2010) : Switching from rose.h to sage3.
 #include "sage3basic.h"
 #include "fixupUseAndUsingDeclarations.h"
 
-void fixupFortranUseDeclarations (SgNode* node)
-   {
-  // DQ (7/7/2005): Introduce tracking of performance of ROSE.
-     TimingPerformance timer ("Fixup Fortran Use Declarations:");
-
-  // printf ("Fixup the Fortran Use Declarations ... \n");
-
-     FixupFortranUseDeclarations astFixupTraversal;
-
-#if 0
-  // DQ (4/14/2010): Now I see that this is not used!
-
-  // I think the default should be preorder so that the interfaces would be more uniform
-     astFixupTraversal.traverse(node,preorder);
-#endif
-   }
+void
+fixupFortranUseDeclarations(SgNode* /*node*/) {
+    TimingPerformance timer ("Fixup Fortran Use Declarations:");
+    FixupFortranUseDeclarations astFixupTraversal;
+}
 
 void
-FixupFortranUseDeclarations::visit ( SgNode* node )
-   {
-  // DQ (11/24/2007): Output the current IR node for debugging the traversal of the Fortran AST.
-  // printf ("node = %s \n",node->class_name().c_str());
-   }
+FixupFortranUseDeclarations::visit(SgNode* /*node*/) {
+}
 
 
 
