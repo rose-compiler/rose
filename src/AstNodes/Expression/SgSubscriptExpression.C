db73efe8e45a93a5e3561d5516121e70b1993cfa
Topher Johnson <johnson502@llnl.gov>
2025-03-12 09:33:46 -0700

    (cleanup) removed old printf debugging statement
    Issue #521

--- src/AstNodes/Expression/SgSubscriptExpression.C
+++ src/AstNodes/Expression/SgSubscriptExpression.C
@@ -32,22 +32,6 @@ SgSubscriptExpression::get_type() const
        else
         {
           returnType = get_lowerBound()->get_type();
-          if (isUpperBoundNullExpression == true)
-             {
-            // Not a problem since the lower bound was specified
-             }
-            else
-             {
-            // Just make sure that they are the same type, not clear how important this is!
-               SgType* upperBoundType = get_upperBound()->get_type();
-            // ROSE_ASSERT(upperBoundType->variantT() == returnType->variantT());
-               if (upperBoundType->variantT() != returnType->variantT())
-                  {
-                 // These are a different type..., I wonder why
-                    printf ("In SgSubscriptExpression::get_type(): Note that lowerBound = %s and upperBound = %s are different types \n",returnType->class_name().c_str(),upperBoundType->class_name().c_str());
-                    this->get_startOfConstruct()->display("In SgSubscriptExpression::get_type(): lowerBound and upperBound are different types");
-                  }
-             }
         }
 
      return returnType;
