3098f645e758534c38de887449bdc7e423f8ee9b
John Sarracino <sarracino2@llnl.gov>
2025-06-21 11:39:11 -0700

    (CMake) Search for z3folder when finding Z3
    Issue #676

--- cmake/FindZ3.cmake
+++ cmake/FindZ3.cmake
@@ -10,14 +10,15 @@
 #                     * Else require Z3 to exist at specified location and use it
 #
 #  OUTPUTS:
-#    Z3_FOUND      -- Boolean: whether the z3 executable or the Z3 library was found.
-#    Z3_FOUND_EXE  -- Boolean: whether the z3 executable was found
-#    Z3_FOUND_LIB  -- Boolean: whether the Z3 library was found
-#    Z3_EXECUTABLE -- String:  the z3 executable name
-#    Z3_VERSION    -- String:  Z3 version string, such as "4.8.1"
-#    Z3_VERSION_H  -- Boolean: whether the z3_version.h file exists
-#    Z3_LIBRARIES  -- String:  names of libraries necessary to use Z3
-include(CheckIncludeFiles) 
+#    Z3_FOUND       -- Boolean: whether the z3 executable or the Z3 library was found.
+#    Z3_FOUND_EXE   -- Boolean: whether the z3 executable was found
+#    Z3_FOUND_LIB   -- Boolean: whether the Z3 library was found
+#    Z3_EXECUTABLE  -- String:  the z3 executable name
+#    Z3_VERSION     -- String:  Z3 version string, such as "4.8.1"
+#    Z3_VERSION_H   -- Boolean: whether the z3_version.h file exists
+#    Z3_LIBRARIES   -- String:  names of libraries necessary to use Z3
+#    Z3_HEADER_PATH -- String:  location of z3 headers, suitable for including via -I
+include(CheckIncludeFileCXX) 
 macro(find_z3)
   if("${Z3_ROOT}" STREQUAL "no")
     # Do not use Z3, and therefore do not even search for it. Make sure all outputs are cleared to avoid problems with
@@ -47,18 +48,6 @@ macro(find_z3)
       endif()
     endif()
 
-    # Header files. z3_version.h is absent from older versions of Z3, and newer versions don't include it automatically
-    # in z3.h, therefore we have to detect it ourselves and conditionally include it in the ROSE source code.
-    if("${Z3_ROOT}" STREQUAL "")
-      check_include_files("z3_version.h" Z3_VERSION_H)
-    else()
-      include_directories("${Z3_ROOT}/include")
-      check_include_files("${Z3_ROOT}/include/z3_version.h" Z3_VERSION_H)
-    endif()
-    if(Z3_VERSION_H AND NOT Z3_VERSION)
-      message(FATAL_ERROR "Z3 version from z3_version.h not implemented in cmake yet")
-    endif()
-
     # Z3 library.
     if("${Z3_ROOT}" STREQUAL "")
       find_library(Z3_LIBRARY NAMES z3)
@@ -69,6 +58,18 @@ macro(find_z3)
       set(Z3_FOUND TRUE)
       set(Z3_FOUND_LIB TRUE)
       set(Z3_LIBRARIES z3)
+      # Newer versions of z3 add an extra directory of indirection, placing headers at include/z3/*.h,
+      # so we check if include/z3 is a path and include it if so.
+      get_filename_component(Z3_HEADER_PATH ${Z3_LIBRARY} DIRECTORY)
+      set(Z3_HEADER_PATH "${Z3_HEADER_PATH}/../include")
+      get_filename_component(Z3_HEADER_PATH ${Z3_LIBRARY} DIRECTORY)
+      set(Z3_HEADER_PATH "${Z3_HEADER_PATH}/../include")
+      if (EXISTS "${Z3_HEADER_PATH}/z3" AND IS_DIRECTORY "${Z3_HEADER_PATH}/z3")
+        set(Z3_HEADER_PATH "${Z3_HEADER_PATH}/z3")
+      endif()
+
+      include_directories(${Z3_HEADER_PATH})
+      
       if(NOT Z3_VERSION)
 	message(FATAL_ERROR "Z3 version cannot be obtained from the library")
       endif()
@@ -78,7 +79,24 @@ macro(find_z3)
     if((NOT ("${Z3_ROOT}" STREQUAL "")) AND NOT Z3_FOUND)
       message(FATAL_ERROR "Z3 requested by user at '${Z3_ROOT}' but not found")
     endif()
+
+    # Header files. z3_version.h is absent from older versions of Z3, and newer versions don't include it automatically
+    # in z3.h, therefore we have to detect it ourselves and conditionally include it in the ROSE source code.
+    if("${Z3_ROOT}" STREQUAL "")
+      if (Z3_HEADER_PATH)
+        set(CMAKE_REQUIRED_INCLUDES ${Z3_HEADER_PATH})
+      endif()
+      check_include_file_cxx("z3_version.h" Z3_VERSION_H)
+    else()
+      include_directories("${Z3_ROOT}/include")
+      check_include_files("${Z3_ROOT}/include/z3_version.h" Z3_VERSION_H)
+    endif()
+    if(Z3_VERSION_H AND NOT Z3_VERSION)
+      message(FATAL_ERROR "Z3 version from z3_version.h not implemented in cmake yet")
+    endif()
   endif()
+
+    
   
   # Summarize
   if(VERBOSE)
@@ -92,9 +110,10 @@ macro(find_z3)
 	message(STATUS "Z3_EXECUTABLE = '${Z3_EXECUTABLE}'")
       endif()
       if(Z3_FOUND_LIB)
-	message(STATUS "Z3_VERSION_H  = '${Z3_VERSION_H}'")
-	message(STATUS "Z3_LIBRARY    = '${Z3_LIBRARY}'")
-	message(STATUS "Z3_LIBRARIES  = '${Z3_LIBRARIES}'")
+	message(STATUS "Z3_VERSION_H    = '${Z3_VERSION_H}'")
+	message(STATUS "Z3_LIBRARY      = '${Z3_LIBRARY}'")
+	message(STATUS "Z3_LIBRARIES    = '${Z3_LIBRARIES}'")
+  message(STATUS "Z3_HEADER_PATH  = '${Z3_HEADER_PATH}'")
       endif()
     endif()
   endif()
