1a390d584d91cc9e0f8cabcc4e47f5a4d4910261
Robb Matzke <matzke1@llnl.gov>
2025-10-04 13:19:00 -0400

    (CMake) Set GCRYPT_HEADER_PATH and fix rose-config flags
    The FindGcrypt.cmake module now sets GCRYPT_HEADER_PATH to the
    directory containing the GCrypt library headers. This variable is used
    in src/CMakeLists.txt to set rose-config installed cppflags for GCrypt
    header paths, similar to how Z3 is already handled.
    
    Issue #783

--- cmake/FindGcrypt.cmake
+++ cmake/FindGcrypt.cmake
@@ -1,15 +1,17 @@
 # GCrypt detection for ROSE.
 #
 #  INPUTS:
-#    GCRYPT_ROOT       -- GCrypt installation directory or "no" or empty
-#                       * If "no" then do not use GCrypt and do not search for it.
-#                       * If empty use GCrypt if found, no error if not found
-#                       * Else require GCrypt to exist at specified location and use it
+#    GCRYPT_ROOT        -- GCrypt installation directory or "no" or empty
+#                        * If "no" then do not use GCrypt and do not search for it.
+#                        * If empty use GCrypt if found, no error if not found
+#                        * Else require GCrypt to exist at specified location and use it
 #
 #  OUTPUTS:
-#    GCRYPT_FOUND      -- Boolean: whether the GCrypt library was found.
-#    GCRYPT_LIBRARY    -- String: full name of GCrypt library of a string that ends with NOTFOUND
-#    GCRYPT_LIBRARIES  -- String: names of libraries necessary to use GCrypt
+#    GCRYPT_FOUND        -- Boolean: whether the GCrypt library was found.
+#    GCRYPT_LIBRARY      -- String: full name of GCrypt library of a string that ends with NOTFOUND
+#    GCRYPT_LIBRARY_PATH -- String: directory containing the GCrypt library
+#    GCRYPT_LIBRARIES    -- String: names of libraries necessary to use GCrypt
+#    GCRYPT_HEADER_PATH  -- String: location of gcrypt headers, suitable for including via -I
 
 macro(find_gcrypt)
   if("${GCRYPT_ROOT}" STREQUAL "no")
@@ -17,16 +19,11 @@ macro(find_gcrypt)
     # users maybe setting them.
     set(GCRYPT_FOUND FALSE)
     set(GCRYPT_LIBRARY "")
+    set(GCRYPT_LIBRARY_PATH "")
     set(GCRYPT_LIBRARIES "")
+    set(GCRYPT_HEADER_PATH "")
 
   else()
-    # Header files.
-    if("${GCRYPT_ROOT}" STREQUAL "")
-      # no extra include directories necessary
-    else()
-      include_directories("${GCRYPT_ROOT}/include")
-    endif()
-
     # GCrypt library.
     if("${GCRYPT_ROOT}" STREQUAL "")
       find_library(GCRYPT_LIBRARY NAMES gcrypt)
@@ -36,6 +33,9 @@ macro(find_gcrypt)
     if(GCRYPT_LIBRARY)
       set(GCRYPT_FOUND TRUE)
       set(GCRYPT_LIBRARIES gcrypt)
+      get_filename_component(GCRYPT_LIBRARY_PATH "${GCRYPT_LIBRARY}" DIRECTORY)
+      get_filename_component(GCRYPT_HEADER_PATH "${GCRYPT_LIBRARY_PATH}/../include" ABSOLUTE)
+      include_directories("${GCRYPT_HEADER_PATH}")
     endif()
 
     # Error if not found?
@@ -46,11 +46,13 @@ macro(find_gcrypt)
   
   # Summarize
   if(VERBOSE)
-    message(STATUS "GCRYPT_ROOT       = '${GCRYPT_ROOT}'")
-    message(STATUS "GCRYPT_FOUND      = '${GCRYPT_FOUND}'")
+    message(STATUS "GCRYPT_ROOT         = '${GCRYPT_ROOT}'")
+    message(STATUS "GCRYPT_FOUND        = '${GCRYPT_FOUND}'")
     if(GCRYPT_FOUND)
-      message(STATUS "GCRYPT_LIBRARY    = '${GCRYPT_LIBRARY}'")
-      message(STATUS "GCRYPT_LIBRARIES  = '${GCRYPT_LIBRARIES}'")
+      message(STATUS "GCRYPT_LIBRARY      = '${GCRYPT_LIBRARY}'")
+      message(STATUS "GCRYPT_LIBRARY_PATH = '${GCRYPT_LIBRARY_PATH}'")
+      message(STATUS "GCRYPT_LIBRARIES    = '${GCRYPT_LIBRARIES}'")
+      message(STATUS "GCRYPT_HEADER_PATH  = '${GCRYPT_HEADER_PATH}'")
     endif()
   endif()
 
