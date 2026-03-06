5ce887e7c7f60a80c2ed1af77a2664c95a4f3efb
Craig Rasmussen <rasmussen17@llnl.gov>
2026-03-05 16:29:34 -0800

    (Engineering) Fix compiler warnings issued by (Apple) clang
      1. Fixed warnings associated with unused/private variable(s): astNode, inheritedAttribute, childAttributes
      2. Replaced ROSE_ASSERT(X != NULL) with ASSERT_not_null(X)
      3. Replaced NULL with nullptr outside of comments and strings
      4. Removed DEAD code
    
    Issue #444

--- tools/classMemberVariablesInLambdas/classMemberVariablesInLambdas.C
+++ tools/classMemberVariablesInLambdas/classMemberVariablesInLambdas.C
@@ -8,111 +8,89 @@
 #include "rose.h"
 
 // Inherited attribute (see ROSE Tutorial (Chapter 9)).
-class InheritedAttribute
-   {
-     public:
-          InheritedAttribute();
-   };
+struct InheritedAttribute {
+    InheritedAttribute();
+};
 
 // Constructor (not really needed)
-InheritedAttribute::InheritedAttribute()
-   {
-   }
+InheritedAttribute::InheritedAttribute() {
+}
 
 // Synthesized attribute (see ROSE Tutorial (Chapter 9)).
-class SynthesizedAttribute
-   {
-     public:
-          SynthesizedAttribute();
-   };
+struct SynthesizedAttribute {
+    SynthesizedAttribute();
+};
 
 // Constructor
-SynthesizedAttribute::SynthesizedAttribute()
-   {
-   }
+SynthesizedAttribute::SynthesizedAttribute() {
+}
 
-class Traversal : public SgTopDownBottomUpProcessing<InheritedAttribute,SynthesizedAttribute>
-   {
-     public:
-          Traversal();
+struct Traversal : public SgTopDownBottomUpProcessing<InheritedAttribute,SynthesizedAttribute> {
+    Traversal();
 
-       // Functions required
-          InheritedAttribute   evaluateInheritedAttribute   ( SgNode* astNode, InheritedAttribute inheritedAttribute );
-          SynthesizedAttribute evaluateSynthesizedAttribute ( SgNode* astNode, InheritedAttribute inheritedAttribute, SubTreeSynthesizedAttributes synthesizedAttributeList );
-   };
-
+    // Functions required
+    InheritedAttribute evaluateInheritedAttribute(SgNode* astNode, InheritedAttribute inheritedAttribute);
+    SynthesizedAttribute evaluateSynthesizedAttribute(SgNode* astNode, InheritedAttribute inheritedAttribute,
+                                                      SubTreeSynthesizedAttributes synthesizedAttributeList);
+};
 
 InheritedAttribute
-Traversal::evaluateInheritedAttribute ( SgNode* astNode, InheritedAttribute inheritedAttribute )
-   {
-     SgLambdaCapture* lambdaCapture = isSgLambdaCapture(astNode);
-     if (lambdaCapture != NULL)
-        {
-          SgExpression* captureVariable = lambdaCapture->get_capture_variable();
-          if (captureVariable != NULL)
-             {
-               SgThisExp* thisExp = isSgThisExp(captureVariable);
-               if (thisExp != NULL)
-                  {
-                    printf ("Identified use of C++ \"this\" expression in lambda capture list: \n");
+Traversal::evaluateInheritedAttribute(SgNode* astNode, InheritedAttribute inheritedAttribute) {
+    SgLambdaCapture* lambdaCapture{isSgLambdaCapture(astNode)};
+    if (lambdaCapture != nullptr) {
+        SgExpression* captureVariable{lambdaCapture->get_capture_variable()};
+        if (captureVariable != nullptr) {
+            if (isSgThisExp(captureVariable) != nullptr) {
+                printf("Identified use of C++ \"this\" expression in lambda capture list: \n");
 
-                    std::string filename  = lambdaCapture->get_file_info()->get_filenameString();
-                    int lineNumber   = lambdaCapture->get_file_info()->get_line();
-                    int columnNumber = lambdaCapture->get_file_info()->get_col();
+                std::string filename{lambdaCapture->get_file_info()->get_filenameString()};
+                int lineNumber{lambdaCapture->get_file_info()->get_line()};
+                int columnNumber{lambdaCapture->get_file_info()->get_col()};
 
-                    printf ("   --- location: \n");
-                    printf ("   --- --- file = %s \n",filename.c_str());
-                    printf ("   --- --- line = %d column = %d \n",lineNumber,columnNumber);
-                  }
-             }
+                printf ("   --- location: \n");
+                printf ("   --- --- file = %s \n",filename.c_str());
+                printf ("   --- --- line = %d column = %d \n",lineNumber,columnNumber);
+            }
         }
+    }
 
-  // This will call the default copy constructor for InheritedAttribute.
-     return inheritedAttribute;
-   }
-
+    // This will call the default copy constructor for InheritedAttribute.
+    return inheritedAttribute;
+}
 
 SynthesizedAttribute
-Traversal::evaluateSynthesizedAttribute ( SgNode* astNode, InheritedAttribute inheritedAttribute, SynthesizedAttributesList childAttributes )
-   {
-     SynthesizedAttribute localResult;
+Traversal::evaluateSynthesizedAttribute(SgNode* /*astNode*/,
+                                        InheritedAttribute /*inheritedAttribute*/,
+                                        SynthesizedAttributesList /*childAttributes*/) {
+    return SynthesizedAttribute{};
+}
 
-  // printf ("evaluateSynthesizedAttribute(): astNode = %p = %s \n",astNode,astNode->class_name().c_str());
-
-     return localResult;
-   }
-
-
-Traversal::Traversal()
-   {
-   }
+Traversal::Traversal() {
+}
 
 int
-main ( int argc, char* argv[] )
-   {
-  // Get the command line so that we can add an option.
-     std::vector<std::string> commandLine = CommandlineProcessing::generateArgListFromArgcArgv(argc, argv);
+main(int argc, char* argv[]) {
+    // Get the command line so that we can add an option.
+    std::vector<std::string> commandLine = CommandlineProcessing::generateArgListFromArgcArgv(argc, argv);
 
-  // Use -rose:no_optimize_flag_for_frontend option to avoid issue with builtin functions not yet supported in ROSE.
-  // This allows more general support of the latest versions of compilers that have additional builtin functions.
-  // NOTE: This does not effect optimizations at the backend compiler.
-     commandLine.push_back("-rose:no_optimize_flag_for_frontend");
+    // Use -rose:no_optimize_flag_for_frontend option to avoid issue with builtin functions not yet supported in ROSE.
+    // This allows more general support of the latest versions of compilers that have additional builtin functions.
+    // NOTE: This does not effect optimizations at the backend compiler.
+    commandLine.push_back("-rose:no_optimize_flag_for_frontend");
 
-  // CmdOptions::GetInstance()->SetOptions(argc, argv);
-     SgProject* project = frontend(commandLine);
-     ROSE_ASSERT (project != NULL);
+    // CmdOptions::GetInstance()->SetOptions(argc, argv);
+    SgProject* project{frontend(commandLine)};
+    ASSERT_not_null(project);
 
-  // Build the inherited attribute
-     InheritedAttribute inheritedAttribute;
+    // Build the inherited attribute
+    InheritedAttribute inheritedAttribute{};
 
-  // Define the traversal
-     Traversal astTraversal;
+    // Define the traversal
+    Traversal astTraversal{};
 
-  // Call the traversal starting at the project (root) node of the AST
-     astTraversal.traverseInputFiles(project,inheritedAttribute);
+    // Call the traversal starting at the project (root) node of the AST
+    astTraversal.traverseInputFiles(project, inheritedAttribute);
 
-  // This tool only does analysis so we can compile the original source code.
-     return backendCompilesUsingOriginalInputFile(project);
-   }
-
-
+    // This tool only does analysis so we can compile the original source code.
+    return backendCompilesUsingOriginalInputFile(project);
+}
